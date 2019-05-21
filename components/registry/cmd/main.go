6d888e372088f4d5733e837266ff6df7bdef0b03
Karol Furgoł <32010494+kfurgol@users.noreply.github.com>
2019-05-21 12:51:51 +0200

    Add echo servers for chart testing (#8)

--- components/registry/cmd/main.go
+++ components/registry/cmd/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+const serverPort = "3000"
+
+func EchoHandler(writer http.ResponseWriter, request *http.Request) {
+
+	log.Println("Echoing back request made to " + request.URL.Path + " to client (" + request.RemoteAddr + ")")
+
+	writer.Header().Set("Access-Control-Allow-Origin", "*")
+
+	writer.Header().Set("Access-Control-Allow-Headers", "Content-Range, Content-Disposition, Content-Type, ETag")
+
+	err := request.Write(writer)
+	if err != nil {
+		log.Println("error: ", err)
+	}
+}
 
 func main() {
-	fmt.Println("registry")
+
+	log.Println("starting server, listening on port " + serverPort)
+
+	http.HandleFunc("/", EchoHandler)
+	err := http.ListenAndServe(":"+serverPort, nil)
+	if err != nil {
+		log.Println("error: ", err)
+	}
 }
