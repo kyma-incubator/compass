92c4dee0b6aafdd21978b7a0910b911a9c3ec17c
Karol Furgol <furgolkarol@gmail.com>
2019-05-30 07:43:48 +0200

    add dummy test, installation script

---
+++ tests/compass-integration/README.md
@@ -0,0 +1,1 @@
+# Compass Integration Tests
---
+++ tests/compass-integration/compass_test.go
@@ -0,0 +1,14 @@
+package compass_integration
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestApiSpec(t *testing.T) {
+
+	t.Run("Compass integration", func(t *testing.T) {
+		fmt.Println("test")
+	})
+
+}
