6341cc028ddee39f59d0f0dc86467517181ac56e
Paweł Kosiec <pawel.kosiec@gmail.com>
2019-06-05 17:28:31 +0200

    Fix proxy

--- components/gateway/cmd/main.go
+++ components/gateway/cmd/main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
-	"net/http/httptest"
 	"net/http/httputil"
 	"net/url"
+	"strings"
 
 	"github.com/pkg/errors"
 
@@ -30,34 +29,21 @@ func main() {
 	exitOnError(err, "Error while parsing Director URL")
 
 	log.Printf("Proxying requests to Director: %s\n", cfg.DirectorOrigin)
-	directorProxy := httputil.NewSingleHostReverseProxy(directorUrl)
+
+	directorProxy := httputil.ReverseProxy{
+		Director: func(r *http.Request) {
+			r.URL.Scheme = directorUrl.Scheme
+			r.URL.Host = directorUrl.Host
+			r.URL.Path = singleJoiningSlash(directorUrl.Path, r.URL.Path)
+			r.Header = http.Header{
+				"Content-Type": []string{"application/json"},
+			}
+			r.Host = directorUrl.Host
+		},
+	}
 
 	router := mux.NewRouter()
-	router.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-
-		x, err := httputil.DumpRequest(request, true)
-		if err != nil {
-			http.Error(writer, fmt.Sprint(err), http.StatusInternalServerError)
-			return
-		}
-		log.Printf(fmt.Sprintf("request:\n%q\n\n", x))
-		rec := httptest.NewRecorder()
-		directorProxy.ServeHTTP(rec, request)
-		log.Printf(fmt.Sprintf("response\n%q\n\n", rec.Body))
-
-		// this copies the recorded response to the response writer
-		for k, v := range rec.HeaderMap {
-			writer.Header()[k] = v
-		}
-		writer.WriteHeader(rec.Code)
-		_, err = rec.Body.WriteTo(writer)
-		if err != nil {
-			fmt.Println("err")
-		}
-
-	}) // GraphQL Playground
-
-	// TODO: To proxy Subscriptions, recompile against Go 1.12: https://github.com/golang/go/issues/26937
+	router.HandleFunc("/", directorProxy.ServeHTTP) // GraphQL Playground
 	router.HandleFunc("/graphql", directorProxy.ServeHTTP) // GraphQL API Endpoint
 
 	router.HandleFunc("/healthz", func(writer http.ResponseWriter, request *http.Request) {
@@ -81,4 +67,16 @@ func exitOnError(err error, context string) {
 		wrappedError := errors.Wrap(err, context)
 		log.Fatal(wrappedError)
 	}
+}
+
+func singleJoiningSlash(a, b string) string {
+	aslash := strings.HasSuffix(a, "/")
+	bslash := strings.HasPrefix(b, "/")
+	switch {
+	case aslash && bslash:
+		return a + b[1:]
+	case !aslash && !bslash:
+		return a + "/" + b
+	}
+	return a + b
 }
