2f3ce0e81f8ca4543e0b323dc7fe6e619ec001ac
Paweł Kosiec <pawel.kosiec@gmail.com>
2019-06-05 12:46:28 +0200

    Proxy calls to Director

--- components/gateway/cmd/main.go
+++ components/gateway/cmd/main.go
@@ -3,31 +3,57 @@ package main
 import (
 	"log"
 	"net/http"
+	"net/http/httputil"
+	"net/url"
+
+	"github.com/pkg/errors"
+
+	"github.com/gorilla/mux"
+	"github.com/vrischmann/envconfig"
 )
 
-const serverPort = "3000"
+type config struct {
+	Address string `envconfig:"default=127.0.0.1:3001"`
 
-func EchoHandler(writer http.ResponseWriter, request *http.Request) {
-
-	log.Println("Echoing back request made to " + request.URL.Path + " to client (" + request.RemoteAddr + ")")
-
-	writer.Header().Set("Access-Control-Allow-Origin", "*")
-
-	writer.Header().Set("Access-Control-Allow-Headers", "Content-Range, Content-Disposition, Content-Type, ETag")
-
-	err := request.Write(writer)
-	if err != nil {
-		log.Println("error: ", err)
-	}
+	DirectorOrigin string `envconfig:"default=http://127.0.0.1:3000"`
 }
 
 func main() {
 
-	log.Println("starting server, listening on port " + serverPort)
+	cfg := config{}
+	err := envconfig.InitWithPrefix(&cfg, "APP")
+	exitOnError(err, "Error while loading app config")
 
-	http.HandleFunc("/", EchoHandler)
-	err := http.ListenAndServe(":"+serverPort, nil)
+	directorUrl, err := url.Parse(cfg.DirectorOrigin)
+	exitOnError(err, "Error while parsing Director URL")
+
+	directorProxy := httputil.NewSingleHostReverseProxy(directorUrl)
+
+	router := mux.NewRouter()
+	router.HandleFunc("/", directorProxy.ServeHTTP) // GraphQL Playground
+
+	// TODO: To proxy Subscriptions, recompile against Go 1.12: https://github.com/golang/go/issues/26937
+	router.HandleFunc("/graphql", directorProxy.ServeHTTP) // GraphQL API Endpoint
+
+	router.HandleFunc("/healthz", func(writer http.ResponseWriter, request *http.Request) {
+		writer.WriteHeader(200)
+		_, err := writer.Write([]byte("ok"))
+		if err != nil {
+			log.Println(errors.Wrapf(err, "while writing to response body").Error())
+		}
+	})
+
+	http.Handle("/", router)
+
+	log.Printf("Listening on %s", cfg.Address)
+	if err := http.ListenAndServe(cfg.Address, nil); err != nil {
+		panic(err)
+	}
+}
+
+func exitOnError(err error, context string) {
 	if err != nil {
-		log.Println("error: ", err)
+		wrappedError := errors.Wrap(err, context)
+		log.Fatal(wrappedError)
 	}
 }
