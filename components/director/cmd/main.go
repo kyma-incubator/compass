5fdc8cd5c54e656cc620f68fd46010d7cbcb4e7a
Paweł Kosiec <pawel.kosiec@gmail.com>
2019-06-05 12:15:17 +0200

    Make Director address configurable

--- components/director/cmd/main.go
+++ components/director/cmd/main.go
@@ -1,18 +1,29 @@
 package main
 
 import (
+	"github.com/pkg/errors"
+	"log"
 	"net/http"
 
 	"github.com/99designs/gqlgen/handler"
 	"github.com/gorilla/mux"
 	"github.com/kyma-incubator/compass/components/director/internal/gqlschema"
+	"github.com/vrischmann/envconfig"
 )
 
+type config struct {
+	Address string `envconfig:"default=127.0.0.1:3000"`
+}
+
 func main() {
-	cfg := gqlschema.Config{
+	cfg := config{}
+	err := envconfig.InitWithPrefix(&cfg, "APP")
+	exitOnError(err, "Error while loading app config")
+
+	gqlCfg := gqlschema.Config{
 		Resolvers: &gqlschema.Resolver{},
 	}
-	executableSchema := gqlschema.NewExecutableSchema(cfg)
+	executableSchema := gqlschema.NewExecutableSchema(gqlCfg)
 
 	router := mux.NewRouter()
 	router.HandleFunc("/", handler.Playground("Dataloader", "/graphql"))
@@ -20,7 +31,15 @@ func main() {
 
 	http.Handle("/", router)
 
-	if err := http.ListenAndServe(":3000", nil); err != nil {
+	log.Printf("Listening on %s", cfg.Address)
+	if err := http.ListenAndServe(cfg.Address, nil); err != nil {
 		panic(err)
+	}
+}
+
+func exitOnError(err error, context string) {
+	if err != nil {
+		wrappedError := errors.Wrap(err, context)
+		log.Fatal(wrappedError)
 	}
 }
