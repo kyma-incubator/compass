bb163c86dc8ac435be6c177a11b4b4caf7bc8a07
Paweł Kosiec <pawel.kosiec@gmail.com>
2019-06-06 15:35:07 +0200

    Do not filter headers on Gateway while proxying requests (#41)
    * Do not filter headers
    
    * Refactor proxy package
    
    * Use /director path for Director

--- components/gateway/cmd/main.go
+++ components/gateway/cmd/main.go
@@ -3,10 +3,8 @@ package main
 import (
 	"log"
 	"net/http"
-	"net/http/httputil"
-	"net/url"
-	"strings"
 
+	"github.com/kyma-incubator/compass/components/gateway/pkg/proxy"
 	"github.com/pkg/errors"
 
 	"github.com/gorilla/mux"
@@ -20,31 +18,17 @@ type config struct {
 }
 
 func main() {
-
 	cfg := config{}
 	err := envconfig.InitWithPrefix(&cfg, "APP")
 	exitOnError(err, "Error while loading app config")
 
-	directorUrl, err := url.Parse(cfg.DirectorOrigin)
-	exitOnError(err, "Error while parsing Director URL")
+	directorProxy, err := proxy.New(cfg.DirectorOrigin, "/director")
+	exitOnError(err, "Error while initializing proxy")
 
 	log.Printf("Proxying requests to Director: %s\n", cfg.DirectorOrigin)
 
-	directorProxy := httputil.ReverseProxy{
-		Director: func(r *http.Request) {
-			r.URL.Scheme = directorUrl.Scheme
-			r.URL.Host = directorUrl.Host
-			r.URL.Path = singleJoiningSlash(directorUrl.Path, r.URL.Path)
-			r.Header = http.Header{
-				"Content-Type": []string{"application/json"},
-			}
-			r.Host = directorUrl.Host
-		},
-	}
-
 	router := mux.NewRouter()
-	router.HandleFunc("/", directorProxy.ServeHTTP)        // GraphQL Playground
-	router.HandleFunc("/graphql", directorProxy.ServeHTTP) // GraphQL API Endpoint
+	router.PathPrefix("/director").HandlerFunc(directorProxy.ServeHTTP)
 
 	router.HandleFunc("/healthz", func(writer http.ResponseWriter, request *http.Request) {
 		writer.WriteHeader(200)
@@ -67,16 +51,4 @@ func exitOnError(err error, context string) {
 		wrappedError := errors.Wrap(err, context)
 		log.Fatal(wrappedError)
 	}
-}
-
-func singleJoiningSlash(a, b string) string {
-	aslash := strings.HasSuffix(a, "/")
-	bslash := strings.HasPrefix(b, "/")
-	switch {
-	case aslash && bslash:
-		return a + b[1:]
-	case !aslash && !bslash:
-		return a + "/" + b
-	}
-	return a + b
 }
---
+++ components/gateway/pkg/proxy/proxy_test.go
@@ -0,0 +1,39 @@
+package proxy
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestRequestURL(t *testing.T) {
+	testCases := []struct{
+		requestPath string
+		proxyPath string
+		expectedResult string
+	}{
+		{
+			requestPath: "/foo/bar/graphql",
+			proxyPath: "/foo",
+			expectedResult: "/bar/graphql",
+		},
+		{
+			requestPath: "/foo/bar/graphql",
+			proxyPath: "/",
+			expectedResult: "/foo/bar/graphql",
+		},
+		{
+			requestPath: "/foo/bar/graphql",
+			proxyPath: "/foo/bar/graphql",
+			expectedResult: "/",
+		},
+	}
+
+
+	for tN, tC := range testCases {
+		t.Run(fmt.Sprintf("%d: Request path %s, when server is proxied on %s", tN, tC.requestPath, tC.proxyPath), func(t *testing.T) {
+			result := requestURL(tC.requestPath, tC.proxyPath)
+			assert.Equal(t, tC.expectedResult, result)
+		})
+	}
+}
