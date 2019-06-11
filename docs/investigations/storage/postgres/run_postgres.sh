87018c6206bd0124729c6bdb2fbe36ca9bacc3ec
Szecowka, Adam <adam.szecowka@gmail.com>
2019-06-11 09:31:39 +0200

    fix path

--- docs/investigations/storage/postgres/run_postgres.sh
+++ docs/investigations/storage/postgres/run_postgres.sh
@@ -4,7 +4,7 @@ go run gen.go
 docker rm -f my-postgres
 
 docker run --name my-postgres \
-    -v ${GOPATH}/src/github.com/kyma-incubator/compass/docs/investigations/postgres/sql:/docker-entrypoint-initdb.d \
+    -v ${GOPATH}/src/github.com/kyma-incubator/compass/docs/investigations/storage/postgres/sql:/docker-entrypoint-initdb.d \
     -e POSTGRES_PASSWORD=mysecretpassword \
     -p 5432:5432 \
      postgres
