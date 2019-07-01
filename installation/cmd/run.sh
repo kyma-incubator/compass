0dfd595cb68a0c9f35a9ba4da0246e1fd9ef6fc3
Paweł Kosiec <pawel.kosiec@gmail.com>
2019-07-01 10:40:15 +0200

    Fix installation script

--- installation/cmd/run.sh
+++ installation/cmd/run.sh
@@ -6,4 +6,4 @@ defaultRelease="master"
 KYMA_RELEASE=${1:-$defaultRelease}
 
 kyma provision minikube
-kyma install -o "${ROOT_PATH}"/installation/resources/installer-cr.yaml "${ROOT_PATH}"/installation/resources/installer-config.yaml --release "${KYMA_RELEASE}"
+kyma install -o "${ROOT_PATH}"/installation/resources/installer-cr.yaml -o "${ROOT_PATH}"/installation/resources/installer-config.yaml --release "${KYMA_RELEASE}"
