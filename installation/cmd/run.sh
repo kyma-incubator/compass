4250b07fd7a56a7ab3da4538259003521696dc2c
Paweł Kosiec <pawel.kosiec@gmail.com>
2019-07-01 11:43:50 +0200

    Add Compass entries to /etc/hosts temporary

--- installation/cmd/run.sh
+++ installation/cmd/run.sh
@@ -7,3 +7,7 @@ KYMA_RELEASE=${1:-$defaultRelease}
 
 kyma provision minikube
 kyma install -o "${ROOT_PATH}"/installation/resources/installer-cr.yaml -o "${ROOT_PATH}"/installation/resources/installer-config.yaml --release "${KYMA_RELEASE}"
+
+# TODO: Remove it after next CLI release
+echo "Adding Compass entries to /etc/hosts...\n"	
+sudo sh -c 'echo "\n$(minikube ip) compass-gateway.kyma.local" >> /etc/hosts' 
