66d18c3ad03b438a0c329b658e3678c9cefad25c
Tomasz Gorgol <t.gorgol@sap.com>
2019-07-04 11:17:14 +0200

    Adjust installer script to latest kyma cli

--- installation/cmd/run.sh
+++ installation/cmd/run.sh
@@ -4,10 +4,16 @@ ROOT_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../..
 
 defaultRelease="master"
 KYMA_RELEASE=${1:-$defaultRelease}
+COMPASS_HELM_RELEASE_NAME="compass"
+COMPASS_HELM_RELEASE_NAMESPACE="compass-system"
 
 kyma provision minikube
 kyma install -o "${ROOT_PATH}"/installation/resources/installer-cr.yaml -o "${ROOT_PATH}"/installation/resources/installer-config.yaml --release "${KYMA_RELEASE}"
 
-# TODO: Remove it after next CLI release
-echo "Adding Compass entries to /etc/hosts...\n"	
-sudo sh -c 'echo "\n$(minikube ip) compass-gateway.kyma.local" >> /etc/hosts' 
+#Get Tiller tls client certificates
+kubectl get -n kyma-installer secret helm-secret -o jsonpath="{.data['global\.helm\.ca\.crt']}" | base64 --decode > "$(helm home)/ca.pem"
+kubectl get -n kyma-installer secret helm-secret -o jsonpath="{.data['global\.helm\.tls\.crt']}" | base64 --decode > "$(helm home)/cert.pem"
+kubectl get -n kyma-installer secret helm-secret -o jsonpath="{.data['global\.helm\.tls\.key']}" | base64 --decode > "$(helm home)/key.pem"
+echo "Secrets with Tiller tls client certificates have been created \n"
+
+helm install --name "${COMPASS_HELM_RELEASE_NAME}" --namespace "${COMPASS_HELM_RELEASE_NAMESPACE}" "${ROOT_PATH}"/chart/compass --tls
