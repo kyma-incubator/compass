6ecec4fb3cd182405a8c9bda7ba68d482c8c0b82
Tomasz Gorgol <t.gorgol@sap.com>
2019-07-09 14:07:25 +0200

    Add old run script back as a separate script

---
+++ installation/cmd/run-from-kyma.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+ROOT_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../..
+
+defaultRelease="master"
+KYMA_RELEASE=${1:-$defaultRelease}
+
+kyma provision minikube
+kyma install -o "${ROOT_PATH}"/installation/resources/installer-cr-with-compass.yaml -o "${ROOT_PATH}"/installation/resources/installer-config.yaml --release "${KYMA_RELEASE}"
+
+# TODO: Remove it after next CLI release
+echo "Adding Compass entries to /etc/hosts...\n"
+sudo sh -c 'echo "\n$(minikube ip) compass-gateway.kyma.local" >> /etc/hosts'
