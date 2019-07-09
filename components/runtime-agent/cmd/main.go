da092e9e519b53087ef1d4888a81e6ff620be9a6
Arkadiusz Galwas <arkadiusz.galwas@sap.com>
2019-07-09 15:45:59 +0200

    Application periodically prints to the log

--- components/runtime-agent/cmd/main.go
+++ components/runtime-agent/cmd/main.go
@@ -1,44 +1,13 @@
 package main
 
 import (
-	"log"
-	"net/http"
+	"time"
 
-	"github.com/gorilla/mux"
-	"github.com/pkg/errors"
-	"github.com/vrischmann/envconfig"
+	log "github.com/sirupsen/logrus"
 )
 
-type config struct {
-	Address string `envconfig:"default=127.0.0.1:3001"`
-}
-
 func main() {
-	router := mux.NewRouter()
-
-	cfg := config{}
-	err := envconfig.InitWithPrefix(&cfg, "APP")
-	exitOnError(err, "Error while loading app config")
-
-	router.HandleFunc("/healthz", func(writer http.ResponseWriter, request *http.Request) {
-		writer.WriteHeader(200)
-		_, err := writer.Write([]byte("ok"))
-		if err != nil {
-			log.Println(errors.Wrapf(err, "while writing to response body").Error())
-		}
-	})
-
-	http.Handle("/", router)
-
-	log.Printf("Listening on %s", cfg.Address)
-	if err := http.ListenAndServe(cfg.Address, nil); err != nil {
-		panic(err)
-	}
-}
-
-func exitOnError(err error, context string) {
-	if err != nil {
-		wrappedError := errors.Wrap(err, context)
-		log.Fatal(wrappedError)
+	for range time.Tick(time.Second * 30) {
+		log.Info("Runtime agent works.")
 	}
 }
