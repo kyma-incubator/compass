4b9d1df1676ba6876ad518fbacb4b56348e5b333
Tomasz Gorgol <t.gorgol@sap.com>
2019-07-19 14:49:22 +0200

    Change overrides

--- installation/cmd/run-from-kyma.sh
+++ installation/cmd/run-from-kyma.sh
@@ -5,8 +5,15 @@ ROOT_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../..
 defaultRelease="master"
 KYMA_RELEASE=${1:-$defaultRelease}
 
+OVERRIDES_MINIKUBE_PATH="${ROOT_PATH}"/installation/resources/installer-overrides-minikube.yaml
+CR_KYMA_LITE_COMPASS_PATH="${ROOT_PATH}"/installation/resources/installer-cr-kyma-lite-compass.yaml
+
 kyma provision minikube
-kyma install -o "${ROOT_PATH}"/installation/resources/installer-cr-with-compass.yaml -o "${ROOT_PATH}"/installation/resources/installer-config.yaml --release "${KYMA_RELEASE}"
+MINIKUBE_IP=$(eval minikube ip)
+sed -i.bak 's/IP_PLACEHOLDER/'$MINIKUBE_IP'/g' $OVERRIDES_MINIKUBE_PATH
+kyma install -o $CR_KYMA_LITE_COMPASS_PATH -o $OVERRIDES_MINIKUBE_PATH --release "${KYMA_RELEASE}"
+sed -i.bak 's/'$MINIKUBE_IP'/IP_PLACEHOLDER/g' $OVERRIDES_MINIKUBE_PATH
+rm ${OVERRIDES_MINIKUBE_PATH}.bak
 
 # TODO: Remove it after next CLI release
 echo "Adding Compass entries to /etc/hosts...\n"
