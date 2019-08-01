cda671fdf16372debeddec1c238e461fd853f181
Paweł Kosiec <pawel.kosiec@gmail.com>
2019-08-01 11:01:11 +0200

    Unify scripts

--- components/director/run.sh
+++ components/director/run.sh
@@ -1,61 +1,62 @@
 #!/usr/bin/env bash
 
+# This script is responsible for running Director with PostgreSQL.
+
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 INVERTED='\033[7m'
 NC='\033[0m' # No Color
 
+set -e
 
-NETWORK="migration-dev-network"
-POSTGRES_CONTAINER="compass-dev-postgres"
+ROOT_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+
+POSTGRES_CONTAINER="test-postgres"
 POSTGRES_VERSION="11"
-MIGRATOR_IMG_NAME="compass-schema-migrator"
 
 DB_USER="usr"
 DB_PWD="pwd"
 DB_NAME="compass"
 DB_PORT="5432"
-DB_SSL_PARAM="disable"
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-cd ${DIR}
 
 function cleanup() {
-	echo -e "${GREEN}Removing database container...${NC}"
+    echo -e "${GREEN}Cleanup Postgres container${NC}"
     docker rm --force ${POSTGRES_CONTAINER}
-    docker network rm ${NETWORK}
 }
 
 trap cleanup EXIT
 
-echo -e "${GREEN}Creating network...${NC}"
-docker network create --driver bridge ${NETWORK}
-
-echo -e "${GREEN}Running database container...${NC}"
+echo -e "${GREEN}Start Postgres in detached mode${NC}"
 docker run -d --name ${POSTGRES_CONTAINER} \
-			--network=${NETWORK} \
             -e POSTGRES_USER=${DB_USER} \
             -e POSTGRES_PASSWORD=${DB_PWD} \
             -e POSTGRES_DB=${DB_NAME} \
             -p 5432:5432 \
             postgres:${POSTGRES_VERSION}
 
-echo -e "${GREEN}Building migration image...${NC}"
-cd ../schema-migrator/ && make build-image
+set +e
+echo '# WAITING FOR CONNECTION WITH DATABASE #'
+for i in {1..30}
+do
+    pg_isready -U "$DB_USER" -h 127.0.0.1 -p "$DB_PORT" -d "$DB_NAME"
+    if [ $? -eq 0 ]
+    then
+        dbReady=true
+        break
+    fi
+    sleep 1
+done
 
-echo -e "${GREEN}Running migration...${NC}"
+if [ "${dbReady}" != true ] ; then
+    echo '# COULD NOT ESTABLISH CONNECTION TO DATABASE #'
+    exit 1
+fi
 
-docker run --rm --network=${NETWORK} \
-        -e DB_USER=${DB_USER} \
-        -e DB_PASSWORD=${DB_PWD} \
-        -e DB_HOST=${POSTGRES_CONTAINER} \
-        -e DB_PORT=${DB_PORT} \
-        -e DB_NAME=${DB_NAME} \
-        -e DB_SSL=${DB_SSL_PARAM} \
-        -e DIRECTION="up" \
-    ${MIGRATOR_IMG_NAME}
+set -e
 
-echo -e "${GREEN}Running application...${NC}"
-cd ../director/
-APP_DB_USER=${DB_USER} APP_DB_PASSWORD=${DB_PWD} APP_DB_NAME=${DB_NAME} go run cmd/main.go
+echo -e "${GREEN}Populate DB${NC}"
+
+PGPASSWORD=pwd psql -U ${DB_USER} -h 127.0.0.1 -f <(cat ${ROOT_PATH}/../schema-migrator/migrations/*.up.sql) ${DB_NAME}
+
+APP_DB_USER=${DB_USER} APP_DB_PASSWORD=${DB_PWD} APP_DB_NAME=${DB_NAME} go run ${ROOT_PATH}/cmd/main.go
