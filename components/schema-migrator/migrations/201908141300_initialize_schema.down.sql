888d707f982168ef7efae83957cea2a051098ef9
Paweł Kosiec <pawel.kosiec@gmail.com>
2019-08-14 15:24:00 +0200

    Prepare DB schema for all tables

--- components/schema-migrator/migrations/201908141300_initialize_schema.down.sql
+++ components/schema-migrator/migrations/201908141300_initialize_schema.down.sql
@@ -10,27 +10,29 @@ DROP TYPE application_status_condition;
 
 -- Webhooks
 
--- TODO:
+DROP TABLE webhooks;
+DROP TYPE webhook_type;
 
 -- API Definitions
 
--- TODO:
+DROP TABLE api_definitions;
+DROP TYPE api_spec_format;
+DROP TYPE api_spec_type;
 
 -- Event API Definitions
 
--- TODO:
-
--- Documents
-
--- TODO:
-
--- Fetch Requests
-
--- TODO:
+DROP TABLE event_api_definitions;
+DROP TYPE event_api_spec_format;
+DROP TYPE event_api_spec_type;
 
 -- Runtime Auths
 
--- TODO:
+DROP TABLE runtime_auths;
+
+-- Documents
+
+DROP TABLE documents;
+DROP TYPE document_format;
 
 -- Label Definition
 
@@ -39,3 +41,10 @@ DROP TABLE label_definitions;
 -- Label
 
 DROP TABLE labels;
+
+-- Fetch Request
+
+DROP TABLE fetch_requests;
+DROP TYPE fetch_request_mode;
+DROP TYPE fetch_request_status_condition;
+
--- components/schema-migrator/migrations/201908141300_initialize_schema.up.sql
+++ components/schema-migrator/migrations/201908141300_initialize_schema.up.sql
@@ -7,7 +7,7 @@ CREATE TYPE runtime_status_condition AS ENUM (
 );
 
 CREATE TABLE runtimes (
-    id uuid NOT NULL CONSTRAINT runtime_pk PRIMARY KEY,
+    id uuid PRIMARY KEY,
     tenant_id uuid NOT NULL,
     name varchar(256) NOT NULL,
     description text,
@@ -17,8 +17,7 @@ CREATE TABLE runtimes (
 );
 
 ALTER TABLE runtimes
-    ADD CONSTRAINT runtimes_id_name_unique UNIQUE (tenant_id, name);
-
+    ADD CONSTRAINT runtime_id_name_unique UNIQUE (tenant_id, name);
 
 -- Application
 
@@ -27,65 +26,176 @@ CREATE TYPE application_status_condition AS ENUM (
     'UNKNOWN',
     'READY',
     'FAILED'
-    );
-
-CREATE TABLE applications (
-                          id uuid NOT NULL CONSTRAINT application_pk PRIMARY KEY,
-                          tenant_id uuid NOT NULL,
-                          name varchar(36) NOT NULL,
-                          description text,
-                          status_condition application_status_condition DEFAULT 'INITIAL' ::application_status_condition NOT NULL,
-                          status_timestamp timestamp NOT NULL,
-                          healthcheck_url varchar(256)
 );
 
+CREATE TABLE applications (
+    id uuid PRIMARY KEY,
+    tenant_id uuid NOT NULL,
+    name varchar(36) NOT NULL,
+    description text,
+    status_condition application_status_condition DEFAULT 'INITIAL' ::application_status_condition NOT NULL,
+    status_timestamp timestamp NOT NULL,
+    healthcheck_url varchar(256)
+);
 
--- Webhooks
+ALTER TABLE applications
+    ADD CONSTRAINT application_id_name_unique UNIQUE (tenant_id, name);
 
--- TODO:
+-- Webhook
 
--- API Definitions
+CREATE TYPE webhooks_type AS ENUM (
+    'CONFIGURATION_CHANGED'
+);
 
--- TODO:
+CREATE TABLE webhooks (
+    id uuid PRIMARY KEY,
+    tenant_id uuid NOT NULL,
+    app_id uuid REFERENCES applications (id) ON DELETE CASCADE NOT NULL,
+    url varchar(256) NOT NULL,
+    type webhooks_type NOT NULL,
+    auth jsonb
+);
 
--- Event API Definitions
+-- API Definition
 
--- TODO:
+CREATE TYPE api_spec_format AS ENUM (
+    'YAML',
+    'JSON'
+);
 
--- Documents
+CREATE TYPE api_spec_type AS ENUM (
+    'ODATA',
+    'OPEN_API'
+);
 
--- TODO:
+CREATE TABLE api_definitions (
+    id uuid PRIMARY KEY,
+    tenant_id uuid NOT NULL,
+    app_id uuid REFERENCES applications (id) ON DELETE CASCADE NOT NULL,
+    name varchar(256) NOT NULL,
+    description text,
+    group varchar(256),
+    spec_data text,
+    spec_format api_spec_format,
+    spec_type api_spec_type,
+    target_url varchar(256) NOT NULL,
+    default_auth jsonb,
+    version_value varchar(256),
+    version_deprecated bool,
+    version_deprecated_since varchar(256),
+    version_for_removal bool
+);
 
--- Fetch Requests
+-- Event API Definition
 
--- TODO:
+CREATE TYPE event_api_spec_format AS ENUM (
+    'YAML',
+    'JSON'
+);
 
--- Runtime Auths
+CREATE TYPE event_api_spec_type AS ENUM (
+    'ASYNC_API'
+);
 
--- TODO:
+CREATE TABLE event_api_definitions (
+    id uuid PRIMARY KEY,
+    tenant_id uuid NOT NULL,
+    app_id uuid REFERENCES applications (id) ON DELETE CASCADE NOT NULL,
+    name varchar(256) NOT NULL,
+    description text,
+    group varchar(256),
+    spec_data text,
+    spec_format event_api_spec_format,
+    spec_type event_api_spec_type,
+    version_value varchar(256),
+    version_deprecated bool,
+    version_deprecated_since varchar(256),
+    version_for_removal bool
+);
 
+-- Runtime Auth
+
+CREATE TABLE runtime_auths (
+    id uuid PRIMARY KEY,
+    tenant_id uuid NOT NULL,
+    runtime_id uuid REFERENCES runtimes (id) ON DELETE CASCADE NOT NULL,
+    app_def_id uuid REFERENCES api_definitions (id) ON DELETE CASCADE NOT NULL,
+    value jsonb
+);
+
+-- Document
+
+CREATE TYPE document_format AS ENUM (
+    'MARKDOWN'
+);
+
+CREATE TABLE documents (
+    id uuid PRIMARY KEY,
+    tenant_id uuid NOT NULL,
+    app_id uuid REFERENCES applications (id) ON DELETE CASCADE NOT NULL,
+    title varchar(256),
+    display_name varchar(256),
+    description text,
+    format document_format,
+    kind varchar(256),
+    data text
+);
 
 -- Label Definition
 
 CREATE TABLE label_definitions (
-                                   id uuid PRIMARY KEY,
-                                   tenant_id uuid NOT NULL,
-                                   KEY varchar(256) NOT NULL,
-                                   SCHEMA jsonb
+    id uuid PRIMARY KEY,
+    tenant_id uuid NOT NULL,
+    key varchar(256) NOT NULL,
+    schema jsonb
 );
 
-CREATE UNIQUE INDEX ON label_definitions (tenant_id, KEY);
+CREATE UNIQUE INDEX ON label_definitions (tenant_id, key);
 
 -- Label
 
 CREATE TABLE labels (
-                        id uuid PRIMARY KEY,
-                        tenant_id uuid NOT NULL,
-                        KEY varchar(256) NOT NULL,
-                        app_id uuid REFERENCES applications (id) ON DELETE CASCADE,
-                        runtime_id uuid REFERENCES runtimes (id) ON DELETE CASCADE,
-                        value jsonb
+    id uuid PRIMARY KEY,
+    tenant_id uuid NOT NULL,
+    key varchar(256) NOT NULL,
+    app_id uuid REFERENCES applications (id) ON DELETE CASCADE,
+    runtime_id uuid REFERENCES runtimes (id) ON DELETE CASCADE,
+    value jsonb
 );
 
-CREATE UNIQUE INDEX ON labels (tenant_id, KEY, runtime_id, app_id);
+CREATE UNIQUE INDEX ON labels (tenant_id, key, runtime_id, app_id);
 
+-- Fetch Request
+
+CREATE TYPE fetch_request_status_condition AS ENUM (
+    'INITIAL',
+    'SUCCEEDED',
+    'FAILED'
+);
+
+CREATE TYPE fetch_request_mode AS ENUM (
+    'SINGLE',
+    'PACKAGE',
+    'INDEX'
+);
+
+CREATE TABLE fetch_requests (
+    id uuid PRIMARY KEY,
+    tenant_id uuid NOT NULL,
+    document_id uuid REFERENCES documents (id) ON DELETE CASCADE,
+    api_def_id uuid REFERENCES api_definitions (id) ON DELETE CASCADE,
+    event_api_def_id uuid REFERENCES event_api_definitions (id) ON DELETE CASCADE,
+    url varchar(256) NOT NULL,
+    auth jsonb,
+    mode fetch_request_mode NOT NULL,
+    filter varchar(256),
+    status_condition fetch_request_status_condition NOT NULL,
+    status_timestamp timestamp NOT NULL
+);
+
+ALTER TABLE api_definitions
+    ADD fetch_request_id uuid REFERENCES fetch_requests(id) ON DELETE CASCADE;
+ALTER TABLE documents
+    ADD fetch_request_id uuid REFERENCES fetch_requests(id) ON DELETE CASCADE;
+ALTER TABLE event_api_definitions
+    ADD fetch_request_id uuid REFERENCES fetch_requests(id) ON DELETE CASCADE;
