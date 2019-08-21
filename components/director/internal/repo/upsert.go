5a871c4fd952dacc293e22bef9ab1b71dac09d2c
Tomasz Gorgol <t.gorgol@sap.com>
2019-08-21 13:53:23 +0200

    Add upsert

---
+++ components/director/internal/repo/upsert.go
@@ -0,0 +1,61 @@
+package repo
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/lib/pq"
+
+	"github.com/kyma-incubator/compass/components/director/internal/persistence"
+	"github.com/pkg/errors"
+)
+
+type Upserter struct {
+	tableName     string
+	createColumns []string
+	idColumns     []string
+	updateColumns []string
+}
+
+func NewUpserter(tableName string, createColumns []string, idColumns []string, updateColumns []string) *Upserter {
+	return &Upserter{
+		tableName:     tableName,
+		createColumns: createColumns,
+		idColumns:     idColumns,
+		updateColumns: updateColumns,
+	}
+}
+
+func (u *Upserter) Upsert(ctx context.Context, dbEntity interface{}) error {
+	if dbEntity == nil {
+		return errors.New("item cannot be nil")
+	}
+
+	persist, err := persistence.FromCtx(ctx)
+	if err != nil {
+		return err
+	}
+
+	var values []string
+	for _, c := range u.createColumns {
+		values = append(values, fmt.Sprintf(":%s", c))
+	}
+
+	var update []string
+	for _, c := range u.updateColumns {
+		update = append(update, fmt.Sprintf("%[1]s=EXCLUDED.%[1]s", c))
+	}
+
+	stmtWithoutUpsert := fmt.Sprintf("INSERT INTO %s ( %s ) VALUES ( %s )", u.tableName, strings.Join(u.createColumns, ", "), strings.Join(values, ", "))
+	stmtWithUpsert := fmt.Sprintf("%s ON CONFLICT ( %s ) DO UPDATE SET %s", stmtWithoutUpsert, strings.Join(u.idColumns, ", "), strings.Join(update, ", "))
+
+	_, err = persist.NamedExec(stmtWithUpsert, dbEntity)
+	if pqerr, ok := err.(*pq.Error); ok {
+		if pqerr.Code == persistence.UniqueViolation {
+			return &notUniqueError{}
+		}
+	}
+
+	return errors.Wrapf(err, "while upserting row to '%s' table", u.tableName)
+}
---
+++ components/director/internal/repo/upsert_test.go
@@ -0,0 +1,100 @@
+package repo_test
+
+import (
+	"context"
+	"regexp"
+	"testing"
+
+	"github.com/lib/pq"
+
+	"github.com/DATA-DOG/go-sqlmock"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/kyma-incubator/compass/components/director/internal/persistence"
+	"github.com/kyma-incubator/compass/components/director/internal/repo"
+	"github.com/kyma-incubator/compass/components/director/internal/repo/testdb"
+	"github.com/stretchr/testify/require"
+)
+
+func TestUpsert(t *testing.T) {
+	expectedQuery := regexp.QuoteMeta("INSERT INTO users ( id_col, tenant_col, first_name, last_name, age ) VALUES ( ?, ?, ?, ?, ? ) ON CONFLICT ( id_col, tenant_col ) DO UPDATE SET first_name=EXCLUDED.first_name, last_name=EXCLUDED.last_name, age=EXCLUDED.age")
+	sut := repo.NewUpserter("users", []string{"id_col", "tenant_col", "first_name", "last_name", "age"}, []string{"id_col", "tenant_col"}, []string{"first_name", "last_name", "age"})
+	t.Run("success", func(t *testing.T) {
+		// GIVEN
+		db, mock := testdb.MockDatabase(t)
+		ctx := persistence.SaveToContext(context.TODO(), db)
+		defer mock.AssertExpectations(t)
+		givenUser := User{
+			ID:        "given_id",
+			Tenant:    "given_tenant",
+			FirstName: "given_first_name",
+			LastName:  "given_last_name",
+			Age:       55,
+		}
+
+		mock.ExpectExec(expectedQuery).
+			WithArgs("given_id", "given_tenant", "given_first_name", "given_last_name", 55).WillReturnResult(sqlmock.NewResult(1, 1))
+		// WHEN
+		err := sut.Upsert(ctx, givenUser)
+		// THEN
+		require.NoError(t, err)
+	})
+
+	t.Run("returns error when operation on db failed", func(t *testing.T) {
+		// GIVEN
+		db, mock := testdb.MockDatabase(t)
+		ctx := persistence.SaveToContext(context.TODO(), db)
+		defer mock.AssertExpectations(t)
+		givenUser := User{}
+
+		mock.ExpectExec(expectedQuery).
+			WillReturnError(someError())
+		// WHEN
+		err := sut.Upsert(ctx, givenUser)
+		// THEN
+		require.EqualError(t, err, "while upserting row to 'users' table: some error")
+	})
+
+	t.Run("returns non unique error", func(t *testing.T) {
+		// GIVEN
+		db, mock := testdb.MockDatabase(t)
+		ctx := persistence.SaveToContext(context.TODO(), db)
+		defer mock.AssertExpectations(t)
+		givenUser := User{}
+
+		mock.ExpectExec(expectedQuery).
+			WillReturnError(&pq.Error{Code: persistence.UniqueViolation})
+		// WHEN
+		err := sut.Upsert(ctx, givenUser)
+		// THEN
+		require.True(t, repo.IsNotUnique(err))
+	})
+
+	t.Run("returns error if missing persistence context", func(t *testing.T) {
+		// WHEN
+		err := sut.Upsert(context.TODO(), User{})
+		// THEN
+		require.EqualError(t, err, "unable to fetch database from context")
+	})
+
+	t.Run("returns error if destination is nil", func(t *testing.T) {
+		// WHEN
+		err := sut.Upsert(context.TODO(), nil)
+		// THEN
+		require.EqualError(t, err, "item cannot be nil")
+	})
+}
+
+func TestUpsertWhenWrongConfiguration(t *testing.T) {
+	sut := repo.NewUpserter("users", []string{"id_col", "tenant_col", "column_does_not_exist"}, []string{"id_col", "tenant_col"}, []string{"column_does_not_exist"})
+	// GIVEN
+	db, mock := testdb.MockDatabase(t)
+	ctx := persistence.SaveToContext(context.TODO(), db)
+	defer mock.AssertExpectations(t)
+	// WHEN
+	err := sut.Upsert(ctx, User{})
+	// THEN
+	require.Error(t, err)
+	assert.Contains(t, err.Error(), "could not find name column_does_not_exist")
+}
