e5daeb53325463420dd42e006d07df9b5bcf7e1e
Tomasz Gorgol <45565988+tgorgol@users.noreply.github.com>
2019-09-10 12:58:48 +0200

    Modify extended gql types (#299)
    * Modify extended gql types
    
    * Bump values

--- components/director/pkg/graphql/event_api.go
+++ components/director/pkg/graphql/event_api.go
@@ -1,29 +1,22 @@
 package graphql
 
 type EventAPISpec struct {
-	Data   *CLOB            `json:"data"`
-	Type   EventAPISpecType `json:"type"`
-	Format SpecFormat       `json:"format"`
-	DefinitionID string // Needed to resolve FetchRequest for given APISpec
+	Data         *CLOB            `json:"data"`
+	Type         EventAPISpecType `json:"type"`
+	Format       SpecFormat       `json:"format"`
+	DefinitionID string           // Needed to resolve FetchRequest for given APISpec
 }
 
 // Extended types used by external API
 
 type EventAPIDefinitionPageExt struct {
-	Data       []*EventAPIDefinitionExt `json:"data"`
-	PageInfo   *PageInfo                `json:"pageInfo"`
-	TotalCount int                      `json:"totalCount"`
+	EventAPIDefinitionPage
+	Data []*EventAPIDefinitionExt `json:"data"`
 }
 
 type EventAPIDefinitionExt struct {
-	ID            string  `json:"id"`
-	ApplicationID string  `json:"applicationID"`
-	Name          string  `json:"name"`
-	Description   *string `json:"description"`
-	// group allows you to find the same API but in different version
-	Group   *string       `json:"group"`
-	Spec    *EventAPISpec `json:"spec"`
-	Version *Version      `json:"version"`
+	EventAPIDefinition
+	Spec *EventAPISpecExt `json:"spec"`
 }
 
 type EventAPISpecExt struct {
