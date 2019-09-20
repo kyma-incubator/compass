0bc5cd21b45599cea7a669eb8cf607664e5a61d5
Szecowka, Adam <adam.szecowka@gmail.com>
2019-09-20 14:48:09 +0200

    wip

--- components/director/pkg/scope/main_test.go
+++ components/director/pkg/scope/main_test.go
@@ -1,7 +1,12 @@
 package scope_test
 
+import (
+	"go.uber.org/goleak"
+	"testing"
+)
+
 // TestMain is used to verify that there are no unexpected goroutines running at the end of a test
 // TODO
-//func TestMain(m *testing.M) {
-//	goleak.VerifyTestMain(m)
-//}
+func TestMain(m *testing.M) {
+	goleak.VerifyTestMain(m)
+}
