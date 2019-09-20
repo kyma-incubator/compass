80e29f27a79bceb1193e980b1bc5fbf43d3c14f0
Szecowka, Adam <adam.szecowka@gmail.com>
2019-09-20 11:17:43 +0200

    wip

--- components/director/internal/scope/errors.go
+++
@@ -1,7 +0,0 @@
-package scope
-
-import "errors"
-
-var NoScopesInContextError = errors.New("cannot read scopes from context")
-var InsufficientScopesError = errors.New("insufficient scopes provided")
-var RequiredScopesNotDefinedError = errors.New("required scopes are not defined")
