9443f96e3c74e1c3d9628b0d9f28f0a55e817dcd
Paweł Kosiec <pawel.kosiec@gmail.com>
2019-09-20 09:13:50 +0200

    Add mock tenant mapping handler for Director (#315)
    * Add mock tenant mapping handler for Director
    * Improve logging for mock tenant mapping handler
    * Ignore log files

--- components/director/cmd/main.go
+++ components/director/cmd/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/kyma-incubator/compass/components/director/internal/tenantmapping"
+
 	"github.com/kyma-incubator/compass/components/director/internal/persistence"
 	"github.com/kyma-incubator/compass/components/director/internal/tenant"
 
@@ -32,6 +34,7 @@ type config struct {
 		SSLMode  string `envconfig:"default=disable,APP_DB_SSL"`
 	}
 	APIEndpoint           string `envconfig:"default=/graphql"`
+	TenantMappingEndpoint string `envconfig:"default=/tenant-mapping"`
 	PlaygroundAPIEndpoint string `envconfig:"default=/graphql"`
 }
 
@@ -61,14 +64,18 @@ func main() {
 	}
 	executableSchema := graphql.NewExecutableSchema(gqlCfg)
 
-	log.Infof("Registering endpoint on %s...", cfg.APIEndpoint)
 	router := mux.NewRouter()
 
+	log.Infof("Registering GraphQL endpoint on %s...", cfg.APIEndpoint)
 	router.Use(tenant.RequireAndPassContext)
 	router.HandleFunc("/", handler.Playground("Dataloader", cfg.PlaygroundAPIEndpoint))
 	router.HandleFunc(cfg.APIEndpoint, handler.GraphQL(executableSchema))
 
 	http.Handle("/", router)
+
+	log.Infof("Registering Tenant Mapping endpoint on %s...", cfg.TenantMappingEndpoint)
+	tenantMappingHandler := tenantmapping.NewHandler()
+	http.Handle(cfg.TenantMappingEndpoint, tenantMappingHandler)
 
 	log.Infof("Listening on %s...", cfg.Address)
 	if err := http.ListenAndServe(cfg.Address, nil); err != nil {
---
+++ components/director/internal/tenantmapping/handler.go
@@ -0,0 +1,87 @@
+package tenantmapping
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+
+	"github.com/pkg/errors"
+	log "github.com/sirupsen/logrus"
+)
+
+type Data struct {
+	Subject string      `json:"subject"`
+	Extra   interface{} `json:"extra"`
+	Header  interface{} `json:"header"`
+}
+
+type Handler struct{}
+
+func NewHandler() *Handler {
+	return &Handler{}
+}
+
+func (h *Handler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	if request.Method != http.MethodPost {
+		http.Error(writer, "Bad request method. Got %s, expected POST", http.StatusBadRequest)
+		return
+	}
+
+	logBuilder := strings.Builder{}
+	logBuilder.WriteString(fmt.Sprintf("\nHeaders: %+v", request.Header))
+
+	writer.Header().Set("Content-Type", "application/json")
+
+	var data Data
+	err := json.NewDecoder(request.Body).Decode(&data)
+	if err != nil {
+		if err == io.EOF {
+			http.Error(writer, "Request body is empty", http.StatusBadRequest)
+			return
+		}
+
+		wrappedErr := errors.Wrap(err, "while decoding request body")
+		http.Error(writer, wrappedErr.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	defer func() {
+		err := request.Body.Close()
+		if err != nil {
+			wrappedErr := errors.Wrap(err, "while decoding request body")
+			log.Error(wrappedErr)
+			http.Error(writer, wrappedErr.Error(), http.StatusInternalServerError)
+		}
+	}()
+
+	logBuilder.WriteString(fmt.Sprintf("\nInput: %+v", data))
+
+	if data.Extra == nil {
+		data.Extra = make(map[string]interface{})
+	}
+
+	extraMap, ok := data.Extra.(map[string]interface{})
+	if !ok {
+		err := fmt.Errorf("Incorrect type %T; expected map[string]interface{}\n", data.Extra)
+		log.Info(logBuilder.String())
+		log.Error(err)
+		http.Error(writer, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	extraMap["tenant"] = "9ac609e1-7487-4aa6-b600-0904b272b11f"
+	data.Extra = extraMap
+
+	logBuilder.WriteString(fmt.Sprintf("\nOutput: %+v\n", data))
+	log.Info(logBuilder.String())
+
+	err = json.NewEncoder(writer).Encode(data)
+	if err != nil {
+		wrappedErr := errors.Wrap(err, "while encoding data")
+		log.Error(wrappedErr)
+		http.Error(writer, wrappedErr.Error(), http.StatusInternalServerError)
+		return
+	}
+}
