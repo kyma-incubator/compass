e72571a2d8001ee246beb15032158f70a4f76d02
Arkadiusz Galwas <arkadiusz.galwas@sap.com>
2019-10-03 15:49:11 +0200

    Contract for graphql layer updated and repository interface added

--- components/provisioner/internal/persistence/repository.go
+++ components/provisioner/internal/persistence/repository.go
@@ -1,1 +1,60 @@
 package persistence
+
+import (
+	"database/sql"
+	"github.com/kyma-incubator/compass/components/provisioner/internal/model"
+	"time"
+)
+
+type Repository interface {
+	BeginTransaction() (*sql.Tx, error)
+	InsertCluster(runtimeID string, time time.Time, tx *sql.Tx) error
+	InsertClusterConfig(runtimeID string, config model.ClusterConfig, tx *sql.Tx) error
+	InsertKymaConfig(runtimeID string, config model.KymaConfig, tx *sql.Tx) error
+	InsertOperation(operation model.Operation, tx *sql.Tx) error
+	DeleteCluster(runtimeID string) error
+	GetRuntimeStatus(runtimeID string) (model.RuntimeStatus, error)
+	GetLastOperation(runtimeID string) (model.Operation, error)
+}
+
+type repository struct {
+	connection *sql.DB
+}
+
+func NewRepository(db *sql.DB) Repository {
+	return repository{
+		connection: db,
+	}
+}
+
+func (r repository) InsertCluster(runtimeID string, time time.Time, tx *sql.Tx) error {
+	return nil
+}
+
+func (r repository) InsertClusterConfig(runtimeID string, config model.ClusterConfig, tx *sql.Tx) error {
+	return nil
+}
+
+func (r repository) InsertKymaConfig(runtimeID string, config model.KymaConfig, tx *sql.Tx) error {
+	return nil
+}
+
+func (r repository) InsertOperation(operation model.Operation, tx *sql.Tx) error {
+	return nil
+}
+
+func (r repository) DeleteCluster(runtimeID string) error {
+	return nil
+}
+
+func (r repository) GetRuntimeStatus(runtimeID string) (model.RuntimeStatus, error) {
+	return model.RuntimeStatus{}, nil
+}
+
+func (r repository) BeginTransaction() (*sql.Tx, error) {
+	return r.connection.Begin()
+}
+
+func (r repository) GetLastOperation(runtimeID string) (model.Operation, error) {
+	return model.Operation{}, nil
+}
---
+++ components/provisioner/internal/persistence/runtime.go
@@ -0,0 +1,105 @@
+package persistence
+
+import (
+	"database/sql"
+	"github.com/kyma-incubator/compass/components/provisioner/internal/model"
+	"github.com/satori/go.uuid"
+	"github.com/sirupsen/logrus"
+	"time"
+)
+
+type RuntimeService interface {
+	GetStatus(runtimeID string) (model.RuntimeStatus, error)
+	SetProvisioningStarted(runtimeID string, clusterConfig model.ClusterConfig, kymaConfig model.KymaConfig) (model.Operation, error)
+	SetDeprovisioningStarted(runtimeID string) (model.Operation, error)
+	SetUpgradeStarted(runtimeID string) (model.Operation, error)
+	GetLastOperation(runtimeID string) (model.Operation, error)
+	Update(runtimeID string, kubeconfig string, deprovisioningContext string) error
+}
+
+type runtimeService struct {
+	repository Repository
+}
+
+func NewRuntimeService(repository Repository) RuntimeService {
+	return runtimeService{
+		repository: repository,
+	}
+}
+
+func (r runtimeService) GetStatus(runtimeID string) (model.RuntimeStatus, error) {
+	return model.RuntimeStatus{}, nil
+}
+
+func (r runtimeService) SetProvisioningStarted(runtimeID string, clusterConfig model.ClusterConfig, kymaConfig model.KymaConfig) (model.Operation, error) {
+
+	started := time.Now()
+
+	transaction, err := r.repository.BeginTransaction()
+	if err != nil {
+		return model.Operation{}, err
+	}
+
+	err = r.repository.InsertCluster(runtimeID, started, transaction)
+	if err != nil {
+		rollback(transaction, runtimeID)
+		return model.Operation{}, err
+	}
+
+	err = r.repository.InsertClusterConfig(runtimeID, clusterConfig, transaction)
+	if err != nil {
+		rollback(transaction, runtimeID)
+		return model.Operation{}, err
+	}
+
+	err = r.repository.InsertKymaConfig(runtimeID, kymaConfig, transaction)
+	if err != nil {
+		rollback(transaction, runtimeID)
+		return model.Operation{}, err
+	}
+
+	operation := model.Operation{
+		OperationID: uuid.NewV4().String(),
+		Operation:   model.Provision,
+		Started:     started,
+		State:       model.InProgress,
+		RuntimeID:   runtimeID,
+	}
+
+	err = r.repository.InsertOperation(operation, transaction)
+	if err != nil {
+		rollback(transaction, runtimeID)
+		return model.Operation{}, err
+	}
+
+	err = transaction.Commit()
+	if err != nil {
+		logrus.Errorf("Failed to commit transaction for runtime: '%s'.", runtimeID)
+		return model.Operation{}, err
+	}
+
+	return operation, nil
+}
+
+func rollback(transaction *sql.Tx, runtimeID string) {
+	err := transaction.Rollback()
+	if err != nil {
+		logrus.Errorf("Failed to rollback transaction for runtime: '%s'.", runtimeID)
+	}
+}
+
+func (r runtimeService) SetDeprovisioningStarted(runtimeID string) (model.Operation, error) {
+	return model.Operation{}, nil
+}
+
+func (r runtimeService) SetUpgradeStarted(runtimeID string) (model.Operation, error) {
+	return model.Operation{}, nil
+}
+
+func (r runtimeService) GetLastOperation(runtimeID string) (model.Operation, error) {
+	return model.Operation{}, nil
+}
+
+func (r runtimeService) Update(runtimeID string, kubeconfig string, deprovisioningContext string) error {
+	return nil
+}
