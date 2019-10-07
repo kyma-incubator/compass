d72e8bc63900effae90fc383f37ba86ebe0f3c21
Szymon Gibala <szymon.gibala@sap.com>
2019-10-07 12:10:42 +0200

    Modify API and Database schema

--- components/provisioner/assets/database/provisioner.sql
+++ components/provisioner/assets/database/provisioner.sql
@@ -1,3 +1,37 @@
+-- Cluster Config
+
+CREATE TABLE "GardenerConfig"
+(
+    id uuid PRIMARY KEY CHECK (id <> '00000000-0000-0000-0000-000000000000'),
+    name varchar(256) NOT NULL,
+    kubernetesVersion varchar(256) NOT NULL,
+    nodeCount integer NOT NULL,
+    volumeSize varchar(256) NOT NULL,
+    machineType varchar(256) NOT NULL,
+    region varchar(256) NOT NULL,
+    targetProvider varchar(256) NOT NULL,
+    targetSecret varchar(256) NOT NULL,
+    zone varchar(256) NOT NULL,
+    cidr varchar(256) NOT NULL,
+    autoScalerMin integer NOT NULL,
+    autoScalerMax integer NOT NULL,
+    maxSurge integer NOT NULL,
+    maxUnavailable integer NOT NULL
+);
+
+CREATE TABLE "GCPConfig"
+(
+    id uuid PRIMARY KEY CHECK (id <> '00000000-0000-0000-0000-000000000000'),
+    name varchar(256) NOT NULL,
+    kubernetesVersion varchar(256) NOT NULL,
+    numberOfNodes integer NOT NULL,
+    bootDiskSize varchar(256) NOT NULL,
+    machineType varchar(256) NOT NULL,
+    region varchar(256) NOT NULL,
+    zone varchar(256) NOT NULL
+);
+
+
 -- Cluster
 
 CREATE TABLE "Cluster"
@@ -5,32 +39,12 @@ CREATE TABLE "Cluster"
     id uuid PRIMARY KEY CHECK (id <> '00000000-0000-0000-0000-000000000000'),
     kubeconfig text,
     terraform_state jsonb,
-    creation_timestamp timestamp without time zone NOT NULL
-);
-
-
--- Cluster Config
-
-CREATE TABLE "InfrastructureProvider"
-(
-    id uuid PRIMARY KEY CHECK (id <> '00000000-0000-0000-0000-000000000000'),
-    name varchar(256) NOT NULL,
-    schema jsonb NOT NULL
-);
-
-CREATE TABLE "ClusterConfig"
-(
-    id uuid PRIMARY KEY CHECK (id <> '00000000-0000-0000-0000-000000000000'),
-    name varchar(256) NOT NULL,
-    node_count integer NOT NULL,
-    disk_size varchar(256) NOT NULL,
-    machine_type varchar(256) NOT NULL,
-    compute_zone varchar(256) NOT NULL,
-    version varchar(256) NOT NULL,
-    infrastructure_provider_id uuid NOT NULL,
-    foreign key (infrastructure_provider_id) REFERENCES "InfrastructureProvider" (id) ON DELETE CASCADE,
-    cluster_id uuid NOT NULL,
-    foreign key (cluster_id) REFERENCES "Cluster" (id) ON DELETE CASCADE
+    creation_timestamp timestamp without time zone NOT NULL,
+    gardener_config_id uuid,
+    gcp_config_id uuid,
+    foreign key (gardener_config_id) REFERENCES "GardenerConfig" (id) ON DELETE CASCADE,
+    foreign key (gcp_config_id) REFERENCES "GCPConfig" (id) ON DELETE CASCADE,
+    CHECK ( (gardener_config_id IS NULL AND gcp_config_id NOTNULL) OR (gcp_config_id IS NULL AND gardener_config_id NOTNULL) )
 );
 
 
