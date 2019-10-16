e4d261546be1321a1b1ba3566dc2b86d247363f0
Szymon Gibala <szymon.gibala@sap.com>
2019-10-16 13:43:38 +0200

    Adjust charts

--- tests/provisioner-tests/test/testkit/config.go
+++ tests/provisioner-tests/test/testkit/config.go
@@ -25,15 +25,10 @@ func ReadConfig() (TestConfig, error) {
 	cfg := TestConfig{}
 
 	err := envconfig.InitWithPrefix(&cfg, "APP")
-	exitOnError(err, "Error while loading app config")
+	if err != nil {
+		return TestConfig{}, errors.Wrap(err, "Error while loading app config")
+	}
 
 	log.Printf("Read configuration: %+v", cfg)
 	return cfg, nil
-}
-
-func exitOnError(err error, context string) {
-	if err != nil {
-		wrappedError := errors.Wrap(err, context)
-		log.Fatal(wrappedError)
-	}
 }
