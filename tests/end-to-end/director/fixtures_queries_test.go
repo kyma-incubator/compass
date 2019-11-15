15d310557193bd8566f894613ffa5c21d75f72ad
Karol Jaksik <jaksikkarol@gmail.com>
2019-11-15 11:36:23 +0100

    Add integration tests

--- tests/end-to-end/director/fixtures_queries_test.go
+++ tests/end-to-end/director/fixtures_queries_test.go
@@ -229,9 +229,7 @@ func createIntegrationSystem(t *testing.T, ctx context.Context, name string) *gr
 	if err != nil {
 		return nil
 	}
-
 	req := fixCreateIntegrationSystemRequest(in)
-
 	out := &graphql.IntegrationSystemExt{}
 	err = tc.RunOperation(ctx, req, out)
 	require.NotEmpty(t, out)
