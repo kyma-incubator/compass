cf0ddbf7285a26fb5a3bc256e13b56d8a5f84cc3
Krystian Cieslik <crabtree@users.noreply.github.com>
2019-11-20 08:05:41 +0100

    Cleanup director main.go file

---
+++ components/director/internal/persistence/config.go
@@ -0,0 +1,16 @@
+package persistence
+
+import "fmt"
+
+type DatabaseConfig struct {
+	User     string `envconfig:"default=postgres,APP_DB_USER"`
+	Password string `envconfig:"default=pgsql@12345,APP_DB_PASSWORD"`
+	Host     string `envconfig:"default=localhost,APP_DB_HOST"`
+	Port     string `envconfig:"default=5432,APP_DB_PORT"`
+	Name     string `envconfig:"default=postgres,APP_DB_NAME"`
+	SSLMode  string `envconfig:"default=disable,APP_DB_SSL"`
+}
+
+func GetConnString(cfg DatabaseConfig) string {
+	return fmt.Sprintf(connStringf, cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.Name, cfg.SSLMode)
+}
