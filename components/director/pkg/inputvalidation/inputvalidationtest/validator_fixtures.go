0d7ba2de454e9c3ac426bc428317e5c79fbee57b
Tomasz Gorgol <t.gorgol@sap.com>
2019-12-02 20:57:27 +0100

    Rebase

--- components/director/pkg/inputvalidation/inputvalidationtest/validator_fixtures.go
+++ components/director/pkg/inputvalidation/inputvalidationtest/validator_fixtures.go
@@ -13,8 +13,6 @@ const (
 	EmptyString = ""
 	ValidName   = "thi5-1npu7.15-valid"
 	InvalidName = "0iNvALiD"
-	ValidUUID   = "d221a3d4-258d-430d-ad5e-5729aa400ffc"
-	InvalidUUID = "zxc4-258d430dad5e-5fc"
 	ValidURL    = "https://kyma-project.io"
 	InvalidURL  = "http:/kyma-projectio/path/"
 )
