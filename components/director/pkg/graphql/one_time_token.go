2a8ee6fd3179a548ae1fdbf2f81ed66cdb138b48
Karol Jaksik <jaksikkarol@gmail.com>
2019-12-13 11:27:08 +0100

    Add ext struct

--- components/director/pkg/graphql/one_time_token.go
+++ components/director/pkg/graphql/one_time_token.go
@@ -4,3 +4,9 @@ type OneTimeToken struct {
 	Token        string `json:"token"`
 	ConnectorURL string `json:"connectorURL"`
 }
+
+type OneTimeTokenExt struct {
+	OneTimeToken
+	Raw        string `json:"raw"`
+	RawEncoded string `json:"rawEncoded"`
+}
