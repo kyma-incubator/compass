763bc3eebefaeda49847607734850c08ad7d5cca
Karol Jaksik <jaksikkarol@gmail.com>
2020-01-10 13:01:43 +0100

    Create db migrations

---
+++ components/schema-migrator/migrations/202001101149_name.down.sql
@@ -0,0 +1,24 @@
+alter table api_definitions
+drop constraint api_definitions_tenant_id_fkey;
+alter table api_runtime_auths
+drop constraint api_runtime_auths_tenant_id_fkey;
+alter table applications
+drop constraint applications_tenant_id_fkey;
+alter table documents
+drop constraint documents_tenant_id_fkey;
+alter table event_api_definitions
+drop constraint event_api_definitions_tenant_id_fkey;
+alter table fetch_requests
+drop constraint fetch_requests_tenant_id_fkey;
+alter table label_definitions
+drop constraint label_definitions_tenant_id_fkey;
+alter table labels
+drop constraint labels_tenant_id_fkey;
+alter table runtimes
+drop constraint runtimes_tenant_id_fkey;
+alter table system_auths
+drop constraint system_auths_tenant_id_fkey;
+alter table webhooks
+drop constraint webhooks_tenant_id_fkey;
+
+drop table tenant_mapping;
