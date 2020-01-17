e01fda922a7aaa631470102fd12737d6e91840c9
Michal Kempski <michal.kempski@sap.com>
2020-01-17 15:09:30 +0100

    Register KEB as an integration system (#635)

--- components/kyma-environment-broker/cmd/broker/main.go
+++ components/kyma-environment-broker/cmd/broker/main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"crypto/tls"
 	"log"
 	"net/http"
 	"os"
+	"time"
+
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/config"
+
+	"github.com/kyma-incubator/compass/components/kyma-environment-broker/internal/director"
+	"github.com/kyma-incubator/compass/components/kyma-environment-broker/internal/director/oauth"
 
 	"github.com/kyma-incubator/compass/components/kyma-environment-broker/internal/storage"
 
@@ -26,6 +34,7 @@ type Config struct {
 	Port string `envconfig:"default=8080"`
 
 	Provisioning broker.ProvisioningConfig
+	Director     director.Config
 	Database     storage.Config
 
 	// feature flag indicates whether use Provisioner API which returns RuntimeID
@@ -55,16 +64,35 @@ func main() {
 		provisionerClient = provisioner.NewProvisionerClient(cfg.Provisioning.URL, true)
 	}
 
+	k8sCfg, err := config.GetConfig()
+	fatalOnError(err)
+	cli, err := client.New(k8sCfg, client.Options{})
+	fatalOnError(err)
+
+	oauthClient := oauth.NewOauthClient(newHTTPClient(cfg.Director.SkipCertVerification), cli, cfg.Director.OauthCredentialsSecretName, cfg.Director.Namespace)
+	fatalOnError(oauthClient.WaitForCredentials())
+
+	director.NewDirectorClient(oauthClient)
+
 	db, err := storage.New(cfg.Database.ConnectionURL())
 	fatalOnError(err)
 
-	broker, err := broker.NewBroker(provisionerClient, cfg.Provisioning, db.Instances())
+	kymaEnvBroker, err := broker.NewBroker(provisionerClient, cfg.Provisioning, db.Instances())
 	fatalOnError(err)
 
-	brokerAPI := brokerapi.New(broker, logger, brokerCredentials)
+	brokerAPI := brokerapi.New(kymaEnvBroker, logger, brokerCredentials)
 	r := handlers.LoggingHandler(os.Stdout, brokerAPI)
 
 	fatalOnError(http.ListenAndServe(cfg.Host+":"+cfg.Port, r))
+}
+
+func newHTTPClient(skipCertVeryfication bool) *http.Client {
+	return &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: skipCertVeryfication},
+		},
+		Timeout: 30 * time.Second,
+	}
 }
 
 func fatalOnError(err error) {
