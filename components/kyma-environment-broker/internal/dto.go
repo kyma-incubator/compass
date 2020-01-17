480eb53f35ecea9b2c107a12716798d4e1718fea
Piotr Miśkiewicz <piotr.miskiewicz@sap.com>
2020-01-17 12:26:27 +0100

    HB sends optional components in json schema (#663)

--- components/kyma-environment-broker/internal/dto.go
+++ components/kyma-environment-broker/internal/dto.go
@@ -1,16 +1,17 @@
 package internal
 
 type ProvisioningParametersDTO struct {
-	Name           string  `json:"name"`
-	NodeCount      *int    `json:"nodeCount"`
-	VolumeSizeGb   *int    `json:"volumeSizeGb"`
-	MachineType    *string `json:"machineType"`
-	Region         *string `json:"region"`
-	Zone           *string `json:"zone"`
-	AutoScalerMin  *int    `json:"autoScalerMin"`
-	AutoScalerMax  *int    `json:"autoScalerMax"`
-	MaxSurge       *int    `json:"maxSurge"`
-	MaxUnavailable *int    `json:"maxUnavailable"`
+	Name           string   `json:"name"`
+	NodeCount      *int     `json:"nodeCount"`
+	VolumeSizeGb   *int     `json:"volumeSizeGb"`
+	MachineType    *string  `json:"machineType"`
+	Region         *string  `json:"region"`
+	Zone           *string  `json:"zone"`
+	AutoScalerMin  *int     `json:"autoScalerMin"`
+	AutoScalerMax  *int     `json:"autoScalerMax"`
+	MaxSurge       *int     `json:"maxSurge"`
+	MaxUnavailable *int     `json:"maxUnavailable"`
+	Components     []string `json:"components"`
 }
 
 type ERSContext struct {
