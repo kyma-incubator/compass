3d838a543c48400edb8564d59d7766be00134490
Karol Jaksik <jaksikkarol@gmail.com>
2020-01-24 11:48:35 +0100

    Remove unnecesary migrations

--- components/schema-migrator/migrations/202001221149_add_tenant_storage.up.sql
+++ components/schema-migrator/migrations/202001221149_add_tenant_storage.up.sql
@@ -10,13 +10,3 @@ status tenant_status default 'Active'
 
 alter table business_tenant_mappings
 add unique (external_tenant, provider_name);
-
-insert into business_tenant_mappings(id)
-select tenant_id from applications a2 union select tenant_id from runtimes r2;
-
-update business_tenant_mappings set external_tenant = id;
-
-update business_tenant_mappings set external_name = 'Tenant';
-
-update business_tenant_mappings set provider_name = 'Compass';
-
