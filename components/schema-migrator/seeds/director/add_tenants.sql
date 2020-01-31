d07411d20b47463470af322e87b759b90f48fd2c
Karol Jaksik <jaksikkarol@gmail.com>
2020-01-31 10:40:30 +0100

    Seed db with tenants (#759)
    Co-authored-by: Krystian Cieślik <3641496+crabtree@users.noreply.github.com>

---
+++ components/schema-migrator/seeds/director/add_tenants.sql
@@ -0,0 +1,6 @@
+INSERT INTO public.business_tenant_mappings (id, external_name, external_tenant, provider_name, status) VALUES
+('3e64ebae-38b5-46a0-b1ed-9ccee153a0ae', 'default', '3e64ebae-38b5-46a0-b1ed-9ccee153a0ae', 'Compass', 'Active'),
+('1eba80dd-8ff6-54ee-be4d-77944d17b10b', 'foo', '1eba80dd-8ff6-54ee-be4d-77944d17b10b', 'Compass', 'Active'),
+('9ca034f1-11ab-5b25-b76f-dc77106f571d', 'bar', '9ca034f1-11ab-5b25-b76f-dc77106f571d', 'Compass', 'Active'),
+('af9f84a9-1d3a-4d9f-ae0c-94f883b33b6e', 'foobar', 'af9f84a9-1d3a-4d9f-ae0c-94f883b33b6e', 'Compass', 'Active'),
+('2bf03de1-23b1-4063-9d3e-67096800accc', 'foobaz' , '2bf03de1-23b1-4063-9d3e-67096800accc', 'Compass' , 'Active');
