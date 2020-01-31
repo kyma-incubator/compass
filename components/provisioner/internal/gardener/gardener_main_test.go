f71409a89e47e53e2a459d496fe090fd05b7f7ef
Szymon Gibala <szymon.gibala@sap.com>
2020-01-31 12:29:59 +0100

    Remove crd from env test

--- components/provisioner/internal/gardener/gardener_main_test.go
+++ components/provisioner/internal/gardener/gardener_main_test.go
@@ -4,11 +4,8 @@ import (
 	"os"
 	"testing"
 
-	"github.com/kyma-incubator/compass/components/provisioner/internal/util"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
-	apiextensionsv1beta1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
-	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/rest"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
 )
@@ -32,31 +29,7 @@ func TestMain(m *testing.M) {
 }
 
 func setupEnv() error {
-	testEnv = &envtest.Environment{
-		CRDs: []*apiextensionsv1beta1.CustomResourceDefinition{
-			{
-				ObjectMeta: v1.ObjectMeta{Name: "shoots.core.gardener.cloud"},
-				Spec: apiextensionsv1beta1.CustomResourceDefinitionSpec{
-					Group: "core.gardener.cloud",
-					Names: apiextensionsv1beta1.CustomResourceDefinitionNames{
-						Plural:   "shoots",
-						Singular: "shoot",
-						Kind:     "Shoot",
-					},
-					Scope:        "",
-					Validation:   nil,
-					Subresources: nil,
-					Versions: []apiextensionsv1beta1.CustomResourceDefinitionVersion{
-						{
-							Name:    "v1beta1",
-							Storage: true,
-						},
-					},
-					PreserveUnknownFields: util.BoolPtr(true),
-				},
-			},
-		},
-	}
+	testEnv = &envtest.Environment{}
 
 	var err error
 	cfg, err = testEnv.Start()
