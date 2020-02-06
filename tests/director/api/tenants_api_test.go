67572c9056d970803833860db5c3541bb9d894e8
Karol Jaksik <jaksikkarol@gmail.com>
2020-02-06 16:48:17 +0100

    Fix tenants in e2e tests

--- tests/director/api/tenants_api_test.go
+++ tests/director/api/tenants_api_test.go
@@ -43,8 +43,8 @@ func fixDefaultTenants() []*graphql.Tenant {
 	return []*graphql.Tenant{
 		fixTenant("3e64ebae-38b5-46a0-b1ed-9ccee153a0ae", "default"),
 		fixTenant("1eba80dd-8ff6-54ee-be4d-77944d17b10b", "foo"),
-		fixTenant("9ca034f1-11ab-5b25-b76f-dc77106f571d", "bar"),
-		fixTenant("af9f84a9-1d3a-4d9f-ae0c-94f883b33b6e", "foobar"),
-		fixTenant("2bf03de1-23b1-4063-9d3e-67096800accc", "foobaz"),
+		fixTenant("9ca034f1-11ab-5b25-b76f-dc77106f571d", "test-default-tenant"),
+		fixTenant("af9f84a9-1d3a-4d9f-ae0c-94f883b33b6e", "bar"),
+		fixTenant("2bf03de1-23b1-4063-9d3e-67096800accc", "foobar"),
 	}
 }
