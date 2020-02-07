08151eb6e7786e4eb000e135f2ad190f4bebc016
Karol Furgol <furgolkarol@gmail.com>
2020-02-07 16:18:21 +0100

    fix down migration

--- components/schema-migrator/migrations/director/202002071200_add_package_definitions.down.sql
+++ components/schema-migrator/migrations/director/202002071200_add_package_definitions.down.sql
@@ -1,14 +1,14 @@
-ALTER TABLE api_definitions DROP COLUMN package_definition_id;
 ALTER TABLE api_definitions
     DROP CONSTRAINT api_definitions_package_definition_id_fk;
+ALTER TABLE api_definitions DROP COLUMN package_definition_id;
 
-ALTER TABLE event_api_definitions DROP COLUMN package_definition_id;
 ALTER TABLE event_api_definitions
     DROP CONSTRAINT event_api_definitions_package_definition_id_fk;
+ALTER TABLE event_api_definitions DROP COLUMN package_definition_id;
 
-ALTER TABLE documents DROP COLUMN package_definition_id;
 ALTER TABLE documents
     DROP CONSTRAINT documents_package_definition_id_fk;
+ALTER TABLE documents DROP COLUMN package_definition_id;
 
 DROP TABLE api_instance_auths;
 DROP TABLE package_definitions;
