b7bc820091fbce6cff440465a3b1b8d36f637755
Karol Furgol <furgolkarol@gmail.com>
2020-02-10 07:15:46 +0100

    set NOT NULL constraint on app_id on down migration

--- components/schema-migrator/migrations/director/202002071200_add_package_definitions.down.sql
+++ components/schema-migrator/migrations/director/202002071200_add_package_definitions.down.sql
@@ -1,11 +1,14 @@
+ALTER TABLE api_definitions ALTER COLUMN app_id SET NOT NULL;
 ALTER TABLE api_definitions
     DROP CONSTRAINT api_definitions_package_definition_id_fk;
 ALTER TABLE api_definitions DROP COLUMN package_definition_id;
 
+ALTER TABLE event_api_definitions ALTER COLUMN app_id SET NOT NULL;
 ALTER TABLE event_api_definitions
     DROP CONSTRAINT event_api_definitions_package_definition_id_fk;
 ALTER TABLE event_api_definitions DROP COLUMN package_definition_id;
 
+ALTER TABLE documents ALTER COLUMN app_id SET NOT NULL;
 ALTER TABLE documents
     DROP CONSTRAINT documents_package_definition_id_fk;
 ALTER TABLE documents DROP COLUMN package_definition_id;
