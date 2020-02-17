abed786f7705d58a39ec74b1f9bd30f1509581a9
Paweł Kosiec <pawel.kosiec@gmail.com>
2020-02-17 16:48:15 +0100

    Validate Package Create and Update Input

--- components/director/pkg/graphql/package_validation.go
+++ components/director/pkg/graphql/package_validation.go
@@ -1,11 +1,34 @@
 package graphql
 
-// TODO: Replace with real implementation
+import (
+	validation "github.com/go-ozzo/ozzo-validation"
+	"github.com/go-ozzo/ozzo-validation/is"
+	"github.com/kyma-incubator/compass/components/director/pkg/inputvalidation"
+)
+
 func (i PackageCreateInput) Validate() error {
-	return nil
+	return validation.ValidateStruct(&i,
+		validation.Field(&i.Name, validation.Required, is.PrintableASCII, validation.Length(1, 100)),
+		validation.Field(&i.Description, validation.RuneLength(0, descriptionStringLengthLimit)),
+		validation.Field(&i.DefaultInstanceAuth, validation.NilOrNotEmpty),
+		validation.Field(&i.InstanceAuthRequestInputSchema, validation.NilOrNotEmpty),
+		validation.Field(&i.APIDefinitions, inputvalidation.Each(validation.Required)),
+		validation.Field(&i.EventDefinitions, inputvalidation.Each(validation.Required)),
+		validation.Field(&i.Documents, inputvalidation.Each(validation.Required)),
+	)
+}
+
+func (i PackageUpdateInput) Validate() error {
+	return validation.ValidateStruct(&i,
+		validation.Field(&i.Name, validation.Required, is.PrintableASCII, validation.Length(1, 100)),
+		validation.Field(&i.Description, validation.RuneLength(0, descriptionStringLengthLimit)),
+		validation.Field(&i.DefaultInstanceAuth, validation.NilOrNotEmpty),
+		validation.Field(&i.InstanceAuthRequestInputSchema, validation.NilOrNotEmpty),
+	)
 }
 
 // TODO: Replace with real implementation
-func (i PackageUpdateInput) Validate() error {
+func (i PackageInstanceAuthRequestInput) Validate() error {
 	return nil
 }
+
