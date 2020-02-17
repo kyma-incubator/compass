ebf1ce3f7d55660da8683b47d038d56b102d6196
Arkadiusz Galwas <arkadiusz.galwas@sap.com>
2020-02-17 14:51:33 +0100

    Image updated

--- tests/connectivity-adapter/test/testkit/config.go
+++ tests/connectivity-adapter/test/testkit/config.go
@@ -8,7 +8,7 @@ import (
 type Configuration struct {
 	ConnectivityAdapterUrl     string `envconfig:"default=https://adapter-gateway.kyma.local"`
 	ConnectivityAdapterMtlsUrl string `envconfig:"default=https://adapter-gateway-mtls.kyma.local"`
-	DirectorUrl                string `envconfig:"default=http://127.0.0.1:3000/graphql"`
+	DirectorUrl                string `envconfig:"default=http://compass-director.compass-system.svc.cluster.local:3000/graphql"`
 	SkipSslVerify              bool   `envconfig:"default=true"`
 	EventsBaseURL              string `envconfig:"default=https://events.com"`
 	Tenant                     string `envconfig:"default=3e64ebae-38b5-46a0-b1ed-9ccee153a0ae"`
