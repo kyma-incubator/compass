fce820ef154c8fb200c66343f4b8533db8e67153
Kamil Sputo <sputo.kamil@gmail.com>
2020-03-05 10:32:19 +0100

    Implement e2e provisioning test (#736)
    * Implemented e2e provisioning test from call to KEB to provisioning Kyma runtime with clean up procedure (removing uaa Service Instance before deprovisioning)

---
+++ tests/e2e/provisioning/internal/director/query.go
@@ -0,0 +1,17 @@
+package director
+
+import "fmt"
+
+const instanceIDLabelKey = "broker_instance_id"
+
+type queryProvider struct{}
+
+func (qp queryProvider) Runtime(instanceID string) string {
+	return fmt.Sprintf(`query {
+	result: runtimes(filter: { key: "%s" query: "\"%s\"" }) {
+    data {
+      id
+	}
+}
+}`, instanceIDLabelKey, instanceID)
+}
