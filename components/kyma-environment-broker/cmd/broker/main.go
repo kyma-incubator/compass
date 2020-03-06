b88f815c8e307afba78a89adb8d14cc2b75a9a13
Rakesh Garimella <rakesh.garimella@sap.com>
2020-03-06 14:57:56 +0100

    add glue code

--- components/kyma-environment-broker/cmd/broker/main.go
+++ components/kyma-environment-broker/cmd/broker/main.go
@@ -132,6 +132,7 @@ func main() {
 	initialisation := provisioning.NewInitialisationStep(db.Operations(), db.Instances(), provisionerClient, directorClient, inputFactory, cfg.ManagementPlaneURL)
 
 	resolveCredentialsStep := provisioning.NewResolveCredentialsStep(db.Operations(), accountProvider)
+	backupSetupStep := provisioning.NewSetupBackupStep(db.Operations(), db.Instances(), provisionerClient, cfg.ServiceManager)
 
 	runtimeStep := provisioning.NewCreateRuntimeStep(db.Operations(), db.Instances(), provisionerClient, cfg.ServiceManager)
 
@@ -139,6 +140,8 @@ func main() {
 	stepManager := process.NewManager(db.Operations(), logs)
 	stepManager.InitStep(initialisation)
 	stepManager.AddStep(1, resolveCredentialsStep)
+
+	stepManager.AddStep(1, backupSetupStep)
 	stepManager.AddStep(2, runtimeStep)
 
 	queue := process.NewQueue(stepManager)
