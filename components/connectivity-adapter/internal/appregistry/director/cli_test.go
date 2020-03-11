cff3f1ee49d5737a26ee06283ee26635a93d9e2d
Paweł Kosiec <pawel.kosiec@gmail.com>
2020-03-11 21:35:36 +0100

    Add tests for CreateAPIDefinition

--- components/connectivity-adapter/internal/appregistry/director/cli_test.go
+++ components/connectivity-adapter/internal/appregistry/director/cli_test.go
@@ -1,6 +1,7 @@
 package director_test
 
 import (
+	"github.com/pkg/errors"
 	"testing"
 
 	"github.com/kyma-incubator/compass/components/connectivity-adapter/internal/appregistry/director"
@@ -14,6 +15,8 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+var testErr = errors.New("Test error")
+
 func TestDirectorClient_CreateAPIDefinition(t *testing.T) {
 	packageID := "foo"
 	in := graphql.APIDefinitionInput{
@@ -24,7 +27,6 @@ func TestDirectorClient_CreateAPIDefinition(t *testing.T) {
 		Name                 string
 		GQLClientFn          func() *gcliautomock.GraphQLClient
 		GraphqlizerFn        func() *automock.GraphQLizer
-		GqQLFieldsProviderFn func() *automock.GqlFieldsProvider
 		ExpectedResult       *string
 		ExpectedErr          error
 	}{
@@ -52,19 +54,47 @@ func TestDirectorClient_CreateAPIDefinition(t *testing.T) {
 				am.On("APIDefinitionInputToGQL", in).Return("input", nil).Once()
 				return am
 			},
-			GqQLFieldsProviderFn: func() *automock.GqlFieldsProvider {
-				am := &automock.GqlFieldsProvider{}
-				am.On("ForAPIDefinition").Return("fields").Once()
+			ExpectedResult: str.Ptr("pkgID"),
+		},
+		{
+			Name: "Error - GraphQL input",
+			GQLClientFn: func() *gcliautomock.GraphQLClient {
+				am := &gcliautomock.GraphQLClient{}
 				return am
 			},
-			ExpectedResult: str.Ptr("pkgID"),
+			GraphqlizerFn: func() *automock.GraphQLizer {
+				am := &automock.GraphQLizer{}
+				am.On("APIDefinitionInputToGQL", in).Return("", testErr).Once()
+				return am
+			},
+			ExpectedErr: testErr,
+		},
+		{
+			Name: "Error - GraphQL client",
+			GQLClientFn: func() *gcliautomock.GraphQLClient {
+				am := &gcliautomock.GraphQLClient{}
+				am.On("Run",
+					mock.Anything,
+					gcli.NewRequest("mutation {\n\t\t\tresult: addAPIDefinitionToPackage(packageID: \"foo\", in: input) {\n\t\t\t\t\tfields\n\t\t\t\t}\n\t\t\t}"),
+					mock.AnythingOfType("*director.SuccessAPIDefinition"),
+				).Return(testErr).Once()
+				return am
+			},
+			GraphqlizerFn: func() *automock.GraphQLizer {
+				am := &automock.GraphQLizer{}
+				am.On("APIDefinitionInputToGQL", in).Return("input", nil).Once()
+				return am
+			},
+			ExpectedErr: testErr,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.Name, func(t *testing.T) {
 			gqlCli := tt.GQLClientFn()
 			gqlizer := tt.GraphqlizerFn()
-			gqlFieldsProvider := tt.GqQLFieldsProviderFn()
+
+			gqlFieldsProvider := &automock.GqlFieldsProvider{}
+			gqlFieldsProvider.On("ForAPIDefinition").Return("fields").Maybe()
 
 			dirCli := director.NewClient(gqlCli, gqlizer, gqlFieldsProvider)
 
@@ -74,7 +104,7 @@ func TestDirectorClient_CreateAPIDefinition(t *testing.T) {
 				assert.Equal(t, *tt.ExpectedResult, result)
 			} else {
 				require.Error(t, err)
-				assert.Equal(t, tt.ExpectedErr.Error(), err.Error())
+				assert.Contains(t, err.Error(), tt.ExpectedErr.Error())
 			}
 
 			mock.AssertExpectationsForObjects(t, gqlCli, gqlizer, gqlFieldsProvider)
@@ -82,379 +112,3 @@ func TestDirectorClient_CreateAPIDefinition(t *testing.T) {
 	}
 }
 
-//
-//func Test_directorClient_CreateDocument(t *testing.T) {
-//	type fields struct {
-//		cli               gqlcli.GraphQLClient
-//		graphqlizer       GraphQLizer
-//		gqlFieldsProvider GqlFieldsProvider
-//	}
-//	type args struct {
-//		packageID     string
-//		documentInput graphql.DocumentInput
-//	}
-//	tests := []struct {
-//		name    string
-//		fields  fields
-//		args    args
-//		want    string
-//		wantErr bool
-//	}{
-//		// TODO: Add test cases.
-//	}
-//	for _, tt := range tests {
-//		t.Run(tt.name, func(t *testing.T) {
-//			c := &directorClient{
-//				cli:               tt.fields.cli,
-//				graphqlizer:       tt.fields.graphqlizer,
-//				gqlFieldsProvider: tt.fields.gqlFieldsProvider,
-//			}
-//			got, err := c.CreateDocument(tt.args.packageID, tt.args.documentInput)
-//			if (err != nil) != tt.wantErr {
-//				t.Errorf("CreateDocument() error = %v, wantErr %v", err, tt.wantErr)
-//				return
-//			}
-//			if got != tt.want {
-//				t.Errorf("CreateDocument() got = %v, want %v", got, tt.want)
-//			}
-//		})
-//	}
-//}
-//
-//func Test_directorClient_CreateEventDefinition(t *testing.T) {
-//	type fields struct {
-//		cli               gqlcli.GraphQLClient
-//		graphqlizer       GraphQLizer
-//		gqlFieldsProvider GqlFieldsProvider
-//	}
-//	type args struct {
-//		packageID            string
-//		eventDefinitionInput graphql.EventDefinitionInput
-//	}
-//	tests := []struct {
-//		name    string
-//		fields  fields
-//		args    args
-//		want    string
-//		wantErr bool
-//	}{
-//		// TODO: Add test cases.
-//	}
-//	for _, tt := range tests {
-//		t.Run(tt.name, func(t *testing.T) {
-//			c := &directorClient{
-//				cli:               tt.fields.cli,
-//				graphqlizer:       tt.fields.graphqlizer,
-//				gqlFieldsProvider: tt.fields.gqlFieldsProvider,
-//			}
-//			got, err := c.CreateEventDefinition(tt.args.packageID, tt.args.eventDefinitionInput)
-//			if (err != nil) != tt.wantErr {
-//				t.Errorf("CreateEventDefinition() error = %v, wantErr %v", err, tt.wantErr)
-//				return
-//			}
-//			if got != tt.want {
-//				t.Errorf("CreateEventDefinition() got = %v, want %v", got, tt.want)
-//			}
-//		})
-//	}
-//}
-//
-//func Test_directorClient_CreatePackage(t *testing.T) {
-//	type fields struct {
-//		cli               gqlcli.GraphQLClient
-//		graphqlizer       GraphQLizer
-//		gqlFieldsProvider GqlFieldsProvider
-//	}
-//	type args struct {
-//		appID string
-//		in    graphql.PackageCreateInput
-//	}
-//	tests := []struct {
-//		name    string
-//		fields  fields
-//		args    args
-//		want    string
-//		wantErr bool
-//	}{
-//		// TODO: Add test cases.
-//	}
-//	for _, tt := range tests {
-//		t.Run(tt.name, func(t *testing.T) {
-//			c := &directorClient{
-//				cli:               tt.fields.cli,
-//				graphqlizer:       tt.fields.graphqlizer,
-//				gqlFieldsProvider: tt.fields.gqlFieldsProvider,
-//			}
-//			got, err := c.CreatePackage(tt.args.appID, tt.args.in)
-//			if (err != nil) != tt.wantErr {
-//				t.Errorf("CreatePackage() error = %v, wantErr %v", err, tt.wantErr)
-//				return
-//			}
-//			if got != tt.want {
-//				t.Errorf("CreatePackage() got = %v, want %v", got, tt.want)
-//			}
-//		})
-//	}
-//}
-//
-//func Test_directorClient_DeleteAPIDefinition(t *testing.T) {
-//	type fields struct {
-//		cli               gqlcli.GraphQLClient
-//		graphqlizer       GraphQLizer
-//		gqlFieldsProvider GqlFieldsProvider
-//	}
-//	type args struct {
-//		apiID string
-//	}
-//	tests := []struct {
-//		name    string
-//		fields  fields
-//		args    args
-//		wantErr bool
-//	}{
-//		// TODO: Add test cases.
-//	}
-//	for _, tt := range tests {
-//		t.Run(tt.name, func(t *testing.T) {
-//			c := &directorClient{
-//				cli:               tt.fields.cli,
-//				graphqlizer:       tt.fields.graphqlizer,
-//				gqlFieldsProvider: tt.fields.gqlFieldsProvider,
-//			}
-//			if err := c.DeleteAPIDefinition(tt.args.apiID); (err != nil) != tt.wantErr {
-//				t.Errorf("DeleteAPIDefinition() error = %v, wantErr %v", err, tt.wantErr)
-//			}
-//		})
-//	}
-//}
-//
-//func Test_directorClient_DeleteDocument(t *testing.T) {
-//	type fields struct {
-//		cli               gqlcli.GraphQLClient
-//		graphqlizer       GraphQLizer
-//		gqlFieldsProvider GqlFieldsProvider
-//	}
-//	type args struct {
-//		documentID string
-//	}
-//	tests := []struct {
-//		name    string
-//		fields  fields
-//		args    args
-//		wantErr bool
-//	}{
-//		// TODO: Add test cases.
-//	}
-//	for _, tt := range tests {
-//		t.Run(tt.name, func(t *testing.T) {
-//			c := &directorClient{
-//				cli:               tt.fields.cli,
-//				graphqlizer:       tt.fields.graphqlizer,
-//				gqlFieldsProvider: tt.fields.gqlFieldsProvider,
-//			}
-//			if err := c.DeleteDocument(tt.args.documentID); (err != nil) != tt.wantErr {
-//				t.Errorf("DeleteDocument() error = %v, wantErr %v", err, tt.wantErr)
-//			}
-//		})
-//	}
-//}
-//
-//func Test_directorClient_DeleteEventDefinition(t *testing.T) {
-//	type fields struct {
-//		cli               gqlcli.GraphQLClient
-//		graphqlizer       GraphQLizer
-//		gqlFieldsProvider GqlFieldsProvider
-//	}
-//	type args struct {
-//		eventID string
-//	}
-//	tests := []struct {
-//		name    string
-//		fields  fields
-//		args    args
-//		wantErr bool
-//	}{
-//		// TODO: Add test cases.
-//	}
-//	for _, tt := range tests {
-//		t.Run(tt.name, func(t *testing.T) {
-//			c := &directorClient{
-//				cli:               tt.fields.cli,
-//				graphqlizer:       tt.fields.graphqlizer,
-//				gqlFieldsProvider: tt.fields.gqlFieldsProvider,
-//			}
-//			if err := c.DeleteEventDefinition(tt.args.eventID); (err != nil) != tt.wantErr {
-//				t.Errorf("DeleteEventDefinition() error = %v, wantErr %v", err, tt.wantErr)
-//			}
-//		})
-//	}
-//}
-//
-//func Test_directorClient_DeletePackage(t *testing.T) {
-//	type fields struct {
-//		cli               gqlcli.GraphQLClient
-//		graphqlizer       GraphQLizer
-//		gqlFieldsProvider GqlFieldsProvider
-//	}
-//	type args struct {
-//		packageID string
-//	}
-//	tests := []struct {
-//		name    string
-//		fields  fields
-//		args    args
-//		wantErr bool
-//	}{
-//		// TODO: Add test cases.
-//	}
-//	for _, tt := range tests {
-//		t.Run(tt.name, func(t *testing.T) {
-//			c := &directorClient{
-//				cli:               tt.fields.cli,
-//				graphqlizer:       tt.fields.graphqlizer,
-//				gqlFieldsProvider: tt.fields.gqlFieldsProvider,
-//			}
-//			if err := c.DeletePackage(tt.args.packageID); (err != nil) != tt.wantErr {
-//				t.Errorf("DeletePackage() error = %v, wantErr %v", err, tt.wantErr)
-//			}
-//		})
-//	}
-//}
-//
-//func Test_directorClient_GetApplicationsByNameRequest(t *testing.T) {
-//	type fields struct {
-//		cli               gqlcli.GraphQLClient
-//		graphqlizer       GraphQLizer
-//		gqlFieldsProvider GqlFieldsProvider
-//	}
-//	type args struct {
-//		appName string
-//	}
-//	tests := []struct {
-//		name   string
-//		fields fields
-//		args   args
-//		want   *gcli.Request
-//	}{
-//		// TODO: Add test cases.
-//	}
-//	for _, tt := range tests {
-//		t.Run(tt.name, func(t *testing.T) {
-//			c := &directorClient{
-//				cli:               tt.fields.cli,
-//				graphqlizer:       tt.fields.graphqlizer,
-//				gqlFieldsProvider: tt.fields.gqlFieldsProvider,
-//			}
-//			if got := c.GetApplicationsByNameRequest(tt.args.appName); !reflect.DeepEqual(got, tt.want) {
-//				t.Errorf("GetApplicationsByNameRequest() = %v, want %v", got, tt.want)
-//			}
-//		})
-//	}
-//}
-//
-//func Test_directorClient_GetPackage(t *testing.T) {
-//	type fields struct {
-//		cli               gqlcli.GraphQLClient
-//		graphqlizer       GraphQLizer
-//		gqlFieldsProvider GqlFieldsProvider
-//	}
-//	type args struct {
-//		appID     string
-//		packageID string
-//	}
-//	tests := []struct {
-//		name    string
-//		fields  fields
-//		args    args
-//		want    graphql.PackageExt
-//		wantErr bool
-//	}{
-//		// TODO: Add test cases.
-//	}
-//	for _, tt := range tests {
-//		t.Run(tt.name, func(t *testing.T) {
-//			c := &directorClient{
-//				cli:               tt.fields.cli,
-//				graphqlizer:       tt.fields.graphqlizer,
-//				gqlFieldsProvider: tt.fields.gqlFieldsProvider,
-//			}
-//			got, err := c.GetPackage(tt.args.appID, tt.args.packageID)
-//			if (err != nil) != tt.wantErr {
-//				t.Errorf("GetPackage() error = %v, wantErr %v", err, tt.wantErr)
-//				return
-//			}
-//			if !reflect.DeepEqual(got, tt.want) {
-//				t.Errorf("GetPackage() got = %v, want %v", got, tt.want)
-//			}
-//		})
-//	}
-//}
-//
-//func Test_directorClient_ListPackages(t *testing.T) {
-//	type fields struct {
-//		cli               gqlcli.GraphQLClient
-//		graphqlizer       GraphQLizer
-//		gqlFieldsProvider GqlFieldsProvider
-//	}
-//	type args struct {
-//		appID string
-//	}
-//	tests := []struct {
-//		name    string
-//		fields  fields
-//		args    args
-//		want    []*graphql.PackageExt
-//		wantErr bool
-//	}{
-//		// TODO: Add test cases.
-//	}
-//	for _, tt := range tests {
-//		t.Run(tt.name, func(t *testing.T) {
-//			c := &directorClient{
-//				cli:               tt.fields.cli,
-//				graphqlizer:       tt.fields.graphqlizer,
-//				gqlFieldsProvider: tt.fields.gqlFieldsProvider,
-//			}
-//			got, err := c.ListPackages(tt.args.appID)
-//			if (err != nil) != tt.wantErr {
-//				t.Errorf("ListPackages() error = %v, wantErr %v", err, tt.wantErr)
-//				return
-//			}
-//			if !reflect.DeepEqual(got, tt.want) {
-//				t.Errorf("ListPackages() got = %v, want %v", got, tt.want)
-//			}
-//		})
-//	}
-//}
-//
-//func Test_directorClient_UpdatePackage(t *testing.T) {
-//	type fields struct {
-//		cli               gqlcli.GraphQLClient
-//		graphqlizer       GraphQLizer
-//		gqlFieldsProvider GqlFieldsProvider
-//	}
-//	type args struct {
-//		packageID string
-//		in        graphql.PackageUpdateInput
-//	}
-//	tests := []struct {
-//		name    string
-//		fields  fields
-//		args    args
-//		wantErr bool
-//	}{
-//		// TODO: Add test cases.
-//	}
-//	for _, tt := range tests {
-//		t.Run(tt.name, func(t *testing.T) {
-//			c := &directorClient{
-//				cli:               tt.fields.cli,
-//				graphqlizer:       tt.fields.graphqlizer,
-//				gqlFieldsProvider: tt.fields.gqlFieldsProvider,
-//			}
-//			if err := c.UpdatePackage(tt.args.packageID, tt.args.in); (err != nil) != tt.wantErr {
-//				t.Errorf("UpdatePackage() error = %v, wantErr %v", err, tt.wantErr)
-//			}
-//		})
-//	}
-//}
