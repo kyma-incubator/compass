410a39c95b9f44bce461dffea361a15373675d03
Szymon Gibala <szymon.gibala@sap.com>
2020-03-17 14:56:01 +0100

    Add more tests

--- components/provisioner/internal/installation/release/on_demand_test.go
+++ components/provisioner/internal/installation/release/on_demand_test.go
@@ -82,3 +82,116 @@ func TestOnDemand_GetReleaseByVersion(t *testing.T) {
 	})
 
 }
+
+func TestOnDemand_GetReleaseByVersion_Error(t *testing.T) {
+
+	t.Run("should return error when failed to get release from database", func(t *testing.T) {
+		// given
+		repo := &mocks.Repository{}
+		repo.On("GetReleaseByVersion", onDemandVersion).Return(model.Release{}, dberrors.Internal("error"))
+
+		onDemand := NewOnDemandWrapper(nil, repo)
+
+		// when
+		_, err := onDemand.GetReleaseByVersion(onDemandVersion)
+
+		// then
+		require.Error(t, err)
+	})
+
+	t.Run("should return error when non on-demand version not found in database", func(t *testing.T) {
+		// given
+		repo := &mocks.Repository{}
+		repo.On("GetReleaseByVersion", "not-supported-version").Return(model.Release{}, dberrors.NotFound("error"))
+
+		onDemand := NewOnDemandWrapper(nil, repo)
+
+		// when
+		_, err := onDemand.GetReleaseByVersion("not-supported-version")
+
+		// then
+		require.Error(t, err)
+	})
+
+	t.Run("should return error when failed to save release", func(t *testing.T) {
+		// given
+		httpClient := newTestClient(func(req *http.Request) *http.Response {
+			if strings.HasSuffix(req.URL.String(), "kyma-installer-cluster.yaml") {
+				return &http.Response{
+					StatusCode: http.StatusOK,
+					Body:       ioutil.NopCloser(bytes.NewBufferString("installer")),
+				}
+			}
+			if strings.HasSuffix(req.URL.String(), "tiller.yaml") {
+				return &http.Response{
+					StatusCode: http.StatusOK,
+					Body:       ioutil.NopCloser(bytes.NewBufferString("tiller")),
+				}
+			}
+			return &http.Response{
+				StatusCode: http.StatusBadRequest,
+			}
+		})
+
+		repo := &mocks.Repository{}
+		repo.On("GetReleaseByVersion", onDemandVersion).Return(model.Release{}, dberrors.NotFound("error"))
+		repo.On("SaveRelease", mock.AnythingOfType("model.Release")).Return(model.Release{}, dberrors.Internal("error"))
+
+		onDemand := NewOnDemandWrapper(httpClient, repo)
+
+		// when
+		_, err := onDemand.GetReleaseByVersion(onDemandVersion)
+
+		// then
+		require.Error(t, err)
+	})
+
+	for _, testCase := range []struct {
+		description string
+		httpClient  *http.Client
+	}{
+		{
+			description: "should return error when failed to download tiller",
+			httpClient: newTestClient(func(req *http.Request) *http.Response {
+				if strings.HasSuffix(req.URL.String(), "kyma-installer-cluster.yaml") {
+					return &http.Response{
+						StatusCode: http.StatusOK,
+						Body:       ioutil.NopCloser(bytes.NewBufferString("installer")),
+					}
+				}
+				if strings.HasSuffix(req.URL.String(), "tiller.yaml") {
+					return &http.Response{
+						StatusCode: http.StatusBadRequest,
+						Body:       ioutil.NopCloser(bytes.NewBufferString("")),
+					}
+				}
+				return &http.Response{
+					StatusCode: http.StatusBadRequest,
+				}
+			}),
+		},
+		{
+			description: "should return error when failed to download installer",
+			httpClient: newTestClient(func(req *http.Request) *http.Response {
+				return &http.Response{
+					StatusCode: http.StatusBadRequest,
+					Body:       ioutil.NopCloser(bytes.NewBufferString("")),
+				}
+			}),
+		},
+	} {
+		t.Run(testCase.description, func(t *testing.T) {
+			repo := &mocks.Repository{}
+			repo.On("GetReleaseByVersion", onDemandVersion).Return(model.Release{}, dberrors.NotFound("error"))
+
+			onDemand := NewOnDemandWrapper(testCase.httpClient, repo)
+
+			// when
+			_, err := onDemand.GetReleaseByVersion(onDemandVersion)
+
+			// then
+			require.Error(t, err)
+		})
+	}
+
+}
