85a1972f1199bc176fdc003691aaa1350aae2af2
Nils Schmidt <nils.schmidt02@sap.com>
2020-03-18 13:01:46 +0100

    Split provider and client

--- components/kyma-environment-broker/internal/hyperscaler/azure/client.go
+++ components/kyma-environment-broker/internal/hyperscaler/azure/client.go
@@ -1,107 +1,61 @@
 package azure
 
 import (
-	"fmt"
+	"context"
 
 	"github.com/Azure/azure-sdk-for-go/services/eventhub/mgmt/2017-04-01/eventhub"
 	"github.com/Azure/azure-sdk-for-go/services/resources/mgmt/2019-05-01/resources"
-	"github.com/Azure/go-autorest/autorest"
-	"github.com/Azure/go-autorest/autorest/adal"
-	"github.com/Azure/go-autorest/autorest/azure"
 )
 
-type HyperscalerProvider interface {
-	GetClient(config *Config) (AzureInterface, error)
+type AzureInterface interface {
+	GetEventhubAccessKeys(ctx context.Context, resourceGroupName string, namespaceName string, authorizationRuleName string) (result eventhub.AccessKeys, err error)
+	CreateResourceGroup(ctx context.Context, config *Config, name string) (resources.Group, error)
+	CreateNamespace(ctx context.Context, azureCfg *Config, groupName, namespace string) (*eventhub.EHNamespace, error)
 }
 
-var _ HyperscalerProvider = (*azureProvider)(nil)
+var _ AzureInterface = (*AzureClient)(nil)
 
-type azureProvider struct{}
-
-func NewAzureProvider() HyperscalerProvider {
-	return &azureProvider{}
+type AzureClient struct {
+	// the actual azure client
+	eventhubNamespaceClient eventhub.NamespacesClient
+	resourcegroupClient     resources.GroupsClient
 }
 
-// GetClient gets a client for interacting with Azure
-func (ac *azureProvider) GetClient(config *Config) (AzureInterface, error) {
-
-	environment, err := config.Environment()
-	if err != nil {
-		return nil, err
+func NewAzureClient(namespaceClient eventhub.NamespacesClient, resourcegroupClient resources.GroupsClient) *AzureClient {
+	return &AzureClient{
+		eventhubNamespaceClient: namespaceClient,
+		resourcegroupClient:     resourcegroupClient,
 	}
-
-	authorizer, err := ac.getResourceManagementAuthorizer(config, environment)
-	if err != nil {
-		return nil, fmt.Errorf("failed to initialize authorizer with error: %v", err)
-	}
-
-	// create namespace client
-	nsClient, err := ac.getNamespaceClient(config, authorizer)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create namespace client with error: %v", err)
-	}
-
-	// create resource group client
-	resourcegroupClient, err := ac.getGroupsClient(config, authorizer)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create resource group client with error: %v", err)
-	}
-
-	// create azure client
-	return NewAzureClient(nsClient, resourcegroupClient), nil
+}
+func (nc *AzureClient) GetEventhubAccessKeys(ctx context.Context, resourceGroupName string, namespaceName string, authorizationRuleName string) (result eventhub.AccessKeys, err error) {
+	return nc.eventhubNamespaceClient.ListKeys(ctx, resourceGroupName, namespaceName, authorizationRuleName)
 }
 
-// getGroupsClient gets a client for handling of Azure Namespaces
-func (ac *azureProvider) getNamespaceClient(config *Config, authorizer autorest.Authorizer) (eventhub.NamespacesClient, error) {
-	nsClient := eventhub.NewNamespacesClient(config.subscriptionID)
-	nsClient.Authorizer = authorizer
-
-	if err := nsClient.AddToUserAgent(config.userAgent); err != nil {
-		return eventhub.NamespacesClient{}, fmt.Errorf("failed to add use agent [%s] with error: %v", config.userAgent, err)
-	}
-	return nsClient, nil
+func (nc *AzureClient) CreateResourceGroup(ctx context.Context, config *Config, name string) (resources.Group, error) {
+	// we need to use a copy of the location, because the following azure call will modify it
+	locationCopy := config.GetLocation()
+	return nc.resourcegroupClient.CreateOrUpdate(ctx, name, resources.Group{Location: &locationCopy})
 }
 
-// getGroupsClient gets a client for handling of Azure ResourceGroups
-func (ac *azureProvider) getGroupsClient(config *Config, authorizer autorest.Authorizer) (resources.GroupsClient, error) {
-	client := resources.NewGroupsClient(config.subscriptionID)
-	client.Authorizer = authorizer
-
-	if err := client.AddToUserAgent(config.userAgent); err != nil {
-		return resources.GroupsClient{}, fmt.Errorf("failed to add use agent [%s] with error: %v", config.userAgent, err)
-	}
-
-	return client, nil
+func (nc *AzureClient) CreateNamespace(ctx context.Context, azureCfg *Config, groupName, namespace string) (*eventhub.EHNamespace, error) {
+	// we need to use a copy of the location, because the following azure call will modify it
+	locationCopy := azureCfg.GetLocation()
+	parameters := eventhub.EHNamespace{Location: &locationCopy}
+	ehNamespace, err := nc.createAndWait(ctx, groupName, namespace, parameters)
+	return &ehNamespace, err
 }
 
-func (ac *azureProvider) getResourceManagementAuthorizer(config *Config, environment *azure.Environment) (autorest.Authorizer, error) {
-	armAuthorizer, err := ac.getAuthorizerForResource(config, environment)
+func (nc *AzureClient) createAndWait(ctx context.Context, resourceGroupName string, namespaceName string, parameters eventhub.EHNamespace) (result eventhub.EHNamespace, err error) {
+	future, err := nc.eventhubNamespaceClient.CreateOrUpdate(ctx, resourceGroupName, namespaceName, parameters)
 	if err != nil {
-		return nil, err
+		return eventhub.EHNamespace{}, err
 	}
 
-	return armAuthorizer, err
-}
-
-func (ac *azureProvider) getEnvironment(config *Config) (*azure.Environment, error) {
-	environment, err := config.Environment()
+	err = future.WaitForCompletionRef(ctx, nc.eventhubNamespaceClient.Client)
 	if err != nil {
-		return nil, err
+		return eventhub.EHNamespace{}, err
 	}
 
-	return environment, nil
-}
-
-func (ac *azureProvider) getAuthorizerForResource(config *Config, environment *azure.Environment) (autorest.Authorizer, error) {
-
-	oauthConfig, err := adal.NewOAuthConfig(environment.ActiveDirectoryEndpoint, config.tenantID)
-	if err != nil {
-		return nil, err
-	}
-
-	token, err := adal.NewServicePrincipalToken(*oauthConfig, config.clientID, config.clientSecret, environment.ResourceManagerEndpoint)
-	if err != nil {
-		return nil, err
-	}
-	return autorest.NewBearerAuthorizer(token), err
+	result, err = future.Result(nc.eventhubNamespaceClient)
+	return result, err
 }
