f3fd9c0e311be96e521810ab7d832edd5c6fa8da
Szecowka, Adam <adam.szecowka@gmail.com>
2020-03-18 14:26:56 +0100

    Allow to configure connection pool to the database in Director, Tenant Fetcher and Tenant Loader

--- components/director/pkg/persistence/config.go
+++ components/director/pkg/persistence/config.go
@@ -5,14 +5,16 @@ import "fmt"
 const connStringf string = "host=%s port=%s user=%s password=%s dbname=%s sslmode=%s"
 
 type DatabaseConfig struct {
-	User     string `envconfig:"default=postgres,APP_DB_USER"`
-	Password string `envconfig:"default=pgsql@12345,APP_DB_PASSWORD"`
-	Host     string `envconfig:"default=localhost,APP_DB_HOST"`
-	Port     string `envconfig:"default=5432,APP_DB_PORT"`
-	Name     string `envconfig:"default=postgres,APP_DB_NAME"`
-	SSLMode  string `envconfig:"default=disable,APP_DB_SSL"`
+	User               string `envconfig:"default=postgres,APP_DB_USER"`
+	Password           string `envconfig:"default=pgsql@12345,APP_DB_PASSWORD"`
+	Host               string `envconfig:"default=localhost,APP_DB_HOST"`
+	Port               string `envconfig:"default=5432,APP_DB_PORT"`
+	Name               string `envconfig:"default=postgres,APP_DB_NAME"`
+	SSLMode            string `envconfig:"default=disable,APP_DB_SSL"`
+	MaxOpenConnections int    `envconfig:"APP_DB_MAX_OPEN_CONNECTIONS"`
+	MaxIdleConnections int    `envconfig:"APP_DB_MAX_IDLE_CONNECTIONS"`
 }
 
-func GetConnString(cfg DatabaseConfig) string {
+func (cfg DatabaseConfig) GetConnString() string {
 	return fmt.Sprintf(connStringf, cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.Name, cfg.SSLMode)
 }
