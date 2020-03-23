06e19a8633d8df8f2e502fbb126db0c2432f895a
Karol Jaksik <jaksikkarol@gmail.com>
2020-03-23 15:10:23 +0100

    Add tenant constraint

--- components/schema-migrator/migrations/director/202003231306_add_automatic_scenario_assignement.up.sql
+++ components/schema-migrator/migrations/director/202003231306_add_automatic_scenario_assignement.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE automatic_scenario_assignements (
     scenario VARCHAR(128),
-    tenant_id UUID NOT NULL REFERENCES business_tenant_mappings(id),
+    tenant_id UUID NOT NULL,
     key VARCHAR(256) NOT NULL,
     value JSONB);
 
@@ -8,4 +8,11 @@ CREATE TABLE automatic_scenario_assignements (
 CREATE INDEX ON automatic_scenario_assignements (tenant_id);
 
 ALTER TABLE automatic_scenario_assignements
-ADD CONSTRAINT automatic_scenario_assignements_pk PRIMARY KEY (tenant_id, scenario);
+    ADD CONSTRAINT automatic_scenario_assignements_pk
+    PRIMARY KEY (tenant_id, scenario);
+
+ALTER TABLE automatic_scenario_assignements
+    ADD CONSTRAINT automatic_scenario_assignements_tenant_constraint
+    FOREIGN KEY (tenant_id)
+    REFERENCES business_tenant_mappings(id)
+    ON DELETE CASCADE;
