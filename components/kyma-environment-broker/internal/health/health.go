643bf8ab0f78e980a69f4f727bc8b3954b2aa709
Kamil Sputo <sputo.kamil@gmail.com>
2020-03-23 17:27:35 +0100

    Add response body 'ok' to liveness probe; Remove multiplied header writing from readiness probe

--- components/kyma-environment-broker/internal/health/health.go
+++ components/kyma-environment-broker/internal/health/health.go
@@ -10,6 +10,10 @@ func LivenessHandler(logger lager.Logger) func(w http.ResponseWriter, req *http.
 	return func(w http.ResponseWriter, req *http.Request) {
 		logger.Info("liveness check ok")
 		w.WriteHeader(http.StatusOK)
+		_, err := w.Write([]byte("ok"))
+		if err != nil {
+			logger.Error("while writing to response body", err)
+		}
 	}
 }
 
@@ -19,10 +23,12 @@ func ReadinessHandler(logger lager.Logger) func(w http.ResponseWriter, req *http
 		if err != nil {
 			logger.Error("while sending request on readiness check", err)
 			w.WriteHeader(http.StatusInternalServerError)
+			return
 		}
 		if resp.StatusCode != http.StatusOK {
 			logger.Info("got unexpected status on readiness check")
 			w.WriteHeader(http.StatusInternalServerError)
+			return
 		}
 
 		w.WriteHeader(http.StatusOK)
