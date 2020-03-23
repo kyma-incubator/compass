0f731790ed699fc19380be35ff805b9f2689201f
Karol Jaksik <jaksikkarol@gmail.com>
2020-03-23 16:25:10 +0100

    Fix typo

--- components/schema-migrator/migrations/director/202003231306_add_automatic_scenario_assignement.down.sql
+++ components/schema-migrator/migrations/director/202003231306_add_automatic_scenario_assignement.down.sql
@@ -1,1 +1,1 @@
-DROP TABLE automatic_scenario_assignements;
+DROP TABLE automatic_scenario_assignments;
--- components/schema-migrator/migrations/director/202003231306_add_automatic_scenario_assignement.up.sql
+++ components/schema-migrator/migrations/director/202003231306_add_automatic_scenario_assignement.up.sql
@@ -1,18 +1,18 @@
-CREATE TABLE automatic_scenario_assignements (
+CREATE TABLE automatic_scenario_assignments (
     scenario VARCHAR(128),
     tenant_id UUID NOT NULL,
     key VARCHAR(256) NOT NULL,
     value JSONB);
 
 
-CREATE INDEX ON automatic_scenario_assignements (tenant_id);
+CREATE INDEX ON automatic_scenario_assignments (tenant_id);
 
-ALTER TABLE automatic_scenario_assignements
-    ADD CONSTRAINT automatic_scenario_assignements_pk
+ALTER TABLE automatic_scenario_assignments
+    ADD CONSTRAINT automatic_scenario_assignments_pk
     PRIMARY KEY (tenant_id, scenario);
 
-ALTER TABLE automatic_scenario_assignements
-    ADD CONSTRAINT automatic_scenario_assignements_tenant_constraint
+ALTER TABLE automatic_scenario_assignments
+    ADD CONSTRAINT automatic_scenario_assignments_tenant_constraint
     FOREIGN KEY (tenant_id)
     REFERENCES business_tenant_mappings(id)
     ON DELETE CASCADE;
