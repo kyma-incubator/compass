3ad1145a19f9f3cd66668296942154db09b5e14b
Karol Jaksik <jaksikkarol@gmail.com>
2020-03-24 07:43:49 +0100

    Change the type of value

--- components/schema-migrator/migrations/director/202003231306_add_automatic_scenario_assignement.up.sql
+++ components/schema-migrator/migrations/director/202003231306_add_automatic_scenario_assignement.up.sql
@@ -2,7 +2,7 @@ CREATE TABLE automatic_scenario_assignments (
     scenario VARCHAR(128),
     tenant_id UUID NOT NULL,
     key VARCHAR(256) NOT NULL,
-    value JSONB);
+    value VARCHAR(256));
 
 
 CREATE INDEX ON automatic_scenario_assignments (tenant_id);
