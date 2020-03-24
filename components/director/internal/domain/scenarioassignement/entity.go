52eecb1193609d8ef450f8412be73107b2ef8200
Karol Jaksik <jaksikkarol@gmail.com>
2020-03-24 07:56:22 +0100

    Fmt the files

--- components/director/internal/domain/scenarioassignement/entity.go
+++ components/director/internal/domain/scenarioassignement/entity.go
@@ -1,8 +1,8 @@
 package scenarioassignement
 
 type Entity struct {
-	Scenario string `db:"scenario"`
-	TenantID string `db:"tenant_id"`
-	SelectorKey      string `db:"selector_key"`
-	SelectorValue    string `db:"selector_value"`
+	Scenario      string `db:"scenario"`
+	TenantID      string `db:"tenant_id"`
+	SelectorKey   string `db:"selector_key"`
+	SelectorValue string `db:"selector_value"`
 }
