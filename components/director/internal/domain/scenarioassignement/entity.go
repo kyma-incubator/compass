3e531eabcd798cc82297ad5d72b17a1eefb63de1
Karol Jaksik <jaksikkarol@gmail.com>
2020-03-24 07:40:38 +0100

    Apply suggestions from code review
    Co-Authored-By: Krystian Cieślik <3641496+crabtree@users.noreply.github.com>

--- components/director/internal/domain/scenarioassignement/entity.go
+++ components/director/internal/domain/scenarioassignement/entity.go
@@ -3,6 +3,6 @@ package scenarioassignement
 type Entity struct {
 	Scenario string `db:"scenario"`
 	TenantID string `db:"tenant_id"`
-	Key      string `db:"key"`
-	Value    string `db:"value"`
+	SelectorKey      string `db:"selector_key"`
+	SelectorValue    string `db:"selector_value"`
 }
