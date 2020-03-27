560989a60d2c23364aaeb28cf33c37cd7fc4fd7e
Gaurav Abbi <gaurav.abbi@sap.com>
2020-03-27 15:41:23 +0100

    Add environment variable to disable avs (#1093)

--- components/kyma-environment-broker/internal/avs/config.go
+++ components/kyma-environment-broker/internal/avs/config.go
@@ -8,4 +8,5 @@ type Config struct {
 	ApiEndpoint        string
 	DefinitionType     string `envconfig:"default=BASIC"`
 	ApiKey             string
+	Disabled           bool `envconfig:"default=false"`
 }
