1706e49e8768f47acb0025663ec700588c142ae9
parostatkiem <jan.sudczak@sap.com>
2020-04-07 11:37:58 +0200

    handle existing "name" labels in the migration script

--- components/schema-migrator/migrations/director/202007041321_add_name_labels_to_runtimes.up.sql
+++ components/schema-migrator/migrations/director/202007041321_add_name_labels_to_runtimes.up.sql
@@ -1,10 +1,10 @@
-IF NOT EXISTS ( SELECT * FROM labels 
-                   WHERE "key"='name'
-                   AND runtime_id=id
-                   )
-BEGIN
-    INSERT INTO labels
-        (id ,tenant_id, runtime_id, "key","value")
-    SELECT uuid_in(md5(random()::text || clock_timestamp()::text)::cstring), tenant_id, id, 'name', to_json("name")
-    FROM runtimes 
-END
+
+INSERT INTO labels
+    (id ,tenant_id, runtime_id, "key", "value")
+SELECT uuid_in(md5(random()::text || clock_timestamp()::text)::cstring), unnamed.tenant_id, unnamed.runtime_id, 'name', to_json(unnamed.runtime_name)
+FROM (
+    SELECT r.id as runtime_id, r.name as runtime_name, r.tenant_id as tenant_id
+    FROM runtimes r 
+    LEFT OUTER JOIN labels l on l.runtime_id = r.id AND l.key = 'name'
+    WHERE "value" IS NULL
+) as unnamed; 
