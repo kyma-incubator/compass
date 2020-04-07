7b892ea40c23b7af1703e2337b110d18d45b1f4c
Szymon Janota <szymon.janota@sap.com>
2020-04-07 12:08:35 +0200

    Set name label for runtimes by default

--- components/director/internal/domain/runtime/service.go
+++ components/director/internal/domain/runtime/service.go
@@ -13,6 +13,10 @@ import (
 	"github.com/pkg/errors"
 )
 
+const (
+	nameKey   = "name"
+)
+
 //go:generate mockery -name=RuntimeRepository -output=automock -outpkg=automock -case=underscore
 type RuntimeRepository interface {
 	Exists(ctx context.Context, tenant, id string) (bool, error)
@@ -141,6 +145,8 @@ func (s *service) Create(ctx context.Context, in model.RuntimeInput) (string, er
 		return "", errors.Wrapf(err, "while ensuring Label Definition with key %s exists", model.ScenariosKey)
 	}
 	s.scenariosService.AddDefaultScenarioIfEnabled(&in.Labels)
+
+	in.Labels[nameKey] = in.Name
 
 	err = s.labelUpsertService.UpsertMultipleLabels(ctx, rtmTenant, model.RuntimeLabelableObject, id, in.Labels)
 	if err != nil {
