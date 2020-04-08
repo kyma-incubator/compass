bdec1d3b07ca38f66ee95f22550e0257a540d274
Szymon Janota <szymon.janota@sap.com>
2020-04-08 14:29:12 +0200

    Use uuid-ossp to generate uuids during migration

--- components/schema-migrator/migrations/director/202007041321_add_name_labels_to_runtimes.up.sql
+++ components/schema-migrator/migrations/director/202007041321_add_name_labels_to_runtimes.up.sql
@@ -1,7 +1,8 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 INSERT INTO labels
     (id ,tenant_id, runtime_id, "key", "value")
-SELECT uuid_in(md5(random()::text || clock_timestamp()::text)::cstring), unnamed.tenant_id, unnamed.runtime_id, 'name', to_json(unnamed.runtime_name)
+SELECT uuid_generate_v4(), unnamed.tenant_id, unnamed.runtime_id, 'name', to_json(unnamed.runtime_name)
 FROM (
     SELECT r.id as runtime_id, r.name as runtime_name, r.tenant_id as tenant_id
     FROM runtimes r 
