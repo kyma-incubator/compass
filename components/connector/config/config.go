73f136cd620f92d740806500ba1c19d445db1370
franpog859 <franciszek.pog@gmail.com>
2020-04-09 11:54:39 +0200

    Make secret keys configurable

--- components/connector/config/config.go
+++ components/connector/config/config.go
@@ -20,9 +20,16 @@ type Config struct {
 		Locality           string `envconfig:"default=Locality"`
 		Province           string `envconfig:"default=State"`
 	}
-	CertificateValidityTime     time.Duration `envconfig:"default=2160h"`
-	CASecretName                string        `envconfig:"default=kyma-integration/connector-service-app-ca"`
-	RootCACertificateSecretName string        `envconfig:"optional"`
+	CertificateValidityTime time.Duration `envconfig:"default=2160h"`
+	CASecret                struct {
+		Name           string `envconfig:"default=kyma-integration/connector-service-app-ca"`
+		CertificateKey string `envconfig:"default=ca.crt"`
+		KeyKey         string `envconfig:"default=ca.key"`
+	}
+	RootCASecret struct {
+		Name           string `envconfig:"optional"`
+		CertificateKey string `envconfig:"default=cacert"`
+	}
 
 	CertificateDataHeader   string `envconfig:"default=Certificate-Data"`
 	RevocationConfigMapName string `envconfig:"default=compass-system/revocations-Config"`
@@ -42,7 +49,8 @@ func (c *Config) String() string {
 	return fmt.Sprintf("ExternalAddress: %s, InternalAddress: %s, APIEndpoint: %s, HydratorAddress: %s, "+
 		"CSRSubjectCountry: %s, CSRSubjectOrganization: %s, CSRSubjectOrganizationalUnit: %s, "+
 		"CSRSubjectLocality: %s, CSRSubjectProvince: %s, "+
-		"CertificateValidityTime: %s, CASecretName: %s, RootCACertificateSecretName: %s, CertificateDataHeader: %s, "+
+		"CertificateValidityTime: %s, CASecretName: %s, CASecretCertificateKey: %s, CASecretKeyKey: %s, "+
+		"RootCASecretName: %s, RootCASecretCertificateKey: %s, CertificateDataHeader: %s, "+
 		"CertificateSecuredConnectorURL: %s, "+
 		"RevocationConfigMapName: %s, "+
 		"TokenLength: %d, TokenRuntimeExpiration: %s, TokenApplicationExpiration: %s, TokenCSRExpiration: %s, "+
@@ -50,7 +58,8 @@ func (c *Config) String() string {
 		c.ExternalAddress, c.InternalAddress, c.APIEndpoint, c.HydratorAddress,
 		c.CSRSubject.Country, c.CSRSubject.Organization, c.CSRSubject.OrganizationalUnit,
 		c.CSRSubject.Locality, c.CSRSubject.Province,
-		c.CertificateValidityTime, c.CASecretName, c.RootCACertificateSecretName, c.CertificateDataHeader,
+		c.CertificateValidityTime, c.CASecret.Name, c.CASecret.CertificateKey, c.CASecret.KeyKey,
+		c.RootCASecret.Name, c.RootCASecret.CertificateKey, c.CertificateDataHeader,
 		c.CertificateSecuredConnectorURL,
 		c.RevocationConfigMapName,
 		c.Token.Length, c.Token.RuntimeExpiration.String(), c.Token.ApplicationExpiration.String(), c.Token.CSRExpiration.String(),
