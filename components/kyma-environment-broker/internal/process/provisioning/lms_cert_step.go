1106f87ca6f4191463f85a14d70ca6fb404de744
Miskiewicz, Piotr <piotr.miskiewicz@sap.com>
2020-04-22 09:54:09 +0200

    Add lms tenant id to logs

--- components/kyma-environment-broker/internal/process/provisioning/lms_cert_step.go
+++ components/kyma-environment-broker/internal/process/provisioning/lms_cert_step.go
@@ -77,7 +77,7 @@ func (s *lmsCertStep) Run(operation internal.ProvisioningOperation, logger logru
 	// check if LMS tenant is ready
 	status, err := s.provider.GetTenantStatus(operation.Lms.TenantID)
 	if err != nil {
-		logger.Errorf("Unable to get LMS Tenant status: %s", err.Error())
+		logger.Errorf("Unable to get LMS Tenant (id=%s) status: %s", operation.Lms.TenantID, err.Error())
 		if time.Since(operation.Lms.RequestedAt) > lmsTimeout {
 			logger.Error("Setting LMS operation failed - tenant provisioning timed out, last error: %s", err.Error())
 			return s.failLmsAndUpdate(operation)
