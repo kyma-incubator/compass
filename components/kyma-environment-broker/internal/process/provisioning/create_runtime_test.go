7e461a5d8645aba9e61568362ef8523547a22361
Benjamin Somhegyi <benjamin.somhegyi@sap.com>
2020-04-24 17:21:29 +0200

    Adjust mock data

--- components/kyma-environment-broker/internal/process/provisioning/create_runtime_test.go
+++ components/kyma-environment-broker/internal/process/provisioning/create_runtime_test.go
@@ -64,7 +64,7 @@ func TestCreateRuntimeStep_Run(t *testing.T) {
 				Region:            "europe-west4-a",
 				Provider:          "gcp",
 				WorkerCidr:        "10.250.0.0/19",
-				AutoScalerMin:     2,
+				AutoScalerMin:     3,
 				AutoScalerMax:     4,
 				MaxSurge:          4,
 				MaxUnavailable:    1,
