39e3c263b8ce0d01ef23d3391f52650e93e23787
Rakesh Garimella <rakesh.garimella@sap.com>
2020-05-05 17:47:04 +0200

    Fix config

--- components/kyma-environment-broker/internal/process/provisioning/audit_log_step.go
+++ components/kyma-environment-broker/internal/process/provisioning/audit_log_step.go
@@ -69,20 +69,20 @@ func (alo *AuditLogOverrides) Run(operation internal.ProvisioningOperation, logg
 	operation.InputCreator.AppendOverrides("logging", []*gqlschema.ConfigEntryInput{
 		{Key: "fluent-bit.conf.script", Value: replaceTenantID},
 		{Key: "fluent-bit.conf.extra", Value: fmt.Sprintf(`
+[INPUT]
+        Name              tail
+        Tag               dex.*
+        Path              /var/log/containers/*_dex-*.log
+        DB                /var/log/flb_kube_dex.db
+        parser            docker
+        Mem_Buf_Limit     5MB
+        Skip_Long_Lines   On
+        Refresh_Interval  10
 [FILTER]
         Name    lua
         Match   dex.*
         script  script.lua
         call    reformat
-
-[FILTER]
-        Name    lua
-        Match   dex.*
-        script  script.lua
-        call    reformat
-[OUTPUT]
-        Name    stdout
-        Match   dex.*
 [OUTPUT]
         Name             http
         Match            dex.*
