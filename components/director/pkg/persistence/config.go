f7fd46e4d503cfa1f80a4a5f4df53062f81dbb00
Damian Badura <45110612+dbadura@users.noreply.github.com>
2020-05-12 09:26:14 +0200

    Add maxConneciton live parameter to director (#1255)
    * add maxConneciton live parameter to fix broken pipe after time
    
    * update values
    
    * Update components/director/README.md

--- components/director/pkg/persistence/config.go
+++ components/director/pkg/persistence/config.go
@@ -1,18 +1,22 @@
 package persistence
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 const connStringf string = "host=%s port=%s user=%s password=%s dbname=%s sslmode=%s"
 
 type DatabaseConfig struct {
-	User               string `envconfig:"default=postgres,APP_DB_USER"`
-	Password           string `envconfig:"default=pgsql@12345,APP_DB_PASSWORD"`
-	Host               string `envconfig:"default=localhost,APP_DB_HOST"`
-	Port               string `envconfig:"default=5432,APP_DB_PORT"`
-	Name               string `envconfig:"default=compass,APP_DB_NAME"`
-	SSLMode            string `envconfig:"default=disable,APP_DB_SSL"`
-	MaxOpenConnections int    `envconfig:"default=2,APP_DB_MAX_OPEN_CONNECTIONS"`
-	MaxIdleConnections int    `envconfig:"default=2,APP_DB_MAX_IDLE_CONNECTIONS"`
+	User               string        `envconfig:"default=postgres,APP_DB_USER"`
+	Password           string        `envconfig:"default=pgsql@12345,APP_DB_PASSWORD"`
+	Host               string        `envconfig:"default=localhost,APP_DB_HOST"`
+	Port               string        `envconfig:"default=5432,APP_DB_PORT"`
+	Name               string        `envconfig:"default=compass,APP_DB_NAME"`
+	SSLMode            string        `envconfig:"default=disable,APP_DB_SSL"`
+	MaxOpenConnections int           `envconfig:"default=2,APP_DB_MAX_OPEN_CONNECTIONS"`
+	MaxIdleConnections int           `envconfig:"default=2,APP_DB_MAX_IDLE_CONNECTIONS"`
+	ConnMaxLifetime    time.Duration `envconfig:"default=30m,APP_DB_CONNECTION_MAX_LIFETIME"`
 }
 
 func (cfg DatabaseConfig) GetConnString() string {
