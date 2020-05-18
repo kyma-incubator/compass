d29031e886284e5e7af7850654a85f0c20ceda1d
Karol Jaksik <jaksikkarol@gmail.com>
2020-05-18 16:04:28 +0200

    Add integration tests

---
+++ tests/director/gateway-integration/tenants_test.go
@@ -0,0 +1,51 @@
+package gateway_integration
+
+import (
+	"context"
+	"testing"
+
+	"github.com/kyma-incubator/compass/components/director/pkg/graphql"
+	"github.com/kyma-incubator/compass/tests/director/pkg/gql"
+	"github.com/kyma-incubator/compass/tests/director/pkg/idtokenprovider"
+	"github.com/kyma-incubator/compass/tests/director/pkg/ptr"
+	"github.com/stretchr/testify/require"
+)
+
+const (
+	wrongTenant = "b1e46bd5-18ba-4a02-b96d-631a9e803504"
+	emptyTenant = ""
+)
+
+func TestTenantErrors(t *testing.T) {
+
+	ctx := context.Background()
+
+	t.Log("Get Dex id_token")
+	dexToken, err := idtokenprovider.GetDexToken()
+	require.NoError(t, err)
+
+	dexGraphQLClient := gql.NewAuthorizedGraphQLClient(dexToken)
+
+	t.Log("Register application as Static User")
+	appInput := graphql.ApplicationRegisterInput{
+		Name:         "app-static-user",
+		ProviderName: ptr.String("compass"),
+	}
+	_, err = registerApplicationWithinTenant(t, ctx, dexGraphQLClient, wrongTenant, appInput)
+	require.Error(t, err)
+
+	_, err = registerApplicationWithinTenant(t, ctx, dexGraphQLClient, emptyTenant, appInput)
+	require.Error(t, err)
+
+	is := registerIntegrationSystem(t, ctx, dexGraphQLClient, testConfig.DefaultTenant, "test")
+
+	req := fixGenerateClientCredentialsForIntegrationSystem(is.ID)
+
+	var credentials graphql.SystemAuth
+	err = tc.RunOperationWithCustomTenant(ctx, dexGraphQLClient, wrongTenant, req, &credentials)
+	require.Error(t, err)
+
+	err = tc.RunOperationWithCustomTenant(ctx, dexGraphQLClient, emptyTenant, req, &credentials)
+	require.NoError(t, err)
+
+}
