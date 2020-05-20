18671f83c2b43ea51c032a76907e5096e757ca76
marcobebway <marco.bebway@sap.com>
2020-05-20 11:03:51 +0200

    Make Azure resource names in lower case

--- components/kyma-environment-broker/internal/process/provisioning/event_hub_step_test.go
+++ components/kyma-environment-broker/internal/process/provisioning/event_hub_step_test.go
@@ -187,6 +187,32 @@ func Test_StepsUnhappyPath(t *testing.T) {
 	}
 }
 
+func Test_getAzureResourceName(t *testing.T) {
+	tests := []struct {
+		name             string
+		givenName        string
+		wantResourceName string
+	}{
+		{
+			name:             "all lowercase and starts with digit",
+			givenName:        "1a23238d-1b04-3a9c-c139-405b75796ceb",
+			wantResourceName: "k1a23238d-1b04-3a9c-c139-405b75796ceb",
+		},
+		{
+			name:             "all uppercase and starts with digit",
+			givenName:        "1A23238D-1B04-3A9C-C139-405B75796CEB",
+			wantResourceName: "k1a23238d-1b04-3a9c-c139-405b75796ceb",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			got := getAzureResourceName(test.givenName)
+			assert.Equal(t, test.wantResourceName, got)
+		})
+	}
+}
+
 // operationManager.OperationFailed(...)
 // manager.go: if processedOperation.State != domain.InProgress { return 0, nil } => repeat
 // queue.go: if err == nil && when != 0 => repeat
