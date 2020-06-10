6086a0421bda38461eba710a5854ffd632488420
Karol Jaksik <jaksikkarol@gmail.com>
2020-06-10 13:27:37 +0200

    Install Compass as a separate component (#1405)
    * WIP
    
    * Use existing operator image
    
    * Move charts from kyma to compass and create run.sh
    
    * Update overrides
    
    * Update run.sh
    
    * Update overrides
    
    * Update codeowners
    
    * Update KYMA_VERSION
    
    * Remove minikube values
    
    * Fix run.sh
    
    * Fix after cr
    
    * Update owners aliases
    
    * Apply suggestions from code review
    
    Co-authored-by: Tomasz Gorgol <45565988+tgorgol@users.noreply.github.com>
    
    * Update installation/cmd/run.sh
    
    Co-authored-by: Tomasz Gorgol <45565988+tgorgol@users.noreply.github.com>
    
    * Fix after cr
    
    * Add comment to dockerfile
    
    * Move comment
    
    * Update testing.sh
    
    * Update testing.sh
    
    * Update tools/compass-installer/compass.Dockerfile
    
    Co-authored-by: Tomasz Gorgol <45565988+tgorgol@users.noreply.github.com>
    
    * Try reverting the application-connector secret
    
    * Try reverting the overrides back
    
    * Revert keb tests changes
    
    Co-authored-by: Tomasz Gorgol <t.gorgol@sap.com>
    Co-authored-by: Tomasz Gorgol <45565988+tgorgol@users.noreply.github.com>

--- installation/cmd/run.sh
+++ installation/cmd/run.sh
@@ -3,19 +3,15 @@
 set -o errexit
 
 ROOT_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../..
-
-defaultRelease="latest-published"
+defaultRelease=$(<"${ROOT_PATH}"/installation/resources/KYMA_VERSION)
 KYMA_RELEASE=${1:-$defaultRelease}
-COMPASS_HELM_RELEASE_NAME="compass"
-COMPASS_HELM_RELEASE_NAMESPACE="compass-system"
-
 INSTALLER_CR_PATH="${ROOT_PATH}"/installation/resources/installer-cr-kyma-diet.yaml
 OVERRIDES_COMPASS_GATEWAY="${ROOT_PATH}"/installation/resources/installer-overrides-compass-gateway.yaml
 ISTIO_OVERRIDES="${ROOT_PATH}"/installation/resources/installer-overrides-istio.yaml
-MINIKUBE_HELM_VALUES="${ROOT_PATH}"/installation/resources/minikube-values.yaml
+API_GATEWAY_OVERRIDES="${ROOT_PATH}"/installation/resources/installer-overrides-api-gateway.yaml
 
 kyma provision minikube
-kyma install -o $INSTALLER_CR_PATH  -o $OVERRIDES_COMPASS_GATEWAY -o $ISTIO_OVERRIDES --source "${KYMA_RELEASE}"
+kyma install -o $INSTALLER_CR_PATH  -o $OVERRIDES_COMPASS_GATEWAY -o $ISTIO_OVERRIDES -o $API_GATEWAY_OVERRIDES "${KYMA_RELEASE}"
 
 #Get Tiller tls client certificates
 kubectl get -n kyma-installer secret helm-secret -o jsonpath="{.data['global\.helm\.ca\.crt']}" | base64 --decode > "$(helm home)/ca.pem"
@@ -23,15 +19,8 @@ kubectl get -n kyma-installer secret helm-secret -o jsonpath="{.data['global\.he
 kubectl get -n kyma-installer secret helm-secret -o jsonpath="{.data['global\.helm\.tls\.key']}" | base64 --decode > "$(helm home)/key.pem"
 echo -e "Secrets with Tiller tls client certificates have been created \n"
 
-MINIKUBE_IP=$(eval minikube ip)
-helm install \
--f=${MINIKUBE_HELM_VALUES} \
---set=global.minikubeIP=${MINIKUBE_IP} \
---name "${COMPASS_HELM_RELEASE_NAME}" \
---namespace "${COMPASS_HELM_RELEASE_NAMESPACE}" \
-"${ROOT_PATH}"/chart/compass \
---tls --wait
+bash "${ROOT_PATH}"/installation/scripts/run-compass-installer.sh
+bash "${ROOT_PATH}"/installation/scripts/is-installed.sh
 
-# TODO: Remove it after next CLI release
 echo "Adding Compass entries to /etc/hosts..."
 sudo sh -c 'echo "\n$(minikube ip) adapter-gateway.kyma.local adapter-gateway-mtls.kyma.local compass-gateway-mtls.kyma.local compass-gateway-auth-oauth.kyma.local compass-gateway.kyma.local compass.kyma.local compass-mf.kyma.local kyma-env-broker.kyma.local" >> /etc/hosts'
