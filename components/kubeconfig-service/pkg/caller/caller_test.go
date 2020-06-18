4472df8b497497b3cb5f7c80c80a024d53e4b948
Tomasz Smelcerz <tomasz.smelcerz@sap.com>
2020-06-18 08:30:22 +0200

    Add tests for caller package

--- components/kubeconfig-service/pkg/caller/caller_test.go
+++ components/kubeconfig-service/pkg/caller/caller_test.go
@@ -13,8 +13,10 @@ import (
 )
 
 const (
-	testTenant      = "testTenant1"
-	testRuntimeID   = "testRuntime1"
+	testTenant     = "testTenant1"
+	testRuntimeID  = "testRuntime1"
+	testKubeconfig = "some-test-kubeconfig"
+
 	mockGQLResponse = `{
 		"data": {
 			"result": {
@@ -34,9 +36,9 @@ func TestSpec(t *testing.T) {
 			Convey("Should return a RuntimeStatus in a happy path scenario", func(c C) {
 
 				//given
-				testKubeconfig := "some-test-kubeconfig"
-
 				srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+					c.So(r.Method, ShouldEqual, http.MethodPost)
 
 					//Assertion on tenant parameter passed in a HTTP header
 					c.So(r.Header.Get(caller.TenantHeader), ShouldEqual, testTenant)
@@ -61,6 +63,50 @@ func TestSpec(t *testing.T) {
 				//then
 				So(err, ShouldBeNil)
 				So(*res.RuntimeConfiguration.Kubeconfig, ShouldEqual, testKubeconfig)
+			})
+
+			Convey("Should return a RuntimeStatus in a single-error scenario", func(c C) {
+
+				//given
+
+				var calls = 0
+
+				srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+					c.So(r.Method, ShouldEqual, http.MethodPost)
+
+					//Assertion on tenant parameter passed in a HTTP header
+					c.So(r.Header.Get(caller.TenantHeader), ShouldEqual, testTenant)
+
+					b, err := ioutil.ReadAll(r.Body)
+					c.So(err, ShouldBeNil)
+
+					//Assertion on runtimeStatus parameter (embedded in the query)
+					c.So(string(b), ShouldContainSubstring, fmt.Sprintf(`result: runtimeStatus(id: \"%s\")`, testRuntimeID))
+
+					if calls == 0 {
+						w.WriteHeader(http.StatusInternalServerError)
+						io.WriteString(w, `Internal Server Error`)
+					} else {
+						//Mock response, our contract on what the endpoint shall return
+						_, err = io.WriteString(w, fmt.Sprintf(mockGQLResponse, testKubeconfig))
+						c.So(err, ShouldBeNil)
+					}
+
+					calls++
+				}))
+				defer srv.Close()
+
+				cllr := caller.NewCaller(srv.URL, testTenant)
+
+				//when
+				res, err := cllr.RuntimeStatus(testRuntimeID)
+
+				//then
+				So(calls, ShouldEqual, 2)
+				So(err, ShouldBeNil)
+				So(*res.RuntimeConfiguration.Kubeconfig, ShouldEqual, testKubeconfig)
+
 			})
 		})
 	})
