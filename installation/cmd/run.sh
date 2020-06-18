182a34b06312d87e4bfdd02c47f4508082167dc9
Karol Jaksik <jaksikkarol@gmail.com>
2020-06-18 11:08:35 +0200

    Fix after cr

--- installation/cmd/run.sh
+++ installation/cmd/run.sh
@@ -4,14 +4,16 @@ set -o errexit
 
 ROOT_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../..
 defaultRelease=$(<"${ROOT_PATH}"/installation/resources/KYMA_VERSION)
-KYMA_TAG=${1:-$defaultRelease}
+KYMA_RELEASE=${1:-$defaultRelease}
 INSTALLER_CR_PATH="${ROOT_PATH}"/installation/resources/installer-cr-kyma-dependencies.yaml
 OVERRIDES_COMPASS_GATEWAY="${ROOT_PATH}"/installation/resources/installer-overrides-compass-gateway.yaml
 ISTIO_OVERRIDES="${ROOT_PATH}"/installation/resources/installer-overrides-istio.yaml
 API_GATEWAY_OVERRIDES="${ROOT_PATH}"/installation/resources/installer-overrides-api-gateway.yaml
 
-if [[ $KYMA_TAG == *PR-* ]] || [ $KYMA_TAG == master ]; then
+if [[ KYMA_RELEASE == *PR-* ]] || [ KYMA_RELEASE == master ]; then
   KYMA_TAG=$(curl -L https://storage.googleapis.com/kyma-development-artifacts/${KYMA_TAG}/kyma-installer-cluster.yaml | grep 'image: eu.gcr.io/kyma-project/kyma-installer:'| sed 's+image: eu.gcr.io/kyma-project/kyma-installer:++g' | tr -d '[:space:]')
+else
+  KYMA_TAG=${KYMA_RELEASE}
 fi
 kyma provision minikube
 kyma install -o $INSTALLER_CR_PATH  -o $OVERRIDES_COMPASS_GATEWAY -o $ISTIO_OVERRIDES -o $API_GATEWAY_OVERRIDES --source "eu.gcr.io/kyma-project/kyma-installer:${KYMA_TAG}"
