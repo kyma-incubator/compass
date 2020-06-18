c54bbf9e3d8f50a8903a90fbb1beb8fa2616f6ad
Jakub Błaszczyk <jakub.blaszczyk@sap.com>
2020-06-18 10:49:03 +0200

    Revert authn change

--- components/kubeconfig-service/pkg/authn/middleware.go
+++ components/kubeconfig-service/pkg/authn/middleware.go
@@ -11,7 +11,9 @@ func AuthMiddleware(a authenticator.Request) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-			_, ok, err := a.AuthenticateRequest(r)
+			restoreAuthHeaderValue := r.Header.Get("Authorization") //Preserve original value
+
+			_, ok, err := a.AuthenticateRequest(r) //Strips "Authorization" Header value on auth success!
 			if err != nil {
 				log.Errorf("Unable to authenticate the request due to an error: %v", err)
 			}
@@ -19,6 +21,8 @@ func AuthMiddleware(a authenticator.Request) func(http.Handler) http.Handler {
 				http.Error(w, "Unauthorized", http.StatusUnauthorized)
 				return
 			}
+
+			r.Header.Set("Authorization", restoreAuthHeaderValue)
 
 			next.ServeHTTP(w, r)
 		})
