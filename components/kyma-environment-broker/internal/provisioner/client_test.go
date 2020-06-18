8825a65bf4fecb7e86694279c75f5666bcd58a9c
Jasiak <piotr.jasiak@sap.com>
2020-06-18 08:50:38 +0200

    handle errors in tests

--- components/kyma-environment-broker/internal/provisioner/client_test.go
+++ components/kyma-environment-broker/internal/provisioner/client_test.go
@@ -2,6 +2,7 @@ package provisioner
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -24,82 +25,170 @@ const (
 )
 
 func TestClient_ProvisionRuntime(t *testing.T) {
-	// Given
-	tr := &testResolver{t: t, runtime: &testRuntime{}}
-	testServer := fixHTTPServer(tr)
-	defer testServer.Close()
+	t.Run("should trigger provisioning", func(t *testing.T) {
+		// Given
+		tr := &testResolver{t: t, runtime: &testRuntime{}}
+		testServer := fixHTTPServer(tr)
+		defer testServer.Close()
 
-	client := NewProvisionerClient(testServer.URL, false)
+		client := NewProvisionerClient(testServer.URL, false)
 
-	// When
-	status, err := client.ProvisionRuntime(testAccountID, testSubAccountID, fixProvisionRuntimeInput())
+		// When
+		status, err := client.ProvisionRuntime(testAccountID, testSubAccountID, fixProvisionRuntimeInput())
 
-	// Then
-	assert.NoError(t, err)
-	assert.Equal(t, ptr.String(provisionRuntimeOperationID), status.ID)
-	assert.Equal(t, schema.OperationStateInProgress, status.State)
-	assert.Equal(t, ptr.String(provisionRuntimeID), status.RuntimeID)
+		// Then
+		assert.NoError(t, err)
+		assert.Equal(t, ptr.String(provisionRuntimeOperationID), status.ID)
+		assert.Equal(t, schema.OperationStateInProgress, status.State)
+		assert.Equal(t, ptr.String(provisionRuntimeID), status.RuntimeID)
 
-	assert.Equal(t, "test", tr.getRuntime().name)
+		assert.Equal(t, "test", tr.getRuntime().name)
+	})
+
+	t.Run("provisioner should return error", func(t *testing.T) {
+		// Given
+		tr := &testResolver{t: t, runtime: &testRuntime{}, failed: true}
+		testServer := fixHTTPServer(tr)
+		defer testServer.Close()
+
+		client := NewProvisionerClient(testServer.URL, false)
+
+		// When
+		status, err := client.ProvisionRuntime(testAccountID, testSubAccountID, fixProvisionRuntimeInput())
+
+		// Then
+		assert.Error(t, err)
+		assert.Empty(t, status)
+
+		assert.Equal(t, "", tr.getRuntime().name)
+	})
 }
 
 func TestClient_DeprovisionRuntime(t *testing.T) {
-	// Given
-	tr := &testResolver{t: t, runtime: &testRuntime{}}
-	testServer := fixHTTPServer(tr)
-	defer testServer.Close()
+	t.Run("should trigger deprovisioning", func(t *testing.T) {
+		// Given
+		tr := &testResolver{t: t, runtime: &testRuntime{}}
+		testServer := fixHTTPServer(tr)
+		defer testServer.Close()
 
-	client := NewProvisionerClient(testServer.URL, false)
-	operation, err := client.ProvisionRuntime(testAccountID, testSubAccountID, fixProvisionRuntimeInput())
-	assert.NoError(t, err)
+		client := NewProvisionerClient(testServer.URL, false)
+		operation, err := client.ProvisionRuntime(testAccountID, testSubAccountID, fixProvisionRuntimeInput())
+		assert.NoError(t, err)
 
-	// When
-	operationId, err := client.DeprovisionRuntime(testAccountID, *operation.RuntimeID)
+		// When
+		operationId, err := client.DeprovisionRuntime(testAccountID, *operation.RuntimeID)
 
-	// Then
-	assert.NoError(t, err)
-	assert.Equal(t, deprovisionRuntimeOperationID, operationId)
+		// Then
+		assert.NoError(t, err)
+		assert.Equal(t, deprovisionRuntimeOperationID, operationId)
 
-	assert.Empty(t, tr.getRuntime().runtimeID)
+		assert.Empty(t, tr.getRuntime().runtimeID)
+	})
+
+	t.Run("provisioner should return error", func(t *testing.T) {
+		// Given
+		tr := &testResolver{t: t, runtime: &testRuntime{}}
+		testServer := fixHTTPServer(tr)
+		defer testServer.Close()
+
+		client := NewProvisionerClient(testServer.URL, false)
+		operation, err := client.ProvisionRuntime(testAccountID, testSubAccountID, fixProvisionRuntimeInput())
+		assert.NoError(t, err)
+
+		tr.failed = true
+
+		// When
+		operationId, err := client.DeprovisionRuntime(testAccountID, *operation.RuntimeID)
+
+		// Then
+		assert.Error(t, err)
+		assert.Equal(t, "", operationId)
+
+		assert.Equal(t, provisionRuntimeID, tr.getRuntime().runtimeID)
+	})
 }
 
 func TestClient_ReconnectRuntimeAgent(t *testing.T) {
-	// Given
-	tr := &testResolver{t: t, runtime: &testRuntime{}}
-	testServer := fixHTTPServer(tr)
-	defer testServer.Close()
+	t.Run("should reconnect runtime agent", func(t *testing.T) {
+		// Given
+		tr := &testResolver{t: t, runtime: &testRuntime{}}
+		testServer := fixHTTPServer(tr)
+		defer testServer.Close()
 
-	client := NewProvisionerClient(testServer.URL, false)
-	operation, err := client.ProvisionRuntime(testAccountID, testSubAccountID, fixProvisionRuntimeInput())
-	assert.NoError(t, err)
+		client := NewProvisionerClient(testServer.URL, false)
+		operation, err := client.ProvisionRuntime(testAccountID, testSubAccountID, fixProvisionRuntimeInput())
+		assert.NoError(t, err)
 
-	// When
-	operationId, err := client.ReconnectRuntimeAgent(testAccountID, *operation.RuntimeID)
+		// When
+		operationId, err := client.ReconnectRuntimeAgent(testAccountID, *operation.RuntimeID)
 
-	// Then
-	assert.NoError(t, err)
-	assert.Equal(t, provisionRuntimeOperationID, operationId)
+		// Then
+		assert.NoError(t, err)
+		assert.Equal(t, provisionRuntimeOperationID, operationId)
+	})
+
+	t.Run("provisioner should return error", func(t *testing.T) {
+		// Given
+		tr := &testResolver{t: t, runtime: &testRuntime{}}
+		testServer := fixHTTPServer(tr)
+		defer testServer.Close()
+
+		client := NewProvisionerClient(testServer.URL, false)
+		operation, err := client.ProvisionRuntime(testAccountID, testSubAccountID, fixProvisionRuntimeInput())
+		assert.NoError(t, err)
+
+		tr.failed = true
+
+		// When
+		operationId, err := client.ReconnectRuntimeAgent(testAccountID, *operation.RuntimeID)
+
+		// Then
+		assert.Error(t, err)
+		assert.Equal(t, "", operationId)
+	})
 }
 
 func TestClient_RuntimeOperationStatus(t *testing.T) {
-	// Given
-	tr := &testResolver{t: t, runtime: &testRuntime{}}
-	testServer := fixHTTPServer(tr)
-	defer testServer.Close()
+	t.Run("should return runtime operation status", func(t *testing.T) {
+		// Given
+		tr := &testResolver{t: t, runtime: &testRuntime{}}
+		testServer := fixHTTPServer(tr)
+		defer testServer.Close()
 
-	client := NewProvisionerClient(testServer.URL, false)
-	_, err := client.ProvisionRuntime(testAccountID, testSubAccountID, fixProvisionRuntimeInput())
-	assert.NoError(t, err)
+		client := NewProvisionerClient(testServer.URL, false)
+		_, err := client.ProvisionRuntime(testAccountID, testSubAccountID, fixProvisionRuntimeInput())
+		assert.NoError(t, err)
 
-	// When
-	status, err := client.RuntimeOperationStatus(testAccountID, provisionRuntimeID)
+		// When
+		status, err := client.RuntimeOperationStatus(testAccountID, provisionRuntimeID)
 
-	// Then
-	assert.NoError(t, err)
-	assert.Equal(t, ptr.String(provisionRuntimeID), status.RuntimeID)
-	assert.Equal(t, ptr.String(provisionRuntimeOperationID), status.ID)
-	assert.Equal(t, schema.OperationStateInProgress, status.State)
-	assert.Equal(t, schema.OperationTypeProvision, status.Operation)
+		// Then
+		assert.NoError(t, err)
+		assert.Equal(t, ptr.String(provisionRuntimeID), status.RuntimeID)
+		assert.Equal(t, ptr.String(provisionRuntimeOperationID), status.ID)
+		assert.Equal(t, schema.OperationStateInProgress, status.State)
+		assert.Equal(t, schema.OperationTypeProvision, status.Operation)
+	})
+
+	t.Run("provisioner should return error", func(t *testing.T) {
+		// Given
+		tr := &testResolver{t: t, runtime: &testRuntime{}}
+		testServer := fixHTTPServer(tr)
+		defer testServer.Close()
+
+		client := NewProvisionerClient(testServer.URL, false)
+		_, err := client.ProvisionRuntime(testAccountID, testSubAccountID, fixProvisionRuntimeInput())
+		assert.NoError(t, err)
+
+		tr.failed = true
+
+		// When
+		status, err := client.RuntimeOperationStatus(testAccountID, provisionRuntimeID)
+
+		// Then
+		assert.Error(t, err)
+		assert.Empty(t, status)
+	})
 }
 
 type testRuntime struct {
@@ -114,6 +203,7 @@ type testRuntime struct {
 type testResolver struct {
 	t       *testing.T
 	runtime *testRuntime
+	failed  bool
 }
 
 func fixHTTPServer(tr *testResolver) *httptest.Server {
@@ -140,12 +230,12 @@ func fixHTTPServer(tr *testResolver) *httptest.Server {
 
 func (tr testResolver) Mutation() schema.MutationResolver {
 	tr.t.Log("Mutation TestResolver")
-	return &testMutationResolver{t: tr.t, runtime: tr.runtime}
+	return &testMutationResolver{t: tr.t, runtime: tr.runtime, failed: tr.failed}
 }
 
 func (tr testResolver) Query() schema.QueryResolver {
 	tr.t.Log("Query TestResolver")
-	return &testQueryResolver{t: tr.t, runtime: tr.runtime}
+	return &testQueryResolver{t: tr.t, runtime: tr.runtime, failed: tr.failed}
 }
 
 func (tr testResolver) getRuntime() *testRuntime {
@@ -155,10 +245,15 @@ func (tr testResolver) getRuntime() *testRuntime {
 type testMutationResolver struct {
 	t       *testing.T
 	runtime *testRuntime
+	failed  bool
 }
 
 func (tmr *testMutationResolver) ProvisionRuntime(_ context.Context, config schema.ProvisionRuntimeInput) (*schema.OperationStatus, error) {
 	tmr.t.Log("ProvisionRuntime testMutationResolver")
+
+	if tmr.failed {
+		return nil, fmt.Errorf("provision runtime failed for %s", config.RuntimeInput.Name)
+	}
 
 	tmr.runtime.name = config.RuntimeInput.Name
 	tmr.runtime.runtimeID = provisionRuntimeID
@@ -178,6 +273,10 @@ func (tmr testMutationResolver) UpgradeRuntime(_ context.Context, id string, con
 func (tmr testMutationResolver) DeprovisionRuntime(_ context.Context, id string) (string, error) {
 	tmr.t.Log("DeprovisionRuntime testMutationResolver")
 
+	if tmr.failed {
+		return "", fmt.Errorf("deprovision failed for %s", id)
+	}
+
 	if tmr.runtime.runtimeID == id {
 		tmr.runtime.runtimeID = ""
 		tmr.runtime.name = ""
@@ -194,6 +293,10 @@ func (tmr testMutationResolver) RollBackUpgradeOperation(_ context.Context, id s
 func (tmr testMutationResolver) ReconnectRuntimeAgent(_ context.Context, id string) (string, error) {
 	tmr.t.Log("ReconnectRuntimeAgent testMutationResolver")
 
+	if tmr.failed {
+		return "", fmt.Errorf("reconnect runtime agent failed for %s", id)
+	}
+
 	if tmr.runtime.runtimeID == id {
 		return tmr.runtime.provisionOperationID, nil
 	}
@@ -204,6 +307,7 @@ func (tmr testMutationResolver) ReconnectRuntimeAgent(_ context.Context, id stri
 type testQueryResolver struct {
 	t       *testing.T
 	runtime *testRuntime
+	failed  bool
 }
 
 func (tqr testQueryResolver) RuntimeStatus(_ context.Context, id string) (*schema.RuntimeStatus, error) {
@@ -212,6 +316,10 @@ func (tqr testQueryResolver) RuntimeStatus(_ context.Context, id string) (*schem
 
 func (tqr testQueryResolver) RuntimeOperationStatus(_ context.Context, id string) (*schema.OperationStatus, error) {
 	tqr.t.Log("RuntimeOperationStatus - testQueryResolver")
+
+	if tqr.failed {
+		return nil, fmt.Errorf("query about runtime operation status failed for %s", id)
+	}
 
 	if tqr.runtime.runtimeID == id {
 		return &schema.OperationStatus{
