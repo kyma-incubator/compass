a80f43733130b9986049101c92ac4ab8cfd258a6
Julia Iskierka <48293543+colunira@users.noreply.github.com>
2020-06-22 14:43:04 +0200

    Kubeconfig service (#1420)
    * Create basic structure
    
    * Basic api
    
    * Add caller package
    
    * Add graphql flag
    
    * Small change
    
    * Add more flags
    
    * Add path variables
    
    * Enable caller logic in endpoints
    
    * Transform the raw kubconfig
    
    * Make sublime happy
    
    * Pass oidc configuration
    
    * Small changes
    
    * Switch to go templates
    
    * Add Makefile
    
    * Switch flags to envconfig
    
    * Use env package
    
    * Fix imports
    
    * Simple quality changes
    
    * Fix dockerfile
    
    * Fix dockerfile v2, add helm skeleton, renaming
    
    * Add chart to compass setup
    
    * Revert component name change
    
    * Add tests for transformer package
    
    * Add Readme, change default values
    
    * Add tests for transformer package
    
    * Update alpine version
    
    * Add tests for caller package
    
    * Add tests for caller package
    
    * Address review sugiestions
    
    * Add authentication
    
    * Fix imports
    
    * Make linter happy
    
    * Unify params with envconfig
    
    * Expand ports
    
    * Add tests for caller package
    
    * Add tests for caller package
    
    * Update charts to use env
    
    * Fix values; Remove header preserving
    
    * Revert authn change
    
    * Unify values
    
    * Experiment - move kubeconfig service from subchart to separate chart
    
    * Experiment - cleanup
    
    * Cleanup values
    
    * Fix typo
    
    * Review fix
    
    * Update codeowners, make values more transparent
    
    * Simplify queryProvider
    
    * Apply suggestions from code review
    
    Co-authored-by: Maja Kurcius <maja.kurcius@sap.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Maja Kurcius <maja.kurcius@sap.com>
    
    Co-authored-by: Jakub Błaszczyk <jakub.blaszczyk@sap.com>
    Co-authored-by: Tomasz Smelcerz <tomasz.smelcerz@sap.com>
    Co-authored-by: Maja Kurcius <maja.kurcius@sap.com>

---
+++ components/kubeconfig-service/go.mod
@@ -0,0 +1,20 @@
+module github.com/kyma-incubator/compass/components/kubeconfig-service
+
+go 1.13
+
+require (
+	github.com/avast/retry-go v2.6.0+incompatible
+	github.com/gorilla/mux v1.7.4
+	github.com/howeyc/fsnotify v0.9.0
+	github.com/kyma-incubator/compass/components/provisioner v0.0.0-20200610112737-6086a0421bda
+	github.com/machinebox/graphql v0.2.3-0.20181106130121-3a9253180225
+	github.com/pkg/errors v0.9.1
+	github.com/sirupsen/logrus v1.6.0
+	github.com/smartystreets/goconvey v1.6.4
+	github.com/stretchr/testify v1.5.1
+	github.com/vrischmann/envconfig v1.2.0
+	gopkg.in/yaml.v2 v2.3.0
+	k8s.io/apiserver v0.17.2
+)
+
+replace github.com/census-instrumentation/opencensus-proto v0.1.0-0.20181214143942-ba49f56771b8 => github.com/census-instrumentation/opencensus-proto v0.0.3-0.20181214143942-ba49f56771b8
---
+++ components/kubeconfig-service/pkg/caller/caller.go
@@ -0,0 +1,77 @@
+package caller
+
+import (
+	"context"
+	"reflect"
+	"time"
+
+	"github.com/avast/retry-go"
+	schema "github.com/kyma-incubator/compass/components/provisioner/pkg/gqlschema"
+	"github.com/machinebox/graphql"
+	"github.com/pkg/errors"
+)
+
+const (
+	TenantHeader = "Tenant"
+)
+
+//Caller wrapper structure for the graphQL Client
+type Caller struct {
+	tenant        string
+	client        *graphql.Client
+	queryProvider queryProvider
+}
+
+//NewCaller return a new Caller instance
+func NewCaller(endpoint, tenant string) *Caller {
+	gqlClient := graphql.NewClient(endpoint)
+
+	return &Caller{
+		tenant:        tenant,
+		client:        gqlClient,
+		queryProvider: queryProvider{},
+	}
+}
+
+func (c Caller) newRequest(query string) *graphql.Request {
+	req := graphql.NewRequest(query)
+	req.Header.Add(TenantHeader, c.tenant)
+
+	return req
+}
+
+//RuntimeStatus return schema.RuntimeStatus
+func (c Caller) RuntimeStatus(runtimeID string) (schema.RuntimeStatus, error) {
+	query := c.queryProvider.runtimeStatus(runtimeID)
+	req := c.newRequest(query)
+
+	var response schema.RuntimeStatus
+	err := c.executeRequest(req, &response)
+	if err != nil {
+		return schema.RuntimeStatus{}, errors.Wrap(err, "Failed to get Runtime status")
+	}
+	return response, nil
+}
+
+type graphQLResponseWrapper struct {
+	Result interface{} `json:"result"`
+}
+
+// executeRequest executes GraphQL request and unmarshal response to respDestination.
+func (c Caller) executeRequest(req *graphql.Request, respDestination interface{}) error {
+	if reflect.ValueOf(respDestination).Kind() != reflect.Ptr {
+		return errors.New("destination is not of pointer type")
+	}
+
+	wrapper := &graphQLResponseWrapper{Result: respDestination}
+
+	err := retry.Do(func() error {
+		return c.client.Run(context.Background(), req, wrapper)
+	}, retry.Delay(1*time.Second), retry.Attempts(5))
+
+	if err != nil {
+		return errors.Wrap(err, "Failed to execute request")
+	}
+
+	return nil
+}
