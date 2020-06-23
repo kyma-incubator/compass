b453587136fe2d7f9f5ef410180a6a24657e5db7
Tomasz Gorgol <t.gorgol@sap.com>
2020-06-23 13:25:05 +0200

    Adjust run.sh to changes in CLI

--- installation/cmd/run.sh
+++ installation/cmd/run.sh
@@ -10,13 +10,20 @@ OVERRIDES_COMPASS_GATEWAY="${ROOT_PATH}"/installation/resources/installer-overri
 ISTIO_OVERRIDES="${ROOT_PATH}"/installation/resources/installer-overrides-istio.yaml
 API_GATEWAY_OVERRIDES="${ROOT_PATH}"/installation/resources/installer-overrides-api-gateway.yaml
 
-if [[ $KYMA_RELEASE == *PR-* ]] || [ $KYMA_RELEASE == master ]; then
+if [[ $KYMA_RELEASE == *PR-* ]]; then
   KYMA_TAG=$(curl -L https://storage.googleapis.com/kyma-development-artifacts/${KYMA_RELEASE}/kyma-installer-cluster.yaml | grep 'image: eu.gcr.io/kyma-project/kyma-installer:'| sed 's+image: eu.gcr.io/kyma-project/kyma-installer:++g' | tr -d '[:space:]')
+  if [ -z "$KYMA_TAG" ]; then echo "ERROR: Kyma artifacts for ${KYMA_RELEASE} not found."; exit 1; fi
+  KYMA_SOURCE="eu.gcr.io/kyma-project/kyma-installer:${KYMA_TAG}"
+elif [[ $KYMA_RELEASE == master ]]; then
+  KYMA_SOURCE="latest-published"
+elif [[ $KYMA_RELEASE == *master-* ]]; then
+  KYMA_SOURCE=$(echo $KYMA_RELEASE | sed 's+master-++g' | tr -d '[:space:]')
 else
-  KYMA_TAG=${KYMA_RELEASE}
+  KYMA_SOURCE="${KYMA_RELEASE}"
 fi
+
 kyma provision minikube
-kyma install -c $INSTALLER_CR_PATH  -o $OVERRIDES_COMPASS_GATEWAY -o $ISTIO_OVERRIDES -o $API_GATEWAY_OVERRIDES --source "eu.gcr.io/kyma-project/kyma-installer:${KYMA_TAG}"
+kyma install -c $INSTALLER_CR_PATH  -o $OVERRIDES_COMPASS_GATEWAY -o $ISTIO_OVERRIDES -o $API_GATEWAY_OVERRIDES --source $KYMA_SOURCE
 
 bash "${ROOT_PATH}"/installation/scripts/run-compass-installer.sh
 bash "${ROOT_PATH}"/installation/scripts/is-installed.sh
