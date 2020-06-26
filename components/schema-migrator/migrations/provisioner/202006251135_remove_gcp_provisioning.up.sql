edce4fcf37ad63e93ab8dddce2f8ea323f822779
Szymon Gibala <szymon.gibala@sap.com>
2020-06-26 09:10:31 +0200

    Remove GCP - migrations

--- components/schema-migrator/migrations/provisioner/202006251135_remove_gcp_provisioning.up.sql
+++ components/schema-migrator/migrations/provisioner/202006251135_remove_gcp_provisioning.up.sql
@@ -3,4 +3,6 @@ BEGIN;
 ALTER TABLE cluster DROP COLUMN terraform_state;
 ALTER TABLE cluster DROP COLUMN credentials_secret_name;
 
+DROP TABLE gcp_config;
+
 COMMIT;
--- components/schema-migrator/migrations/provisioner/202006251135_remove_terraform_state_and_credentials_secret.down.sql
+++
@@ -1,6 +0,0 @@
-BEGIN;
-
-ALTER TABLE cluster ADD COLUMN terraform_state bytea;
-ALTER TABLE cluster ADD COLUMN credentials_secret_name varchar(256) NOT NULL default '';
-
-COMMIT;
