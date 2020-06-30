c98e2234b501babcb30953c2cb49ba5ae8e96fa3
Kamil Sputo <sputo.kamil@gmail.com>
2020-06-30 13:13:20 +0200

    Add environments cleanup CronJob to KEB (#1428)

--- components/kyma-environment-broker/internal/storage/dbsession/factory.go
+++ components/kyma-environment-broker/internal/storage/dbsession/factory.go
@@ -18,6 +18,7 @@ type Factory interface {
 //go:generate mockery -name=ReadSession
 type ReadSession interface {
 	FindAllInstancesJoinedWithOperation(prct ...predicate.Predicate) ([]internal.InstanceWithOperation, dberr.Error)
+	FindAllInstancesForRuntimes(runtimeIdList []string) ([]internal.Instance, dberr.Error)
 	GetInstanceByID(instanceID string) (internal.Instance, dberr.Error)
 	GetOperationByID(opID string) (dbmodel.OperationDTO, dberr.Error)
 	GetOperationsInProgressByType(operationType dbmodel.OperationType) ([]dbmodel.OperationDTO, dberr.Error)
