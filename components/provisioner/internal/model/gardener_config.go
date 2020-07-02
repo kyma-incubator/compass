68a08b004e6d2173e05639a2256c93171df785a8
Przemyslaw Golicz <przemyslaw.golicz@sap.com>
2020-07-02 08:33:49 +0200

    continue working on cluster gardener shoot cluster upgrade

--- components/provisioner/internal/model/gardener_config.go
+++ components/provisioner/internal/model/gardener_config.go
@@ -4,6 +4,9 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"github.com/kyma-incubator/compass/components/provisioner/internal/model/infrastructure/aws"
+	"github.com/kyma-incubator/compass/components/provisioner/internal/model/infrastructure/azure"
+	"github.com/kyma-incubator/compass/components/provisioner/internal/model/infrastructure/gcp"
 
 	"github.com/kyma-incubator/compass/components/provisioner/internal/util"
 	"github.com/kyma-incubator/compass/components/provisioner/pkg/gqlschema"
@@ -196,6 +199,54 @@ func (c GCPGardenerConfig) AsProviderSpecificConfig() gqlschema.ProviderSpecific
 }
 
 func (c GCPGardenerConfig) ExtendShootConfig(gardenerConfig GardenerConfig, shoot *gardener_types.Shoot) error {
+
+	if len(shoot.Spec.Provider.Workers) == 0 {
+		return c.ExtendShootWithNewGCPConfig(gardenerConfig, shoot)
+	}
+
+	return c.UpdateExistingShootWithGCPConfig(gardenerConfig, shoot)
+}
+
+func (c GCPGardenerConfig) UpdateExistingShootWithGCPConfig(gardenerConfig GardenerConfig, shoot *gardener_types.Shoot) error {
+
+	updateWorkerConfig(gardenerConfig, shoot, c.input.Zones)
+
+	var gcpInfra gcp.InfrastructureConfig
+	err := json.Unmarshal(shoot.Spec.Provider.InfrastructureConfig.RawExtension.Raw, &gcpInfra)
+
+	if err != nil {
+		return fmt.Errorf("error decoding infrastructure config: %s", err.Error())
+	}
+
+	gcpInfra.Networks.Workers = util.StringPtr(gardenerConfig.WorkerCidr)
+	gcpInfra.Networks.Worker = gardenerConfig.WorkerCidr
+
+	var gcpControlPlane gcp.ControlPlaneConfig
+	err = json.Unmarshal(shoot.Spec.Provider.ControlPlaneConfig.RawExtension.Raw, &gcpControlPlane)
+
+	if err != nil {
+		return fmt.Errorf("error decoding control plane config: %s", err.Error())
+	}
+
+	gcpControlPlane.Zone = c.input.Zones[0]
+
+	jsonCPData, err := json.Marshal(gcpControlPlane)
+	if err != nil {
+		return fmt.Errorf("error encoding control plane config: %s", err.Error())
+	}
+
+	jsonData, err := json.Marshal(gcpInfra)
+	if err != nil {
+		return fmt.Errorf("error encoding infrastructure config: %s", err.Error())
+	}
+
+	shoot.Spec.Provider.ControlPlaneConfig.RawExtension = apimachineryRuntime.RawExtension{Raw: jsonCPData}
+	shoot.Spec.Provider.InfrastructureConfig.RawExtension = apimachineryRuntime.RawExtension{Raw: jsonData}
+
+	return nil
+}
+
+func (c GCPGardenerConfig) ExtendShootWithNewGCPConfig(gardenerConfig GardenerConfig, shoot *gardener_types.Shoot) error {
 	shoot.Spec.CloudProfileName = "gcp"
 
 	workers := []gardener_types.Worker{getWorkerConfig(gardenerConfig, c.input.Zones)}
@@ -267,6 +318,88 @@ type AWSGardenerConfig struct {
 }
 
 func (c AzureGardenerConfig) ExtendShootConfig(gardenerConfig GardenerConfig, shoot *gardener_types.Shoot) error {
+	if len(shoot.Spec.Provider.Workers) == 0 {
+		return c.ExtendShootWithNewAzureConfig(gardenerConfig, shoot)
+	}
+
+	return c.UpdateExistingShootWithAzureConfig(gardenerConfig, shoot)
+}
+
+func (c AzureGardenerConfig) UpdateExistingShootWithAzureConfig(gardenerConfig GardenerConfig, shoot *gardener_types.Shoot) error {
+
+	updateWorkerConfig(gardenerConfig, shoot, c.input.Zones)
+
+	var azInfra azure.InfrastructureConfig
+	err := json.Unmarshal(shoot.Spec.Provider.InfrastructureConfig.RawExtension.Raw, &azInfra)
+
+	if err != nil {
+		return fmt.Errorf("error decoding infrastructure config: %s", err.Error())
+	}
+
+	azInfra.Zoned = len(c.input.Zones) > 0
+	azInfra.Networks.Workers = gardenerConfig.WorkerCidr
+	azInfra.Networks.VNet.CIDR = &c.input.VnetCidr
+
+	/*
+	isZoned := len(azConfig.input.Zones) > 0
+		return &azure.InfrastructureConfig{
+			TypeMeta: v1.TypeMeta{
+				Kind:       infrastructureConfigKind,
+				APIVersion: azureAPIVersion,
+			},
+			Networks: azure.NetworkConfig{
+				Workers: workerCIDR,
+				VNet: azure.VNet{
+					CIDR: &azConfig.input.VnetCidr,
+				},
+			},
+			Zoned: isZoned,
+		}
+	*/
+
+	return nil
+	//GCP
+	/*
+	updateWorkerConfig(gardenerConfig, shoot, c.input.Zones)
+
+		var gcpInfra gcp.InfrastructureConfig
+		err := json.Unmarshal(shoot.Spec.Provider.InfrastructureConfig.RawExtension.Raw, &gcpInfra)
+
+		if err != nil {
+			return fmt.Errorf("error decoding infrastructure config: %s", err.Error())
+		}
+
+		gcpInfra.Networks.Workers = util.StringPtr(gardenerConfig.WorkerCidr)
+		gcpInfra.Networks.Worker = gardenerConfig.WorkerCidr
+
+		var gcpControlPlane gcp.ControlPlaneConfig
+		err = json.Unmarshal(shoot.Spec.Provider.ControlPlaneConfig.RawExtension.Raw, &gcpControlPlane)
+
+		if err != nil {
+			return fmt.Errorf("error decoding control plane config: %s", err.Error())
+		}
+
+		gcpControlPlane.Zone = c.input.Zones[0]
+
+		jsonCPData, err := json.Marshal(gcpControlPlane)
+		if err != nil {
+			return fmt.Errorf("error encoding control plane config: %s", err.Error())
+		}
+
+		jsonData, err := json.Marshal(gcpInfra)
+		if err != nil {
+			return fmt.Errorf("error encoding infrastructure config: %s", err.Error())
+		}
+
+		shoot.Spec.Provider.ControlPlaneConfig.RawExtension = apimachineryRuntime.RawExtension{Raw: jsonCPData}
+		shoot.Spec.Provider.InfrastructureConfig.RawExtension = apimachineryRuntime.RawExtension{Raw: jsonData}
+
+		return nil
+
+	*/
+}
+
+func (c AzureGardenerConfig) ExtendShootWithNewAzureConfig(gardenerConfig GardenerConfig, shoot *gardener_types.Shoot) error {
 	shoot.Spec.CloudProfileName = "az"
 
 	workers := []gardener_types.Worker{getWorkerConfig(gardenerConfig, c.input.Zones)}
@@ -373,4 +506,16 @@ func getWorkerConfig(gardenerConfig GardenerConfig, zones []string) gardener_typ
 		Minimum: int32(gardenerConfig.AutoScalerMin),
 		Zones:   zones,
 	}
+}
+
+func updateWorkerConfig(gardenerConfig GardenerConfig, shoot *gardener_types.Shoot, zones []string) {
+
+	shoot.Spec.Provider.Workers[0].MaxSurge = util.IntOrStrPtr(intstr.FromInt(gardenerConfig.MaxSurge))
+	shoot.Spec.Provider.Workers[0].MaxUnavailable = util.IntOrStrPtr(intstr.FromInt(gardenerConfig.MaxUnavailable))
+	shoot.Spec.Provider.Workers[0].Machine.Type = gardenerConfig.MachineType
+	shoot.Spec.Provider.Workers[0].Volume.Type = &gardenerConfig.DiskType
+	shoot.Spec.Provider.Workers[0].Volume.Size = fmt.Sprintf("%dGi", gardenerConfig.VolumeSizeGB)
+	shoot.Spec.Provider.Workers[0].Maximum = int32(gardenerConfig.AutoScalerMax)
+	shoot.Spec.Provider.Workers[0].Minimum = int32(gardenerConfig.AutoScalerMin)
+	shoot.Spec.Provider.Workers[0].Zones = zones
 }
