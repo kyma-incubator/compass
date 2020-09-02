4ef897475608fd89efecabd90adb4060a62ab725
NickyMateev <bgdunkers@gmail.com>
2020-09-02 09:53:06 +0300

    Add event definitions migration

--- components/schema-migrator/migrations/director/202009011737_introduce_bundles.up.sql
+++ components/schema-migrator/migrations/director/202009011737_introduce_bundles.up.sql
@@ -5,7 +5,7 @@ ALTER TABLE packages
 
 CREATE TABLE packages (
     id UUID PRIMARY KEY CHECK (id <> '00000000-0000-0000-0000-000000000000'),
-    owner_app_id uuid NOT NULL, /* if a packag is rreturned by a system, only that system is expeccted iis expected to return this package, otherwise -> validation error */
+    owner_app_id uuid NOT NULL, /* if a package is returned by a system, only that system is expected iis expected to return this package, otherwise -> validation error */
     FOREIGN KEY (owner_app_id) REFERENCES applications (id) ON DELETE CASCADE,
     title VARCHAR(256) NOT NULL,
     short_description VARCHAR(256) NOT NULL,
@@ -25,7 +25,7 @@ CREATE TABLE packages (
 );
 
 ALTER TABLE bundles
-    ADD COLUMN package_id UUID; /* NULLable because we don't have packages for existing systems */
+    ADD COLUMN package_id UUID; /* NULLable and no foreign key because we don't have packages for existing systems */
 
 ALTER TABLE api_definitions
     RENAME COLUMN name TO title,
@@ -55,5 +55,29 @@ ALTER TABLE api_definitions
     ADD COLUMN last_updated TIMESTAMP NOT NULL,
     ADD COLUMN extensions JSOB; /* The spec MAY be extended with custom properties. Their property names MUST start with "x-"  */
 
+ALTER TABLE event_api_definitions
+    RENAME COLUMN name TO title,
+    RENAME COLUMN package_id TO bundle_id,
+    RENAME COLUMN version_value TO version;
+
+ALTER TABLE event_api_definitions
+DROP CONSTRAINT event_api_definitions_package_id_fk,
+ADD CONSTRAINT event_api_definitions_package_id_fk
+    FOREIGN KEY (bundle_id)
+    REFERENCES bundles(id)
+    ON DELETE CASCADE;
+
+ALTER TABLE event_api_definitions
+    ADD COLUMN short_description VARCHAR(256) NOT NULL,
+    ADD COLUMN event_definitions JSONB NOT NULL, /* spec_data, spec_format and spec_type wiil be populated programatically baased on the first element here */
+    ADD COLUMN tags JSONB,
+    ADD COLUMN documentation VARCHAR(512),
+    ADD COLUMN changelog_entries JSONB,
+    ADD COLUMN logo VARCHAR(512),
+    ADD COLUMN image VARCHAR(512),
+    ADD COLUMN url VARCHAR(512),
+    ADD COLUMN release_status VARCHAR(64) NOT NULL, /* should be ENUM */
+    ADD COLUMN last_updated TIMESTAMP NOT NULL,
+    ADD COLUMN extensions JSOB; /* The spec MAY be extended with custom properties. Their property names MUST start with "x-"  */
 
 COMMIT;
