adf163ce7bd015ada80fad55e8405de8729f600a
NickyMateev <bgdunkers@gmail.com>
2020-09-21 18:49:00 +0300

    Add Dockerfile

--- components/od-service/commons.sh
+++ components/od-service/commons.sh
@@ -5,6 +5,18 @@ export GREEN='\033[0;32m'
 export RED='\033[0;31m'
 export NC='\033[0m'
 
+# exits with a fail message $2 and errorcode 1 if $1 is empty or 'null'
+die_on_noval() {
+  if [[ "null" == "$1" ]] || [[ -z "$1" ]]; then
+    die "$2"
+  fi
+}
+
+die() {
+    log_error "$1";
+    exit 1
+}
+
 log_section() {
   echo -e "${GREEN}------------------------------------------------------------------------------------------${NC}"
   echo -e "${GREEN}${1}${NC}"
--- components/od-service/run.sh
+++ components/od-service/run.sh
@@ -1,32 +1,6 @@
 #!/usr/bin/env bash
 
-COMPONENT_DIR="$GOPATH/src/github.com/kyma-incubator/compass/components/od-service"
-OLINGO_JPA_LIB_DIR="$COMPONENT_DIR/olingo-jpa-processor-v4"
-OLINGO_VERSION_TAG="0.3.7-a"
-
-source "$COMPONENT_DIR/formatting.sh"
-
-if [[ -d "$OLINGO_JPA_LIB_DIR" ]]
-then
-    log_section "Olingo JPA library already exists locally. Will attempt to sync it with remote..."
-    cd "$OLINGO_JPA_LIB_DIR"
-    git checkout "$OLINGO_VERSION_TAG"
-    git pull
-    cd "$COMPONENT_DIR"
-else
-    log_section "Pulling Olingo JPA library..."
-    git clone --single-branch --branch "$OLINGO_VERSION_TAG" https://github.com/SAP/olingo-jpa-processor-v4.git "$OLINGO_JPA_LIB_DIR"
-fi
-
-cd "$OLINGO_JPA_LIB_DIR/jpa/"
-
-log_section "Installing Olingo JPA Library..."
-mvn clean install -DskipTests
-
-cd "$COMPONENT_DIR"
-
-log_section "Installing Open Discovery Service..."
-mvn clean install -DskipTests
+source "$(dirname $0)/build.sh"
 
 log_section "Starting Open Discovery Service..."
-java -jar "$COMPONENT_DIR/target/od-service-0.0.1-SNAPSHOT.jar"
+java -jar "$COMPONENT_DIR/target/od-service-$ARTIFACT_VERSION.jar"
