8b7f915f69a56282026717a33ea0fd7622cf9ed3
NickyMateev <bgdunkers@gmail.com>
2020-09-21 16:32:34 +0300

    Improve run.sh

--- components/od-service/run.sh
+++ components/od-service/run.sh
@@ -1,31 +1,32 @@
 #!/usr/bin/env bash
 
-source ./formatting.sh
-
-OLINGO_JPA_LIB_DIR="olingo-jpa-processor-v4"
+COMPONENT_DIR="$GOPATH/src/github.com/kyma-incubator/compass/components/od-service"
+OLINGO_JPA_LIB_DIR="$COMPONENT_DIR/olingo-jpa-processor-v4"
 OLINGO_VERSION_TAG=0.3.7-a
 
-if [ -d "./$OLINGO_JPA_LIB_DIR" ]
+source "$COMPONENT_DIR/formatting.sh"
+
+if [[ -d "$OLINGO_JPA_LIB_DIR" ]]
 then
     log_section "Olingo JPA library already exists locally. Will attempt to sync it with remote..."
-    cd "$OLINGO_JPA_LIB_DIR/"
+    cd "$OLINGO_JPA_LIB_DIR"
     git checkout 0.3.7-a
     git pull
-    cd ../
+    cd "$COMPONENT_DIR"
 else
     log_section "Pulling Olingo JPA library..."
-    git clone --single-branch --branch $OLINGO_VERSION_TAG https://github.com/SAP/olingo-jpa-processor-v4.git
+    git clone --single-branch --branch "$OLINGO_VERSION_TAG" https://github.com/SAP/olingo-jpa-processor-v4.git
 fi
 
-cd $OLINGO_JPA_LIB_DIR/jpa/
+cd "$OLINGO_JPA_LIB_DIR/jpa/"
 
 log_section "Installing Olingo JPA Library..."
 mvn clean install -DskipTests
 
-cd ../..
+cd "$COMPONENT_DIR"
 
 log_section "Installing Open Discovery Service..."
 mvn clean install -DskipTests
 
 log_section "Starting Open Discovery Service..."
-java -jar target/od-service-0.0.1-SNAPSHOT.jar
+java -jar "$COMPONENT_DIR/target/od-service-0.0.1-SNAPSHOT.jar"
