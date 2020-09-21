dff8beb1f27b3c9a409ebcab4af6ac9b362ddd49
NickyMateev <bgdunkers@gmail.com>
2020-09-21 16:43:57 +0300

    Fix git clone when outside OD-Service directory

--- components/od-service/run.sh
+++ components/od-service/run.sh
@@ -2,7 +2,7 @@
 
 COMPONENT_DIR="$GOPATH/src/github.com/kyma-incubator/compass/components/od-service"
 OLINGO_JPA_LIB_DIR="$COMPONENT_DIR/olingo-jpa-processor-v4"
-OLINGO_VERSION_TAG=0.3.7-a
+OLINGO_VERSION_TAG="0.3.7-a"
 
 source "$COMPONENT_DIR/formatting.sh"
 
@@ -10,12 +10,12 @@ if [[ -d "$OLINGO_JPA_LIB_DIR" ]]
 then
     log_section "Olingo JPA library already exists locally. Will attempt to sync it with remote..."
     cd "$OLINGO_JPA_LIB_DIR"
-    git checkout 0.3.7-a
+    git checkout "$OLINGO_VERSION_TAG"
     git pull
     cd "$COMPONENT_DIR"
 else
     log_section "Pulling Olingo JPA library..."
-    git clone --single-branch --branch "$OLINGO_VERSION_TAG" https://github.com/SAP/olingo-jpa-processor-v4.git
+    git clone --single-branch --branch "$OLINGO_VERSION_TAG" https://github.com/SAP/olingo-jpa-processor-v4.git "$OLINGO_JPA_LIB_DIR"
 fi
 
 cd "$OLINGO_JPA_LIB_DIR/jpa/"
