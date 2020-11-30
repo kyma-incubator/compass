4af6802f27395ef249c8c7e267c2fd0119df0454
NickyMateev <bgdunkers@gmail.com>
2020-11-30 02:59:56 +0200

    Add conditional DNS name check for app names

--- components/director/pkg/graphql/app_validation.go
+++ components/director/pkg/graphql/app_validation.go
@@ -3,17 +3,39 @@ package graphql
 import (
 	validation "github.com/go-ozzo/ozzo-validation"
 	"github.com/kyma-incubator/compass/components/director/pkg/inputvalidation"
+	"os"
+	"strconv"
 )
 
+const appNormalizationEnvVar = "APP_ENABLE_APP_NAME_NORMALIZATION"
+
+var appNameNormalizationEnabled bool
+
+func init() {
+	appNameNormalizationEnabled = false
+
+	value, isSet := os.LookupEnv(appNormalizationEnvVar)
+	if isSet {
+		parsedValue, err := strconv.ParseBool(value)
+		if err == nil {
+			appNameNormalizationEnabled = parsedValue
+		}
+	}
+}
+
 func (i ApplicationRegisterInput) Validate() error {
-	return validation.ValidateStruct(&i,
-		validation.Field(&i.Name, validation.Required, inputvalidation.DNSName),
-		validation.Field(&i.ProviderName, validation.RuneLength(0, longStringLengthLimit)),
-		validation.Field(&i.Description, validation.RuneLength(0, descriptionStringLengthLimit)),
-		validation.Field(&i.Labels, inputvalidation.EachKey(validation.Required, validation.Match(alphanumericUnderscoreRegexp))),
-		validation.Field(&i.HealthCheckURL, inputvalidation.IsURL, validation.RuneLength(0, longStringLengthLimit)),
-		validation.Field(&i.Webhooks, validation.Each(validation.Required)),
-	)
+	fieldRules := make([]*validation.FieldRules, 0)
+	if !appNameNormalizationEnabled {
+		fieldRules = append(fieldRules, validation.Field(&i.Name, validation.Required, inputvalidation.DNSName))
+	}
+
+	fieldRules = append(fieldRules, validation.Field(&i.ProviderName, validation.RuneLength(0, longStringLengthLimit)))
+	fieldRules = append(fieldRules, validation.Field(&i.Description, validation.RuneLength(0, descriptionStringLengthLimit)))
+	fieldRules = append(fieldRules, validation.Field(&i.Labels, inputvalidation.EachKey(validation.Required, validation.Match(alphanumericUnderscoreRegexp))))
+	fieldRules = append(fieldRules, validation.Field(&i.HealthCheckURL, inputvalidation.IsURL, validation.RuneLength(0, longStringLengthLimit)))
+	fieldRules = append(fieldRules, validation.Field(&i.Webhooks, validation.Each(validation.Required)))
+
+	return validation.ValidateStruct(&i, fieldRules...)
 }
 
 func (i ApplicationUpdateInput) Validate() error {
