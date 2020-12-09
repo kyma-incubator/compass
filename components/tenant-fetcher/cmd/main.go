129eb350476f0009473df559e73af0c031d35a5b
NickyMateev <bgdunkers@gmail.com>
2020-12-09 19:08:21 +0200

    Tweaks

--- components/tenant-fetcher/cmd/main.go
+++ components/tenant-fetcher/cmd/main.go
@@ -1,19 +1,36 @@
+/*
+ * Copyright 2020 The Compass Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package main
 
 import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+
 	"github.com/gorilla/mux"
 	timeouthandler "github.com/kyma-incubator/compass/components/director/pkg/handler"
 	"github.com/kyma-incubator/compass/components/director/pkg/log"
 	"github.com/kyma-incubator/compass/components/director/pkg/signal"
 	"github.com/pkg/errors"
 	"github.com/vrischmann/envconfig"
-	"io/ioutil"
-	"net/http"
-	"os"
-	"time"
 )
 
 const compassURL = "https://github.com/kyma-incubator/compass"
