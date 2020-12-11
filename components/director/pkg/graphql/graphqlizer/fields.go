98de4ef0baf528c51ea3183513c6839473f97e2f
Nikolay Yordanov <nikolay.yordanoff@gmail.com>
2020-12-11 15:14:01 +0200

    Add omit logic for more fields

--- components/director/pkg/graphql/graphqlizer/fields.go
+++ components/director/pkg/graphql/graphqlizer/fields.go
@@ -30,21 +30,31 @@ func addFieldsFromContext(oldFields string, ctx []FieldCtx, keys []string) strin
 	return fmt.Sprintf("%s\n%s", oldFields, strings.Join(newFields, "\n"))
 }
 
-func buildProperties(props map[string]string, omit []string) string {
-	arrayProps := make([]string, 0, len(props))
-	for key, prop := range props {
-		omitted := false
-		for _, om := range omit {
+func buildProperties(allProperties map[string]string, omittedProperties []string) string {
+	resultProperties := make([]string, 0, len(allProperties))
+	for key, prop := range allProperties {
+		shouldBeOmitted := false
+		for _, om := range omittedProperties {
 			if strings.HasPrefix(key, om) {
-				omitted = true
+				shouldBeOmitted = true
 				break
 			}
 		}
-		if !omitted {
-			arrayProps = append(arrayProps, prop)
+		if !shouldBeOmitted {
+			resultProperties = append(resultProperties, prop)
 		}
 	}
-	return strings.Join(arrayProps, "\n")
+	return strings.Join(resultProperties, "\n")
+}
+
+func extractOmitFor(omittedProperties []string, propertyName string) []string {
+	result := make([]string, 0)
+	for _, om := range omittedProperties {
+		if strings.HasPrefix(om, propertyName+".") {
+			result = append(result, strings.TrimPrefix(om, propertyName+"."))
+		}
+	}
+	return result
 }
 
 func (fp *GqlFieldsProvider) Page(item string) string {
@@ -56,19 +66,9 @@ func (fp *GqlFieldsProvider) Page(item string) string {
 	`, item, fp.ForPageInfo())
 }
 
-func extractOmitFor(omit []string, name string) []string {
-	result := make([]string, 0)
-	for _, om := range omit {
-		if strings.HasPrefix(om, name+".") {
-			result = append(result, strings.TrimPrefix(om, name+"."))
-		}
-	}
-
-	return result
-}
-
-func (fp *GqlFieldsProvider) OmitForApplication(omit []string) string {
-	packagesOmit := extractOmitFor(omit, "packages")
+func (fp *GqlFieldsProvider) OmitForApplication(omittedProperties []string) string {
+	packagesOmittedProperties := extractOmitFor(omittedProperties, "packages")
+	webhooksOmittedProperties := extractOmitFor(omittedProperties, "webhooks")
 
 	return buildProperties(map[string]string{
 		"id":                    "id",
@@ -78,12 +78,12 @@ func (fp *GqlFieldsProvider) OmitForApplication(omit []string) string {
 		"integrationSystemID":   "integrationSystemID",
 		"labels":                "labels",
 		"status":                "status { condition timestamp }",
-		"webhooks":              fmt.Sprintf("webhooks {%s}", fp.ForWebhooks()),
+		"webhooks":              fmt.Sprintf("webhooks {%s}", fp.OmitForWebhooks(webhooksOmittedProperties)),
 		"healthCheckURL":        "healthCheckURL",
-		"packages":              fmt.Sprintf("packages {%s}", fp.Page(fp.OmitForPackage(packagesOmit))),
+		"packages":              fmt.Sprintf("packages {%s}", fp.Page(fp.OmitForPackage(packagesOmittedProperties))),
 		"auths":                 fmt.Sprintf("auths {%s}", fp.ForSystemAuth()),
 		"eventingConfiguration": "eventingConfiguration { defaultURL }",
-	}, omit)
+	}, omittedProperties)
 }
 
 func (fp *GqlFieldsProvider) ForApplication(ctx ...FieldCtx) string {
@@ -105,13 +105,24 @@ func (fp *GqlFieldsProvider) ForApplication(ctx ...FieldCtx) string {
 }
 
 func (fp *GqlFieldsProvider) ForApplicationTemplate(ctx ...FieldCtx) string {
-	return fmt.Sprintf(`id
+	return fmt.Sprintf(`
+		id
 		name
 		description
 		applicationInput
 		placeholders {%s}
 		accessLevel
 	`, fp.ForPlaceholders())
+}
+
+func (fp *GqlFieldsProvider) OmitForWebhooks(omittedProperties []string) string {
+	return buildProperties(map[string]string{
+		"id":            "id",
+		"applicationID": "applicationID",
+		"type":          "type",
+		"url":           "url",
+		"auth":          fmt.Sprintf("auth {%s}", fp.ForAuth()),
+	}, omittedProperties)
 }
 
 func (fp *GqlFieldsProvider) ForWebhooks() string {
@@ -125,20 +136,24 @@ func (fp *GqlFieldsProvider) ForWebhooks() string {
 		}`, fp.ForAuth())
 }
 
-func (fp *GqlFieldsProvider) OmitForAPIDefinition(omit []string) string {
-	omitForSpec := extractOmitFor(omit, "spec")
+func (fp *GqlFieldsProvider) OmitForAPIDefinition(omittedProperties []string) string {
+	specOmittedProperties := extractOmitFor(omittedProperties, "spec")
+	versionOmittedProperties := extractOmitFor(omittedProperties, "version")
+
 	return buildProperties(map[string]string{
+		"id":          "id",
 		"name":        "name",
 		"description": "description",
-		"spec":        fmt.Sprintf("spec {%s}", fp.OmitForApiSpec(omitForSpec)),
+		"spec":        fmt.Sprintf("spec {%s}", fp.OmitForApiSpec(specOmittedProperties)),
 		"targetURL":   "targetURL",
 		"group":       "group",
-		"version":     fmt.Sprintf("version {%s}", fp.ForVersion()),
-	}, omit)
+		"version":     fmt.Sprintf("version {%s}", fp.OmitForVersion(versionOmittedProperties)),
+	}, omittedProperties)
 }
 
 func (fp *GqlFieldsProvider) ForAPIDefinition(ctx ...FieldCtx) string {
-	return addFieldsFromContext(fmt.Sprintf(`id
+	return addFieldsFromContext(fmt.Sprintf(`
+		id
 		name
 		description
 		spec {%s}
@@ -154,13 +169,15 @@ func (fp *GqlFieldsProvider) ForSystemAuth() string {
 		auth {%s}`, fp.ForAuth())
 }
 
-func (fp *GqlFieldsProvider) OmitForApiSpec(omit []string) string {
+func (fp *GqlFieldsProvider) OmitForApiSpec(omittedProperties []string) string {
+	frOmittedProperties := extractOmitFor(omittedProperties, "fetchRequest")
+
 	return buildProperties(map[string]string{
 		"data":         "data",
 		"format":       "format",
 		"type":         "type",
-		"fetchRequest": fmt.Sprintf("fetchRequest {%s}", fp.ForFetchRequest()),
-	}, omit)
+		"fetchRequest": fmt.Sprintf("fetchRequest {%s}", fp.OmitForFetchRequest(frOmittedProperties)),
+	}, omittedProperties)
 }
 
 func (fp *GqlFieldsProvider) ForApiSpec() string {
@@ -171,8 +188,19 @@ func (fp *GqlFieldsProvider) ForApiSpec() string {
 		fetchRequest {%s}`, fp.ForFetchRequest())
 }
 
+func (fp *GqlFieldsProvider) OmitForFetchRequest(omittedProperties []string) string {
+	return buildProperties(map[string]string{
+		"url":    "url",
+		"auth":   fmt.Sprintf("auth {%s}", fp.ForAuth()),
+		"mode":   "mode",
+		"filter": "filter",
+		"status": "status {condition message timestamp}",
+	}, omittedProperties)
+}
+
 func (fp *GqlFieldsProvider) ForFetchRequest() string {
-	return fmt.Sprintf(`url
+	return fmt.Sprintf(`
+		url
 		auth {%s}
 		mode
 		filter
@@ -182,6 +210,15 @@ func (fp *GqlFieldsProvider) ForFetchRequest() string {
 func (fp *GqlFieldsProvider) ForAPIRuntimeAuth() string {
 	return fmt.Sprintf(`runtimeID
 		auth {%s}`, fp.ForAuth())
+}
+
+func (fp *GqlFieldsProvider) OmitForVersion(omittedProperties []string) string {
+	return buildProperties(map[string]string{
+		"value":           "value",
+		"deprecated":      "deprecated",
+		"deprecatedSince": "deprecatedSince",
+		"forRemoval":      "forRemoval",
+	}, omittedProperties)
 }
 
 func (fp *GqlFieldsProvider) ForVersion() string {
@@ -197,16 +234,18 @@ func (fp *GqlFieldsProvider) ForPageInfo() string {
 		hasNextPage`
 }
 
-func (fp *GqlFieldsProvider) OmitForEventDefinition(omit []string) string {
-	omitForSpec := extractOmitFor(omit, "spec")
+func (fp *GqlFieldsProvider) OmitForEventDefinition(omittedProperties []string) string {
+	specOmittedProperties := extractOmitFor(omittedProperties, "spec")
+	versionOmittedProperties := extractOmitFor(omittedProperties, "version")
+
 	return buildProperties(map[string]string{
 		"id":          "id",
 		"name":        "name",
 		"description": "description",
 		"group":       "group",
-		"spec":        fmt.Sprintf("spec {%s}", fp.OmitForEventSpec(omitForSpec)),
-		"version":     fmt.Sprintf("version {%s}", fp.ForVersion()),
-	}, omit)
+		"spec":        fmt.Sprintf("spec {%s}", fp.OmitForEventSpec(specOmittedProperties)),
+		"version":     fmt.Sprintf("version {%s}", fp.OmitForVersion(versionOmittedProperties)),
+	}, omittedProperties)
 
 }
 
@@ -221,13 +260,15 @@ func (fp *GqlFieldsProvider) ForEventDefinition() string {
 		`, fp.ForEventSpec(), fp.ForVersion())
 }
 
-func (fp *GqlFieldsProvider) OmitForEventSpec(omit []string) string {
+func (fp *GqlFieldsProvider) OmitForEventSpec(omittedProperties []string) string {
+	frOmittedProperties := extractOmitFor(omittedProperties, "fetchRequest")
+
 	return buildProperties(map[string]string{
 		"data":         "data",
-		"format":       "format",
 		"type":         "type",
-		"fetchRequest": fmt.Sprintf("fetchRequest {%s}", fp.ForFetchRequest()),
-	}, omit)
+		"format":       "format",
+		"fetchRequest": fmt.Sprintf("fetchRequest {%s}", fp.OmitForFetchRequest(frOmittedProperties)),
+	}, omittedProperties)
 }
 
 func (fp *GqlFieldsProvider) ForEventSpec() string {
@@ -238,7 +279,9 @@ func (fp *GqlFieldsProvider) ForEventSpec() string {
 		fetchRequest {%s}`, fp.ForFetchRequest())
 }
 
-func (fp *GqlFieldsProvider) OmitForDocument(omit []string) string {
+func (fp *GqlFieldsProvider) OmitForDocument(omittedProperties []string) string {
+	frOmittedProperties := extractOmitFor(omittedProperties, "fetchRequest")
+
 	return buildProperties(map[string]string{
 		"id":           "id",
 		"title":        "title",
@@ -247,8 +290,8 @@ func (fp *GqlFieldsProvider) OmitForDocument(omit []string) string {
 		"format":       "format",
 		"kind":         "kind",
 		"data":         "data",
-		"fetchRequest": fmt.Sprintf("fetchRequest {%s}", fp.ForFetchRequest()),
-	}, omit)
+		"fetchRequest": fmt.Sprintf("fetchRequest {%s}", fp.OmitForFetchRequest(frOmittedProperties)),
+	}, omittedProperties)
 }
 
 func (fp *GqlFieldsProvider) ForDocument() string {
@@ -379,6 +422,25 @@ func (fp *GqlFieldsProvider) ForTenant() string {
 		initialized`
 }
 
+func (fp *GqlFieldsProvider) OmitForPackage(omittedProperties []string) string {
+	apiDefOmittedProperties := extractOmitFor(omittedProperties, "apiDefinitions")
+	eventDefOmittedProperties := extractOmitFor(omittedProperties, "eventDefinitions")
+	documentsOmittedProperties := extractOmitFor(omittedProperties, "documents")
+	instanceAuthsOmittedProperties := extractOmitFor(omittedProperties, "instanceAuths")
+
+	return buildProperties(map[string]string{
+		"id":                             "id",
+		"name":                           "name",
+		"description":                    "description",
+		"instanceAuthRequestInputSchema": "instanceAuthRequestInputSchema",
+		"instanceAuths":                  fmt.Sprintf("instanceAuths {%s}", fp.OmitForPackageInstanceAuth(instanceAuthsOmittedProperties)),
+		"defaultInstanceAuth":            fmt.Sprintf("defaultInstanceAuth {%s}", fp.ForAuth()),
+		"apiDefinitions":                 fmt.Sprintf("apiDefinitions {%s}", fp.Page(fp.OmitForAPIDefinition(apiDefOmittedProperties))),
+		"eventDefinitions":               fmt.Sprintf("eventDefinitions {%s}", fp.Page(fp.OmitForEventDefinition(eventDefOmittedProperties))),
+		"documents":                      fmt.Sprintf("documents {%s}", fp.Page(fp.OmitForDocument(documentsOmittedProperties))),
+	}, omittedProperties)
+}
+
 func (fp *GqlFieldsProvider) ForPackage(ctx ...FieldCtx) string {
 	return addFieldsFromContext(fmt.Sprintf(`
 		id
@@ -393,22 +455,16 @@ func (fp *GqlFieldsProvider) ForPackage(ctx ...FieldCtx) string {
 		ctx, []string{"Package.instanceAuth"})
 }
 
-func (fp *GqlFieldsProvider) OmitForPackage(omit []string) string {
-	apiDefinitionOmit := extractOmitFor(omit, "apiDefinitions")
-	eventDefinitionOmit := extractOmitFor(omit, "eventDefinitions")
-	documentsOmit := extractOmitFor(omit, "documents")
+func (fp *GqlFieldsProvider) OmitForPackageInstanceAuth(omittedProperties []string) string {
+	statusOmittedProperties := extractOmitFor(omittedProperties, "status")
 
 	return buildProperties(map[string]string{
-		"id":                             "id",
-		"name":                           "name",
-		"description":                    "description",
-		"instanceAuthRequestInputSchema": "instanceAuthRequestInputSchema",
-		"instanceAuths":                  fmt.Sprintf("instanceAuths {%s}", fp.ForPackageInstanceAuth()),
-		"defaultInstanceAuth":            fmt.Sprintf("defaultInstanceAuth {%s}", fp.ForAuth()),
-		"apiDefinitions":                 fmt.Sprintf("apiDefinitions {%s}", fp.Page(fp.OmitForAPIDefinition(apiDefinitionOmit))),
-		"eventDefinitions":               fmt.Sprintf("eventDefinitions {%s}", fp.Page(fp.OmitForEventDefinition(eventDefinitionOmit))),
-		"documents":                      fmt.Sprintf("documents {%s}", fp.Page(fp.OmitForDocument(documentsOmit))),
-	}, omit)
+		"id":          "id",
+		"context":     "context",
+		"inputParams": "inputParams",
+		"auth":        fmt.Sprintf("auth {%s}", fp.ForAuth()),
+		"status":      fmt.Sprintf("status {%s}", fp.OmitForPackageInstanceAuthStatus(statusOmittedProperties)),
+	}, omittedProperties)
 }
 
 func (fp *GqlFieldsProvider) ForPackageInstanceAuth() string {
@@ -418,6 +474,15 @@ func (fp *GqlFieldsProvider) ForPackageInstanceAuth() string {
 		inputParams
 		auth {%s}
 		status {%s}`, fp.ForAuth(), fp.ForPackageInstanceAuthStatus())
+}
+
+func (fp *GqlFieldsProvider) OmitForPackageInstanceAuthStatus(omittedProperties []string) string {
+	return buildProperties(map[string]string{
+		"condition": "condition",
+		"timestamp": "timestamp",
+		"message":   "message",
+		"reason":    "reason",
+	}, omittedProperties)
 }
 
 func (fp *GqlFieldsProvider) ForPackageInstanceAuthStatus() string {
--- components/director/pkg/graphql/graphqlizer/fields_test.go
+++ components/director/pkg/graphql/graphqlizer/fields_test.go
@@ -17,7 +17,7 @@ func TestGqlFieldsProvider_Page(t *testing.T) {
 	})
 }
 
-func TestGqlFieldsProvider_OmitForApplication(t *testing.T) {
+func TestGqlFieldsProvider_OmitCombinedFieldsForApplication(t *testing.T) {
 	type testCase struct {
 		name               string
 		fp                 *graphqlizer.GqlFieldsProvider
@@ -26,50 +26,41 @@ func TestGqlFieldsProvider_OmitForApplication(t *testing.T) {
 	}
 	tests := []testCase{
 		{
-			name: "with no omitted fields",
+			name: "with omitted top level complex fields",
 			fp:   &graphqlizer.GqlFieldsProvider{},
-			omit: []string{},
+			omit: []string{"packages", "webhooks"},
 			expectedProperties: map[string]int{
-				"id":           7,
-				"name":         4,
-				"fetchRequest": 3,
-			},
-		},
-		{
-			name: "with omitted top level simple field",
-			fp:   &graphqlizer.GqlFieldsProvider{},
-			omit: []string{"integrationSystemID"},
-			expectedProperties: map[string]int{
-				"id":                  7,
-				"name":                4,
-				"packages":            1,
-				"apiDefinitions":      1,
-				"integrationSystemID": 0,
-			},
-		},
-		{
-			name: "with omitted top level complex field",
-			fp:   &graphqlizer.GqlFieldsProvider{},
-			omit: []string{"packages"},
-			expectedProperties: map[string]int{
-				"id":                  3,
+				"id":                  2,
 				"name":                1,
 				"integrationSystemID": 1,
+				"status":              1,
+				"auths":               1,
 				"packages":            0,
+				"instanceAuths":       0,
+				"webhooks":            0,
 				"apiDefinitions":      0,
 				"eventDefinitions":    0,
-				"targetURL":           0,
+				"documents":           0,
+				"fetchRequest":        0,
 			},
 		},
 		{
-			name: "with omitted nested fields",
+			name: "with multiple omitted 'fetchRequest' fields",
 			fp:   &graphqlizer.GqlFieldsProvider{},
 			omit: []string{"packages.apiDefinitions.spec.fetchRequest", "packages.eventDefinitions.spec.fetchRequest", "packages.documents.fetchRequest"},
 			expectedProperties: map[string]int{
-				"packages":         1,
-				"apiDefinitions":   1,
-				"eventDefinitions": 1,
-				"fetchRequest":     0,
+				"id":                  8,
+				"name":                4,
+				"integrationSystemID": 1,
+				"status":              2,
+				"auths":               1,
+				"packages":            1,
+				"instanceAuths":       1,
+				"webhooks":            1,
+				"apiDefinitions":      1,
+				"eventDefinitions":    1,
+				"documents":           1,
+				"fetchRequest":        0,
 			},
 		},
 		{
@@ -77,18 +68,346 @@ func TestGqlFieldsProvider_OmitForApplication(t *testing.T) {
 			fp:   &graphqlizer.GqlFieldsProvider{},
 			omit: []string{"packages.apiDefinitions.spec.fetchRequest", "packages.eventDefinitions.spec.fetchRequest"},
 			expectedProperties: map[string]int{
-				"apiDefinitions":   1,
-				"eventDefinitions": 1,
-				"documents":        1,
-				"fetchRequest":     1,
+				"id":                  8,
+				"name":                4,
+				"integrationSystemID": 1,
+				"status":              3,
+				"auths":               1,
+				"packages":            1,
+				"instanceAuths":       1,
+				"webhooks":            1,
+				"apiDefinitions":      1,
+				"eventDefinitions":    1,
+				"documents":           1,
+				"fetchRequest":        1,
 			},
 		},
 		{
-			name: "with omit for non-existing field",
+			name: "with omitted nested fields",
 			fp:   &graphqlizer.GqlFieldsProvider{},
-			omit: []string{"packages.nonExisting"},
+			omit: []string{"auths", "webhooks", "status", "packages.instanceAuths", "packages.documents", "packages.apiDefinitions.spec.fetchRequest", "packages.eventDefinitions.spec.fetchRequest"},
 			expectedProperties: map[string]int{
-				"id":               7,
+				"id":                  4,
+				"name":                4,
+				"integrationSystemID": 1,
+				"status":              0,
+				"auths":               0,
+				"packages":            1,
+				"instanceAuths":       0,
+				"webhooks":            0,
+				"apiDefinitions":      1,
+				"eventDefinitions":    1,
+				"documents":           0,
+				"fetchRequest":        0,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			actual := tt.fp.OmitForApplication(tt.omit)
+			for expectedProp, expectedCount := range tt.expectedProperties {
+				fieldRegex := regexp.MustCompile(`\b` + expectedProp + `\b`)
+
+				matches := fieldRegex.FindAllStringIndex(actual, -1)
+				actualCount := len(matches)
+
+				assert.Equal(t, expectedCount, actualCount, expectedProp)
+			}
+		})
+	}
+}
+
+func TestGqlFieldsProvider_OmitSeparatelyFieldsForApplication(t *testing.T) {
+	type testCase struct {
+		name               string
+		fp                 *graphqlizer.GqlFieldsProvider
+		omit               []string
+		expectedProperties map[string]int
+	}
+	tests := []testCase{
+		{
+			name: "with no omitted fields on 'applications' level",
+			fp:   &graphqlizer.GqlFieldsProvider{},
+			omit: []string{},
+			expectedProperties: map[string]int{
+				"id":           8,
+				"name":         4,
+				"fetchRequest": 3,
+			},
+		},
+		{
+			name: "with omitted simple field on 'applications' level",
+			fp:   &graphqlizer.GqlFieldsProvider{},
+			omit: []string{"integrationSystemID"},
+			expectedProperties: map[string]int{
+				"id":                  8,
+				"name":                4,
+				"integrationSystemID": 0,
+				"packages":            1,
+				"apiDefinitions":      1,
+				"fetchRequest":        3,
+			},
+		},
+		{
+			name: "with omitted complex field 'webhooks' on 'applications' level",
+			fp:   &graphqlizer.GqlFieldsProvider{},
+			omit: []string{"webhooks"},
+			expectedProperties: map[string]int{
+				"id":                  7,
+				"name":                4,
+				"integrationSystemID": 1,
+				"packages":            1,
+				"webhooks":            0,
+				"apiDefinitions":      1,
+				"fetchRequest":        3,
+			},
+		},
+		{
+			name: "with omitted simple field on 'webhooks' level",
+			fp:   &graphqlizer.GqlFieldsProvider{},
+			omit: []string{"webhooks.id"},
+			expectedProperties: map[string]int{
+				"id":                  7,
+				"name":                4,
+				"integrationSystemID": 1,
+				"packages":            1,
+				"webhooks":            1,
+				"apiDefinitions":      1,
+				"fetchRequest":        3,
+			},
+		},
+		{
+			name: "with omitted complex field 'packages' on 'applications' level",
+			fp:   &graphqlizer.GqlFieldsProvider{},
+			omit: []string{"packages"},
+			expectedProperties: map[string]int{
+				"id":                  3,
+				"name":                1,
+				"integrationSystemID": 1,
+				"packages":            0,
+				"webhooks":            1,
+				"apiDefinitions":      0,
+				"fetchRequest":        0,
+			},
+		},
+		{
+			name: "with omitted simple field on 'packages' level",
+			fp:   &graphqlizer.GqlFieldsProvider{},
+			omit: []string{"packages.id"},
+			expectedProperties: map[string]int{
+				"id":                  7,
+				"name":                4,
+				"integrationSystemID": 1,
+				"packages":            1,
+				"webhooks":            1,
+				"apiDefinitions":      1,
+				"fetchRequest":        3,
+			},
+		},
+		{
+			name: "with omitted simple field on 'instanceAuth' level",
+			fp:   &graphqlizer.GqlFieldsProvider{},
+			omit: []string{"packages.instanceAuths.id"},
+			expectedProperties: map[string]int{
+				"id":                  7,
+				"name":                4,
+				"integrationSystemID": 1,
+				"packages":            1,
+				"webhooks":            1,
+				"apiDefinitions":      1,
+				"fetchRequest":        3,
+				"reason":              1,
+			},
+		},
+		{
+			name: "with omitted simple field on 'instanceAuth.status' level",
+			fp:   &graphqlizer.GqlFieldsProvider{},
+			omit: []string{"packages.instanceAuths.status.reason"},
+			expectedProperties: map[string]int{
+				"id":                  8,
+				"name":                4,
+				"integrationSystemID": 1,
+				"packages":            1,
+				"webhooks":            1,
+				"apiDefinitions":      1,
+				"fetchRequest":        3,
+				"reason":              0,
+			},
+		},
+		{
+			name: "with omitted simple field on 'apiDefinitions' level",
+			fp:   &graphqlizer.GqlFieldsProvider{},
+			omit: []string{"packages.apiDefinitions.id"},
+			expectedProperties: map[string]int{
+				"id":                  7,
+				"name":                4,
+				"integrationSystemID": 1,
+				"packages":            1,
+				"webhooks":            1,
+				"apiDefinitions":      1,
+				"fetchRequest":        3,
+				"filter":              3,
+			},
+		},
+		{
+			name: "with omitted 'fetchRequest' field on 'apiDefinitions.spec' level",
+			fp:   &graphqlizer.GqlFieldsProvider{},
+			omit: []string{"packages.apiDefinitions.spec.fetchRequest"},
+			expectedProperties: map[string]int{
+				"id":                  8,
+				"name":                4,
+				"integrationSystemID": 1,
+				"packages":            1,
+				"webhooks":            1,
+				"apiDefinitions":      1,
+				"fetchRequest":        2,
+				"filter":              2,
+			},
+		},
+		{
+			name: "with omitted simple field on 'apiDefinitions.spec.fetchRequest' level",
+			fp:   &graphqlizer.GqlFieldsProvider{},
+			omit: []string{"packages.apiDefinitions.spec.fetchRequest.filter"},
+			expectedProperties: map[string]int{
+				"id":                  8,
+				"name":                4,
+				"integrationSystemID": 1,
+				"packages":            1,
+				"webhooks":            1,
+				"apiDefinitions":      1,
+				"fetchRequest":        3,
+				"filter":              2,
+				"forRemoval":          2,
+			},
+		},
+		{
+			name: "with omitted simple field on 'apiDefinitions.version' level",
+			fp:   &graphqlizer.GqlFieldsProvider{},
+			omit: []string{"packages.apiDefinitions.version.forRemoval"},
+			expectedProperties: map[string]int{
+				"id":                  8,
+				"name":                4,
+				"integrationSystemID": 1,
+				"packages":            1,
+				"webhooks":            1,
+				"apiDefinitions":      1,
+				"fetchRequest":        3,
+				"forRemoval":          1,
+			},
+		},
+		{
+			name: "with omitted simple field on 'eventDefinitions' level",
+			fp:   &graphqlizer.GqlFieldsProvider{},
+			omit: []string{"packages.eventDefinitions.id"},
+			expectedProperties: map[string]int{
+				"id":                  7,
+				"name":                4,
+				"integrationSystemID": 1,
+				"packages":            1,
+				"webhooks":            1,
+				"eventDefinitions":    1,
+				"fetchRequest":        3,
+				"filter":              3,
+			},
+		},
+		{
+			name: "with omitted 'fetchRequest' field on 'eventDefinitions.spec' level",
+			fp:   &graphqlizer.GqlFieldsProvider{},
+			omit: []string{"packages.eventDefinitions.spec.fetchRequest"},
+			expectedProperties: map[string]int{
+				"id":                  8,
+				"name":                4,
+				"integrationSystemID": 1,
+				"packages":            1,
+				"webhooks":            1,
+				"eventDefinitions":    1,
+				"fetchRequest":        2,
+				"filter":              2,
+			},
+		},
+		{
+			name: "with omitted simple field on 'eventDefinitions.spec.fetchRequest' level",
+			fp:   &graphqlizer.GqlFieldsProvider{},
+			omit: []string{"packages.eventDefinitions.spec.fetchRequest.filter"},
+			expectedProperties: map[string]int{
+				"id":                  8,
+				"name":                4,
+				"integrationSystemID": 1,
+				"packages":            1,
+				"webhooks":            1,
+				"eventDefinitions":    1,
+				"fetchRequest":        3,
+				"filter":              2,
+				"forRemoval":          2,
+			},
+		},
+		{
+			name: "with omitted simple field on 'eventDefinitions.version' level",
+			fp:   &graphqlizer.GqlFieldsProvider{},
+			omit: []string{"packages.eventDefinitions.version.forRemoval"},
+			expectedProperties: map[string]int{
+				"id":                  8,
+				"name":                4,
+				"integrationSystemID": 1,
+				"packages":            1,
+				"webhooks":            1,
+				"eventDefinitions":    1,
+				"fetchRequest":        3,
+				"forRemoval":          1,
+			},
+		},
+		{
+			name: "with omitted simple field on 'documents' level",
+			fp:   &graphqlizer.GqlFieldsProvider{},
+			omit: []string{"packages.documents.id"},
+			expectedProperties: map[string]int{
+				"id":                  7,
+				"name":                4,
+				"integrationSystemID": 1,
+				"packages":            1,
+				"webhooks":            1,
+				"documents":           1,
+				"fetchRequest":        3,
+				"filter":              3,
+			},
+		},
+		{
+			name: "with omitted 'fetchRequest' field on 'documents' level",
+			fp:   &graphqlizer.GqlFieldsProvider{},
+			omit: []string{"packages.documents.fetchRequest"},
+			expectedProperties: map[string]int{
+				"id":                  8,
+				"name":                4,
+				"integrationSystemID": 1,
+				"packages":            1,
+				"webhooks":            1,
+				"documents":           1,
+				"fetchRequest":        2,
+				"filter":              2,
+			},
+		},
+		{
+			name: "with omitted simple field on 'documents.fetchRequest' level",
+			fp:   &graphqlizer.GqlFieldsProvider{},
+			omit: []string{"packages.documents.fetchRequest.filter"},
+			expectedProperties: map[string]int{
+				"id":                  8,
+				"name":                4,
+				"integrationSystemID": 1,
+				"packages":            1,
+				"webhooks":            1,
+				"documents":           1,
+				"fetchRequest":        3,
+				"filter":              2,
+				"forRemoval":          2,
+			},
+		},
+		{
+			name: "with omitted non-existing fields",
+			fp:   &graphqlizer.GqlFieldsProvider{},
+			omit: []string{"packages.nonExisting", "idTypo", "packagesTypo.id", "packages.apiDefinitions.idTypo"},
+			expectedProperties: map[string]int{
+				"id":               8,
 				"name":             4,
 				"packages":         1,
 				"fetchRequest":     3,
