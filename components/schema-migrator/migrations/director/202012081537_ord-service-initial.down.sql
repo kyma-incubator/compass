3b3151619f8906d238e1220e7a1d79f9653a7507
Dimitar Petrov <dimitar.petrov@sap.com>
2020-12-18 10:35:15 +0200

    Fix down migration

--- components/schema-migrator/migrations/director/202012081537_ord-service-initial.down.sql
+++ components/schema-migrator/migrations/director/202012081537_ord-service-initial.down.sql
@@ -4,14 +4,13 @@ ALTER TABLE packages
     DROP COLUMN ord_id,
     DROP COLUMN short_description,
     DROP COLUMN version,
+    DROP COLUMN package_links,
     DROP COLUMN links,
-    DROP COLUMN terms_of_service,
     DROP COLUMN licence_type,
-    DROP COLUMN licence,
     DROP COLUMN provider,
     DROP COLUMN tags,
-    DROP COLUMN actions,
-    DROP COLUMN extensions;
+    DROP COLUMN countries,
+    DROP COLUMN labels;
 
 DROP TRIGGER set_release_status_api_def ON api_definitions;
 DROP TRIGGER set_release_status_event_def ON event_api_definitions;
@@ -20,16 +19,18 @@ DROP FUNCTION set_release_status;
 ALTER TABLE api_definitions
     DROP COLUMN ord_id,
     DROP COLUMN short_description,
-    DROP COLUMN documentation,
     DROP COLUMN system_instance_aware,
     DROP COLUMN api_protocol,
     DROP COLUMN tags,
+    DROP COLUMN countries,
     DROP COLUMN api_definitions,
     DROP COLUMN links,
-    DROP COLUMN actions,
+    DROP COLUMN api_resource_links,
     DROP COLUMN release_status,
+    DROP COLUMN sunset_date,
+    DROP COLUMN successor,
     DROP COLUMN changelog_entries,
-    DROP COLUMN extensions;
+    DROP COLUMN labels;
 
 ALTER TABLE event_api_definitions
     DROP COLUMN ord_id,
@@ -38,19 +39,24 @@ ALTER TABLE event_api_definitions
     DROP COLUMN changelog_entries,
     DROP COLUMN links,
     DROP COLUMN tags,
+    DROP COLUMN countries,
     DROP COLUMN release_status,
+    DROP COLUMN sunset_date,
+    DROP COLUMN successor,
     DROP COLUMN event_definitions,
-    DROP COLUMN extensions;
+    DROP COLUMN labels;
 
 DROP TYPE api_protocol;
 DROP TYPE release_status;
 
 DROP VIEW links;
 DROP VIEW providers;
+DROP VIEW ord_labels;
 DROP VIEW tags;
-DROP VIEW package_actions;
+DROP VIEW countries;
+DROP VIEW package_links;
 DROP VIEW api_resource_definitions;
-DROP VIEW api_actions;
+DROP VIEW api_resource_links;
 DROP VIEW changelog_entries;
 DROP VIEW event_resource_definitions;
 
