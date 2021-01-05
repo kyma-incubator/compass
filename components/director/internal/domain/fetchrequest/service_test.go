ba512a945da38d8a0686c1b9dadc07fb9903915e
Stanislav Stefanov <stanislavsvetlinovstefanov@gmail.com>
2021-01-05 09:27:47 +0200

    fix tests

--- components/director/internal/domain/fetchrequest/service_test.go
+++ components/director/internal/domain/fetchrequest/service_test.go
@@ -4,9 +4,9 @@ import (
 	"bytes"
 	"context"
 	"errors"
+	"fmt"
 	"github.com/kyma-incubator/compass/components/director/pkg/str"
 	"github.com/stretchr/testify/mock"
-	"io"
 	"io/ioutil"
 	"net/http"
 	"testing"
@@ -19,14 +19,6 @@ import (
 	"github.com/kyma-incubator/compass/components/director/internal/model"
 	"github.com/stretchr/testify/assert"
 )
-
-type nopCloser struct {
-	io.Reader
-}
-
-func (n *nopCloser) Close() error {
-	return nil
-}
 
 type RoundTripFunc func(req *http.Request) *http.Response
 
@@ -46,6 +38,11 @@ func NewTestClient(fn RoundTripFunc) *http.Client {
 }
 
 func TestService_HandleAPISpec(t *testing.T) {
+	const username = "username"
+	const password = "password"
+	const clientId = "clId"
+	const secret = "clSecret"
+	const url = "mocked-url/oauth/token"
 	mockSpec := "spec"
 	timestamp := time.Now()
 
@@ -65,6 +62,19 @@ func TestService_HandleAPISpec(t *testing.T) {
 		Filter: str.Ptr("filter"),
 	}
 
+	modelInputBasicCredentials := model.FetchRequest{
+		ID: "test",
+		Auth: &model.Auth{
+			Credential: model.CredentialData{
+				Basic: &model.BasicCredentialData{
+					Username: username,
+					Password: password,
+				},
+			},
+		},
+		Mode: model.FetchModeSingle,
+	}
+
 	modelInputMissingCredentials := model.FetchRequest{
 		ID: "test",
 		Auth: &model.Auth{
@@ -76,29 +86,16 @@ func TestService_HandleAPISpec(t *testing.T) {
 		Mode: model.FetchModeSingle,
 	}
 
-	modelInputBasicCredentials := model.FetchRequest{
-		ID: "test",
-		Auth: &model.Auth{
-			Credential: model.CredentialData{
-				Basic: &model.BasicCredentialData{
-					Username: "username",
-					Password: "password",
-				},
-			},
-		},
-		Mode: model.FetchModeSingle,
-	}
-
 	modelInputOauth := model.FetchRequest{
 		ID:  "test",
-		URL: "sth",
+		URL: "http://dummy.url.sth",
 		Auth: &model.Auth{
 			Credential: model.CredentialData{
 				Basic: nil,
 				Oauth: &model.OAuthCredentialData{
-					ClientID:     "clId",
-					ClientSecret: "clSecret",
-					URL:          "mocked-url/oauth/token",
+					ClientID:     clientId,
+					ClientSecret: secret,
+					URL:          url,
 				},
 			},
 		},
@@ -106,170 +103,167 @@ func TestService_HandleAPISpec(t *testing.T) {
 	}
 
 	testCases := []struct {
-		Name   string
-		Client *http.Client
-		//FetchRequestRepoFn       func() *automock.FetchRequestRepository
+		Name           string
+		Client         func(t *testing.T) *http.Client
 		InputFr        model.FetchRequest
 		ExpectedResult *string
 		ExpectedStatus *model.FetchRequestStatus
-		//ExpectedBasicCredentials model.BasicCredentialData
 	}{
 
 		{
-			Name: "Success",
-			Client: NewTestClient(func(req *http.Request) *http.Response {
-				return &http.Response{
-					StatusCode: http.StatusOK,
-					Body:       ioutil.NopCloser(bytes.NewBufferString(mockSpec)),
-				}
-			}),
+			Name: "Success without authentication",
+			Client: func(t *testing.T) *http.Client {
+				return NewTestClient(func(req *http.Request) *http.Response {
+					return &http.Response{
+						StatusCode: http.StatusOK,
+						Body:       ioutil.NopCloser(bytes.NewBufferString(mockSpec)),
+					}
+				})
+			},
 			InputFr:        modelInput,
 			ExpectedResult: &mockSpec,
-			ExpectedStatus:
+			ExpectedStatus: fetchrequest.FixStatus(model.FetchRequestStatusConditionSucceeded, nil, timestamp),
 		},
-		//{
-		//	Name: "Nil when fetch request validation fails due to mode Package",
-		//	Client: NewTestClient(func(req *http.Request) *http.Response {
-		//		return &http.Response{}
-		//	}),
-		//	FetchRequestRepoFn: func() *automock.FetchRequestRepository {
-		//		repo := &automock.FetchRequestRepository{}
-		//		repo.On("Update", ctx, &modelInputPackageWithMessage).Return(nil).Once()
-		//		return repo
-		//	},
-		//	InputFr:        modelInputPackage,
-		//	ExpectedResult: nil,
-		//	ExpectedError:  str.Ptr("Invalid data [reason=Unsupported fetch mode: PACKAGE]"),
-		//},
-		//{
-		//	Name: "Nil when fetch request validation fails due to provided filter",
-		//	Client: NewTestClient(func(req *http.Request) *http.Response {
-		//		return &http.Response{}
-		//	}),
-		//	FetchRequestRepoFn: func() *automock.FetchRequestRepository {
-		//		repo := &automock.FetchRequestRepository{}
-		//		repo.On("Update", ctx, &modelInputFilterWithMessage).Return(nil).Once()
-		//		return repo
-		//	},
-		//	InputFr:        modelInputFilter,
-		//	ExpectedResult: nil,
-		//	ExpectedError:  str.Ptr("Invalid data [reason=Filter for Fetch Request was provided, currently it's unsupported]"),
-		//},
-		//{
-		//	Name: "Nil when auth without credentials is provided",
-		//	Client: NewTestClient(func(req *http.Request) *http.Response {
-		//		return &http.Response{}
-		//	}),
-		//	FetchRequestRepoFn: func() *automock.FetchRequestRepository {
-		//		repo := &automock.FetchRequestRepository{}
-		//		repo.On("Update", ctx, &modelInputMissingCredentialsWithMessage).Return(nil).Once()
-		//		return repo
-		//	},
-		//	InputFr:        modelInputMissingCredentials,
-		//	ExpectedResult: nil,
-		//	ExpectedError:  str.Ptr("Invalid data [reason=Credentials not provided]"),
-		//},
-		//{
-		//	Name: "Error when fetch request fails",
-		//	Client: NewTestClient(func(req *http.Request) *http.Response {
-		//		return &http.Response{
-		//			StatusCode: http.StatusInternalServerError,
-		//		}
-		//	}),
-		//	FetchRequestRepoFn: func() *automock.FetchRequestRepository {
-		//		repo := &automock.FetchRequestRepository{}
-		//		repo.On("Update", ctx, &modelInputFailed).Return(nil).Once()
-		//		return repo
-		//	},
-		//	InputFr:         modelInput,
-		//	ExpectedMessage: str.Ptr(fmt.Sprintf("While fetching API Spec status code: %d", http.StatusInternalServerError)),
-		//	ExpectedResult:  nil,
-		//	ExpectedError:   str.Ptr("While fetching API Spec status code: 500"),
-		//},
-		//{
-		//	Name: "Success when basic credentials are provided",
-		//	Client: NewTestClient(func(req *http.Request) *http.Response {
-		//		username, password, _ := req.BasicAuth()
-		//		body := bytes.NewBuffer(
-		//			[]byte(
-		//				fmt.Sprintf(`{"username": "%s", "password":"%s"}`, username, password),
-		//			),
-		//		)
-		//		return &http.Response{
-		//			Body: &nopCloser{
-		//				Reader: body,
-		//			},
-		//			StatusCode: http.StatusOK,
-		//		}
-		//	}),
-		//	FetchRequestRepoFn: func() *automock.FetchRequestRepository {
-		//		repo := &automock.FetchRequestRepository{}
-		//		repo.On("Update", ctx, &modelInputBasicCredentials).Return(nil).Once()
-		//		return repo
-		//	},
-		//	InputFr:        modelInputBasicCredentials,
-		//	ExpectedResult: nil,
-		//	ExpectedBasicCredentials: model.BasicCredentialData{
-		//		Username: "username",
-		//		Password: "password",
-		//	},
-		//},
-		//{
-		//	Name: "when fail to fetch token endpoint",
-		//	Client: NewTestClient(func(req *http.Request) *http.Response {
-		//		return &http.Response{
-		//			StatusCode: http.StatusBadRequest,
-		//		}
-		//	}),
-		//	FetchRequestRepoFn:
-		//	func() *automock.FetchRequestRepository {
-		//		repo := &automock.FetchRequestRepository{}
-		//		repo.On("Update", ctx, &modelInputOauthWithMessageFailedToGetTokenEndpoint).Return(nil).Once()
-		//		return repo
-		//	},
-		//	InputFr:        modelInputOauth,
-		//	ExpectedResult: nil,
-		//	ExpectedError:  str.Ptr("Get \\\"mocked-url//\\\": error"),
-		//},
-		//
-		//{
-		//	Name: "when fail to get token",
-		//	Client: NewTestClient(func(req *http.Request) *http.Response {
-		//		return &http.Response{
-		//			StatusCode: http.StatusInternalServerError,
-		//		}
-		//	}),
-		//	FetchRequestRepoFn:
-		//	func() *automock.FetchRequestRepository {
-		//		repo := &automock.FetchRequestRepository{}
-		//		repo.On("Update", ctx, mock.Anything).Return(nil).Once()
-		//		return repo
-		//	},
-		//	InputFr:        modelInputOauth,
-		//	ExpectedResult: nil,
-		//
-		//	ExpectedError: str.Ptr("oauth2: cannot fetch token"),
-		//},
-		//
-		//
-		//{
-		//	Name: "Nil when failed to update fetch request status",
-		//	Client: NewTestClient(func(req *http.Request) *http.Response {
-		//		return &http.Response{
-		//			StatusCode: http.StatusOK,
-		//			Body:       ioutil.NopCloser(bytes.NewBufferString(mockSpec)),
-		//		}
-		//	}),
-		//	FetchRequestRepoFn: func() *automock.FetchRequestRepository {
-		//		repo := &automock.FetchRequestRepository{}
-		//		repo.On("Update", ctx, &modelInputSucceeded).Return(testErr).Once()
-		//		return repo
-		//	},
-		//	InputFr:         modelInput,
-		//	ExpectedResult:  nil,
-		//	ExpectedMessage: str.Ptr(fmt.Sprintf("An error has occurred while updating fetch request status.")),
-		//	ExpectedError:   str.Ptr(testErr.Error()),
-		//},
+		{
+			Name: "Nil when fetch request validation fails due to mode Package",
+			Client: func(t *testing.T) *http.Client {
+				return NewTestClient(func(req *http.Request) *http.Response {
+					return &http.Response{}
+				})
+			},
+
+			InputFr:        modelInputPackage,
+			ExpectedResult: nil,
+			ExpectedStatus: fetchrequest.FixStatus(model.FetchRequestStatusConditionInitial, str.Ptr("Invalid data [reason=Unsupported fetch mode: PACKAGE]"), timestamp),
+		},
+		{
+			Name: "Nil when fetch request validation fails due to provided filter",
+			Client: func(t *testing.T) *http.Client {
+				return NewTestClient(func(req *http.Request) *http.Response {
+					return &http.Response{}
+				})
+			},
+
+			InputFr:        modelInputFilter,
+			ExpectedResult: nil,
+			ExpectedStatus: fetchrequest.FixStatus(model.FetchRequestStatusConditionInitial, str.Ptr("Invalid data [reason=Filter for Fetch Request was provided, currently it's unsupported]"), timestamp),
+		},
+		{
+			Name: "Success with basic authentication",
+			Client: func(t *testing.T) *http.Client {
+				return NewTestClient(func(req *http.Request) *http.Response {
+					actualUsername, actualPassword, ok := req.BasicAuth()
+					assert.True(t, ok)
+					assert.Equal(t, username, actualUsername)
+					assert.Equal(t, password, actualPassword)
+					return &http.Response{
+						StatusCode: http.StatusOK,
+						Body:       ioutil.NopCloser(bytes.NewBufferString(mockSpec)),
+					}
+				})
+			},
+			InputFr:        modelInputBasicCredentials,
+			ExpectedResult: &mockSpec,
+			ExpectedStatus: fetchrequest.FixStatus(model.FetchRequestStatusConditionSucceeded, nil, timestamp),
+		},
+		{
+			Name: "Fails to execute the request with basic authentication",
+			Client: func(t *testing.T) *http.Client {
+				return NewTestClient(func(req *http.Request) *http.Response {
+					actualUsername, actualPassword, ok := req.BasicAuth()
+					assert.True(t, ok)
+					assert.Equal(t, username, actualUsername)
+					assert.Equal(t, password, actualPassword)
+					return &http.Response{
+						StatusCode: http.StatusInternalServerError,
+					}
+				})
+			},
+			InputFr:        modelInputBasicCredentials,
+			ExpectedStatus: fetchrequest.FixStatus(model.FetchRequestStatusConditionFailed, str.Ptr("While fetching API Spec status code: 500"), timestamp),
+		},
+		{
+			Name: "Nil when auth without credentials is provided",
+			Client: func(t *testing.T) *http.Client {
+				return NewTestClient(func(req *http.Request) *http.Response {
+					return &http.Response{}
+				})
+			},
+
+			InputFr:        modelInputMissingCredentials,
+			ExpectedResult: nil,
+			ExpectedStatus: fetchrequest.FixStatus(model.FetchRequestStatusConditionFailed, str.Ptr("While fetching API Spec: Invalid data [reason=Credentials not provided]"), timestamp),
+		},
+		{
+			Name: "Success with oauth authentication",
+			Client: func(t *testing.T) *http.Client {
+				return NewTestClient(func(req *http.Request) *http.Response {
+					if req.URL.String() == url {
+						actualClientId, actualSecret, ok := req.BasicAuth()
+						assert.True(t, ok)
+						assert.Equal(t, clientId, actualClientId)
+						assert.Equal(t, secret, actualSecret)
+						return &http.Response{
+							StatusCode: http.StatusOK,
+							Body:       ioutil.NopCloser(bytes.NewBufferString(`{"access_token":"token"}`)),
+						}
+					}
+
+					return &http.Response{
+						StatusCode: http.StatusOK,
+						Body:       ioutil.NopCloser(bytes.NewBufferString(mockSpec)),
+					}
+				})
+			},
+			InputFr:        modelInputOauth,
+			ExpectedResult: &mockSpec,
+			ExpectedStatus: fetchrequest.FixStatus(model.FetchRequestStatusConditionSucceeded, nil, timestamp),
+		},
+		{
+			Name: "Fails to fetch oauth token with oauth authentication",
+			Client: func(t *testing.T) *http.Client {
+				return NewTestClient(func(req *http.Request) *http.Response {
+					actualClientId, actualSecret, ok := req.BasicAuth()
+					if ok {
+						assert.Equal(t, clientId, actualClientId)
+						assert.Equal(t, secret, actualSecret)
+					} else {
+						credentials, err := ioutil.ReadAll(req.Body)
+						assert.NoError(t, err)
+						assert.Contains(t, string(credentials), fmt.Sprintf("client_id=%s&client_secret=%s&grant_type=client_credentials", clientId, secret))
+					}
+					return &http.Response{
+						StatusCode: http.StatusInternalServerError,
+					}
+				})
+			},
+			InputFr:        modelInputOauth,
+			ExpectedStatus: fetchrequest.FixStatus(model.FetchRequestStatusConditionFailed, str.Ptr("While fetching API Spec: Get \"http://dummy.url.sth\": oauth2: cannot fetch token: \nResponse: "), timestamp),
+		},
+		{
+			Name: "Fails to execute the request with oauth authentication",
+			Client: func(t *testing.T) *http.Client {
+				return NewTestClient(func(req *http.Request) *http.Response {
+					if req.URL.String() == url {
+						actualClientId, actualSecret, ok := req.BasicAuth()
+						assert.True(t, ok)
+						assert.Equal(t, clientId, actualClientId)
+						assert.Equal(t, secret, actualSecret)
+						return &http.Response{
+							StatusCode: http.StatusOK,
+							Body:       ioutil.NopCloser(bytes.NewBufferString(`{"access_token":"token"}`)),
+						}
+					}
+
+					return &http.Response{
+						StatusCode: http.StatusInternalServerError,
+						Body:       ioutil.NopCloser(bytes.NewBufferString(mockSpec)),
+					}
+				})
+			},
+			InputFr:        modelInputOauth,
+			ExpectedStatus: fetchrequest.FixStatus(model.FetchRequestStatusConditionFailed, str.Ptr("While fetching API Spec status code: 500"), timestamp),
+		},
 	}
 
 	for _, testCase := range testCases {
@@ -278,7 +272,7 @@ func TestService_HandleAPISpec(t *testing.T) {
 			frRepo := &automock.FetchRequestRepository{}
 			frRepo.On("Update", ctx, mock.Anything).Return(nil).Once()
 
-			svc := fetchrequest.NewService(frRepo, testCase.Client)
+			svc := fetchrequest.NewService(frRepo, testCase.Client(t))
 			svc.SetTimestampGen(func() time.Time { return timestamp })
 
 			result := svc.HandleSpec(ctx, &testCase.InputFr)
@@ -288,5 +282,30 @@ func TestService_HandleAPISpec(t *testing.T) {
 
 		})
 	}
+}
 
+func TestService_HandleAPISpec_FailedToUpdateStatusAfterFetching(t *testing.T) {
+	ctx := context.TODO()
+	timestamp := time.Now()
+	frRepo := &automock.FetchRequestRepository{}
+	frRepo.On("Update", ctx, mock.Anything).Return(errors.New("error")).Once()
+
+	svc := fetchrequest.NewService(frRepo, NewTestClient(func(req *http.Request) *http.Response {
+		return &http.Response{
+			StatusCode: http.StatusOK,
+			Body:       ioutil.NopCloser(bytes.NewBufferString("spec")),
+		}
+	}))
+	svc.SetTimestampGen(func() time.Time { return timestamp })
+
+	modelInput := &model.FetchRequest{
+		ID:   "test",
+		Mode: model.FetchModeSingle,
+	}
+
+	result := svc.HandleSpec(ctx, modelInput)
+	expectedStatus := fetchrequest.FixStatus(model.FetchRequestStatusConditionSucceeded, nil, timestamp)
+
+	assert.Equal(t, expectedStatus, modelInput.Status)
+	assert.Nil(t, result)
 }
