2e0cc49d0cda4fb7e48ebd06d23f07055212dfc0
Nikolay Mateev <bgdunkers@gmail.com>
2021-01-19 13:01:57 +0200

    Add ORD Service Default Response Format filter E2E tests (#1691)

--- tests/ord-service/tests/api_test.go
+++ tests/ord-service/tests/api_test.go
@@ -24,6 +24,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	urlpkg "net/url"
+	"strings"
 	"testing"
 	"time"
 
@@ -37,6 +38,8 @@ import (
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/clientcredentials"
 )
+
+const acceptHeader = "Accept"
 
 func TestORDService(t *testing.T) {
 	appInput := directorSchema.ApplicationRegisterInput{
@@ -161,6 +164,14 @@ func TestORDService(t *testing.T) {
 
 	t.Run("401 when requests to ORD Service are unsecured", func(t *testing.T) {
 		makeRequestWithStatusExpect(t, unsecuredHttpClient, testConfig.ORDServiceURL+"/$metadata?$format=json", http.StatusUnauthorized)
+	})
+
+	t.Run("Requesting entities without specifying response format falls back to configured default response type when Accept header allows everything", func(t *testing.T) {
+		makeRequestWithHeaders(t, httpClient, testConfig.ORDServiceURL+"/packages", map[string][]string{acceptHeader: {"*/*"}})
+	})
+
+	t.Run("Requesting entities without specifying response format falls back to response type specified by Accept header when it provides a specific type", func(t *testing.T) {
+		makeRequestWithHeaders(t, httpClient, testConfig.ORDServiceURL+"/packages", map[string][]string{acceptHeader: {"application/json"}})
 	})
 
 	t.Run("Requesting Packages returns them as expected", func(t *testing.T) {
@@ -471,17 +482,48 @@ func TestORDService(t *testing.T) {
 }
 
 func makeRequest(t *testing.T, httpClient *http.Client, url string) string {
-	return makeRequestWithStatusExpect(t, httpClient, url, http.StatusOK)
+	return makeRequestWithHeadersAndStatusExpect(t, httpClient, url, map[string][]string{}, http.StatusOK)
+}
+
+func makeRequestWithHeaders(t *testing.T, httpClient *http.Client, url string, headers map[string][]string) string {
+	return makeRequestWithHeadersAndStatusExpect(t, httpClient, url, headers, http.StatusOK)
 }
 
 func makeRequestWithStatusExpect(t *testing.T, httpClient *http.Client, url string, expectedHTTPStatus int) string {
+	return makeRequestWithHeadersAndStatusExpect(t, httpClient, url, map[string][]string{}, expectedHTTPStatus)
+}
+
+func makeRequestWithHeadersAndStatusExpect(t *testing.T, httpClient *http.Client, url string, headers map[string][]string, expectedHTTPStatus int) string {
 	request, err := http.NewRequest(http.MethodGet, url, nil)
 	require.NoError(t, err)
+
+	for key, values := range headers {
+		for _, value := range values {
+			request.Header.Add(key, value)
+		}
+	}
 
 	response, err := httpClient.Do(request)
 
 	require.NoError(t, err)
 	require.Equal(t, expectedHTTPStatus, response.StatusCode)
+
+	parsedURL, err := urlpkg.Parse(url)
+	require.NoError(t, err)
+
+	if !strings.Contains(parsedURL.Path, "/specification") {
+		formatParam := parsedURL.Query().Get("$format")
+		acceptHeader, acceptHeaderProvided := headers[acceptHeader]
+
+		contentType := response.Header.Get("Content-Type")
+		if formatParam != "" {
+			require.Contains(t, contentType, formatParam)
+		} else if acceptHeaderProvided && acceptHeader[0] != "*/*" {
+			require.Contains(t, contentType, acceptHeader[0])
+		} else {
+			require.Contains(t, contentType, testConfig.ORDServiceDefaultResponseType)
+		}
+	}
 
 	body, err := ioutil.ReadAll(response.Body)
 	require.NoError(t, err)
