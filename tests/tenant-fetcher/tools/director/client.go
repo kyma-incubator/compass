f70b61d2d5b0540cb41dd797feeb7f7b6dddcdbf
Alex Targov <alextargov@gmail.com>
2021-02-05 17:59:55 +0200

    Fix imports and remove redundant code

--- tests/tenant-fetcher/tools/director/client.go
+++ tests/tenant-fetcher/tools/director/client.go
@@ -2,33 +2,17 @@ package director
 
 import (
 	"context"
+	"time"
+
 	schema "github.com/kyma-incubator/compass/components/director/pkg/graphql"
 	gqlTools "github.com/kyma-incubator/compass/tests/director/pkg/gql"
 	"github.com/kyma-incubator/compass/tests/director/pkg/jwtbuilder"
 	gcli "github.com/machinebox/graphql"
-	"time"
 )
-
-type client struct {
-	scopes []string
-	tenant string
-	client *gcli.Client
-}
 
 type TenantsResponse struct {
 	Result []*schema.Tenant
 }
-
-//
-//func getClient(url string, tenant string, scopes []string) (*gcli.Client, error) {
-//
-//	token, err := getToken(tenant, scopes)
-//	if err != nil {
-//		return nil, err
-//	}
-//
-//	return gqlTools.NewAuthorizedGraphQLClientWithCustomURL(token, url), nil
-//}
 
 func getToken(tenant string, scopes []string) (string, error) {
 	token, err := jwtbuilder.Build(tenant, scopes, &jwtbuilder.Consumer{})
