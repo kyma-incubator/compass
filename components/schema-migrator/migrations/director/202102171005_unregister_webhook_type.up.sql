8f90ad39bbc383584ed7929d7b9cad8ddab3e6a0
Dobromir Zahariev <dobromir.zahariev@sap.com>
2021-02-17 16:52:12 +0200

    Adjust enum values using intermediate enum

--- components/schema-migrator/migrations/director/202102171005_unregister_webhook_type.up.sql
+++ components/schema-migrator/migrations/director/202102171005_unregister_webhook_type.up.sql
@@ -1,10 +1,17 @@
 BEGIN;
 
-ALTER TYPE webhook_type ADD VALUE 'UNREGISTER_APPLICATION';
+ALTER TYPE webhook_type RENAME TO webhook_type_old;
+
+CREATE TYPE webhook_type_med AS ENUM (
+    'CONFIGURATION_CHANGED',
+    'REGISTER_APPLICATION',
+    'UNREGISTER_APPLICATION', 
+    'DELETE_APPLICATION'
+);
+
+ALTER TABLE webhooks ALTER COLUMN type TYPE webhook_type_med USING type::text::webhook_type_med;
 
 UPDATE webhooks SET type = 'UNREGISTER_APPLICATION' WHERE type = 'DELETE_APPLICATION';
-
-ALTER TYPE webhook_type RENAME TO webhook_type_old;
 
 CREATE TYPE webhook_type AS ENUM (
     'CONFIGURATION_CHANGED',
@@ -15,5 +22,6 @@ CREATE TYPE webhook_type AS ENUM (
 ALTER TABLE webhooks ALTER COLUMN type TYPE webhook_type USING type::text::webhook_type;
 
 DROP TYPE webhook_type_old;
+DROP TYPE webhook_type_med;
 
 COMMIT;
