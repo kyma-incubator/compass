f9c0db4a76948eeeca63fbc6105b426345bb0682
Dobromir Zahariev <dobromir.zahariev@sap.com>
2021-03-05 17:37:53 +0200

    Remove temporary shell scripts

--- tests/director/run-compass-gateway-external-services-integration.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-ROOT_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-COMPASS_PATH=$( cd "$( dirname "${ROOT_PATH}/../../../" )" && pwd )
-
-suiteName="compass-gateway-external-services-integration"
-nameSpace="kyma-system"
-
-${COMPASS_PATH}/installation/scripts/testing-suite.sh ${nameSpace} ${suiteName}
