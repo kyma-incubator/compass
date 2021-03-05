3a40cc9a9ee500f161fd9ef0551a6408ca3ba7aa
krasish <krasishontov@gmail.com>
2021-03-05 11:06:32 +0200

    Improve migrations

--- components/schema-migrator/migrations/director/20210302174853_add_app_template_to_webhooks.down.sql
+++ components/schema-migrator/migrations/director/20210302174853_add_app_template_to_webhooks.down.sql
@@ -2,6 +2,7 @@ BEGIN;
 
 ALTER TABLE webhooks
     DROP CONSTRAINT IF EXISTS app_or_template,
+    ALTER COLUMN tenant_id SET NOT NULL,
     ALTER COLUMN app_id SET NOT NULL,
     DROP CONSTRAINT IF EXISTS webhooks_app_template_id_fkey,
     DROP COLUMN IF EXISTS app_template_id;
