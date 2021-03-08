ff2133ab2a9d45fc5f050b64eb48fc49b921f8d6
Stanislav Stefanov <stanislavsvetlinovstefanov@gmail.com>
2021-03-08 13:00:24 +0200

    add script for pod preparation for sandbox tests

---
+++ tests/sandbox.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+COMPONENT=$1
+touch td_duplicate.yaml
+kubectl get testdefinition compass-e2e-$COMPONENT -n kyma-system -o json > td_duplicate.json
+export TD_NAME=compass-e2e-$COMPONENT-local
+jq --arg e "$TD_NAME"  '.metadata.name = $e' td_duplicate.json |sponge td_duplicate.json
+jq '.spec.template.spec.containers[0].args[1] = "sleep 99999"' td_duplicate.json |sponge td_duplicate.json
+jq 'del(.metadata.creationTimestamp)' td_duplicate.json |sponge td_duplicate.json
+jq 'del(.metadata.generation)' td_duplicate.json |sponge td_duplicate.json
+jq 'del(.metadata.selfLink)' td_duplicate.json |sponge td_duplicate.json
+jq 'del(.metadata.uid)' td_duplicate.json |sponge td_duplicate.json
+
+kubectl apply -f td_duplicate.json
+#rm td_duplicate.yaml
+sed "s/PLACEHOLDER/$COMPONENT-local/" test-suite.yaml | kubectl -n kyma-system apply -f -
+sleep 35
+kubectl exec -n kyma-system oct-tp-compass-e2e-tests-compass-e2e-$COMPONENT-local-0 -c $COMPONENT-tests -- apk add go
+kubectl cp . kyma-system/oct-tp-compass-e2e-tests-compass-e2e-$COMPONENT-local-0:tests -c $COMPONENT-tests
