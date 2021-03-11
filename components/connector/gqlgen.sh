fef4b3b117b3846b509f0a8f28b54dab3691bdfe
StefanCholakov <stcholakov98@gmail.com>
2021-03-11 13:12:09 +0200

    Add GO111MODULE variable in gqlgen scripts

--- components/connector/gqlgen.sh
+++ components/connector/gqlgen.sh
@@ -8,4 +8,4 @@ COMPONENT_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 cd "$(dirname "$0")"
 
 cd ${COMPONENT_DIR}/pkg/graphql/externalschema
-go run ${COMPONENT_DIR}/hack/gqlgen.go --verbose --config ./config.yaml
+GO111MODULE=on go run ${COMPONENT_DIR}/hack/gqlgen.go --verbose --config ./config.yaml
