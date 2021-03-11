c9c22db8c5d3424c2d8062138f6e397cabbee540
Petar Todorov <petar.todorov02@sap.com>
2021-03-11 23:37:24 +0200

    Update connector component version

--- components/schema-migrator/migrations/director/20210311160954_alter_application_status_condition.up.sql
+++ components/schema-migrator/migrations/director/20210311160954_alter_application_status_condition.up.sql
@@ -7,12 +7,21 @@ ALTER TYPE application_status_condition RENAME TO application_status_condition_o
 CREATE TYPE application_status_condition AS ENUM (
     'INITIAL',
     'CONNECTED',
-    'FAILED'
+    'FAILED',
+    'CREATING',
+    'CREATE_FAILED',
+    'CREATE_SUCCEEDED',
+    'UPDATING',
+    'UPDATE_FAILED',
+    'UPDATE_SUCCEEDED',
+    'DELETING',
+    'DELETE_FAILED',
+    'DELETE_SUCCEEDED'
 );
 
 ALTER TABLE applications
-ALTER COLUMN status_condition TYPE application_status_condition
-    USING status_condition::text::application_status_condition;
+    ALTER COLUMN status_condition TYPE application_status_condition
+        USING status_condition::text::application_status_condition;
 
 ALTER TABLE applications
     ALTER COLUMN status_condition
