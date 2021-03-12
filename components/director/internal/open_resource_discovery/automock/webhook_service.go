882d0f155ee0672bc895e87ad027b2e13179d44a
Nikolay Yordanov <n.yordanov@sap.com>
2021-03-12 18:30:55 +0200

    Adapt webhook changes

--- components/director/internal/open_resource_discovery/automock/webhook_service.go
+++ components/director/internal/open_resource_discovery/automock/webhook_service.go
@@ -14,8 +14,8 @@ type WebhookService struct {
 	mock.Mock
 }
 
-// ListAllApplicationWebhooks provides a mock function with given fields: ctx, applicationID
-func (_m *WebhookService) ListAllApplicationWebhooks(ctx context.Context, applicationID string) ([]*model.Webhook, error) {
+// ListForApplication provides a mock function with given fields: ctx, applicationID
+func (_m *WebhookService) ListForApplication(ctx context.Context, applicationID string) ([]*model.Webhook, error) {
 	ret := _m.Called(ctx, applicationID)
 
 	var r0 []*model.Webhook
