b5ea0cb34a03961e2f05b859dd37bbf034615401
Desislava Asenova <desislava.asenova@sap.com>
2021-03-16 13:01:35 +0200

    Add new scopes and directive

--- components/director/pkg/str/strings.go
+++ components/director/pkg/str/strings.go
@@ -61,3 +61,18 @@ func PtrStrToStr(s *string) string {
 	}
 	return *s
 }
+
+func Matches(actual []string, required []string) bool {
+	actMap := make(map[string]interface{})
+
+	for _, a := range actual {
+		actMap[a] = struct{}{}
+	}
+	for _, r := range required {
+		_, ex := actMap[r]
+		if !ex {
+			return false
+		}
+	}
+	return true
+}
