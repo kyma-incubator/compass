3965823d7edd36fefd6949a8e63d8b933be93ba2
Desislava Asenova <desislava.asenova@sap.com>
2021-03-17 08:24:32 +0200

    Add test

--- components/director/pkg/sanitize/directive.go
+++ components/director/pkg/sanitize/directive.go
@@ -4,10 +4,10 @@ import (
 	"context"
 
 	"github.com/99designs/gqlgen/graphql"
-	"github.com/kyma-incubator/compass/components/director/pkg/apperrors"
 	"github.com/kyma-incubator/compass/components/director/pkg/log"
 	"github.com/kyma-incubator/compass/components/director/pkg/scope"
 	"github.com/kyma-incubator/compass/components/director/pkg/str"
+	"github.com/pkg/errors"
 )
 
 //go:generate mockery -name=ScopesGetter -output=automock -outpkg=automock -case=underscore
@@ -30,14 +30,16 @@ func (d *directive) Sanitize(ctx context.Context, obj interface{}, next graphql.
 	if err != nil {
 		return nil, err
 	}
+
 	requiredScopes, err := d.scopesGetter.GetRequiredScopes(scopesDefinition)
 	if err != nil {
-		log.C(ctx).Warnf("Stripping sensitive data from %T", obj)
-		return nil, nil
+		return nil, errors.Wrap(err, "while getting required scopes")
 	}
 
 	if !str.Matches(actualScopes, requiredScopes) {
-		return nil, apperrors.NewInsufficientScopesError(requiredScopes, actualScopes)
+		log.C(ctx).Warnf("Stripping sensitive data from %T ...", obj)
+		return nil, nil
 	}
+
 	return next(ctx)
 }
