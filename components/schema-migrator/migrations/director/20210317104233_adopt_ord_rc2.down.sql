8df5f7dd7fde69d7a1ebd68ad7ab66a023e45766
Nikolay Yordanov <n.yordanov@sap.com>
2021-03-17 16:46:00 +0200

    Delete product field and add new one

--- components/schema-migrator/migrations/director/20210317104233_adopt_ord_rc2.down.sql
+++ components/schema-migrator/migrations/director/20210317104233_adopt_ord_rc2.down.sql
@@ -5,4 +5,10 @@ ALTER TABLE api_definitions
     DROP COLUMN custom_implementation_standard,
     DROP COLUMN custom_implementation_standard_description;
 
+ALTER TABLE products
+    ADD COLUMN sap_ppms_object_id VARCHAR(256);
+
+ALTER TABLE products
+    DROP COLUMN correlation_ids;
+
 COMMIT;
