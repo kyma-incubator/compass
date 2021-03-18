d035515bdf70130b6f6b6aaff381a55c2ebce676
StefanCholakov <stcholakov98@gmail.com>
2021-03-18 15:11:25 +0200

    Added unit tests

--- components/director/cmd/scopessynchronizer/main.go
+++ components/director/cmd/scopessynchronizer/main.go
@@ -2,24 +2,19 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"net/http"
 	"os"
-	"strings"
 
-	"github.com/kyma-incubator/compass/components/director/pkg/str"
-
-	"github.com/kyma-incubator/compass/components/director/internal/domain/oauth20"
-	"github.com/kyma-incubator/compass/components/director/internal/domain/systemauth"
-	"github.com/kyma-incubator/compass/components/director/internal/model"
-	"github.com/kyma-incubator/compass/components/director/internal/uid"
 	configprovider "github.com/kyma-incubator/compass/components/director/pkg/config"
 	"github.com/kyma-incubator/compass/components/director/pkg/correlation"
+
+	"github.com/kyma-incubator/compass/components/director/internal/domain/oauth20"
+	"github.com/kyma-incubator/compass/components/director/internal/scopesync"
+	"github.com/kyma-incubator/compass/components/director/internal/uid"
 	httputil "github.com/kyma-incubator/compass/components/director/pkg/http"
 	"github.com/kyma-incubator/compass/components/director/pkg/log"
 	"github.com/kyma-incubator/compass/components/director/pkg/persistence"
 	"github.com/kyma-incubator/compass/components/director/pkg/signal"
-	"github.com/pkg/errors"
 	"github.com/vrischmann/envconfig"
 )
 
@@ -61,47 +56,13 @@ func main() {
 		exitOnError(ctx, err, "Error while closing the connection to the database")
 	}()
 
-	clientsFromHydra, err := oAuth20Svc.ListClients(ctx)
+	syncService := scopesync.NewService(oAuth20Svc, transact)
+	err = syncService.UpdateClientScopes(ctx)
 	if err != nil {
-		exitOnError(ctx, err, "Error while listing clients from hydra")
+		exitOnError(ctx, err, "Error while updating client scopes")
 	}
-	clientScopes := convertScopesToMap(clientsFromHydra)
-
-	auths := getAuthsForUpdate(ctx, transact)
-	for _, auth := range auths {
-		clientID := auth.Value.Credential.Oauth.ClientID
-
-		objType, err := auth.GetReferenceObjectType()
-		if err != nil {
-			log.C(ctx).WithError(err).Errorf("Error while getting obj type of client with ID %s: %v", clientID, err)
-			continue
-		}
-
-		expectedScopes, err := oAuth20Svc.GetClientCredentialScopes(objType)
-		if err != nil {
-			log.C(ctx).WithError(err).Errorf("Error while getting client credentials scopes for client with ID %s: %v", clientID, err)
-			continue
-		}
-
-		scopesFromHydra := clientScopes[clientID]
-		if str.Matches(scopesFromHydra, expectedScopes) {
-			err = oAuth20Svc.UpdateClientCredentials(ctx, clientID, objType)
-			if err != nil {
-				log.C(ctx).WithError(err).Errorf("Error while getting obj type of client with ID %s: %v", clientID, err)
-			}
-		}
-	}
-
-	log.C(ctx).Info("Finished synchronization of Hydra scopes")
 }
 
-func convertScopesToMap(clientsFromHydra []oauth20.Client) map[string][]string {
-	clientScopes := make(map[string][]string)
-	for _, s := range clientsFromHydra {
-		clientScopes[s.ClientID] = strings.Split(s.Scopes, " ")
-	}
-	return clientScopes
-}
 func exitOnError(ctx context.Context, err error, context string) {
 	if err != nil {
 		log.C(ctx).WithError(err).Error(context)
@@ -119,47 +80,4 @@ func configProvider(ctx context.Context, cfg config) *configprovider.Provider {
 	exitOnError(ctx, provider.Load(), "Error on loading configuration file")
 
 	return provider
-}
-
-func getAuthsForUpdate(ctx context.Context, transact persistence.Transactioner) []model.SystemAuth {
-	tx, err := transact.Begin()
-	if err != nil {
-		exitOnError(ctx, err, "An error occurred while opening database transaction")
-	}
-	defer transact.RollbackUnlessCommitted(ctx, tx)
-
-	auths := listOauthAuths(ctx, tx)
-
-	err = tx.Commit()
-	exitOnError(ctx, err, fmt.Sprintf("An error occurred while closing database transaction: %v", err))
-
-	return auths
-}
-
-func listOauthAuths(ctx context.Context, persist persistence.PersistenceOp) []model.SystemAuth {
-	var dest systemauth.Collection
-
-	query := "select * from system_auths where (value -> 'Credential' -> 'Oauth') is not null"
-	log.D().Debugf("Executing DB query: %s", query)
-	exitOnError(ctx, persist.Select(&dest, query), "Error while getting Oauth system auths")
-
-	auths, err := multipleFromEntities(dest)
-	exitOnError(ctx, err, "Error while converting entities")
-	return auths
-}
-
-func multipleFromEntities(entities systemauth.Collection) ([]model.SystemAuth, error) {
-	conv := systemauth.NewConverter(nil)
-	var items []model.SystemAuth
-
-	for _, ent := range entities {
-		m, err := conv.FromEntity(ent)
-		if err != nil {
-			return nil, errors.Wrap(err, "while creating system auth model from entity")
-		}
-
-		items = append(items, m)
-	}
-
-	return items, nil
 }
---
+++ components/director/internal/scopesync/service.go
@@ -0,0 +1,138 @@
+package scopesync
+
+import (
+	"context"
+	"strings"
+
+	"github.com/kyma-incubator/compass/components/director/internal/domain/oauth20"
+
+	"github.com/kyma-incubator/compass/components/director/internal/domain/systemauth"
+	"github.com/kyma-incubator/compass/components/director/internal/model"
+	"github.com/kyma-incubator/compass/components/director/pkg/log"
+	"github.com/kyma-incubator/compass/components/director/pkg/persistence"
+	"github.com/kyma-incubator/compass/components/director/pkg/str"
+	"github.com/pkg/errors"
+)
+
+type SyncService interface {
+	UpdateClientScopes(context.Context) error
+}
+
+//go:generate mockery -name=OAuthService -output=automock -outpkg=automock -case=underscore
+type OAuthService interface {
+	ListClients(ctx context.Context) ([]oauth20.Client, error)
+	GetClientCredentialScopes(objType model.SystemAuthReferenceObjectType) ([]string, error)
+	UpdateClientCredentials(ctx context.Context, clientID string, objectType model.SystemAuthReferenceObjectType) error
+}
+
+type service struct {
+	oAuth20Svc OAuthService
+	transact   persistence.Transactioner
+}
+
+func NewService(oAuth20Svc OAuthService, transact persistence.Transactioner) *service {
+	return &service{
+		oAuth20Svc: oAuth20Svc,
+		transact:   transact,
+	}
+}
+
+func (s *service) UpdateClientScopes(ctx context.Context) error {
+	clientsFromHydra, err := s.oAuth20Svc.ListClients(ctx)
+	if err != nil {
+		return errors.Wrap(err, "while listing clients from hydra")
+	}
+	clientScopes := convertScopesToMap(clientsFromHydra)
+
+	auths, err := getAuthsForUpdate(ctx, s.transact)
+	if err != nil {
+		return err
+	}
+
+	for _, auth := range auths {
+		clientID := auth.Value.Credential.Oauth.ClientID
+
+		objType, err := auth.GetReferenceObjectType()
+		if err != nil {
+			log.C(ctx).WithError(err).Errorf("Error while getting obj type of client with ID %s: %v", clientID, err)
+			continue
+		}
+
+		expectedScopes, err := s.oAuth20Svc.GetClientCredentialScopes(objType)
+		if err != nil {
+			log.C(ctx).WithError(err).Errorf("Error while getting client credentials scopes for client with ID %s: %v", clientID, err)
+			continue
+		}
+
+		scopesFromHydra := clientScopes[clientID]
+		if !str.Matches(scopesFromHydra, expectedScopes) {
+			err = s.oAuth20Svc.UpdateClientCredentials(ctx, clientID, objType)
+			if err != nil {
+				log.C(ctx).WithError(err).Errorf("Error while getting obj type of client with ID %s: %v", clientID, err)
+			}
+		}
+	}
+
+	log.C(ctx).Info("Finished synchronization of Hydra scopes")
+	return nil
+}
+
+func convertScopesToMap(clientsFromHydra []oauth20.Client) map[string][]string {
+	clientScopes := make(map[string][]string)
+	for _, s := range clientsFromHydra {
+		clientScopes[s.ClientID] = strings.Split(s.Scopes, " ")
+	}
+	return clientScopes
+}
+
+func getAuthsForUpdate(ctx context.Context, transact persistence.Transactioner) ([]model.SystemAuth, error) {
+	tx, err := transact.Begin()
+	if err != nil {
+		return nil, errors.Wrap(err, "while opening database transaction")
+	}
+	defer transact.RollbackUnlessCommitted(ctx, tx)
+
+	auths, err := listOauthAuths(tx)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := tx.Commit(); err != nil {
+		return nil, errors.Wrap(err, "while database transaction commit")
+	}
+
+	return auths, nil
+}
+
+func listOauthAuths(persist persistence.PersistenceOp) ([]model.SystemAuth, error) {
+	var dest systemauth.Collection
+
+	query := "select * from system_auths where (value -> 'Credential' -> 'Oauth') is not null"
+	log.D().Debugf("Executing DB query: %s", query)
+	err := persist.Select(&dest, query)
+	if err != nil {
+		return nil, errors.Wrap(err, "while getting Oauth system auths")
+	}
+
+	auths, err := multipleFromEntities(dest)
+	if err != nil {
+		return nil, errors.Wrap(err, "while converting entities")
+	}
+	return auths, nil
+}
+
+func multipleFromEntities(entities systemauth.Collection) ([]model.SystemAuth, error) {
+	conv := systemauth.NewConverter(nil)
+	var items []model.SystemAuth
+
+	for _, ent := range entities {
+		m, err := conv.FromEntity(ent)
+		if err != nil {
+			return nil, errors.Wrap(err, "while creating system auth model from entity")
+		}
+
+		items = append(items, m)
+	}
+
+	return items, nil
+}
