f14beaa06b733b224d9354de55073045f7c7e0ea
krasish <krasishontov@gmail.com>
2021-03-29 17:49:54 +0300

    Address comments. Fix bug in values.yaml from last commit.

--- tests/istio/tests/request_payload_limit_test.go
+++ tests/istio/tests/request_payload_limit_test.go
@@ -1,7 +1,9 @@
 package tests
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"io/ioutil"
 	"net/http"
 	"strings"
@@ -24,7 +26,8 @@ import (
 )
 
 const (
-	directorPath = "/director/graphql"
+	directorPath  = "/director/graphql"
+	connectorPath = "/connector/graphql"
 )
 
 var (
@@ -34,7 +37,7 @@ var (
 	dexToken string
 )
 
-func TestCallingCompassWithTooBigRequestBody(t *testing.T) {
+func TestCallingCompassGateways(t *testing.T) {
 	var (
 		ctx    = context.TODO()
 		err    error
@@ -45,76 +48,83 @@ func TestCallingCompassWithTooBigRequestBody(t *testing.T) {
 	dexToken, err = idtokenprovider.GetDexToken()
 	require.NoError(t, err)
 
-	t.Run("when request passes through gateway", func(t *testing.T) {
-		t.Log("Compass Gateway URL: ", conf.CompassGatewayURL)
-		authoizedClient := gql.NewAuthorizedHTTPClient(dexToken)
+	authorizedClient := gql.NewAuthorizedHTTPClient(dexToken)
 
-		t.Log("Creating a request with big payload...")
-		bigBodyPOSTRequest, err := getHTTPBigBodyPOSTRequest(conf.CompassGatewayURL+directorPath, conf.RequestPayloadLimit)
-		require.NoError(t, err)
+	dexGraphQLClient := gql.NewAuthorizedGraphQLClient(dexToken)
+	logrus.Infof("Registering runtime with name: %s, within tenant: %s", runtimeInput.Name, tenant)
 
-		bigBodyPOSTRequest.Header.Set("Tenant", tenant)
-		bigBodyPOSTRequest = bigBodyPOSTRequest.WithContext(context.TODO())
+	runtime := fixtures.RegisterRuntimeFromInputWithinTenant(t, ctx, dexGraphQLClient, tenant, runtimeInput)
+	defer fixtures.UnregisterRuntime(t, ctx, dexGraphQLClient, tenant, runtime.ID)
 
-		t.Log("Executing request with big payload...")
-		resp, err := authoizedClient.Do(bigBodyPOSTRequest)
-		require.NoError(t, err)
-		defer idtokenprovider.CloseRespBody(resp)
+	logrus.Infof("Generating one-time token for runtime with id: %s", runtime.ID)
+	runtimeToken := fixtures.RequestOneTimeTokenForRuntime(t, ctx, dexGraphQLClient, tenant, runtime.ID)
+	oneTimeToken := &externalschema.Token{Token: runtimeToken.Token}
 
-		t.Log("Executed request with big payload successfully")
-		require.Equal(t, http.StatusRequestEntityTooLarge, resp.StatusCode)
+	logrus.Info("Generating private key for cert...")
+	clientKey, err := certs.GenerateKey()
+	require.NoError(t, err)
+	tokenSecuredClient := clients.NewTokenSecuredClient(conf.CompassGatewayURL + connectorPath)
 
-		all, err := ioutil.ReadAll(resp.Body)
-		require.NoError(t, err)
-		require.Contains(t, string(all), "Payload Too Large")
-	})
+	logrus.Infof("Generation certificate for runtime with id: %s", runtime.ID)
+	certResult, _ := clients.GenerateRuntimeCertificate(t, oneTimeToken, tokenSecuredClient, clientKey)
+	certChain := certs.DecodeCertChain(t, certResult.CertificateChain)
+	certSecuredClient := authentication.CreateCertClient(clientKey, certChain...)
 
-	t.Run("when request passes through MTLS gateway", func(t *testing.T) {
-		t.Log("Compass MTLS Gateway URL: ", conf.CompassMTLSGatewayURL)
+	tests := []struct {
+		negativeDescription string
+		positiveDescription string
+		client              *http.Client
+		url                 string
+	}{
+		{
+			negativeDescription: "fails when request is too big and passes through gateway",
+			positiveDescription: "succeeds for a regular applications request passing through gateway",
+			url:                 conf.CompassGatewayURL + directorPath,
+			client:              authorizedClient,
+		},
+		{
+			negativeDescription: "fails when request is too big and passes through MTLS gateway",
+			positiveDescription: "succeeds for a regular applications request passing through MTLS gateway",
+			url:                 conf.CompassMTLSGatewayURL + directorPath,
+			client:              certSecuredClient,
+		},
+	}
 
-		dexGraphQLClient := gql.NewAuthorizedGraphQLClient(dexToken)
-		logrus.Infof("Registering runtime with name: %s, within tenant: %s", runtimeInput.Name, tenant)
+	for _, test := range tests {
+		t.Run(test.negativeDescription, func(t *testing.T) {
+			t.Log("Creating a request with big payload...")
+			bigBodyPOSTRequest := getHTTPBigBodyPOSTRequest(t, test.url, tenant, conf.RequestPayloadLimit)
 
-		runtime := fixtures.RegisterRuntimeFromInputWithinTenant(t, ctx, dexGraphQLClient, tenant, runtimeInput)
-		defer fixtures.UnregisterRuntime(t, ctx, dexGraphQLClient, tenant, runtime.ID)
+			t.Log("Executing request with big payload...")
+			resp, err := test.client.Do(bigBodyPOSTRequest)
+			require.NoError(t, err)
+			defer idtokenprovider.CloseRespBody(resp)
+			t.Log("Successfully executed request with big payload")
 
-		logrus.Infof("Generating one-time token for runtime with id: %s", runtime.ID)
-		runtimeToken := fixtures.RequestOneTimeTokenForRuntime(t, ctx, dexGraphQLClient, tenant, runtime.ID)
-		oneTimeToken := &externalschema.Token{Token: runtimeToken.Token}
+			require.Equal(t, http.StatusRequestEntityTooLarge, resp.StatusCode)
 
-		logrus.Info("Generating private key for cert...")
-		clientKey, err := certs.GenerateKey()
-		require.NoError(t, err)
-		tokenSecuredClient := clients.NewTokenSecuredClient(conf.CompassGatewayURL + "/connector/graphql")
+			t.Log("Response checking for error message ...")
+			all, err := ioutil.ReadAll(resp.Body)
+			require.NoError(t, err)
+			require.Contains(t, string(all), "Payload Too Large")
+		})
 
-		logrus.Infof("Generation certificate for runtime with id: %s", runtime.ID)
-		certResult, _ := clients.GenerateRuntimeCertificate(t, oneTimeToken, tokenSecuredClient, clientKey)
-		certChain := certs.DecodeCertChain(t, certResult.CertificateChain)
-		certSecuredClient := authentication.CreateCertClient(clientKey, certChain...)
+		t.Run(test.positiveDescription, func(t *testing.T) {
+			t.Log("Creating a request for applications...")
+			applicationsPOSTRequest := getHTTPApplicationsPOSTRequest(t, test.url, tenant)
 
-		t.Log("Creating a request with big payload...")
-		bigBodyPOSTRequest, err := getHTTPBigBodyPOSTRequest(conf.CompassMTLSGatewayURL+directorPath, conf.RequestPayloadLimit)
-		require.NoError(t, err)
+			t.Log("Executing request for applications...")
+			appsResp, err := test.client.Do(applicationsPOSTRequest)
+			require.NoError(t, err)
+			defer idtokenprovider.CloseRespBody(appsResp)
+			t.Log("Successfully executed request for applications")
 
-		bigBodyPOSTRequest.Header.Set("Tenant", tenant)
-		bigBodyPOSTRequest = bigBodyPOSTRequest.WithContext(context.TODO())
-
-		t.Log("Executing request with big payload...")
-		resp, err := certSecuredClient.Do(bigBodyPOSTRequest)
-		require.NoError(t, err)
-		defer idtokenprovider.CloseRespBody(resp)
-
-		t.Log("Executed request with big payload successfully")
-		require.Equal(t, http.StatusRequestEntityTooLarge, resp.StatusCode)
-
-		all, err := ioutil.ReadAll(resp.Body)
-		require.NoError(t, err)
-		require.Contains(t, string(all), "Payload Too Large")
-
-	})
+			require.Equal(t, http.StatusOK, appsResp.StatusCode)
+		})
+	}
 }
 
-func getHTTPBigBodyPOSTRequest(url string, bodySize int) (*http.Request, error) {
+func getHTTPBigBodyPOSTRequest(t *testing.T, url, tenant string, bodySize int) *http.Request {
 	var b strings.Builder
 	b.Grow(bodySize)
 	for i := 0; i < bodySize; i++ {
@@ -123,5 +133,34 @@ func getHTTPBigBodyPOSTRequest(url string, bodySize int) (*http.Request, error) 
 	s := b.String()
 	applicationRequest := fixtures.FixGetApplicationRequest(s)
 	reader := strings.NewReader(applicationRequest.Query())
-	return http.NewRequest(http.MethodPost, url, reader)
+	req, err := http.NewRequest(http.MethodPost, url, reader)
+	require.NoError(t, err)
+
+	req.Header.Set("Tenant", tenant)
+	req = req.WithContext(context.TODO())
+	return req
+}
+
+func getHTTPApplicationsPOSTRequest(t *testing.T, url, tenant string) *http.Request {
+	applicationsGQLRequest := fixtures.FixGetApplicationsRequestWithPagination()
+	requestBodyObj := struct {
+		Query     string                 `json:"query"`
+		Variables map[string]interface{} `json:"variables"`
+	}{
+		Query:     applicationsGQLRequest.Query(),
+		Variables: applicationsGQLRequest.Vars(),
+	}
+
+	var requestBuffer bytes.Buffer
+	err := json.NewEncoder(&requestBuffer).Encode(requestBodyObj)
+	require.NoError(t, err)
+
+	req, err := http.NewRequest(http.MethodPost, url, &requestBuffer)
+	require.NoError(t, err)
+
+	req.Header.Set("Tenant", tenant)
+	req.Header.Set("Content-Type", "application/json; charset=utf-8")
+	req.Header.Set("Accept", "application/json; charset=utf-8")
+	req = req.WithContext(context.TODO())
+	return req
 }
