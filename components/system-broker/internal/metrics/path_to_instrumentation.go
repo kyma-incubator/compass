28daea70c522c3cfe1f4a2f94a7ab62dcf27f591
StanislavStefanov <stanislavsvetlinovstefanov@gmail.com>
2021-04-15 18:11:55 +0300

    create path to instrumentation mapping

---
+++ components/system-broker/internal/metrics/path_to_instrumentation.go
@@ -0,0 +1,41 @@
+package metrics
+
+import "net/http"
+
+type Path struct {
+	PathTemplate string
+	HTTPMethods  string
+}
+
+var PathToInstrumentationMapping = map[Path]func(c *Collector, handler http.Handler) http.HandlerFunc{
+	Path{
+		PathTemplate: "/v2/catalog",
+		HTTPMethods:  "GET",
+	}: func(c *Collector, handler http.Handler) http.HandlerFunc {
+		return c.CatalogHandlerWithInstrumentation(handler)
+	},
+	Path{
+		PathTemplate: "/v2/service_instances/{instance_id}",
+		HTTPMethods:  "PUT",
+	}: func(c *Collector, handler http.Handler) http.HandlerFunc {
+		return c.ProvisionHandlerWithInstrumentation(handler)
+	},
+	Path{
+		PathTemplate: "/v2/service_instances/{instance_id}",
+		HTTPMethods:  "DELETE",
+	}: func(c *Collector, handler http.Handler) http.HandlerFunc {
+		return c.DeprovosionHandlerWithInstrumentation(handler)
+	},
+	Path{
+		PathTemplate: "/v2/service_instances/{instance_id}/service_bindings/{binding_id}",
+		HTTPMethods:  "PUT",
+	}: func(c *Collector, handler http.Handler) http.HandlerFunc {
+		return c.BindHandlerWithInstrumentation(handler)
+	},
+	Path{
+		PathTemplate: "/v2/service_instances/{instance_id}/service_bindings/{binding_id}",
+		HTTPMethods:  "DELETE",
+	}: func(c *Collector, handler http.Handler) http.HandlerFunc {
+		return c.UnbindHandlerWithInstrumentation(handler)
+	},
+}
