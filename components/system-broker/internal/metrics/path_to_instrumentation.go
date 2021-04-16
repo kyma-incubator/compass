48e7c703ab1f4313e2571231ae29b684d06be934
StanislavStefanov <stanislavsvetlinovstefanov@gmail.com>
2021-04-16 17:32:44 +0300

    fix path to instrumentation mapping

--- components/system-broker/internal/metrics/path_to_instrumentation.go
+++ components/system-broker/internal/metrics/path_to_instrumentation.go
@@ -7,35 +7,39 @@ type Path struct {
 	HTTPMethods  string
 }
 
-var PathToInstrumentationMapping = map[Path]func(c *Collector, handler http.Handler) http.HandlerFunc{
-	Path{
-		PathTemplate: "/v2/catalog",
-		HTTPMethods:  "GET",
-	}: func(c *Collector, handler http.Handler) http.HandlerFunc {
-		return c.CatalogHandlerWithInstrumentation(handler)
-	},
-	Path{
-		PathTemplate: "/v2/service_instances/{instance_id}",
-		HTTPMethods:  "PUT",
-	}: func(c *Collector, handler http.Handler) http.HandlerFunc {
-		return c.ProvisionHandlerWithInstrumentation(handler)
-	},
-	Path{
-		PathTemplate: "/v2/service_instances/{instance_id}",
-		HTTPMethods:  "DELETE",
-	}: func(c *Collector, handler http.Handler) http.HandlerFunc {
-		return c.DeprovosionHandlerWithInstrumentation(handler)
-	},
-	Path{
-		PathTemplate: "/v2/service_instances/{instance_id}/service_bindings/{binding_id}",
-		HTTPMethods:  "PUT",
-	}: func(c *Collector, handler http.Handler) http.HandlerFunc {
-		return c.BindHandlerWithInstrumentation(handler)
-	},
-	Path{
-		PathTemplate: "/v2/service_instances/{instance_id}/service_bindings/{binding_id}",
-		HTTPMethods:  "DELETE",
-	}: func(c *Collector, handler http.Handler) http.HandlerFunc {
-		return c.UnbindHandlerWithInstrumentation(handler)
-	},
+var PathToInstrumentationMapping map[Path]func(c *Collector, handler http.Handler) http.HandlerFunc
+
+func MapPathToInstrumentation(rootAPI string) {
+	PathToInstrumentationMapping = map[Path]func(c *Collector, handler http.Handler) http.HandlerFunc{
+		Path{
+			PathTemplate: rootAPI + "/v2/catalog",
+			HTTPMethods:  "GET",
+		}: func(c *Collector, handler http.Handler) http.HandlerFunc {
+			return c.CatalogHandlerWithInstrumentation(handler)
+		},
+		Path{
+			PathTemplate: rootAPI + "/v2/service_instances/{instance_id}",
+			HTTPMethods:  "PUT",
+		}: func(c *Collector, handler http.Handler) http.HandlerFunc {
+			return c.ProvisionHandlerWithInstrumentation(handler)
+		},
+		Path{
+			PathTemplate: rootAPI + "/v2/service_instances/{instance_id}",
+			HTTPMethods:  "DELETE",
+		}: func(c *Collector, handler http.Handler) http.HandlerFunc {
+			return c.DeprovosionHandlerWithInstrumentation(handler)
+		},
+		Path{
+			PathTemplate: rootAPI + "/v2/service_instances/{instance_id}/service_bindings/{binding_id}",
+			HTTPMethods:  "PUT",
+		}: func(c *Collector, handler http.Handler) http.HandlerFunc {
+			return c.BindHandlerWithInstrumentation(handler)
+		},
+		Path{
+			PathTemplate: rootAPI + "/v2/service_instances/{instance_id}/service_bindings/{binding_id}",
+			HTTPMethods:  "DELETE",
+		}: func(c *Collector, handler http.Handler) http.HandlerFunc {
+			return c.UnbindHandlerWithInstrumentation(handler)
+		},
+	}
 }
