4b3b38df4682bc576894a519d7b7c6a05554f963
Daniel Gospodinov <daniel.gospodinov@sap.com>
2021-04-27 00:28:00 +0300

    Add missing transaction commits and refactor

--- components/director/internal/systemfetcher/service.go
+++ components/director/internal/systemfetcher/service.go
@@ -2,8 +2,9 @@ package systemfetcher
 
 import (
 	"context"
+	"fmt"
 
-	"github.com/kyma-incubator/compass/components/director/internal/domain/tenant"
+	tenantutil "github.com/kyma-incubator/compass/components/director/internal/domain/tenant"
 	"github.com/kyma-incubator/compass/components/director/internal/model"
 	"github.com/kyma-incubator/compass/components/director/pkg/persistence"
 	"github.com/pkg/errors"
@@ -43,63 +44,95 @@ func NewSystemFetcher(tx persistence.Transactioner, ts TenantService, ss Systems
 
 func (s *SystemFetcher) SyncSystems(ctx context.Context) error {
 	//TODO: Open transact here instead? So that all DB calls are in one transaction - avoid phantom DB stuff, but there's a problem that we have HTTP calls inside of the DB call
-
-	tx, err := s.transaction.Begin()
-	ctx = persistence.SaveToContext(ctx, tx)
+	tenants, err := s.listTenants(ctx)
 	if err != nil {
-		return errors.Wrap(err, "failed to begin transaction")
+		return err
 	}
-
-	tenants, err := s.tenantService.List(ctx)
-	if err != nil {
-		return errors.Wrap(err, "failed to retrieve tenants")
-	}
-
-	s.transaction.RollbackUnlessCommitted(ctx, tx)
 
 	for _, t := range tenants {
 		systems := s.systemsAPIClient.FetchSystemsForTenant(ctx, t.ExternalTenant)
 
-		var appInputs []model.ApplicationRegisterInput
-		for _, s := range systems {
-			sc := s
-
-			initStatusCond := model.ApplicationStatusConditionInitial
-			baseURL := sc.BaseURL
-			if mainURL, ok := sc.AdditionalUrls["mainUrl"]; ok {
-				baseURL = mainURL
-			}
-
-			a := model.ApplicationRegisterInput{
-				Name:                sc.DisplayName,
-				Description:         &sc.ProductDescription,
-				BaseURL:             &baseURL,
-				ProviderName:        &sc.InfrastructureProvider,
-				StatusCondition:     &initStatusCond,
-				Labels:              nil,
-				HealthCheckURL:      nil,
-				IntegrationSystemID: nil,
-				OrdLabels:           nil,
-				Bundles:             nil,
-				Webhooks:            nil,
-			}
-
-			appInputs = append(appInputs, a)
-		}
-
-		tx, err := s.transaction.Begin()
+		err := s.saveSystemsForTenant(ctx, t, systems)
 		if err != nil {
-			return errors.Wrap(err, "failed to begin transaction")
+			return err
 		}
-
-		ctx := tenant.SaveToContext(ctx, t.ID, t.ExternalTenant)
-		ctx = persistence.SaveToContext(ctx, tx)
-		err = s.systemsService.CreateManyIfNotExists(ctx, appInputs)
-		if err != nil {
-			return errors.Wrap(err, "failed to create applications")
-		}
-
-		s.transaction.RollbackUnlessCommitted(ctx, tx)
 	}
+
 	return nil
+}
+
+func (s *SystemFetcher) saveSystemsForTenant(ctx context.Context, tenant *model.BusinessTenantMapping, systems []ProductInstanceExtended) error {
+	var appInputs []model.ApplicationRegisterInput
+	for _, sys := range systems {
+		system := sys
+
+		appInput := s.convertSystemToAppRegisterInput(system)
+		appInputs = append(appInputs, appInput)
+	}
+
+	tx, err := s.transaction.Begin()
+	if err != nil {
+		return errors.Wrap(err, "failed to begin transaction")
+	}
+	defer s.transaction.RollbackUnlessCommitted(ctx, tx)
+
+	ctx = tenantutil.SaveToContext(ctx, tenant.ID, tenant.ExternalTenant)
+	ctx = persistence.SaveToContext(ctx, tx)
+	err = s.systemsService.CreateManyIfNotExists(ctx, appInputs)
+	if err != nil {
+		return errors.Wrap(err, "failed to create applications")
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		return errors.Wrap(err, fmt.Sprintf("failed to commit applications for tenant %s", tenant.ExternalTenant))
+	}
+
+	return nil
+}
+
+func (s *SystemFetcher) listTenants(ctx context.Context) ([]*model.BusinessTenantMapping, error) {
+	tx, err := s.transaction.Begin()
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to begin transaction")
+	}
+	defer s.transaction.RollbackUnlessCommitted(ctx, tx)
+
+	ctx = persistence.SaveToContext(ctx, tx)
+
+	tenants, err := s.tenantService.List(ctx)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to retrieve tenants")
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to commit while reading tenants")
+	}
+
+	return tenants, nil
+}
+
+func (s *SystemFetcher) convertSystemToAppRegisterInput(sc ProductInstanceExtended) model.ApplicationRegisterInput {
+	initStatusCond := model.ApplicationStatusConditionInitial
+	baseURL := sc.BaseURL
+	if mainURL, ok := sc.AdditionalUrls["mainUrl"]; ok {
+		baseURL = mainURL
+	}
+
+	appRegisterInput := model.ApplicationRegisterInput{
+		Name:                sc.DisplayName,
+		Description:         &sc.ProductDescription,
+		BaseURL:             &baseURL,
+		ProviderName:        &sc.InfrastructureProvider,
+		StatusCondition:     &initStatusCond,
+		Labels:              nil,
+		HealthCheckURL:      nil,
+		IntegrationSystemID: nil,
+		OrdLabels:           nil,
+		Bundles:             nil,
+		Webhooks:            nil,
+	}
+
+	return appRegisterInput
 }
