84fac9edb366b4d9cd6d6be4162dc5e30fad8aec
StefanCholakov <stcholakov98@gmail.com>
2021-06-04 11:40:54 +0300

    Fix imports

--- tests/pkg/fixtures/bundle_instance_auth_queries.go
+++ tests/pkg/fixtures/bundle_instance_auth_queries.go
@@ -2,11 +2,12 @@ package fixtures
 
 import (
 	"context"
+	"testing"
+
 	"github.com/kyma-incubator/compass/components/director/pkg/graphql"
 	"github.com/kyma-incubator/compass/tests/pkg/testctx"
 	gcli "github.com/machinebox/graphql"
 	"github.com/stretchr/testify/require"
-	"testing"
 )
 
 func DeleteBundleInstanceAuth(t *testing.T, ctx context.Context, gqlClient *gcli.Client, tenant, id string) graphql.BundleInstanceAuth {
