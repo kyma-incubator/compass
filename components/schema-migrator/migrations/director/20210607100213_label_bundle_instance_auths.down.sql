779ab77e5a39b7abbff8f76573e25c06b282ecc1
Karaimin <aleyidin.karaimin@sap.com>
2021-06-07 10:04:09 +0300

    bump db migrations timestamp

--- components/schema-migrator/migrations/director/20210607100213_label_bundle_instance_auths.down.sql
+++ components/schema-migrator/migrations/director/20210607100213_label_bundle_instance_auths.down.sql
@@ -4,7 +4,7 @@ DROP INDEX IF EXISTS labels_default_values;
 CREATE UNIQUE INDEX ON labels (tenant_id, key, coalesce(app_id, '00000000-0000-0000-0000-000000000000'), coalesce(runtime_id, '00000000-0000-0000-0000-000000000000'), coalesce(runtime_context_id, '00000000-0000-0000-0000-000000000000'));
 
 ALTER TABLE labels
-    DROP CONSTRAINT valid_refs;
+DROP CONSTRAINT valid_refs;
 
 ALTER TABLE labels
     ADD CONSTRAINT valid_refs
--- components/schema-migrator/migrations/director/20210607100213_label_bundle_instance_auths.up.sql
+++ components/schema-migrator/migrations/director/20210607100213_label_bundle_instance_auths.up.sql
@@ -6,16 +6,16 @@ DROP INDEX IF EXISTS labels_tenant_id_key_coalesce_coalesce1_coalesce2_idx;
 CREATE UNIQUE INDEX labels_default_values ON labels (tenant_id, key, coalesce(app_id, '00000000-0000-0000-0000-000000000000'), coalesce(runtime_id, '00000000-0000-0000-0000-000000000000'), coalesce(runtime_context_id, '00000000-0000-0000-0000-000000000000'), coalesce(bundle_instance_auth_id, '00000000-0000-0000-0000-000000000000'));
 
 ALTER TABLE labels
-    DROP CONSTRAINT valid_refs;
+DROP CONSTRAINT valid_refs;
 
 ALTER TABLE labels
     ADD CONSTRAINT valid_refs
         CHECK (app_id IS NOT NULL OR runtime_id IS NOT NULL OR runtime_context_id IS NOT NULL OR bundle_instance_auth_id IS NOT NULL);
 
 CREATE VIEW bundle_instance_auths_with_labels AS
-    SELECT labels.key, labels.value, labels.tenant_id, labels.id as label_id, bundles.id as bundle_id, bundles.app_id, bundle_instance_auths.runtime_id, bundle_instance_auths.id as bundle_instance_auth_id, bundle_instance_auths.status_condition
-    FROM labels
-    INNER JOIN bundle_instance_auths ON labels.bundle_instance_auth_id = bundle_instance_auths.id
-    INNER JOIN bundles ON bundles.id = bundle_instance_auths.bundle_id;
+SELECT labels.key, labels.value, labels.tenant_id, labels.id as label_id, bundles.id as bundle_id, bundles.app_id, bundle_instance_auths.runtime_id, bundle_instance_auths.id as bundle_instance_auth_id, bundle_instance_auths.status_condition
+FROM labels
+         INNER JOIN bundle_instance_auths ON labels.bundle_instance_auth_id = bundle_instance_auths.id
+         INNER JOIN bundles ON bundles.id = bundle_instance_auths.bundle_id;
 
 COMMIT;
