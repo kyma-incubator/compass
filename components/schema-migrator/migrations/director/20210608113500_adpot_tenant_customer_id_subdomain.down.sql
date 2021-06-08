409985e956af88d6c8ddbd22f6f753aeeb1ff6b1
Daniel Gospodinov <daniel.gospodinov@sap.com>
2021-06-08 14:40:21 +0300

    Fix typo in migrations

--- components/schema-migrator/migrations/director/20210608113500_adpot_tenant_customer_id_subdomain.down.sql
+++ components/schema-migrator/migrations/director/20210608113500_adpot_tenant_customer_id_subdomain.down.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-ALTER TABLE business_tenant_mappings DROP COLUMN customerId;
+ALTER TABLE business_tenant_mappings DROP COLUMN customer_id;
 ALTER TABLE business_tenant_mappings DROP COLUMN subdomain;
 
 COMMIT;
--- components/schema-migrator/migrations/director/20210608113500_adpot_tenant_customer_id_subdomain.up.sql
+++ components/schema-migrator/migrations/director/20210608113500_adpot_tenant_customer_id_subdomain.up.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-ALTER TABLE business_tenant_mappings ADD COLUMN customerId VARCHAR(256);
+ALTER TABLE business_tenant_mappings ADD COLUMN customer_id VARCHAR(256);
 ALTER TABLE business_tenant_mappings ADD COLUMN subdomain VARCHAR(256);
 
 COMMIT;
