1a394c6d530cadafc021d87f8cc6b8d625b01b11
Daniel Gospodinov <daniel.gospodinov@sap.com>
2021-06-09 14:48:52 +0300

    Modify unique key in business_tenant_mappings

--- components/schema-migrator/migrations/director/20210608113500_adpot_tenant_customer_id_subdomain.down.sql
+++ components/schema-migrator/migrations/director/20210608113500_adpot_tenant_customer_id_subdomain.down.sql
@@ -1,5 +1,8 @@
 BEGIN;
 
+ALTER TABLE business_tenant_mappings DROP CONSTRAINT business_tenant_mappings_external_tenant_customer_id_unique;
+ALTER TABLE business_tenant_mappings ADD CONSTRAINT business_tenant_mappings_external_tenant_unique UNIQUE (external_tenant);
+
 ALTER TABLE business_tenant_mappings DROP COLUMN customer_id;
 ALTER TABLE business_tenant_mappings DROP COLUMN subdomain;
 
--- components/schema-migrator/migrations/director/20210608113500_adpot_tenant_customer_id_subdomain.up.sql
+++ components/schema-migrator/migrations/director/20210608113500_adpot_tenant_customer_id_subdomain.up.sql
@@ -3,4 +3,7 @@ BEGIN;
 ALTER TABLE business_tenant_mappings ADD COLUMN customer_id VARCHAR(256);
 ALTER TABLE business_tenant_mappings ADD COLUMN subdomain VARCHAR(256);
 
+ALTER TABLE business_tenant_mappings DROP CONSTRAINT business_tenant_mappings_external_tenant_unique;
+ALTER TABLE business_tenant_mappings ADD CONSTRAINT business_tenant_mappings_external_tenant_customer_id_unique UNIQUE (external_tenant, customer_id);
+
 COMMIT;
