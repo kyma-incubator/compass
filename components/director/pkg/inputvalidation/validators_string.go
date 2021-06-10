32fa0ec886f5f286ba0e85ea3e033721472eb6af
Desislava Asenova <desislava.asenova@sap.com>
2021-06-10 17:40:07 +0300

    Remove unnecessary function

--- components/director/pkg/inputvalidation/validators_string.go
+++ components/director/pkg/inputvalidation/validators_string.go
@@ -1,9 +1,7 @@
 package inputvalidation
 
 import (
-	"fmt"
 	"reflect"
-	"regexp"
 
 	"github.com/pkg/errors"
 	k8svalidation "k8s.io/apimachinery/pkg/api/validation"
@@ -11,13 +9,9 @@ import (
 
 var (
 	DNSName     = &dnsNameRule{}
-	RuntimeName = &runtimeNameRule{}
-
-	runtimeNameRgx = regexp.MustCompile(`^[a-zA-Z0-9-._]+$`).MatchString
 )
 
 type dnsNameRule struct{}
-type runtimeNameRule struct{}
 
 func (v *dnsNameRule) Validate(value interface{}) error {
 	s, isNil, err := ensureIsString(value)
@@ -37,25 +31,6 @@ func (v *dnsNameRule) Validate(value interface{}) error {
 	if errorMsg := k8svalidation.NameIsDNSSubdomain(s, false); errorMsg != nil {
 		return errors.Errorf("%v", errorMsg)
 	}
-	return nil
-}
-
-func (v *runtimeNameRule) Validate(value interface{}) error {
-	s, isNil, err := ensureIsString(value)
-	if err != nil {
-		return err
-	}
-	if isNil {
-		return nil
-	}
-	if len(s) > 36 {
-		return errors.New("must be no more than 36 characters")
-	}
-
-	if !runtimeNameRgx(fmt.Sprintf("%v", value)) {
-		return errors.New("must contain only alpha-numeric characters, \".\", \"_\" or \"-\"")
-	}
-
 	return nil
 }
 
