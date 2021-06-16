295212c542e7bdf47c54d958a93bcfb4b9156871
StefanCholakov <stcholakov98@gmail.com>
2021-06-16 12:19:35 +0300

    Refactor DB migrations

--- components/schema-migrator/migrations/director/20210607100213_label_bundle_instance_auths.down.sql
+++ components/schema-migrator/migrations/director/20210607100213_label_bundle_instance_auths.down.sql
@@ -1,10 +1,12 @@
 BEGIN;
 
+DELETE FROM labels WHERE bundle_instance_auth_id IS NOT NULL;
+
 DROP INDEX IF EXISTS labels_default_values;
-CREATE UNIQUE INDEX ON labels (tenant_id, key, coalesce(app_id, '00000000-0000-0000-0000-000000000000'), coalesce(runtime_id, '00000000-0000-0000-0000-000000000000'), coalesce(runtime_context_id, '00000000-0000-0000-0000-000000000000'));
+CREATE UNIQUE INDEX ON labels (tenant_id, key, coalesce(app_id, '00000000-0000-0000-0000-000000000000'), coalesce(runtime_id, '00000000-0000-0000-0000-000000000000'), coalesce(labels.runtime_context_id, '00000000-0000-0000-0000-000000000000'));
 
 ALTER TABLE labels
-DROP CONSTRAINT valid_refs;
+    DROP CONSTRAINT valid_refs;
 
 ALTER TABLE labels
     ADD CONSTRAINT valid_refs
@@ -12,7 +14,7 @@ ALTER TABLE labels
 
 DROP VIEW bundle_instance_auths_scenarios_labels;
 
-DELETE FROM labels WHERE bundle_instance_auth_id IS NOT NULL;
+
 ALTER TABLE labels DROP COLUMN bundle_instance_auth_id;
 
 COMMIT;
