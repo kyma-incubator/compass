87de7c7eda93fe6d56bcf075b23eaaf1d74d10da
Angel Aleksandrov <angel.aleksandrov@sap.com>
2021-06-30 12:39:33 +0300

    Async health checks (#1914)
    * Add async health checks
    
    * Change liveness endpoint
    
    * Adjust default config
    
    * Bump version
    
    * Add quotes
    
    * Improve comments
    
    * Fix quotes
    
    * Revert director health url
    
    * Apply comments
    
    * Make test more resilient

--- components/director/internal/healthz/handler_test.go
+++ components/director/internal/healthz/handler_test.go
@@ -1,48 +1,41 @@
-package healthz
+package healthz_test
 
 import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
-	"github.com/kyma-incubator/compass/components/director/internal/healthz/automock"
-	"github.com/pkg/errors"
+	"github.com/kyma-incubator/compass/components/director/internal/healthz"
 
 	"github.com/stretchr/testify/require"
 )
 
-func TestNewHTTPHandler(t *testing.T) {
+func TestNewLivenessHandler(t *testing.T) {
 	t.Run("should return 200 with ok inside response body", func(t *testing.T) {
 		// GIVEN
-		mockPinger := &automock.Pinger{}
-		defer mockPinger.AssertExpectations(t)
-		req, err := http.NewRequest("GET", "/healthz", nil)
+		req, err := http.NewRequest("GET", "/livez", nil)
 		require.NoError(t, err)
-		mockPinger.On("PingContext", req.Context()).Return(nil)
-
 		rr := httptest.NewRecorder()
-		handler := http.HandlerFunc(NewLivenessHandler(mockPinger))
+		handler := http.HandlerFunc(healthz.NewLivenessHandler())
+
 		// WHEN
 		handler.ServeHTTP(rr, req)
 		// THEN
 		require.Equal(t, http.StatusOK, rr.Code)
-		require.Equal(t, "ok", rr.Body.String())
 	})
+}
 
-	t.Run("should return 500", func(t *testing.T) {
+func TestNewReadinessHandler(t *testing.T) {
+	t.Run("should return 200 with ok inside response body", func(t *testing.T) {
 		// GIVEN
-		mockPinger := &automock.Pinger{}
-		defer mockPinger.AssertExpectations(t)
-		req, err := http.NewRequest("GET", "/healthz", nil)
+		req, err := http.NewRequest("GET", "/readyz", nil)
 		require.NoError(t, err)
-		mockPinger.On("PingContext", req.Context()).Return(errors.New("some error"))
-
 		rr := httptest.NewRecorder()
-		handler := http.HandlerFunc(NewLivenessHandler(mockPinger))
+		handler := http.HandlerFunc(healthz.NewReadinessHandler())
+
 		// WHEN
 		handler.ServeHTTP(rr, req)
 		// THEN
-		require.Equal(t, http.StatusInternalServerError, rr.Code)
+		require.Equal(t, http.StatusOK, rr.Code)
 	})
-
 }
