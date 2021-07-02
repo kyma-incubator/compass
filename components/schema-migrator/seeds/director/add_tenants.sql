db93dc4f77569c49536182fda876145cbe661acb
Dimitar Petrov <buldozer9993@gmail.com>
2021-07-02 10:57:34 +0300

    Fix local director run (#1934)
    * Fix local director run
    
    * Bump PR version

--- components/schema-migrator/seeds/director/add_tenants.sql
+++ components/schema-migrator/seeds/director/add_tenants.sql
@@ -1,6 +1,6 @@
-INSERT INTO public.business_tenant_mappings (id, external_name, external_tenant, provider_name, status) VALUES
-('3e64ebae-38b5-46a0-b1ed-9ccee153a0ae', 'default', '3e64ebae-38b5-46a0-b1ed-9ccee153a0ae', 'Compass', 'Active'),
-('1eba80dd-8ff6-54ee-be4d-77944d17b10b', 'foo', '1eba80dd-8ff6-54ee-be4d-77944d17b10b', 'Compass', 'Active'),
-('af9f84a9-1d3a-4d9f-ae0c-94f883b33b6e', 'bar', 'af9f84a9-1d3a-4d9f-ae0c-94f883b33b6e', 'Compass', 'Active');
+INSERT INTO public.business_tenant_mappings (id, external_name, external_tenant, provider_name, status, type) VALUES
+('3e64ebae-38b5-46a0-b1ed-9ccee153a0ae', 'default', '3e64ebae-38b5-46a0-b1ed-9ccee153a0ae', 'Compass', 'Active', 'account'),
+('1eba80dd-8ff6-54ee-be4d-77944d17b10b', 'foo', '1eba80dd-8ff6-54ee-be4d-77944d17b10b', 'Compass', 'Active', 'account'),
+('af9f84a9-1d3a-4d9f-ae0c-94f883b33b6e', 'bar', 'af9f84a9-1d3a-4d9f-ae0c-94f883b33b6e', 'Compass', 'Active', 'account');
 
 
