686e67dbc1bb7a3b41b410a17a7ae498e8e5cf9f
Angel Aleksandrov <angel.aleksandrov@sap.com>
2021-08-02 10:20:36 +0300

    Add distinct in SA flow

--- components/schema-migrator/migrations/director/20210730100823_add_tenant_mapping_views.up.sql
+++ components/schema-migrator/migrations/director/20210730100823_add_tenant_mapping_views.up.sql
@@ -12,7 +12,7 @@ FROM applications AS apps
     SELECT a.id, t.parent as tenant_id FROM applications AS a
         INNER JOIN  business_tenant_mappings AS t ON t.id = a.tenant_id WHERE t.parent IS NOT NULL
     UNION ALL
-    SELECT l.app_id as id, asa.selector_value::uuid as tenant_id FROM labels AS l
+    SELECT DISTINCT l.app_id as id, asa.selector_value::uuid as tenant_id FROM labels AS l
         INNER JOIN automatic_scenario_assignments AS asa ON asa.tenant_id = l.tenant_id AND l.value ? asa.scenario AND asa.selector_key='global_subaccount_id'
     WHERE l.app_id IS NOT NULL AND l.key = 'scenarios') AS t_apps ON apps.id = t_apps.id;
 
@@ -32,7 +32,7 @@ FROM bundles AS b
         INNER JOIN  business_tenant_mappings AS t ON t.id = a.tenant_id WHERE t.parent IS NOT NULL
     UNION ALL
 --  select SA
-    SELECT l.app_id AS id, asa.selector_value::uuid AS tenant_id FROM labels AS l
+    SELECT DISTINCT l.app_id AS id, asa.selector_value::uuid AS tenant_id FROM labels AS l
         INNER JOIN automatic_scenario_assignments AS asa ON asa.tenant_id = l.tenant_id AND l.value ? asa.scenario AND asa.selector_key='global_subaccount_id'
     WHERE l.app_id IS NOT NULL AND l.key = 'scenarios') AS t_apps ON b.app_id = t_apps.id;
 
@@ -57,7 +57,7 @@ FROM api_definitions AS apis
         INNER JOIN  business_tenant_mappings AS t ON t.id = a.tenant_id WHERE t.parent IS NOT NULL
     UNION ALL
 --  select SA
-    SELECT l.app_id as id, asa.selector_value::uuid as tenant_id FROM labels AS l
+    SELECT DISTINCT l.app_id as id, asa.selector_value::uuid as tenant_id FROM labels AS l
         INNER JOIN automatic_scenario_assignments AS asa ON asa.tenant_id = l.tenant_id AND l.value ? asa.scenario AND asa.selector_key='global_subaccount_id'
     WHERE l.app_id IS NOT NULL AND l.key = 'scenarios') AS t_apps ON apis.app_id = t_apps.id;
 
@@ -80,7 +80,7 @@ FROM event_api_definitions AS events
         INNER JOIN  business_tenant_mappings AS t ON t.id = a.tenant_id WHERE t.parent IS NOT NULL
     UNION ALL
 --  select SA
-    SELECT l.app_id as id, asa.selector_value::uuid as tenant_id FROM labels AS l
+    SELECT DISTINCT l.app_id as id, asa.selector_value::uuid as tenant_id FROM labels AS l
         INNER JOIN automatic_scenario_assignments AS asa ON asa.tenant_id = l.tenant_id AND l.value ? asa.scenario AND asa.selector_key='global_subaccount_id'
     WHERE l.app_id IS NOT NULL AND l.key = 'scenarios') AS t_apps ON events.app_id = t_apps.id;
 
