84974c7fdf75cb4bf625382e07bb2a41865f9a1e
StanislavStefanov <stanislavsvetlinovstefanov@gmail.com>
2021-08-03 11:57:37 +0300

    rename resources, modify job args

--- components/schema-migrator/update-expected-schema-version.sh
+++ components/schema-migrator/update-expected-schema-version.sh
@@ -3,7 +3,7 @@ MIGRATION_PATH="${1}"
 CONFIGMAP_NAME="${2}"
 LATEST_SCHEMA=$(ls -lr migrations/${MIGRATION_PATH} | head -n 2 | tail -n 1 | tr -s ' ' | cut -d ' ' -f9 | cut -d '_' -f1)
 
-kubectl get -n compass-system configmap ${CONFIGMAP_NAME} -o json > temp_configmap.json
-jq --arg e "$LATEST_SCHEMA"  '.data.schemaVersion = $e' temp_configmap.json | sponge temp_configmap.json
+kubectl get -n compass-system configmap ${CONFIGMAP_NAME} -o json | \
+jq --arg e "$LATEST_SCHEMA"  '.data.schemaVersion = $e'  > temp_configmap.json
 kubectl apply -f temp_configmap.json
 rm temp_configmap.json
