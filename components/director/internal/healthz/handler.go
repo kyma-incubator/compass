ffe41dcdb457570491ef67a1725a8ebaacf4d954
StanislavStefanov <39959090+StanislavStefanov@users.noreply.github.com>
2021-08-12 10:09:24 +0300

    Readiness probes (#1966)
    * add schema repo
    
    * modify readinessHandler to perform db ping and shcema compatibility check
    
    * adapt charts
    
    * adjust PR reference numbers
    
    * refactor Ready
    
    * add test cases for ReadinessHandler
    
    * format imports
    
    * fix deployments
    
    * open transaction when getting version from db
    
    * execute migration job paralel on installation
    
    * make imports
    
    * make imports
    
    * adjust migration version
    
    * commit transaction
    
    * add job to upadate expected schema version
    
    * adjust PR number
    
    * copy script into image
    
    * add pre-install annotation to resources
    
    * add service account
    
    * rename resources, modify job args
    
    * fix script name
    
    * fix cm name
    
    * configure ord-service to get expected schema version from configmap
    
    * configure job to be triggered pre-upgrade
    
    * remove migrator job annotations
    
    * add managed by label
    
    * fix template
    
    * fix template
    
    * remove labels
    
    * add delete policy
    
    * add pre-rollback hook to resources and modify delete policy of the job
    
    * reference comments
    
    * reference comments
    
    * remove volume mount
    
    * adjust script for local installation to use migration tool
    
    * fix getting expected schema version

--- components/director/internal/healthz/handler.go
+++ components/director/internal/healthz/handler.go
@@ -10,10 +10,3 @@ func NewLivenessHandler() func(writer http.ResponseWriter, request *http.Request
 		writer.WriteHeader(http.StatusOK)
 	}
 }
-
-// NewReadinessHandler returns handler that always return status OK
-func NewReadinessHandler() func(writer http.ResponseWriter, request *http.Request) {
-	return func(writer http.ResponseWriter, request *http.Request) {
-		writer.WriteHeader(http.StatusOK)
-	}
-}
