5fe1453e20f3092b7f949f797eb5de0547aa9707
Dimitar Petrov <dimitar.petrov@sap.com>
2021-08-13 10:59:44 +0300

    Fix naming

--- components/connector/config/config.go
+++ components/connector/config/config.go
@@ -60,7 +60,7 @@ func (c *Config) String() string {
 	return fmt.Sprintf("ExternalAddress: %s, APIEndpoint: %s, HydratorAddress: %s, "+
 		"CSRSubjectCountry: %s, CSRSubjectOrganization: %s, CSRSubjectOrganizationalUnit: %s, "+
 		"CSRSubjectLocality: %s, CSRSubjectProvince: %s, "+
-		"SAPSubjectCountry: %s, SAPSubjectOrganization: %s, SAPSubjectOrganizationalUnitPattern: %s,"+
+		"ExternalIssuerSubjectCountry: %s, ExternalIssuerSubjectOrganization: %s, ExternalIssuerSubjectOrganizationalUnitPattern: %s,"+
 		"CertificateValidityTime: %s, CASecretName: %s, CASecretCertificateKey: %s, CASecretKeyKey: %s, "+
 		"RootCASecretName: %s, RootCASecretCertificateKey: %s, CertificateDataHeader: %s, "+
 		"CertificateSecuredConnectorURL: %s, "+
