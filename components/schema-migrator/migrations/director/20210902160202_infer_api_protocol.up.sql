c435a1d4140458d5259ae5c7f783cb6f82af64c3
NickyMateev <bgdunkers@gmail.com>
2021-09-02 16:32:43 +0300

    Infer API Protocol for non-ORD compliant systems

---
+++ components/schema-migrator/migrations/director/20210902160202_infer_api_protocol.up.sql
@@ -0,0 +1,32 @@
+BEGIN;
+
+CREATE OR REPLACE VIEW tenants_apis  AS
+SELECT DISTINCT t_apps.tenant_id, apis.id, apis.app_id, apis.name, apis.description, apis.group_name, apis.default_auth,
+                apis.version_value, apis.version_deprecated, apis.version_deprecated_since, apis.version_for_removal,
+                apis.ord_id, apis.short_description, apis.system_instance_aware,
+                CASE
+                    WHEN specs.api_spec_type::text = 'ODATA' THEN 'odata-v2'::api_protocol
+                    WHEN specs.api_spec_type::text = 'OPEN_API' THEN 'rest'::api_protocol
+                    ELSE apis.api_protocol
+                    END                                            AS api_protocol,
+                apis.tags, apis.countries, apis.links, apis.api_resource_links, apis.release_status, apis.sunset_date,
+                apis.changelog_entries, apis.labels, apis.package_id, apis.visibility, apis.disabled, apis.part_of_products,
+                apis.line_of_business, apis.industry, apis.ready, apis.created_at, apis.updated_at, apis.deleted_at, apis.error,
+                apis.implementation_standard, apis.custom_implementation_standard, apis.custom_implementation_standard_description,
+                apis.target_urls, apis.extensible, apis.successors, apis.resource_hash
+FROM api_definitions AS apis
+         INNER JOIN (
+--  select GAs
+    SELECT a1.id, a1.tenant_id FROM applications AS a1
+    UNION ALL
+--  select CRM
+    SELECT a.id, t.parent as tenant_id FROM applications AS a
+                                                INNER JOIN  business_tenant_mappings AS t ON t.id = a.tenant_id WHERE t.parent IS NOT NULL
+    UNION ALL
+--  select SA
+    SELECT l.app_id as id, asa.selector_value::uuid as tenant_id FROM labels AS l
+                                                                          INNER JOIN automatic_scenario_assignments AS asa ON asa.tenant_id = l.tenant_id AND l.value ? asa.scenario AND asa.selector_key='global_subaccount_id'
+    WHERE l.app_id IS NOT NULL AND l.key = 'scenarios') AS t_apps ON apis.app_id = t_apps.id
+         LEFT JOIN specifications as specs on apis.id = specs.api_def_id;
+
+COMMIT;
