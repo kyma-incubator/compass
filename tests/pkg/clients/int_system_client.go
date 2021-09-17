4a5bf60aa1eee1a1fe3d512c0403b2946a60de4f
StefanCholakov <32001216+StefanCholakov@users.noreply.github.com>
2021-09-17 06:26:48 -0700

    Add benchmark test for ord-service (#2022)
    * Add benchmark test for ord-service
    
    * Add ord-service benchmark tests in Dockerfile
    
    * Update values.yaml
    
    * Remove leftovers
    
    * Minor improvements
    
    * Add director url env variable
    
    * Add director url to config
    
    * Change require len statement
    
    * Adjust require len statement
    
    * Minor improvements
    
    * Adress PR comments
    
    * Adress PR comment
    
    * Increase appsCount to 15

---
+++ tests/pkg/clients/int_system_client.go
@@ -0,0 +1,39 @@
+package clients
+
+import (
+	"context"
+	"crypto/tls"
+	"fmt"
+	"net/http"
+	"time"
+
+	directorSchema "github.com/kyma-incubator/compass/components/director/pkg/graphql"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/clientcredentials"
+)
+
+func NewIntegrationSystemClient(ctx context.Context, intSystemCredentials *directorSchema.IntSysSystemAuth) (*http.Client, error) {
+	oauthCredentialData, ok := intSystemCredentials.Auth.Credential.(*directorSchema.OAuthCredentialData)
+	if !ok {
+		return nil, fmt.Errorf("while casting integration system credentials to OAuth credentials")
+	}
+
+	conf := &clientcredentials.Config{
+		ClientID:     oauthCredentialData.ClientID,
+		ClientSecret: oauthCredentialData.ClientSecret,
+		TokenURL:     oauthCredentialData.URL,
+	}
+
+	unsecuredHttpClient := http.DefaultClient
+	unsecuredHttpClient.Transport = &http.Transport{
+		TLSClientConfig: &tls.Config{
+			InsecureSkipVerify: true,
+		},
+	}
+
+	ctx = context.WithValue(ctx, oauth2.HTTPClient, unsecuredHttpClient)
+	httpClient := conf.Client(ctx)
+	httpClient.Timeout = 10 * time.Second
+
+	return httpClient, nil
+}
