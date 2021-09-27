4f550429b02826d966ba8c3f0366879fda751295
Daniel Gospodinow <daniel.gospodinov@sap.com>
2021-09-27 16:36:25 +0300

    Make CensoredFlows environment variable optional (#2036)

--- components/director/internal/metrics/collector.go
+++ components/director/internal/metrics/collector.go
@@ -12,7 +12,7 @@ import (
 // Config configures the behaviour of the metrics collector.
 type Config struct {
 	EnableClientIDInstrumentation bool     `envconfig:"default=true,APP_METRICS_ENABLE_CLIENT_ID_INSTRUMENTATION"`
-	CensoredFlows                 []string `envconfig:"APP_METRICS_CENSORED_FLOWS"`
+	CensoredFlows                 []string `envconfig:"optional,APP_METRICS_CENSORED_FLOWS"`
 }
 
 // Collector missing godoc
