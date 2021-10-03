b099172663a7eba705d96fc6e5a7091271981d1a
StanislavStefanov <stanislavsvetlinovstefanov@gmail.com>
2021-10-03 21:06:17 +0300

    add json tags

--- components/director/internal/consumer/consumer.go
+++ components/director/internal/consumer/consumer.go
@@ -22,9 +22,9 @@ const (
 
 // Consumer missing godoc
 type Consumer struct {
-	ConsumerID string
-	ConsumerType
-	Flow oathkeeper.AuthFlow
+	ConsumerID   string `json:"ConsumerID"`
+	ConsumerType `json:"ConsumerType"`
+	Flow         oathkeeper.AuthFlow `json:"Flow"`
 }
 
 // MapSystemAuthToConsumerType missing godoc
