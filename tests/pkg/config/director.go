eb6427e8f48c9e038e85f87eae6c43b1ba75da69
krasish <krasishontov@gmail.com>
2021-10-07 18:25:42 +0300

    Export on info endpoint compass-gateway host

--- tests/pkg/config/director.go
+++ tests/pkg/config/director.go
@@ -3,7 +3,7 @@ package config
 type DirectorConfig struct {
 	HealthUrl                  string `envconfig:"default=https://director.kyma.local/healthz"`
 	WebhookUrl                 string `envconfig:"default=https://kyma-project.io"`
-	InfoUrl                    string `envconfig:"APP_INFO_API_ENDPOINT"`
+	InfoUrl                    string `envconfig:"APP_INFO_API_ENDPOINT,default=https://director.kyma.local/v1/info"`
 	CertIssuer                 string `envconfig:"APP_INFO_CERT_ISSUER"`
 	CertSubject                string `envconfig:"APP_INFO_CERT_SUBJECT"`
 	DefaultScenario            string `envconfig:"default=DEFAULT"`
