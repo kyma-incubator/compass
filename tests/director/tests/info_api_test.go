59601198f3519f936b8cae0f6e2b3992ac76588f
krasish <krasishontov@gmail.com>
2021-10-08 11:29:08 +0300

    Fix endpoint in tests

--- tests/director/tests/info_api_test.go
+++ tests/director/tests/info_api_test.go
@@ -6,11 +6,13 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/kyma-incubator/compass/tests/pkg/gql"
+
 	"github.com/stretchr/testify/require"
 )
 
 func TestGetInfo(t *testing.T) {
-	get, err := http.DefaultClient.Get(conf.InfoUrl)
+	get, err := http.DefaultClient.Get(infoEndpoint())
 	require.NoError(t, err)
 
 	info := struct {
@@ -26,10 +28,20 @@ func TestGetInfo(t *testing.T) {
 }
 
 func TestCallingInfoEndpointFailForMethodsOtherThanGet(t *testing.T) {
-	req, err := http.NewRequest(http.MethodPost, conf.InfoUrl, strings.NewReader("{}"))
+	req, err := http.NewRequest(http.MethodPost, infoEndpoint(), strings.NewReader("{}"))
 	require.NoError(t, err)
 	get, err := http.DefaultClient.Do(req)
 
 	require.NoError(t, err)
 	require.Equal(t, http.StatusMethodNotAllowed, get.StatusCode)
+}
+
+func infoEndpoint() string {
+	directorPath := "/director"
+	infoEndpoint := gql.GetDirectorURL()
+	if strings.Contains(infoEndpoint, directorPath) {
+		infoEndpoint = infoEndpoint[:strings.Index(infoEndpoint, directorPath)]
+		infoEndpoint = infoEndpoint + conf.InfoUrl
+	}
+	return infoEndpoint
 }
