a72e6e56dc97080d8f0cf5f27f4c20d9e319ec5a
krasish <krasishontov@gmail.com>
2021-10-11 09:18:16 +0300

    Change godoc

--- components/director/internal/info/handler.go
+++ components/director/internal/info/handler.go
@@ -7,13 +7,14 @@ import (
 	"github.com/kyma-incubator/compass/components/director/pkg/httputils"
 )
 
+//Config contains the data that should be exported on the info endpoint
 type Config struct {
 	APIEndpoint string `envconfig:"APP_INFO_API_ENDPOINT,default=/v1/info" json:"-"`
 	Issuer      string `envconfig:"APP_INFO_CERT_ISSUER" json:"certIssuer"`
 	Subject     string `envconfig:"APP_INFO_CERT_SUBJECT" json:"certSubject"`
 }
 
-// NewInfoHandler returns handler returns an endpoint which gives information about the CMP client certificate
+// NewInfoHandler returns handler which gives information about the CMP client certificate
 func NewInfoHandler(ctx context.Context, c Config) func(writer http.ResponseWriter, request *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != http.MethodGet {
