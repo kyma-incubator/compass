90032fb0a1e9eee1a6597362d6d8b7f0a21ab261
Krasimir Shontov <krasishontov@gmail.com>
2021-10-12 09:46:30 +0300

    Expose cert info (#2059)
    * Add api endpoint to director
    
    * Adjust director run.sh
    
    * Make info endpoint more generic
    
    * Add envvars to deployment
    
    * Add e2e test and expose on host starting with director
    
    * Export on info endpoint compass-gateway host
    
    * Bump values.yaml
    
    * Fix endpoint in tests
    
    * Fix tests
    
    * Change godoc
    
    * Update values.yaml
    
    * Address comments.
    
    * Fix golint issue

---
+++ tests/director/tests/info_api_test.go
@@ -0,0 +1,63 @@
+package tests
+
+import (
+	"crypto/tls"
+	"encoding/json"
+	"net/http"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/kyma-incubator/compass/tests/pkg/gql"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestGetInfo(t *testing.T) {
+	client := &http.Client{
+		Timeout: time.Second * 3,
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		},
+	}
+
+	resp, err := client.Get(infoEndpoint())
+	require.NoError(t, err)
+
+	info := struct {
+		Subject string `json:"certSubject"`
+		Issuer  string `json:"certIssuer"`
+	}{}
+
+	err = json.NewDecoder(resp.Body).Decode(&info)
+	require.NoError(t, err)
+
+	require.Equal(t, conf.CertSubject, info.Subject)
+	require.Equal(t, conf.CertIssuer, info.Issuer)
+}
+
+func TestCallingInfoEndpointFailForMethodsOtherThanGet(t *testing.T) {
+	client := &http.Client{
+		Timeout: time.Second * 3,
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		},
+	}
+
+	req, err := http.NewRequest(http.MethodPost, infoEndpoint(), strings.NewReader("{}"))
+	require.NoError(t, err)
+	resp, err := client.Do(req)
+
+	require.NoError(t, err)
+	require.Equal(t, http.StatusForbidden, resp.StatusCode)
+}
+
+func infoEndpoint() string {
+	directorPath := "/director"
+	infoEndpoint := gql.GetDirectorURL()
+	if strings.Contains(infoEndpoint, directorPath) {
+		infoEndpoint = infoEndpoint[:strings.Index(infoEndpoint, directorPath)]
+		infoEndpoint = infoEndpoint + conf.InfoUrl
+	}
+	return infoEndpoint
+}
