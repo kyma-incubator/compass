803a117c5b2359865211ffc8b73a936489c19616
Angel Aleksandrov <angel.aleksandrov@sap.com>
2021-10-20 15:41:30 +0300

    add formation scope to admin users

--- tests/director/tests/formation_api_test.go
+++ tests/director/tests/formation_api_test.go
@@ -15,19 +15,21 @@ import (
 func TestCreateFormation(t *testing.T) {
 	// GIVEN
 	ctx := context.Background()
+	labelKey := "scenarios"
+	defaultValue := conf.DefaultScenario
+	additionalValue := "ADDITIONAL"
 
 	tenantId := tenant.TestTenants.GetDefaultTenantID()
 
 	t.Log("Create application")
 	app, err := fixtures.RegisterApplication(t, ctx, dexGraphQLClient, "app", tenantId)
-	defer fixtures.CleanupApplication(t, ctx, dexGraphQLClient, tenantId, &app)
-	defer fixtures.UnassignApplicationFromScenarios(t, ctx, dexGraphQLClient, tenantId, app.ID, conf.DefaultScenarioEnabled)
+	defer func() {
+		fixtures.UnassignApplicationFromScenarios(t, ctx, dexGraphQLClient, tenantId, app.ID, conf.DefaultScenarioEnabled)
+		fixtures.CleanupApplication(t, ctx, dexGraphQLClient, tenantId, &app)
+		fixtures.CleanupFormation(t, ctx, dexGraphQLClient, tenantId, additionalValue)
+	}()
 	require.NoError(t, err)
 	require.NotEmpty(t, app.ID)
-
-	labelKey := "scenarios"
-	defaultValue := conf.DefaultScenario
-	additionalValue := "ADDITIONAL"
 
 	t.Logf("Update Label Definition scenarios enum with additional value %s", additionalValue)
 
@@ -62,3 +64,55 @@ func TestCreateFormation(t *testing.T) {
 	}
 	assert.Equal(t, formations, actualScenariosEnum)
 }
+
+//
+//func TestDeleteFormation(t *testing.T) {
+//	// GIVEN
+//	ctx := context.Background()
+//
+//	tenantId := tenant.TestTenants.GetDefaultTenantID()
+//
+//	t.Log("Create application")
+//	app, err := fixtures.RegisterApplication(t, ctx, dexGraphQLClient, "app", tenantId)
+//	defer fixtures.CleanupApplication(t, ctx, dexGraphQLClient, tenantId, &app)
+//	defer fixtures.UnassignApplicationFromScenarios(t, ctx, dexGraphQLClient, tenantId, app.ID, conf.DefaultScenarioEnabled)
+//	require.NoError(t, err)
+//	require.NotEmpty(t, app.ID)
+//
+//	labelKey := "scenarios"
+//	defaultValue := conf.DefaultScenario
+//	additionalValue := "ADDITIONAL"
+//
+//	t.Logf("Update Label Definition scenarios enum with additional value %s", additionalValue)
+//
+//	var formation graphql.Formation
+//	createReq := fixtures.FixCreateFormationRequest(additionalValue)
+//
+//	err = testctx.Tc.RunOperation(ctx, dexGraphQLClient, createReq, &formation)
+//	require.NoError(t, err)
+//
+//	formations := []string{additionalValue}
+//	if conf.DefaultScenarioEnabled {
+//		formations = []string{defaultValue, additionalValue}
+//	}
+//	var labelValue interface{} = formations
+//
+//	t.Logf("Set scenario label value %s on application", additionalValue)
+//	fixtures.SetApplicationLabel(t, ctx, dexGraphQLClient, app.ID, labelKey, labelValue)
+//
+//	t.Log("Check if new scenario label value was set correctly")
+//	appRequest := fixtures.FixGetApplicationRequest(app.ID)
+//	app = graphql.ApplicationExt{}
+//
+//	err = testctx.Tc.RunOperation(ctx, dexGraphQLClient, appRequest, &app)
+//	require.NoError(t, err)
+//
+//	scenariosLabel, ok := app.Labels[labelKey].([]interface{})
+//	require.True(t, ok)
+//
+//	var actualScenariosEnum []string
+//	for _, v := range scenariosLabel {
+//		actualScenariosEnum = append(actualScenariosEnum, v.(string))
+//	}
+//	assert.Equal(t, formations, actualScenariosEnum)
+//}
--- tests/pkg/fixtures/fomation_requests.go
+++ tests/pkg/fixtures/fomation_requests.go
@@ -1,16 +1,38 @@
 package fixtures
 
 import (
+	"context"
 	"fmt"
 
+	"github.com/kyma-incubator/compass/components/director/pkg/graphql"
+	"github.com/kyma-incubator/compass/tests/pkg/testctx"
 	gcli "github.com/machinebox/graphql"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func FixCreateFormationRequest(formationName string) *gcli.Request {
 	return gcli.NewRequest(
 		fmt.Sprintf(`mutation{
-				  createFormation(formation: {name: %s}){
+				  createFormation(formation: {name: "%s"}){
 					name
 				  }
 				}`, formationName))
+}
+
+func FixDeleteFormationRequest(formationName string) *gcli.Request {
+	return gcli.NewRequest(
+		fmt.Sprintf(`mutation{
+				  deleteFormation(formation: {name: "%s"}){
+					name
+				  }
+				}`, formationName))
+}
+
+func CleanupFormation(t require.TestingT, ctx context.Context, gqlClient *gcli.Client, tenant, formation string) {
+	deleteRequest := FixDeleteFormationRequest(formation)
+
+	var f graphql.Formation
+	err := testctx.Tc.RunOperationWithCustomTenant(ctx, gqlClient, tenant, deleteRequest, &f)
+	assert.NoError(t, err)
 }
