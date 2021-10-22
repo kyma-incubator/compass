670927c3bb7bf9e62e4b8aee309a3d9c0c948aac
Petar Todorov <petar.todorov02@sap.com>
2021-10-22 09:25:42 +0300

    Linter issues

--- components/director/internal/tenantmapping/cert_service_context_provider.go
+++ components/director/internal/tenantmapping/cert_service_context_provider.go
@@ -41,19 +41,19 @@ func (m *certServiceContextProvider) GetObjectContext(ctx context.Context, _ oat
 	scopes := "runtime:read runtime:write"
 
 	// TODO: Uncomment once we start storing subaccounts as tenants and ord views work with internal tenant IDs
-	//log.C(ctx).Infof("Getting the tenant with external ID: %s", externalTenantID)
-	//tenantMapping, err := m.tenantRepo.GetByExternalTenant(ctx, externalTenantID)
-	//if err != nil {
-	//	if apperrors.IsNotFoundError(err) {
-	//		log.C(ctx).Warningf("Could not find tenant with external ID: %s, error: %s", externalTenantID, err.Error())
+	// log.C(ctx).Infof("Getting the tenant with external ID: %s", externalTenantID)
+	// tenantMapping, err := m.tenantRepo.GetByExternalTenant(ctx, externalTenantID)
+	// if err != nil {
+	// 	if apperrors.IsNotFoundError(err) {
+	// 		log.C(ctx).Warningf("Could not find tenant with external ID: %s, error: %s", externalTenantID, err.Error())
 	//
-	//		log.C(ctx).Infof("Returning tenant context with empty internal tenant ID and external ID %s", externalTenantID)
-	//		return NewObjectContext(NewTenantContext(externalTenantID, ""), m.tenantKeys, scopes, authDetails.AuthID, authDetails.AuthFlow, consumer.Runtime, CertServiceObjectContextProvider), nil
-	//	}
-	//	return ObjectContext{}, errors.Wrapf(err, "while getting external tenant mapping [ExternalTenantID=%s]", externalTenantID)
-	//}
+	// 		log.C(ctx).Infof("Returning tenant context with empty internal tenant ID and external ID %s", externalTenantID)
+	// 		return NewObjectContext(NewTenantContext(externalTenantID, ""), m.tenantKeys, scopes, authDetails.AuthID, authDetails.AuthFlow, consumer.Runtime, CertServiceObjectContextProvider), nil
+	// 	}
+	// 	return ObjectContext{}, errors.Wrapf(err, "while getting external tenant mapping [ExternalTenantID=%s]", externalTenantID)
+	// }
 	//
-	//objCtx := NewObjectContext(NewTenantContext(externalTenantID, tenantMapping.ID), m.tenantKeys, scopes, authDetails.AuthID, authDetails.AuthFlow, consumer.Runtime, CertServiceObjectContextProvider)
+	// objCtx := NewObjectContext(NewTenantContext(externalTenantID, tenantMapping.ID), m.tenantKeys, scopes, authDetails.AuthID, authDetails.AuthFlow, consumer.Runtime, CertServiceObjectContextProvider)
 
 	objCtx := NewObjectContext(NewTenantContext(externalTenantID, externalTenantID), m.tenantKeys, scopes, authDetails.AuthID, authDetails.AuthFlow, consumer.Runtime, CertServiceObjectContextProvider)
 
