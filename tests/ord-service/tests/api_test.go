ff4d9d4c4a25c8e541e5ddf46e232af19ceb6318
StefanCholakov <stcholakov98@gmail.com>
2021-11-10 16:56:42 +0200

    Minor improvement

--- tests/ord-service/tests/api_test.go
+++ tests/ord-service/tests/api_test.go
@@ -37,8 +37,6 @@ import (
 	testingx "github.com/kyma-incubator/compass/tests/pkg/testing"
 	"github.com/stretchr/testify/require"
 	"github.com/tidwall/gjson"
-	"golang.org/x/oauth2"
-	"golang.org/x/oauth2/clientcredentials"
 )
 
 const (
@@ -743,23 +741,6 @@ func makeRequestWithHeaders(t require.TestingT, httpClient *http.Client, url str
 
 func makeRequestWithStatusExpect(t require.TestingT, httpClient *http.Client, url string, expectedHTTPStatus int) string {
 	return request.MakeRequestWithHeadersAndStatusExpect(t, httpClient, url, map[string][]string{}, expectedHTTPStatus, testConfig.ORDServiceDefaultResponseType)
-}
-
-func integrationSystemClient(t require.TestingT, ctx context.Context, base *http.Client, intSystemCredentials *directorSchema.IntSysSystemAuth) *http.Client {
-	oauthCredentialData, ok := intSystemCredentials.Auth.Credential.(*directorSchema.OAuthCredentialData)
-	require.True(t, ok)
-
-	conf := &clientcredentials.Config{
-		ClientID:     oauthCredentialData.ClientID,
-		ClientSecret: oauthCredentialData.ClientSecret,
-		TokenURL:     oauthCredentialData.URL,
-	}
-
-	ctx = context.WithValue(ctx, oauth2.HTTPClient, base)
-	httpClient := conf.Client(ctx)
-	httpClient.Timeout = 20 * time.Second
-
-	return httpClient
 }
 
 // extIssuerCertClient returns http client configured with client certificate manually signed by connector's CA
