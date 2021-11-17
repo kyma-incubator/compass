d7d3c6ed8c13171587cd22e01dc106471b6adf2f
Nikolay Stoykov <nikolay.stoykov@sap.com>
2021-11-17 17:25:50 +0200

    fix director client tests

--- components/director/pkg/graphql_client/director_client_test.go
+++ components/director/pkg/graphql_client/director_client_test.go
@@ -6,8 +6,8 @@ import (
 	"testing"
 
 	"github.com/kyma-incubator/compass/components/director/pkg/graphql"
+	"github.com/kyma-incubator/compass/components/director/pkg/str"
 
-	"github.com/kyma-incubator/compass/components/director/internal/model"
 	"github.com/kyma-incubator/compass/components/director/pkg/graphql_client/automock"
 
 	gcli "github.com/machinebox/graphql"
@@ -18,33 +18,33 @@ import (
 )
 
 func TestDirector_WriteTenants(t *testing.T) {
-	tenantsInput := []model.BusinessTenantMappingInput{
+	tenantsInput := []graphql.BusinessTenantMappingInput{
 		{
 			Name:           "0283bc56-406b-11ec-9356-0242ac130003",
 			ExternalTenant: "123",
-			Parent:         "",
-			Subdomain:      "subdomain1",
-			Region:         "region1",
+			Parent:         str.Ptr(""),
+			Subdomain:      str.Ptr("subdomain1"),
+			Region:         str.Ptr("region1"),
 			Type:           "account",
 			Provider:       "provider1",
 		},
 		{
 			Name:           "109534be-406b-11ec-9356-0242ac130003",
 			ExternalTenant: "456",
-			Parent:         "",
-			Subdomain:      "subdomain2",
-			Region:         "region2",
+			Parent:         str.Ptr(""),
+			Subdomain:      str.Ptr("subdomain2"),
+			Region:         str.Ptr("region2"),
 			Type:           "type2",
 			Provider:       "account",
 		},
 	}
-	expectedQuery := "mutation { writeTenants(in:[{name: \"0283bc56-406b-11ec-9356-0242ac130003\",externalTenant: \"123\",parent: \"\",subdomain: \"subdomain1\",region:\"region1\",type:\"account\",provider: \"provider1\"},{name: \"109534be-406b-11ec-9356-0242ac130003\",externalTenant: \"456\",parent: \"\",subdomain: \"subdomain2\",region:\"region2\",type:\"type2\",provider: \"account\"}])}"
+	expectedQuery := "mutation { writeTenants(in:[{name: \"0283bc56-406b-11ec-9356-0242ac130003\",externalTenant: \"123\",parent: \"\", region:\"region1\", subdomain: \"subdomain1\",type:\"account\",provider: \"provider1\"},{name: \"109534be-406b-11ec-9356-0242ac130003\",externalTenant: \"456\",parent: \"\", region:\"region2\", subdomain: \"subdomain2\",type:\"type2\",provider: \"account\"}])}"
 	testErr := errors.New("Test error")
 
 	testCases := []struct {
 		Name        string
 		GQLClient   func() *automock.GraphQLClient
-		Input       []model.BusinessTenantMappingInput
+		Input       []graphql.BusinessTenantMappingInput
 		ExpectedErr error
 	}{
 		{
@@ -52,7 +52,7 @@ func TestDirector_WriteTenants(t *testing.T) {
 			GQLClient: func() *automock.GraphQLClient {
 				gqlClient := &automock.GraphQLClient{}
 				gqlClient.On("Run", mock.Anything, mock.MatchedBy(func(req *gcli.Request) bool {
-					return strings.EqualFold(req.Query(), expectedQuery)
+					return strings.EqualFold(removeWhitespaces(req.Query()), removeWhitespaces(expectedQuery))
 				}), mock.Anything).Return(nil)
 				return gqlClient
 			},
@@ -93,33 +93,33 @@ func TestDirector_WriteTenants(t *testing.T) {
 }
 
 func TestDirector_DeleteTenants(t *testing.T) {
-	tenantsInput := []model.BusinessTenantMappingInput{
+	tenantsInput := []graphql.BusinessTenantMappingInput{
 		{
 			Name:           "0283bc56-406b-11ec-9356-0242ac130003",
 			ExternalTenant: "123",
-			Parent:         "",
-			Subdomain:      "subdomain1",
-			Region:         "region1",
+			Parent:         str.Ptr(""),
+			Subdomain:      str.Ptr("subdomain1"),
+			Region:         str.Ptr("region1"),
 			Type:           "account",
 			Provider:       "provider1",
 		},
 		{
 			Name:           "109534be-406b-11ec-9356-0242ac130003",
 			ExternalTenant: "456",
-			Parent:         "",
-			Subdomain:      "subdomain2",
-			Region:         "region2",
+			Parent:         str.Ptr(""),
+			Subdomain:      str.Ptr("subdomain2"),
+			Region:         str.Ptr("region2"),
 			Type:           "type2",
 			Provider:       "account",
 		},
 	}
-	expectedQuery := "mutation { deleteTenants(in:[{name: \"0283bc56-406b-11ec-9356-0242ac130003\",externalTenant: \"123\",parent: \"\",subdomain: \"subdomain1\",region:\"region1\",type:\"account\",provider: \"provider1\"},{name: \"109534be-406b-11ec-9356-0242ac130003\",externalTenant: \"456\",parent: \"\",subdomain: \"subdomain2\",region:\"region2\",type:\"type2\",provider: \"account\"}])}"
+	expectedQuery := `mutation { deleteTenants(in:["123", "456"])}`
 	testErr := errors.New("Test error")
 
 	testCases := []struct {
 		Name        string
 		GQLClient   func() *automock.GraphQLClient
-		Input       []model.BusinessTenantMappingInput
+		Input       []graphql.BusinessTenantMappingInput
 		ExpectedErr error
 	}{
 		{
@@ -127,7 +127,7 @@ func TestDirector_DeleteTenants(t *testing.T) {
 			GQLClient: func() *automock.GraphQLClient {
 				gqlClient := &automock.GraphQLClient{}
 				gqlClient.On("Run", mock.Anything, mock.MatchedBy(func(req *gcli.Request) bool {
-					return strings.EqualFold(req.Query(), expectedQuery)
+					return strings.EqualFold(removeWhitespaces(req.Query()), removeWhitespaces(expectedQuery))
 				}), mock.Anything).Return(nil)
 				return gqlClient
 			},
@@ -280,7 +280,7 @@ func TestDirector_UpdateLabelDefinition(t *testing.T) {
 				return gqlClient
 			},
 			Input:       lblDefInput,
-			ExpectedErr: errors.New("while executing gql query"),
+			ExpectedErr: testErr,
 		},
 	}
 
@@ -373,7 +373,8 @@ func TestDirector_SetRuntimeTenant(t *testing.T) {
 			GQLClient: func() *automock.GraphQLClient {
 				gqlClient := &automock.GraphQLClient{}
 				gqlClient.On("Run", mock.Anything, mock.MatchedBy(func(req *gcli.Request) bool {
-					return strings.EqualFold(req.Query(), expectedQuery)
+					isTenantExpected := req.Header.Get("Tenant") == tenantID
+					return isTenantExpected && strings.EqualFold(req.Query(), expectedQuery)
 				}), mock.Anything).Return(nil)
 				return gqlClient
 			},
@@ -390,6 +391,90 @@ func TestDirector_SetRuntimeTenant(t *testing.T) {
 			},
 			RuntimeID:   runtimeID,
 			TenantID:    tenantID,
+			ExpectedErr: testErr,
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.Name, func(t *testing.T) {
+			//GIVEN
+			ctx := context.TODO()
+			gqlClient := testCase.GQLClient()
+			director := NewDirector(gqlClient)
+
+			//WHEN
+			err := director.SetRuntimeTenant(ctx, testCase.RuntimeID, testCase.TenantID, testCase.TenantID)
+
+			//THEN
+			if testCase.ExpectedErr == nil {
+				require.NoError(t, err)
+			} else {
+				require.Error(t, err)
+				require.Contains(t, err.Error(), testCase.ExpectedErr.Error())
+			}
+		})
+	}
+}
+
+func TestDirector_UpdateTenant(t *testing.T) {
+	tenantID := "id1"
+	tenantInput := graphql.BusinessTenantMappingInput{
+		Name:           "0283bc56-406b-11ec-9356-0242ac130003",
+		ExternalTenant: "123",
+		Parent:         str.Ptr(""),
+		Subdomain:      str.Ptr("subdomain1"),
+		Region:         str.Ptr("region1"),
+		Type:           "account",
+		Provider:       "provider1",
+	}
+	expectedQuery := `mutation { updateTenant(id: "id1", in:{
+		name: "0283bc56-406b-11ec-9356-0242ac130003",
+		externalTenant: "123",
+		parent: "",
+		region: "region1",
+		subdomain: "subdomain1",
+		type: "account",
+		provider: "provider1"
+	}) {
+		id
+		internalID
+		name
+		initialized
+		labels
+		parentID
+		type
+	}}`
+	testErr := errors.New("Test error")
+
+	testCases := []struct {
+		Name        string
+		GQLClient   func() *automock.GraphQLClient
+		TenantID    string
+		Input       graphql.BusinessTenantMappingInput
+		ExpectedErr error
+	}{
+		{
+			Name: "Success",
+			GQLClient: func() *automock.GraphQLClient {
+				gqlClient := &automock.GraphQLClient{}
+				gqlClient.On("Run", mock.Anything, mock.MatchedBy(func(req *gcli.Request) bool {
+					return strings.EqualFold(removeWhitespaces(req.Query()), removeWhitespaces(expectedQuery))
+				}), mock.Anything).Return(nil)
+				return gqlClient
+			},
+			TenantID:    tenantID,
+			Input:       tenantInput,
+			ExpectedErr: nil,
+		},
+		{
+			Name: "Returns error when Run fails",
+			GQLClient: func() *automock.GraphQLClient {
+				gqlClient := &automock.GraphQLClient{}
+				gqlClient.On("Run", mock.Anything, mock.Anything, mock.Anything).Return(testErr)
+				return gqlClient
+			},
+			TenantID:    tenantID,
+			Input:       tenantInput,
 			ExpectedErr: errors.New("while executing gql query"),
 		},
 	}
@@ -402,7 +487,7 @@ func TestDirector_SetRuntimeTenant(t *testing.T) {
 			director := NewDirector(gqlClient)
 
 			//WHEN
-			err := director.SetRuntimeTenant(ctx, testCase.RuntimeID, testCase.TenantID)
+			err := director.UpdateTenant(ctx, testCase.TenantID, testCase.Input)
 
 			//THEN
 			if testCase.ExpectedErr == nil {
@@ -413,4 +498,12 @@ func TestDirector_SetRuntimeTenant(t *testing.T) {
 			}
 		})
 	}
+}
+
+func removeWhitespaces(s string) string {
+	s = strings.ReplaceAll(s, " ", "")
+	s = strings.ReplaceAll(s, "\r", "")
+	s = strings.ReplaceAll(s, "\n", "")
+	s = strings.ReplaceAll(s, "\t", "")
+	return s
 }
