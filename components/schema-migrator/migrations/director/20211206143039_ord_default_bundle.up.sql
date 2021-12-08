0186ed0073730b5d6b8dfb9b663cb61d26b660ec
Nikolay Yordanov <n.yordanov@sap.com>
2021-12-08 15:28:06 +0200

    Formatting

--- components/schema-migrator/migrations/director/20211206143039_ord_default_bundle.up.sql
+++ components/schema-migrator/migrations/director/20211206143039_ord_default_bundle.up.sql
@@ -7,9 +7,9 @@ DROP VIEW api_bundle_reference;
 DROP VIEW event_bundle_reference;
 
 CREATE VIEW api_bundle_reference AS
-SELECT api_def_id      AS api_definition_id,
-       api_def_url     AS default_entry_point,
-       bundle_id       AS bundle_id,
+SELECT api_def_id        AS api_definition_id,
+       api_def_url       AS default_entry_point,
+       bundle_id         AS bundle_id,
        is_default_bundle AS default_consumption_bundle
 FROM bundle_references
 WHERE api_def_id IS NOT NULL;
@@ -18,7 +18,7 @@ CREATE VIEW event_bundle_reference AS
 SELECT event_def_id           AS event_definition_id,
        NULL::VARCHAR(256)     AS default_entry_point,
        bundle_id              AS bundle_id,
-       is_default_bundle        AS default_consumption_bundle
+       is_default_bundle      AS default_consumption_bundle
 FROM bundle_references
 WHERE event_def_id IS NOT NULL;
 
