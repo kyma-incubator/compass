b431308d32fdf4bee850b5c09d80ec50df6f7007
StefanCholakov <stcholakov98@gmail.com>
2021-12-08 18:28:53 +0200

    Minor improvement

--- components/director/internal/features/features.go
+++ components/director/internal/features/features.go
@@ -4,7 +4,7 @@ package features
 type Config struct {
 	DefaultScenarioEnabled        bool   `envconfig:"default=true,APP_DEFAULT_SCENARIO_ENABLED"`
 	ProtectedLabelPattern         string `envconfig:"default=.*_defaultEventing|^consumer_subaccount_ids$"`
-	ImmutableLabelPattern         string `envconfig:"default=^xsappnameCMPClone$,APP_SELF_REGISTER_LABEL_KEY_PATTERN"`
+	ImmutableLabelPattern         string `envconfig:"APP_SELF_REGISTER_LABEL_KEY_PATTERN"`
 	SubscriptionProviderLabelKey  string `envconfig:"APP_SUBSCRIPTION_PROVIDER_LABEL_KEY,default=subscriptionProviderId"`
 	ConsumerSubaccountIDsLabelKey string `envconfig:"APP_CONSUMER_SUBACCOUNT_IDS_LABEL_KEY,default=consumer_subaccount_ids"`
 }
