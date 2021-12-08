83f0d85f07f1479f3a8f9bd8da85dc1ec770b686
Petar Todorov <petar.todorov02@sap.com>
2021-12-08 10:42:28 +0200

    Every single time... Make imports.. I love them

--- components/director/pkg/certloader/config.go
+++ components/director/pkg/certloader/config.go
@@ -2,7 +2,7 @@ package certloader
 
 // Config holds external client certificate configuration available for the certificate loader
 type Config struct {
-	ExternalClientCertSecret    string `envconfig:"APP_EXTERNAL_CLIENT_CERT_SECRET"`
-	ExternalClientCertCertKey   string `envconfig:"APP_EXTERNAL_CLIENT_CERT_KEY"`
-	ExternalClientCertKeyKey    string `envconfig:"APP_EXTERNAL_CLIENT_KEY_KEY"`
+	ExternalClientCertSecret  string `envconfig:"APP_EXTERNAL_CLIENT_CERT_SECRET"`
+	ExternalClientCertCertKey string `envconfig:"APP_EXTERNAL_CLIENT_CERT_KEY"`
+	ExternalClientCertKeyKey  string `envconfig:"APP_EXTERNAL_CLIENT_KEY_KEY"`
 }
