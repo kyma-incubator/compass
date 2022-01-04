de74a8880da0a0fcfd8cf281d0fc58e59cfa7a6b
krasish <krasishontov@gmail.com>
2022-01-04 13:24:21 +0200

    Fix new linter errors.

--- components/director/internal/domain/formation/frmtest/uuid.go
+++ components/director/internal/domain/formation/frmtest/uuid.go
@@ -2,6 +2,7 @@ package frmtest
 
 import "github.com/kyma-incubator/compass/components/director/internal/domain/formation/automock"
 
+// UnusedUUIDService returns a mock uid service that does not expect to get called
 func UnusedUUIDService() func() *automock.UidService {
 	return func() *automock.UidService {
 		return &automock.UidService{}
--- components/director/internal/domain/runtime/rtmtest/uuid.go
+++ components/director/internal/domain/runtime/rtmtest/uuid.go
@@ -2,6 +2,7 @@ package rtmtest
 
 import "github.com/kyma-incubator/compass/components/director/internal/domain/runtime/automock"
 
+// UnusedUUIDService returns a mock uid service that does not expect to get called
 func UnusedUUIDService() func() *automock.UidService {
 	return func() *automock.UidService {
 		return &automock.UidService{}
