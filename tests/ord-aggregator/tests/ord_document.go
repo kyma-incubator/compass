baecaf77b1e6fe183c78cb959b93e00866775abd
Dobromir Zahariev <dzahariev@yahoo.com>
2022-02-07 12:24:34 +0200

    Create ord_document.go

---
+++ tests/ord-aggregator/tests/ord_document.go
@@ -0,0 +1,25 @@
+package tests
+
+import (
+	"github.com/kyma-incubator/compass/components/director/pkg/accessstrategy"
+)
+
+// WellKnownEndpoint is the single entry point for the discovery.
+const WellKnownEndpoint = "/.well-known/open-resource-discovery"
+
+// WellKnownConfig represents the whole config object
+type WellKnownConfig struct {
+	BaseURL                 string                  `json:"baseUrl"`
+	OpenResourceDiscoveryV1 OpenResourceDiscoveryV1 `json:"openResourceDiscoveryV1"`
+}
+
+// OpenResourceDiscoveryV1 contains all Documents' details
+type OpenResourceDiscoveryV1 struct {
+	Documents []DocumentDetails `json:"documents"`
+}
+
+// DocumentDetails contains fields related to the fetching of each Document
+type DocumentDetails struct {
+ 	URL              string                          `json:"url"`
+	AccessStrategies accessstrategy.AccessStrategies `json:"accessStrategies"`
+}
