6dbcdc3eb7a6ca1ea74947398ece2e37c2ed6472
StanislavStefanov <39959090+StanislavStefanov@users.noreply.github.com>
2022-03-28 09:54:11 +0300

    Get region label value from environment (#2283)
    * get region from environment
    
    * PR number

--- tests/director/tests/main_test.go
+++ tests/director/tests/main_test.go
@@ -34,6 +34,7 @@ type DirectorConfig struct {
 	certprovider.ExternalCertProviderConfig
 	SelfRegDistinguishLabelKey   string
 	SelfRegDistinguishLabelValue string
+	SelfRegRegion                string
 }
 
 type BaseDirectorConfig struct {
