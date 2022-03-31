489b6a98ed8812016f4fadff34d439897a1649cc
Dobromir Zahariev <dobromir.zahariev@sap.com>
2022-03-31 16:11:00 +0300

    Fix for base image

--- components/connector/hack/regenerate-mocks.sh
+++ components/connector/hack/regenerate-mocks.sh
@@ -7,9 +7,9 @@ set -o pipefail
 PROJECT_ROOT=$(dirname ${BASH_SOURCE})/..
 
 echo "Installing mockery 2.9.0..."
-go get github.com/vektra/mockery/v2/.../@v2.9.0
+go install github.com/vektra/mockery/v2/.../@v2.9.0
 echo "Installing latest failery..."
-go get github.com/kyma-project/kyma/tools/failery/.../
+go install github.com/kyma-project/kyma/tools/failery/.../
 echo "Generating mock implementation for interfaces..."
 cd ${PROJECT_ROOT}
 go generate ./...
