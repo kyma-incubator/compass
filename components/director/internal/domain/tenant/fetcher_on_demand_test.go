b9b64ea705095970bfa0dfe8e90a30490f3f3d55
StefanCholakov <stcholakov98@gmail.com>
2022-04-14 11:00:38 +0300

    Remove leftovers

--- components/director/internal/domain/tenant/fetcher_on_demand_test.go
+++ components/director/internal/domain/tenant/fetcher_on_demand_test.go
@@ -76,15 +76,3 @@ func TestFetchOnDemand(t *testing.T) {
 		})
 	}
 }
-
-func (f RoundTripFunc) RoundTrip(req *http.Request) (*http.Response, error) {
-	return f(req), nil
-}
-
-type RoundTripFunc func(req *http.Request) *http.Response
-
-func NewTestClient(fn RoundTripFunc) *http.Client {
-	return &http.Client{
-		Transport: fn,
-	}
-}
