6940a7879d91d4a3b0c834b72c3b5d712b40b0f7
i322062 <kaloyan.spiridonov@sap.com>
2022-05-03 17:02:40 +0300

    fix apptemplate converter.go

--- components/director/internal/domain/apptemplate/converter.go
+++ components/director/internal/domain/apptemplate/converter.go
@@ -95,8 +95,9 @@ func (c *converter) InputFromGraphQL(in graphql.ApplicationTemplateInput) (model
 		return model.ApplicationTemplateInput{}, errors.Wrapf(err, "error occurred while converting webhooks og GraphQL input to Application Template model with name %s", in.Name)
 	}
 
-	if in.Labels == nil {
-		in.Labels = make(map[string]interface{})
+	var labels map[string]interface{}
+	if in.Labels != nil {
+		labels = in.Labels
 	}
 
 	return model.ApplicationTemplateInput{
@@ -105,7 +106,7 @@ func (c *converter) InputFromGraphQL(in graphql.ApplicationTemplateInput) (model
 		ApplicationInputJSON: appCreateInput,
 		Placeholders:         c.placeholdersFromGraphql(in.Placeholders),
 		AccessLevel:          model.ApplicationTemplateAccessLevel(in.AccessLevel),
-		Labels:               in.Labels,
+		Labels:               labels,
 		Webhooks:             webhooks,
 	}, nil
 }
