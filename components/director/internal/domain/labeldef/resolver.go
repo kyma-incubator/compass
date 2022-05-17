0a7ac640960436756cbb83c8f1b7dba1428b5e45
ivanetenevvasilev <ivantenevvasilev@abv.bg>
2022-05-17 09:44:12 +0300

    run make imports on resolver.go

--- components/director/internal/domain/labeldef/resolver.go
+++ components/director/internal/domain/labeldef/resolver.go
@@ -2,6 +2,7 @@ package labeldef
 
 import (
 	"context"
+
 	"github.com/kyma-incubator/compass/components/director/pkg/resource"
 
 	"github.com/kyma-incubator/compass/components/director/pkg/apperrors"
