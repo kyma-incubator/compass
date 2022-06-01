ba4b1fa72f9cce79eeeeee2983a0a30532b5362d
Dragomir Todorov <dragomir.todorov@sap.com>
2022-06-01 13:28:57 +0300

    add selfRegDistLabelKey in config structs and charts so its ok to use in future use case

--- components/director/cmd/systemfetcher/main.go
+++ components/director/cmd/systemfetcher/main.go
@@ -62,6 +62,8 @@ type config struct {
 	ClientTimeout           time.Duration `envconfig:"default=60s"`
 
 	CertLoaderConfig certloader.Config
+
+	SelfRegisterDistinguishLabelKey string `envconfig:"APP_SELF_REGISTER_DISTINGUISH_LABEL_KEY"`
 }
 
 type appTemplateConfig struct {
@@ -213,7 +215,7 @@ func createSystemFetcher(cfg config, cfgProvider *configprovider.Provider, tx pe
 	eventAPISvc := eventdef.NewService(eventAPIRepo, uidSvc, specSvc, bundleReferenceSvc)
 	docSvc := document.NewService(docRepo, fetchRequestRepo, uidSvc)
 	bundleSvc := bundleutil.NewService(bundleRepo, apiSvc, eventAPISvc, docSvc, uidSvc)
-	appSvc := application.NewService(&normalizer.DefaultNormalizator{}, cfgProvider, "", applicationRepo, webhookRepo, runtimeRepo, labelRepo, intSysRepo, labelSvc, scenariosSvc, bundleSvc, uidSvc)
+	appSvc := application.NewService(&normalizer.DefaultNormalizator{}, cfgProvider, cfg.SelfRegisterDistinguishLabelKey, applicationRepo, webhookRepo, runtimeRepo, labelRepo, intSysRepo, labelSvc, scenariosSvc, bundleSvc, uidSvc)
 	appTemplateConv := apptemplate.NewConverter(appConverter, webhookConverter)
 	appTemplateRepo := apptemplate.NewRepository(appTemplateConv)
 	appTemplateSvc := apptemplate.NewService(appTemplateRepo, webhookRepo, uidSvc, labelSvc, labelRepo)
