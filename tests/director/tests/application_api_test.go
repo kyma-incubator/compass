c1759ffc6d694dab09e26edb90854b0eebcafec8
Dobromir Zahariev <dobromir.zahariev@sap.com>
2022-06-09 17:28:03 +0300

    TestApplicationDeletionInScenario adjustments 2

--- tests/director/tests/application_api_test.go
+++ tests/director/tests/application_api_test.go
@@ -1467,9 +1467,9 @@ func TestApplicationDeletionInScenario(t *testing.T) {
 	err = testctx.Tc.RunOperationWithCustomTenant(ctx, certSecuredGraphQLClient, tenantId, request, nil)
 	require.NoError(t, err)
 
-	fixtures.UpdateScenariosLabelDefinitionWithinTenant(t, ctx, certSecuredGraphQLClient, tenantId, defaultScenarios)
-
-	fixtures.CleanupApplication(t, ctx, certSecuredGraphQLClient, tenantId, &actualApp)
+	request = fixtures.FixUnregisterApplicationRequest(actualApp.ID)
+	err = testctx.Tc.RunOperationWithCustomTenant(ctx, certSecuredGraphQLClient, tenantId, request, nil)
+	require.NoError(t, err)
 
 	actualAppDeleted := graphql.ApplicationExt{}
 	queryAppReq := fixtures.FixGetApplicationRequest(actualApp.ID)
