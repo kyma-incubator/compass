c026d90039d057ab08a29e99f84cda69116c80e8
ivanetenevvasilev <ivantenevvasilev@abv.bg>
2022-06-14 15:15:04 +0300

    rewrite converter tests to be table driven

--- components/director/internal/domain/formationtemplate/converter_test.go
+++ components/director/internal/domain/formationtemplate/converter_test.go
@@ -1,6 +1,8 @@
 package formationtemplate_test
 
 import (
+	"github.com/kyma-incubator/compass/components/director/internal/model"
+	"github.com/kyma-incubator/compass/components/director/pkg/graphql"
 	"testing"
 
 	"github.com/kyma-incubator/compass/components/director/internal/domain/formationtemplate"
@@ -9,66 +11,111 @@ import (
 )
 
 func TestConverter_FromInputGraphQL(t *testing.T) {
-	t.Run("Success", func(t *testing.T) {
-		// GIVEN
-		converter := formationtemplate.NewConverter()
-		// WHEN
-		result := converter.FromInputGraphQL(&inputFormationTemplateGraphQLModel)
+	testCases := []struct {
+		Name     string
+		Input    *graphql.FormationTemplateInput
+		Expected *model.FormationTemplateInput
+	}{{
+		Name:     "Success",
+		Input:    &inputFormationTemplateGraphQLModel,
+		Expected: &inputFormationTemplateModel,
+	}, {
+		Name:     "Empty",
+		Input:    nil,
+		Expected: nil,
+	},
+	}
+	for _, testCase := range testCases {
+		t.Run(testCase.Name, func(t *testing.T) {
+			// GIVEN
+			converter := formationtemplate.NewConverter()
+			// WHEN
+			result := converter.FromInputGraphQL(testCase.Input)
 
-		// THEN
-		assert.Equal(t, *result, inputFormationTemplateModel)
-	})
-	t.Run("Returns nil when given empty model", func(t *testing.T) {
-		// GIVEN
-		converter := formationtemplate.NewConverter()
-		// WHEN
-		result := converter.FromInputGraphQL(nil)
-
-		assert.Nil(t, result)
-	})
+			assert.Equal(t, result, testCase.Expected)
+		})
+	}
 }
 
 func TestConverter_FromModelInputToModel(t *testing.T) {
-	t.Run("Success", func(t *testing.T) {
-		// GIVEN
-		converter := formationtemplate.NewConverter()
-		// WHEN
-		result := converter.FromModelInputToModel(&inputFormationTemplateModel, testID)
+	testCases := []struct {
+		Name     string
+		Input    *model.FormationTemplateInput
+		Expected *model.FormationTemplate
+	}{{
+		Name:     "Success",
+		Input:    &inputFormationTemplateModel,
+		Expected: &formationTemplateModel,
+	}, {
+		Name:     "Empty",
+		Input:    nil,
+		Expected: nil,
+	},
+	}
+	for _, testCase := range testCases {
+		t.Run(testCase.Name, func(t *testing.T) {
+			// GIVEN
+			converter := formationtemplate.NewConverter()
+			// WHEN
+			result := converter.FromModelInputToModel(testCase.Input, testID)
 
-		// THEN
-		assert.Equal(t, *result, formationTemplateModel)
-	})
-	t.Run("Returns nil when given empty model", func(t *testing.T) {
-		// GIVEN
-		converter := formationtemplate.NewConverter()
-		// WHEN
-		result := converter.FromModelInputToModel(nil, "")
-
-		assert.Nil(t, result)
-	})
+			assert.Equal(t, result, testCase.Expected)
+		})
+	}
 }
 
 func TestConverter_ToGraphQL(t *testing.T) {
-	t.Run("Success", func(t *testing.T) {
-		// GIVEN
-		converter := formationtemplate.NewConverter()
-		// WHEN
-		result := converter.ToGraphQL(&formationTemplateModel)
+	testCases := []struct {
+		Name     string
+		Input    *model.FormationTemplate
+		Expected *graphql.FormationTemplate
+	}{{
+		Name:     "Success",
+		Input:    &formationTemplateModel,
+		Expected: &formationTemplateGraphQLModel,
+	}, {
+		Name:     "Empty",
+		Input:    nil,
+		Expected: nil,
+	},
+	}
+	for _, testCase := range testCases {
+		t.Run(testCase.Name, func(t *testing.T) {
+			// GIVEN
+			converter := formationtemplate.NewConverter()
+			// WHEN
+			result := converter.ToGraphQL(testCase.Input)
 
-		// THEN
-		assert.Equal(t, *result, formationTemplateGraphQLModel)
-	})
-	t.Run("Returns nil when given empty model", func(t *testing.T) {
-		// GIVEN
-		converter := formationtemplate.NewConverter()
-		// WHEN
-		result := converter.ToGraphQL(nil)
-
-		assert.Nil(t, result)
-	})
+			assert.Equal(t, result, testCase.Expected)
+		})
+	}
 }
 
 func TestConverter_MultipleToGraphQL(t *testing.T) {
+	testCases := []struct {
+		Name     string
+		Input    []*model.FormationTemplate
+		Expected []*graphql.FormationTemplate
+	}{{
+		Name:     "Success",
+		Input:    formationTemplateModelPage.Data,
+		Expected: formationTemplateGraphQLModelPage.Data,
+	}, {
+		Name:     "Empty",
+		Input:    nil,
+		Expected: nil,
+	},
+	}
+	for _, testCase := range testCases {
+		t.Run(testCase.Name, func(t *testing.T) {
+			// GIVEN
+			converter := formationtemplate.NewConverter()
+			// WHEN
+			result := converter.MultipleToGraphQL(testCase.Input)
+
+			assert.ElementsMatch(t, result, testCase.Expected)
+		})
+	}
 	t.Run("Success", func(t *testing.T) {
 		// GIVEN
 		converter := formationtemplate.NewConverter()
@@ -111,23 +158,29 @@ func TestConverter_ToEntity(t *testing.T) {
 }
 
 func TestConverter_FromEntity(t *testing.T) {
-	t.Run("Success", func(t *testing.T) {
-		// GIVEN
-		converter := formationtemplate.NewConverter()
-		// WHEN
-		result, err := converter.FromEntity(&formationTemplateEntity)
+	testCases := []struct {
+		Name     string
+		Input    *formationtemplate.Entity
+		Expected *model.FormationTemplate
+	}{{
+		Name:     "Success",
+		Input:    &formationTemplateEntity,
+		Expected: &formationTemplateModel,
+	}, {
+		Name:     "Empty",
+		Input:    nil,
+		Expected: nil,
+	},
+	}
+	for _, testCase := range testCases {
+		t.Run(testCase.Name, func(t *testing.T) {
+			// GIVEN
+			converter := formationtemplate.NewConverter()
+			// WHEN
+			result, err := converter.FromEntity(testCase.Input)
 
-		// THEN
-		require.NoError(t, err)
-		assert.Equal(t, result, &formationTemplateModel)
-	})
-	t.Run("Returns nil when given empty model", func(t *testing.T) {
-		// GIVEN
-		converter := formationtemplate.NewConverter()
-		// WHEN
-		result, err := converter.FromEntity(nil)
-
-		assert.NoError(t, err)
-		assert.Nil(t, result)
-	})
+			assert.NoError(t, err)
+			assert.Equal(t, result, testCase.Expected)
+		})
+	}
 }
