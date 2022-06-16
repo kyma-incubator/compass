a7f362cd579a7a7a8c0ba8a3fe74f5bcb84fd6c2
DimitarPetrov <dimitar.petrov@sap.com>
2022-06-16 16:46:42 +0300

    Fix down migration

--- components/schema-migrator/migrations/director/20220615095050_introduce-application-namespace.down.sql
+++ components/schema-migrator/migrations/director/20220615095050_introduce-application-namespace.down.sql
@@ -2,4 +2,7 @@ BEGIN;
 
 ALTER TABLE app_templates DROP COLUMN application_namespace;
 
+DELETE FROM labels
+WHERE app_id IS NOT NULL AND app_id IN (SELECT app_id FROM labels WHERE key = 'applicationType' AND value = '"SAP S/4HANA Cloud"') AND key = 'region' AND value = '"default"';
+
 COMMIT;
