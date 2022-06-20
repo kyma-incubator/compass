c18c5f4ec0b75963c3380494b358b545b670963b
PetarTodorovv <todorovv.petar@gmail.com>
2022-06-20 15:18:36 +0300

    make linter happy

--- components/director/internal/model/formation.go
+++ components/director/internal/model/formation.go
@@ -1,5 +1,6 @@
 package model
 
+// DefaultTemplateName will be used as default formation templane name if no other options are provided
 var DefaultTemplateName = "Side-by-side extensibility with Kyma"
 
 // Formation missing godoc
