e03bb9b902be47ecc509efda5d33f982c60a0b91
ivanetenevvasilev <ivantenevvasilev@abv.bg>
2022-06-21 15:15:15 +0300

    swap up and down migrations

--- components/schema-migrator/migrations/director/20220621145611_make-formation-template-name-unique.down.sql
+++ components/schema-migrator/migrations/director/20220621145611_make-formation-template-name-unique.down.sql
@@ -1,5 +1,5 @@
 BEGIN;
 
-ALTER TABLE formation_templates ADD CONSTRAINT formation_template_unique_name UNIQUE (name);
+ALTER TABLE formation_templates DROP CONSTRAINT formation_template_unique_name;
 
 COMMIT;
--- components/schema-migrator/migrations/director/20220621145611_make-formation-template-name-unique.up.sql
+++ components/schema-migrator/migrations/director/20220621145611_make-formation-template-name-unique.up.sql
@@ -1,5 +1,5 @@
 BEGIN;
 
-ALTER TABLE formation_templates DROP CONSTRAINT formation_template_unique_name;
+ALTER TABLE formation_templates ADD CONSTRAINT formation_template_unique_name UNIQUE (name);
 
 COMMIT;
