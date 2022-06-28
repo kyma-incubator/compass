56c258c2e8a38ad5a135bc6b032b47f21bbf665e
Nikolay Mateev <bgdunkers@gmail.com>
2022-06-28 16:11:05 +0300

    Add unit tests

---
+++ components/director/pkg/retry/retry_test.go
@@ -0,0 +1,78 @@
+package retry
+
+import (
+	"github.com/pkg/errors"
+	"github.com/stretchr/testify/require"
+	"net/http"
+	"testing"
+	"time"
+)
+
+var defaultConfig = Config{
+	Attempts: 3,
+	Delay:    100 * time.Millisecond,
+}
+
+var mockedErr = errors.New("error")
+
+func TestExecuteSuccessfulWhenNoErrors(t *testing.T) {
+	t.Run("should finish successfully when there are no errors when executing the http request", func(t *testing.T) {
+		retryExecutor := NewHTTPExecutor(&defaultConfig)
+
+		invocations := 0
+		mockedResp := &http.Response{}
+		resp, err := retryExecutor.Execute(func() (*http.Response, error) {
+			defer func() {
+				invocations++
+			}()
+
+			return mockedResp, nil
+		})
+
+		require.Equal(t, mockedResp, resp)
+		require.NoError(t, err)
+		require.Equal(t, 1, invocations)
+	})
+}
+
+func TestExecuteFailsInitiallyButSucceedsOnLastTryWithNoErrors(t *testing.T) {
+	t.Run("should finish successfully on the last retry attempt", func(t *testing.T) {
+		retryExecutor := NewHTTPExecutor(&defaultConfig)
+
+		invocations := 0
+		mockedResp := &http.Response{}
+		resp, err := retryExecutor.Execute(func() (*http.Response, error) {
+			defer func() {
+				invocations++
+			}()
+
+			if invocations != (int(defaultConfig.Attempts) - 1) {
+				return nil, mockedErr
+			}
+
+			return mockedResp, nil
+		})
+
+		require.Equal(t, mockedResp, resp)
+		require.NoError(t, err)
+		require.Equal(t, int(defaultConfig.Attempts), invocations)
+	})
+}
+
+func TestExecuteFailsWhenAllRetryAttemptsFinishWithFailure(t *testing.T) {
+	t.Run("should finish with failure when all retry attempts fail", func(t *testing.T) {
+		retryExecutor := NewHTTPExecutor(&defaultConfig)
+
+		invocations := 0
+		resp, err := retryExecutor.Execute(func() (*http.Response, error) {
+			defer func() {
+				invocations++
+			}()
+			return nil, mockedErr
+		})
+
+		require.Nil(t, resp)
+		require.Error(t, err)
+		require.Equal(t, int(defaultConfig.Attempts), invocations)
+	})
+}
