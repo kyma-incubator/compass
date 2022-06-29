146b0c897f73b69f1761cdd5b057e943668b18e2
Alex Targov <alextargov@gmail.com>
2022-06-29 14:05:34 +0300

    Revert logic

--- tests/director/tests/certificates_access_test.go
+++ tests/director/tests/certificates_access_test.go
@@ -6,8 +6,6 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/kyma-incubator/compass/tests/pkg/assertions"
-
 	"github.com/kyma-incubator/compass/tests/pkg/certs/certprovider"
 
 	"github.com/kyma-incubator/compass/components/director/pkg/graphql"
@@ -98,116 +96,117 @@ func TestIntegrationSystemAccess(t *testing.T) {
 	}
 }
 
-func TestApplicationTemplateWithExternalCertificate(t *testing.T) {
-	// GIVEN
-	ctx := context.Background()
-	// Build graphql director client configured with certificate
-	externalCertProviderConfig := buildExternalCertProviderConfig()
-
-	pk, cert := certprovider.NewExternalCertFromConfig(t, ctx, externalCertProviderConfig)
-	directorCertSecuredClient := gql.NewCertAuthorizedGraphQLClientWithCustomURL(conf.DirectorExternalCertSecuredURL, pk, cert, conf.SkipSSLValidation)
-	tenantId := tenant.TestTenants.GetDefaultTenantID()
-
-	t.Run("Create Application Template with external certificate", func(t *testing.T) {
-		// WHEN
-		t.Log("Create application template")
-		name := createAppTemplateName("create-app-template-with-external-cert-name")
-		appTemplateInput := fixAppTemplateInput(name)
-		appTemplate, err := fixtures.CreateApplicationTemplateFromInput(t, ctx, directorCertSecuredClient, tenantId, appTemplateInput)
-		defer fixtures.CleanupApplicationTemplate(t, ctx, directorCertSecuredClient, tenantId, &appTemplate)
-
-		//THEN
-		require.NoError(t, err)
-		require.NotEmpty(t, appTemplate.ID)
-
-		t.Log("Check if application template was created")
-		appTemplateOutput := fixtures.GetApplicationTemplate(t, ctx, directorCertSecuredClient, tenantId, appTemplate.ID)
-		require.NotEmpty(t, appTemplateOutput)
-
-		// Enhance input to match the newly created labels
-		appTemplateInput.Labels[conf.SubscriptionConfig.SelfRegisterLabelKey] = appTemplateOutput.Labels[conf.SubscriptionConfig.SelfRegisterLabelKey]
-		appTemplateInput.Labels["global_subaccount_id"] = conf.ConsumerID
-		assertions.AssertApplicationTemplate(t, appTemplateInput, appTemplateOutput)
-	})
-
-	t.Run("Delete Application Template with external certificate", func(t *testing.T) {
-		t.Log("Create application template")
-		name := createAppTemplateName("delete-app-template-with-external-cert-name")
-		appTemplateInput := fixAppTemplateInput(name)
-		appTemplate, err := fixtures.CreateApplicationTemplateFromInput(t, ctx, directorCertSecuredClient, tenantId, appTemplateInput)
-		defer fixtures.CleanupApplicationTemplate(t, ctx, directorCertSecuredClient, tenantId, &appTemplate)
-
-		require.NoError(t, err)
-		require.NotEmpty(t, appTemplate.ID)
-
-		// WHEN
-		t.Log("Delete application template")
-		fixtures.DeleteApplicationTemplate(t, ctx, directorCertSecuredClient, tenantId, appTemplate.ID)
-
-		//THEN
-		t.Log("Check if application template was deleted")
-		out := fixtures.GetApplicationTemplate(t, ctx, directorCertSecuredClient, tenantId, appTemplate.ID)
-
-		require.Empty(t, out)
-	})
-}
-
-func TestAddBundleToApplicationWithExternalCertificate(t *testing.T) {
-	// GIVEN
-	ctx := context.Background()
-	// Build graphql director client configured with certificate
-	externalCertProviderConfig := buildExternalCertProviderConfig()
-
-	pk, cert := certprovider.NewExternalCertFromConfig(t, ctx, externalCertProviderConfig)
-	directorCertSecuredClient := gql.NewCertAuthorizedGraphQLClientWithCustomURL(conf.DirectorExternalCertSecuredURL, pk, cert, conf.SkipSSLValidation)
-	tenantId := tenant.TestTenants.GetDefaultTenantID()
-
-	name := createAppTemplateName("app-template-with-external-cert-name")
-
-	t.Log("Create application template")
-	appTemplateInput := fixAppTemplateInput(name)
-	appTemplate, err := fixtures.CreateApplicationTemplateFromInput(t, ctx, directorCertSecuredClient, tenantId, appTemplateInput)
-	defer fixtures.CleanupApplicationTemplate(t, ctx, directorCertSecuredClient, tenantId, &appTemplate)
-
-	require.NoError(t, err)
-	require.NotEmpty(t, appTemplate.ID)
-
-	// Register application from app template
-	appFromTmpl := graphql.ApplicationFromTemplateInput{TemplateName: name, Values: []*graphql.TemplateValueInput{{Placeholder: "name", Value: "test-name"}, {Placeholder: "display-name", Value: "test-name"}}}
-	appFromTmplGQL, err := testctx.Tc.Graphqlizer.ApplicationFromTemplateInputToGQL(appFromTmpl)
-	require.NoError(t, err)
-	createAppFromTmplRequest := fixtures.FixRegisterApplicationFromTemplate(appFromTmplGQL)
-	outputApp := graphql.ApplicationExt{}
-
-	err = testctx.Tc.RunOperationWithCustomTenant(ctx, directorCertSecuredClient, tenantId, createAppFromTmplRequest, &outputApp)
-	defer fixtures.UnregisterApplication(t, ctx, directorCertSecuredClient, tenantId, outputApp.ID)
-
-	t.Run("Success", func(t *testing.T) {
-		//WHEN
-		bndlName := "test-bundle"
-		bndl := fixtures.CreateBundle(t, ctx, certSecuredGraphQLClient, tenantId, outputApp.ID, bndlName)
-		defer fixtures.DeleteBundle(t, ctx, certSecuredGraphQLClient, tenantId, bndl.ID)
-
-		//THEN
-		require.NoError(t, err)
-		require.NotEmpty(t, outputApp)
-	})
-
-	t.Run("Error when no header is present", func(t *testing.T) {
-		//WHEN
-		bndlName := "test-bundle"
-		in, err := testctx.Tc.Graphqlizer.BundleCreateInputToGQL(fixtures.FixBundleCreateInput(bndlName))
-		require.NoError(t, err)
-
-		req := fixtures.FixAddBundleRequest(outputApp.ID, in)
-		var resp graphql.BundleExt
-
-		err = testctx.Tc.RunOperationWithoutTenant(ctx, certSecuredGraphQLClient, req, &resp)
-
-		//THEN
-		require.Error(t, err)
-	})
-}
+// Uncomment this tests once the tenant context provider is present in the hydrators
+//func TestApplicationTemplateWithExternalCertificate(t *testing.T) {
+//	// GIVEN
+//	ctx := context.Background()
+//	// Build graphql director client configured with certificate
+//	externalCertProviderConfig := buildExternalCertProviderConfig()
+//
+//	pk, cert := certprovider.NewExternalCertFromConfig(t, ctx, externalCertProviderConfig)
+//	directorCertSecuredClient := gql.NewCertAuthorizedGraphQLClientWithCustomURL(conf.DirectorExternalCertSecuredURL, pk, cert, conf.SkipSSLValidation)
+//	tenantId := tenant.TestTenants.GetDefaultTenantID()
+//
+//	t.Run("Create Application Template with external certificate", func(t *testing.T) {
+//		// WHEN
+//		t.Log("Create application template")
+//		name := createAppTemplateName("create-app-template-with-external-cert-name")
+//		appTemplateInput := fixAppTemplateInput(name)
+//		appTemplate, err := fixtures.CreateApplicationTemplateFromInput(t, ctx, directorCertSecuredClient, tenantId, appTemplateInput)
+//		defer fixtures.CleanupApplicationTemplate(t, ctx, directorCertSecuredClient, tenantId, &appTemplate)
+//
+//		//THEN
+//		require.NoError(t, err)
+//		require.NotEmpty(t, appTemplate.ID)
+//
+//		t.Log("Check if application template was created")
+//		appTemplateOutput := fixtures.GetApplicationTemplate(t, ctx, directorCertSecuredClient, tenantId, appTemplate.ID)
+//		require.NotEmpty(t, appTemplateOutput)
+//
+//		// Enhance input to match the newly created labels
+//		appTemplateInput.Labels[conf.SubscriptionConfig.SelfRegisterLabelKey] = appTemplateOutput.Labels[conf.SubscriptionConfig.SelfRegisterLabelKey]
+//		appTemplateInput.Labels["global_subaccount_id"] = conf.ConsumerID
+//		assertions.AssertApplicationTemplate(t, appTemplateInput, appTemplateOutput)
+//	})
+//
+//	t.Run("Delete Application Template with external certificate", func(t *testing.T) {
+//		t.Log("Create application template")
+//		name := createAppTemplateName("delete-app-template-with-external-cert-name")
+//		appTemplateInput := fixAppTemplateInput(name)
+//		appTemplate, err := fixtures.CreateApplicationTemplateFromInput(t, ctx, directorCertSecuredClient, tenantId, appTemplateInput)
+//		defer fixtures.CleanupApplicationTemplate(t, ctx, directorCertSecuredClient, tenantId, &appTemplate)
+//
+//		require.NoError(t, err)
+//		require.NotEmpty(t, appTemplate.ID)
+//
+//		// WHEN
+//		t.Log("Delete application template")
+//		fixtures.DeleteApplicationTemplate(t, ctx, directorCertSecuredClient, tenantId, appTemplate.ID)
+//
+//		//THEN
+//		t.Log("Check if application template was deleted")
+//		out := fixtures.GetApplicationTemplate(t, ctx, directorCertSecuredClient, tenantId, appTemplate.ID)
+//
+//		require.Empty(t, out)
+//	})
+//}
+//
+//func TestAddBundleToApplicationWithExternalCertificate(t *testing.T) {
+//	// GIVEN
+//	ctx := context.Background()
+//	// Build graphql director client configured with certificate
+//	externalCertProviderConfig := buildExternalCertProviderConfig()
+//
+//	pk, cert := certprovider.NewExternalCertFromConfig(t, ctx, externalCertProviderConfig)
+//	directorCertSecuredClient := gql.NewCertAuthorizedGraphQLClientWithCustomURL(conf.DirectorExternalCertSecuredURL, pk, cert, conf.SkipSSLValidation)
+//	tenantId := tenant.TestTenants.GetDefaultTenantID()
+//
+//	name := createAppTemplateName("app-template-with-external-cert-name")
+//
+//	t.Log("Create application template")
+//	appTemplateInput := fixAppTemplateInput(name)
+//	appTemplate, err := fixtures.CreateApplicationTemplateFromInput(t, ctx, directorCertSecuredClient, tenantId, appTemplateInput)
+//	defer fixtures.CleanupApplicationTemplate(t, ctx, directorCertSecuredClient, tenantId, &appTemplate)
+//
+//	require.NoError(t, err)
+//	require.NotEmpty(t, appTemplate.ID)
+//
+//	// Register application from app template
+//	appFromTmpl := graphql.ApplicationFromTemplateInput{TemplateName: name, Values: []*graphql.TemplateValueInput{{Placeholder: "name", Value: "test-name"}, {Placeholder: "display-name", Value: "test-name"}}}
+//	appFromTmplGQL, err := testctx.Tc.Graphqlizer.ApplicationFromTemplateInputToGQL(appFromTmpl)
+//	require.NoError(t, err)
+//	createAppFromTmplRequest := fixtures.FixRegisterApplicationFromTemplate(appFromTmplGQL)
+//	outputApp := graphql.ApplicationExt{}
+//
+//	err = testctx.Tc.RunOperationWithCustomTenant(ctx, directorCertSecuredClient, tenantId, createAppFromTmplRequest, &outputApp)
+//	defer fixtures.UnregisterApplication(t, ctx, directorCertSecuredClient, tenantId, outputApp.ID)
+//
+//	t.Run("Success", func(t *testing.T) {
+//		//WHEN
+//		bndlName := "test-bundle"
+//		bndl := fixtures.CreateBundle(t, ctx, certSecuredGraphQLClient, tenantId, outputApp.ID, bndlName)
+//		defer fixtures.DeleteBundle(t, ctx, certSecuredGraphQLClient, tenantId, bndl.ID)
+//
+//		//THEN
+//		require.NoError(t, err)
+//		require.NotEmpty(t, outputApp)
+//	})
+//
+//	t.Run("Error when no header is present", func(t *testing.T) {
+//		//WHEN
+//		bndlName := "test-bundle"
+//		in, err := testctx.Tc.Graphqlizer.BundleCreateInputToGQL(fixtures.FixBundleCreateInput(bndlName))
+//		require.NoError(t, err)
+//
+//		req := fixtures.FixAddBundleRequest(outputApp.ID, in)
+//		var resp graphql.BundleExt
+//
+//		err = testctx.Tc.RunOperationWithoutTenant(ctx, certSecuredGraphQLClient, req, &resp)
+//
+//		//THEN
+//		require.Error(t, err)
+//	})
+//}
 
 func buildExternalCertProviderConfig() certprovider.ExternalCertProviderConfig {
 	return certprovider.ExternalCertProviderConfig{
