ea104e942c951e88970737f8ed9277b3cbe70f5d
Nikolay Yordanov <n.yordanov@sap.com>
2022-07-12 12:48:54 +0300

    Add unit test

--- components/director/internal/authenticator/claims/validators_test.go
+++ components/director/internal/authenticator/claims/validators_test.go
@@ -183,6 +183,21 @@ func TestValidator_Validate(t *testing.T) {
 			ExpectedErr: testErr.Error(),
 		},
 		{
+			Name:   "Consumer-provider flow: Error when no subscription was found",
+			Claims: getClaimsForRuntimeConsumerProviderFlow(consumerTenantID, consumerExtTenantID, providerTenantID, providerExtTenantID, scopes, region, clientID),
+			RuntimeServiceFn: func() *automock.RuntimeService {
+				runtimeSvc := &automock.RuntimeService{}
+				runtimeSvc.On("GetByFilters", contextThatHasTenant(providerTenantID), expectedFilters).Return(runtime, nil).Once()
+				return runtimeSvc
+			},
+			RuntimeCtxSvcFn: func() *automock.RuntimeCtxService {
+				rtmCtxSvc := &automock.RuntimeCtxService{}
+				rtmCtxSvc.On("ListByFilter", contextThatHasTenant(consumerTenantID), runtimeID, mock.Anything, 100, "").Return(&model.RuntimeContextPage{}, nil).Once()
+				return rtmCtxSvc
+			},
+			ExpectedErr: fmt.Sprintf("Consumer's external tenant %s was not found as subscription record in the runtime context table for the runtime in the provider tenant %s", consumerExtTenantID, providerTenantID),
+		},
+		{
 			Name:   "Consumer-provider flow: Error when transaction cannot be committed",
 			Claims: getClaimsForRuntimeConsumerProviderFlow(consumerTenantID, consumerExtTenantID, providerTenantID, providerExtTenantID, scopes, region, clientID),
 			RuntimeServiceFn: func() *automock.RuntimeService {
