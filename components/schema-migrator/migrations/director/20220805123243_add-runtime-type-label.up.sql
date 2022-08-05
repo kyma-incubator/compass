caf50aecd6d06660ad0b2a5fa786f6a974b7acfb
StanislavStefanov <39959090+StanislavStefanov@users.noreply.github.com>
2022-08-05 13:56:54 +0300

    Fix runtime type imutability (#2531)
    * Fix runtimeType label immutability (#2517)
    
    * Initial implementation.
    
    * Update values.yaml
    
    * Fix unit tests.
    
    * Fix e2e test.
    
    * Add e2e test for runtimeType immutability.
    
    * Update values.yaml
    
    * Add unit tests.
    
    * Unit tests improvement.
    
    * Cleanup code.
    
    * Fix runtimeType immutability e2e test. (#2528)
    
    * Fix e2e test.
    
    * Update values.yaml
    
    * add runtime type label where missing
    
    * bump PR version
    
    Co-authored-by: Lachezar Bogomilov <64094901+la4ezar@users.noreply.github.com>

---
+++ components/schema-migrator/migrations/director/20220805123243_add-runtime-type-label.up.sql
@@ -0,0 +1,9 @@
+BEGIN;
+
+INSERT INTO labels(id, tenant_id, app_id, runtime_id, key, value, runtime_context_id, version, app_template_id)
+SELECT uuid_generate_v4() as id, NULL::uuid as tenant_id, NULL::uuid as app_id, t.runtime_id, 'runtimeType' as key, '"kyma"'::jsonb as value, NULL::uuid as runtime_context_id, 0 as version, NULL::uuid as app_template_id
+FROM (SELECT DISTINCT runtime_id FROM labels WHERE runtime_id IS NOT NULL
+    EXCEPT
+    SELECT runtime_id FROM labels where key = 'xsappname' or key = 'runtimeType') as t;
+
+COMMIT;
