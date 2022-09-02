19bbd8d950697de1b375c1fae01a3a1ba0d42861
StanislavStefanov <stanislavsvetlinovstefanov@gmail.com>
2022-09-02 14:47:58 +0300

    print path to charts

--- installation/scripts/install-compass.sh
+++ installation/scripts/install-compass.sh
@@ -56,6 +56,7 @@ done
 set -- "${POSITIONAL[@]}" # restore positional parameters
 
 echo "Install Compass"
+echo "Path to compass charts: " ${COMPASS_CHARTS}
 CRDS_FOLDER="${CURRENT_DIR}/../resources/crds"
 kubectl apply -f "${CRDS_FOLDER}"
 helm upgrade --install --wait --debug --timeout "${TIMEOUT}" -f ./mergedOverrides.yaml --create-namespace --namespace compass-system compass "${COMPASS_CHARTS}"
