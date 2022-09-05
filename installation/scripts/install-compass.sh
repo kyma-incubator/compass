f62c3a29b3d65bb260aab60518b082f7d164573f
StanislavStefanov <39959090+StanislavStefanov@users.noreply.github.com>
2022-09-05 17:23:06 +0300

    Fix merge

--- installation/scripts/install-compass.sh
+++ installation/scripts/install-compass.sh
@@ -57,7 +57,5 @@ set -- "${POSITIONAL[@]}" # restore positional parameters
 
 echo "Install Compass"
 echo "Path to compass charts: " ${COMPASS_CHARTS}
-CRDS_FOLDER="${CURRENT_DIR}/../resources/crds"
-kubectl apply -f "${CRDS_FOLDER}"
 helm upgrade --install --wait --timeout "${TIMEOUT}" -f ./mergedOverrides.yaml --create-namespace --namespace compass-system compass "${COMPASS_CHARTS}"
 trap "cleanup_trap" RETURN EXIT INT TERM
