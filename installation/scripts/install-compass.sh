28a2f666f463bc45cba746ce1a4c3cebe2b02c1f
StanislavStefanov <39959090+StanislavStefanov@users.noreply.github.com>
2022-09-05 20:41:53 +0300

    Allow for path to charts to be passed to installation script (#2605)
    * add --compass-charts flag
    
    * print path to charts
    
    * Fix merge

--- installation/scripts/install-compass.sh
+++ installation/scripts/install-compass.sh
@@ -37,6 +37,12 @@ do
             shift # past argument
             shift
         ;;
+        --compass-charts)
+            checkInputParameterValue "${2}"
+            COMPASS_CHARTS="${2}"
+            shift # past argument
+            shift
+        ;;
         --*)
             echo "Unknown flag ${1}"
             exit 1
@@ -50,5 +56,6 @@ done
 set -- "${POSITIONAL[@]}" # restore positional parameters
 
 echo "Install Compass"
+echo "Path to compass charts: " ${COMPASS_CHARTS}
 helm upgrade --install --wait --timeout "${TIMEOUT}" -f ./mergedOverrides.yaml --create-namespace --namespace compass-system compass "${COMPASS_CHARTS}"
 trap "cleanup_trap" RETURN EXIT INT TERM
