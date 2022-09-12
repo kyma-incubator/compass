c80db5772bee99c81e9f7d9c7beab6309192d2b8
PetarTodorovv <31803034+PetarTodorovv@users.noreply.github.com>
2022-09-12 16:16:34 +0300

    [HOTFIX] Destination fetcher fix extension service defaults (#2622)
    * [HOTFIX] Destination fetcher fix extension service defaults
    
    * bump components versions

--- components/director/internal/destinationfetchersvc/service.go
+++ components/director/internal/destinationfetchersvc/service.go
@@ -17,11 +17,7 @@ import (
 	"github.com/pkg/errors"
 )
 
-const (
-	correlationIDPrefix = "sap.s4:communicationScenario:"
-	s4HANAType          = "SAP S/4HANA Cloud"
-	regionLabelKey      = "region"
-)
+const regionLabelKey = "region"
 
 //go:generate mockery --name=UUIDService --output=automock --outpkg=automock --case=underscore --disable-version-string
 // UUIDService missing godoc
