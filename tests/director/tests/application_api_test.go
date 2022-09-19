1403864cf9ea99d920b88179caa6f7f07ec81ffd
Alex Targov <alextargov@gmail.com>
2022-09-19 21:34:43 +0300

    Use env variable

--- tests/director/tests/application_api_test.go
+++ tests/director/tests/application_api_test.go
@@ -431,8 +431,6 @@ func TestCreateApplicationWithNonExistentIntegrationSystem(t *testing.T) {
 }
 
 func TestUpdateApplication(t *testing.T) {
-	supportedApplicationType := "SAP temp1"
-
 	t.Run("Success", func(t *testing.T) {
 		// GIVEN
 		ctx := context.Background()
@@ -482,7 +480,7 @@ func TestUpdateApplication(t *testing.T) {
 		ctx := context.Background()
 
 		appInput := fixtures.FixSampleApplicationRegisterInput("before")
-		appInput.Labels["applicationType"] = supportedApplicationType
+		appInput.Labels["applicationType"] = conf.SupportedORDApplicationType
 		actualApp, err := fixtures.RegisterApplicationFromInput(t, ctx, certSecuredGraphQLClient, tenant.TestTenants.GetDefaultTenantID(), appInput)
 		defer fixtures.CleanupApplication(t, ctx, certSecuredGraphQLClient, tenant.TestTenants.GetDefaultTenantID(), &actualApp)
 		require.NoError(t, err)
@@ -513,7 +511,7 @@ func TestUpdateApplication(t *testing.T) {
 		ctx := context.Background()
 
 		appInput := fixtures.FixSampleApplicationRegisterInput("before")
-		appInput.Labels["applicationType"] = supportedApplicationType
+		appInput.Labels["applicationType"] = conf.SupportedORDApplicationType
 		actualApp, err := fixtures.RegisterApplicationFromInput(t, ctx, certSecuredGraphQLClient, tenant.TestTenants.GetDefaultTenantID(), appInput)
 		defer fixtures.CleanupApplication(t, ctx, certSecuredGraphQLClient, tenant.TestTenants.GetDefaultTenantID(), &actualApp)
 		require.NoError(t, err)
