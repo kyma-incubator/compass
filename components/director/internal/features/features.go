9d53270a8361043d3fd5432e854ffcd018116947
PetarTodorovv <31803034+PetarTodorovv@users.noreply.github.com>
2022-09-21 16:00:24 +0300

    Enrich self register labels for runtimes (#2640)
    * Enrich self register labels
    
    * make linter happy
    
    * make linter happy
    
    * add saas app label assertions in the e2e tests
    
    * add subpath for the self register volumes
    
    * remove subpaths
    
    * adjust tests
    
    * change k3d version
    
    * minor changes
    
    * apply review comment

--- components/director/internal/features/features.go
+++ components/director/internal/features/features.go
@@ -4,7 +4,7 @@ package features
 type Config struct {
 	DefaultScenarioEnabled       bool   `envconfig:"default=true,APP_DEFAULT_SCENARIO_ENABLED"`
 	ProtectedLabelPattern        string `envconfig:"default=.*_defaultEventing"`
-	ImmutableLabelPattern        string `envconfig:"APP_SELF_REGISTER_LABEL_KEY_PATTERN,default=^xsappnameCMPClone$|^runtimeType$"`
+	ImmutableLabelPattern        string `envconfig:"APP_SELF_REGISTER_LABEL_KEY_PATTERN,default=^xsappnameCMPClone$|^runtimeType$|^CMPSaaSAppName$"`
 	SubscriptionProviderLabelKey string `envconfig:"APP_SUBSCRIPTION_PROVIDER_LABEL_KEY,default=subscriptionProviderId"`
 	ConsumerSubaccountLabelKey   string `envconfig:"APP_CONSUMER_SUBACCOUNT_LABEL_KEY,default=global_subaccount_id"`
 	TokenPrefix                  string `envconfig:"APP_TOKEN_PREFIX,default=sb-"`
