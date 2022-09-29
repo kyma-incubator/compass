bf7e57fea8f4a08be924777ec3b36639d5e383b8
Alex Targov <alextargov@gmail.com>
2022-09-29 14:58:44 +0300

    Add migrations

---
+++ components/schema-migrator/migrations/director/20220929154500_remove_unpairapplication_webhook_type.up.sql
@@ -0,0 +1,131 @@
+BEGIN;
+
+-- Drop views that use the webhook type as dependency
+
+DROP VIEW IF EXISTS application_webhooks_tenants;
+DROP VIEW IF EXISTS runtime_webhooks_tenants;
+DROP VIEW IF EXISTS webhooks_tenants;
+
+-- Remove UNPAIR_APPLICATION webhook_type
+
+ALTER TABLE webhooks
+    ALTER COLUMN type TYPE VARCHAR(255);
+
+DROP TYPE webhook_type;
+
+CREATE TYPE webhook_type AS ENUM (
+    'CONFIGURATION_CHANGED',
+    'REGISTER_APPLICATION',
+    'UNREGISTER_APPLICATION',
+    'OPEN_RESOURCE_DISCOVERY'
+    );
+
+ALTER TABLE webhooks
+    ALTER COLUMN type TYPE webhook_type USING (type::webhook_type);
+
+-- Recreate views
+
+CREATE OR REPLACE VIEW webhooks_tenants
+            (id, app_id, url, type, auth, mode, correlation_id_key, retry_interval, timeout, url_template,
+             input_template, header_template, output_template, status_template, runtime_id, integration_system_id,
+             app_template_id, tenant_id, owner)
+AS
+SELECT w.id,
+       w.app_id,
+       w.url,
+       w.type,
+       w.auth,
+       w.mode,
+       w.correlation_id_key,
+       w.retry_interval,
+       w.timeout,
+       w.url_template,
+       w.input_template,
+       w.header_template,
+       w.output_template,
+       w.status_template,
+       w.runtime_id,
+       w.integration_system_id,
+       w.app_template_id,
+       ta.tenant_id,
+       ta.owner
+FROM webhooks w
+         JOIN tenant_applications ta ON w.app_id = ta.id
+UNION ALL
+SELECT w.id,
+       w.app_id,
+       w.url,
+       w.type,
+       w.auth,
+       w.mode,
+       w.correlation_id_key,
+       w.retry_interval,
+       w.timeout,
+       w.url_template,
+       w.input_template,
+       w.header_template,
+       w.output_template,
+       w.status_template,
+       w.runtime_id,
+       w.integration_system_id,
+       w.app_template_id,
+       tr.tenant_id,
+       tr.owner
+FROM webhooks w
+         JOIN tenant_runtimes tr ON w.runtime_id = tr.id;
+
+CREATE OR REPLACE VIEW runtime_webhooks_tenants
+            (id, app_id, url, type, auth, mode, correlation_id_key, retry_interval, timeout, url_template,
+             input_template, header_template, output_template, status_template, runtime_id, integration_system_id,
+             app_template_id, tenant_id, owner)
+AS
+SELECT w.id,
+       w.app_id,
+       w.url,
+       w.type,
+       w.auth,
+       w.mode,
+       w.correlation_id_key,
+       w.retry_interval,
+       w.timeout,
+       w.url_template,
+       w.input_template,
+       w.header_template,
+       w.output_template,
+       w.status_template,
+       w.runtime_id,
+       w.integration_system_id,
+       w.app_template_id,
+       tr.tenant_id,
+       tr.owner
+FROM webhooks w
+         JOIN tenant_runtimes tr ON w.runtime_id = tr.id;
+
+CREATE OR REPLACE VIEW application_webhooks_tenants
+            (id, app_id, url, type, auth, mode, correlation_id_key, retry_interval, timeout, url_template,
+             input_template, header_template, output_template, status_template, runtime_id, integration_system_id,
+             app_template_id, tenant_id, owner)
+AS
+SELECT w.id,
+       w.app_id,
+       w.url,
+       w.type,
+       w.auth,
+       w.mode,
+       w.correlation_id_key,
+       w.retry_interval,
+       w.timeout,
+       w.url_template,
+       w.input_template,
+       w.header_template,
+       w.output_template,
+       w.status_template,
+       w.runtime_id,
+       w.integration_system_id,
+       w.app_template_id,
+       ta.tenant_id,
+       ta.owner
+FROM webhooks w
+         JOIN tenant_applications ta ON w.app_id = ta.id;
+
+COMMIT;
