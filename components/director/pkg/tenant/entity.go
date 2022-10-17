6beeb430eeb2fdcaa2263f081cf1dbcda46954a4
Alex Targov <alextargov@gmail.com>
2022-10-17 16:56:35 +0300

    Add more tenant types

--- components/director/pkg/tenant/entity.go
+++ components/director/pkg/tenant/entity.go
@@ -28,6 +28,12 @@ const (
 	Account Type = "account"
 	// Subaccount tenants must have a parent of type Account.
 	Subaccount Type = "subaccount"
+	// Organization tenants can be parents of Folder tenants.
+	Organization Type = "organization"
+	// Folder tenants must have a parent of type Organization.
+	Folder Type = "folder"
+	// ResourceGroup tenants must have a parent of type Folder.
+	ResourceGroup Type = "resource-group"
 )
 
 // Status is used to determine if a tenant is currently being used or not.
@@ -63,6 +69,12 @@ func StrToType(value string) Type {
 		return Customer
 	case string(Subaccount):
 		return Subaccount
+	case string(Organization):
+		return Organization
+	case string(Folder):
+		return Folder
+	case string(ResourceGroup):
+		return ResourceGroup
 	default:
 		return Unknown
 	}
@@ -77,6 +89,12 @@ func TypeToStr(value Type) string {
 		return string(Customer)
 	case Subaccount:
 		return string(Subaccount)
+	case Organization:
+		return string(Organization)
+	case Folder:
+		return string(Folder)
+	case ResourceGroup:
+		return string(ResourceGroup)
 	default:
 		return string(Unknown)
 	}
