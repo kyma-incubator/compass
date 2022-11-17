f45bd32aedc3a243d7e2e85b62f0edf569556e4b
Dobromir Zahariev <dobromir.zahariev@sap.com>
2022-11-17 17:00:16 +0200

    Lint errors

--- components/director/internal/domain/subscription/fixtures_test.go
+++ components/director/internal/domain/subscription/fixtures_test.go
@@ -95,10 +95,10 @@ func fixModelApplicationFromTemplateInput(name, subscribedAppName, subdomain, re
 	}
 }
 
-func fixModelApplicationFromTemplateInputWithPlaceholderPayload(name, subscribedAppName, tenantID, mainUrl, externalSystemId string) model.ApplicationFromTemplateInput {
+func fixModelApplicationFromTemplateInputWithPlaceholderPayload(name, subscribedAppName, tenantID, mainURL, externalSystemID string) model.ApplicationFromTemplateInput {
 	return model.ApplicationFromTemplateInput{
 		TemplateName:        name,
-		PlaceholdersPayload: str.Ptr(fmt.Sprintf("{\"displayName\":\"%s\",\"systemNumber\":\"%s\",\"additionalUrls\":{\"mainUrl\":\"%s\"},\"externalResourceId\":\"%s\"}", subscribedAppName, tenantID, mainUrl, externalSystemId)),
+		PlaceholdersPayload: str.Ptr(fmt.Sprintf("{\"displayName\":\"%s\",\"systemNumber\":\"%s\",\"additionalUrls\":{\"mainUrl\":\"%s\"},\"externalResourceId\":\"%s\"}", subscribedAppName, tenantID, mainURL, externalSystemID)),
 	}
 }
 
