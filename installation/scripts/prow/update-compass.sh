14d8e5632bdf2ca9abc7d9a170595e63ddcc1946
ivanetenevvasilev <ivantenevvasilev@abv.bg>
2022-11-24 08:41:03 +0200

    delete old image cache

--- installation/scripts/prow/update-compass.sh
+++ installation/scripts/prow/update-compass.sh
@@ -9,4 +9,7 @@ set -o errexit
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 INSTALLATION_DIR=${CURRENT_DIR}/../../
 
+docker exec k3d-kyma-agent-0  sh -c "ctr images rm \$(ctr image list -q name~=eu.gcr.io/kyma-project/incubator)"
+docker exec k3d-kyma-server-0  sh -c "ctr images rm \$(ctr image list -q name~=eu.gcr.io/kyma-project/incubator)"
+
 sudo ${INSTALLATION_DIR}/cmd/run.sh --skip-k3d-start --skip-kyma-start --skip-db-install
