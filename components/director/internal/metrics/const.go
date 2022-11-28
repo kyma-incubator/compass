92fb744c49455663e46bcb6fcfe21b3c9123e633
Emma Karova <emma.karova@sap.com>
2022-11-28 16:11:48 +0200

    Rename constant

--- components/director/internal/metrics/const.go
+++ components/director/internal/metrics/const.go
@@ -15,8 +15,8 @@ const (
 	OrdAggregatorSubsystem = "ordaggregator"
 	// ErrorMetricLabel is the error label used by metrics config for creating CounterVec for Prometheus
 	ErrorMetricLabel = "error"
-	// APPIDMetricLabel is an additional label used by ord aggregator for creating CounterVec for Prometheus
-	APPIDMetricLabel = "app_id"
+	// AppIDMetricLabel is an additional label used by ord aggregator for creating CounterVec for Prometheus
+	AppIDMetricLabel = "app_id"
 	// CorrelationIDMetricLabel is an additional label used by ord aggregator for creating CounterVec for Prometheus
 	CorrelationIDMetricLabel = "x_request_id"
 )
