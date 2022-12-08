5155de7255a0f906e09f842990708256b1d99637
Nikolay Mateev <bgdunkers@gmail.com>
2022-12-08 13:22:17 +0200

    Revert "Skip empty title ORD Bundles"
    This reverts commit e7d7b3473dc793730f2aa1a44099b6c3c1f51518.

--- components/director/internal/open_resource_discovery/ord_document.go
+++ components/director/internal/open_resource_discovery/ord_document.go
@@ -148,13 +148,7 @@ func (docs Documents) Validate(calculatedBaseURL string, apisFromDB map[string]*
 				errs = multierror.Append(errs, errors.Wrapf(err, "error validating package with ord id %q", pkg.OrdID))
 			}
 		}
-
-		finalBundles := make([]*model.BundleCreateInput, 0)
-		for i, bndl := range doc.ConsumptionBundles {
-			if bndl.Name == "" {
-				continue
-			}
-
+		for _, bndl := range doc.ConsumptionBundles {
 			if err := validateBundleInput(bndl); err != nil {
 				errs = multierror.Append(errs, errors.Wrapf(err, "error validating bundle with ord id %q", stringPtrToString(bndl.OrdID)))
 			}
@@ -164,9 +158,7 @@ func (docs Documents) Validate(calculatedBaseURL string, apisFromDB map[string]*
 				}
 				bundleIDs[*bndl.OrdID] = true
 			}
-			finalBundles = append(finalBundles, doc.ConsumptionBundles[i])
 		}
-		doc.ConsumptionBundles = finalBundles
 		for _, product := range doc.Products {
 			if err := validateProductInput(product); err != nil {
 				errs = multierror.Append(errs, errors.Wrapf(err, "error validating product with ord id %q", product.OrdID))
