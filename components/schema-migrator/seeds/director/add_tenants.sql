53d375470519d4e1f49c96050007c3ab137e2566
Nikolay Yordanov <n.yordanov@sap.com>
2022-12-11 17:45:12 +0200

    Add tenant scoped FT for local director run and smoke-tests

--- components/schema-migrator/seeds/director/add_tenants.sql
+++ components/schema-migrator/seeds/director/add_tenants.sql
@@ -3,4 +3,13 @@ INSERT INTO public.business_tenant_mappings (id, external_name, external_tenant,
 ('1eba80dd-8ff6-54ee-be4d-77944d17b10b', 'foo', '1eba80dd-8ff6-54ee-be4d-77944d17b10b', 'Compass', 'Active', 'account'),
 ('af9f84a9-1d3a-4d9f-ae0c-94f883b33b6e', 'bar', 'af9f84a9-1d3a-4d9f-ae0c-94f883b33b6e', 'Compass', 'Active', 'account');
 
+INSERT INTO formation_templates(id, name, application_types, runtime_types, runtime_type_display_name, runtime_artifact_kind, tenant_id)
+VALUES (uuid_generate_v4(), 'Side-by-side extensibility with Kyma',
+        '["SAP Cloud for Customer", "SAP Commerce Cloud", "SAP Field Service Management", "SAP Marketing Cloud"]',
+        '["kyma"]',
+        'SAP BTP Kyma',
+        'ENVIRONMENT_INSTANCE',
+        '3e64ebae-38b5-46a0-b1ed-9ccee153a0ae'
+       );
+
 
