9d3d8837762e38c9d7ba0ba87a90b13ba5b0727b
Alex Targov <alextargov@gmail.com>
2023-01-17 09:45:19 +0200

    Fix all other migrations

--- components/schema-migrator/migrations/director/201909241337_add_system_auths.up.sql
+++ components/schema-migrator/migrations/director/201909241337_add_system_auths.up.sql
@@ -2,9 +2,9 @@ CREATE TABLE system_auths (
     id uuid PRIMARY KEY CHECK (id <> '00000000-0000-0000-0000-000000000000'),
     tenant_id uuid NOT NULL,
     app_id uuid,
-    foreign key (tenant_id, app_id) references applications (tenant_id, id) ON DELETE CASCADE,
+    constraint system_auths_tenant_id_fkey foreign key (tenant_id, app_id) references applications (tenant_id, id) ON DELETE CASCADE,
     runtime_id uuid,
-    foreign key (tenant_id, runtime_id) references runtimes (tenant_id, id) ON DELETE CASCADE,
+    constraint system_auths_tenant_id_fkey1 foreign key (tenant_id, runtime_id) references runtimes (tenant_id, id) ON DELETE CASCADE,
     integration_system_id uuid,
     value jsonb,
     CONSTRAINT valid_refs CHECK (app_id IS NOT NULL OR runtime_id IS NOT NULL OR integration_system_id IS NOT NULL)
