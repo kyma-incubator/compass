3f113bf5ce09fdea44bcda9c9e202c37d4b2a325
Alexander Targov <alextargov@gmail.com>
2023-01-25 18:39:07 +0200

    Application tenancy synchroization (#2836)
    * Initial impl
    
    * Update values.yaml
    
    * Add another e2e test
    
    * Fix lint error
    
    * Use new tenantID
    
    * Unit test fix
    
    * Refactor directive
    
    * Address pr comments
    
    * Address more pr comments

--- components/director/internal/domain/runtime/automock/tenant_service.go
+++ components/director/internal/domain/runtime/automock/tenant_service.go
@@ -14,27 +14,6 @@ type TenantService struct {
 	mock.Mock
 }
 
-// CreateManyIfNotExists provides a mock function with given fields: ctx, tenantInputs
-func (_m *TenantService) CreateManyIfNotExists(ctx context.Context, tenantInputs ...model.BusinessTenantMappingInput) error {
-	_va := make([]interface{}, len(tenantInputs))
-	for _i := range tenantInputs {
-		_va[_i] = tenantInputs[_i]
-	}
-	var _ca []interface{}
-	_ca = append(_ca, ctx)
-	_ca = append(_ca, _va...)
-	ret := _m.Called(_ca...)
-
-	var r0 error
-	if rf, ok := ret.Get(0).(func(context.Context, ...model.BusinessTenantMappingInput) error); ok {
-		r0 = rf(ctx, tenantInputs...)
-	} else {
-		r0 = ret.Error(0)
-	}
-
-	return r0
-}
-
 // GetTenantByExternalID provides a mock function with given fields: ctx, id
 func (_m *TenantService) GetTenantByExternalID(ctx context.Context, id string) (*model.BusinessTenantMapping, error) {
 	ret := _m.Called(ctx, id)
