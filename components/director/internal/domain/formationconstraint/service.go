c8d601029d5d3126d0898a117cb097e6b358b66c
StanislavStefanov <stanislavsvetlinovstefanov@gmail.com>
2023-02-06 15:49:43 +0200

    fix

--- components/director/internal/domain/formationconstraint/service.go
+++ components/director/internal/domain/formationconstraint/service.go
@@ -2,6 +2,7 @@ package formationconstraint
 
 import (
 	"context"
+
 	"github.com/kyma-incubator/compass/components/director/internal/model"
 	"github.com/kyma-incubator/compass/components/director/pkg/formationconstraint"
 	"github.com/kyma-incubator/compass/components/director/pkg/log"
