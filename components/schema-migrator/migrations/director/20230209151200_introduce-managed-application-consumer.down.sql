eb584fb82ea0c28abeefdbbb305eef175ed731bd
Alex Targov <alextargov@gmail.com>
2023-02-10 09:30:01 +0200

    Revert migrations

--- components/schema-migrator/migrations/director/20230209151200_introduce-managed-application-consumer.down.sql
+++
@@ -1,6 +0,0 @@
-BEGIN;
-
-ALTER TABLE cert_subject_mapping
-    ADD CONSTRAINT consumer_type CHECK ( consumer_type IN ('Runtime', 'Integration System', 'Application', 'Super Admin"', 'Business Integration', 'Technical Client', 'global'));
-
-COMMIT;
--- components/schema-migrator/migrations/director/20230209151200_introduce-managed-application-consumer.up.sql
+++
@@ -1,6 +0,0 @@
-BEGIN;
-
-ALTER TABLE cert_subject_mapping
-    DROP CONSTRAINT cert_subject_mapping_consumer_type_check;
-
-COMMIT;
