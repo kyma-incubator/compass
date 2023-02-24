28d081e67ab520ead7d80d14c41d9562c38dd291
Dobromir Zahariev <dobromir.zahariev@sap.com>
2023-02-24 15:48:33 +0200

    Finalise adoption

--- components/schema-migrator/migrations/director/20230224160000_add_policy_level_to_apis_and_events.down.sql
+++ components/schema-migrator/migrations/director/20230224160000_add_policy_level_to_apis_and_events.down.sql
@@ -1,9 +1,22 @@
 
 BEGIN;
 
+-- We need to drop these views because they references the 'policy_level' type. After we modify the `policy_level` type we can recreate the views as is done eventually in this script.
+DROP VIEW IF EXISTS tenants_packages;
+DROP VIEW IF EXISTS packages_tenants;
+
+
 DROP VIEW IF EXISTS tenants_specifications; -- drop tenant_specifications because tenants_apis and tenants_events depend on it. There are no specific changes on this view.
 DROP VIEW IF EXISTS tenants_apis;
 DROP VIEW IF EXISTS tenants_events;
+
+ALTER TYPE policy_level RENAME TO policy_level_old;
+
+CREATE TYPE policy_level AS ENUM ('custom', 'sap:core:v1','sap:partner:v1', 'none');
+
+ALTER TABLE packages
+    ALTER COLUMN policy_level TYPE policy_level
+        USING policy_level::text::policy_level;
 
 ALTER TABLE api_definitions
     DROP COLUMN policy_level,
@@ -149,5 +162,76 @@ FROM specifications spec
                       e.tenant_id
                FROM tenants_events e) t_api_event_def
               ON spec.api_def_id = t_api_event_def.id OR spec.event_def_id = t_api_event_def.id;
+
+CREATE OR REPLACE VIEW tenants_packages
+            (tenant_id, id, ord_id, title, short_description, description, version, package_links,
+             links, licence_type, tags, countries, labels, policy_level, app_id, custom_policy_level, vendor,
+             part_of_products, line_of_business, industry, resource_hash, support_info)
+AS
+SELECT DISTINCT t_apps.tenant_id,
+                p.id,
+                p.ord_id,
+                p.title,
+                p.short_description,
+                p.description,
+                p.version,
+                p.package_links,
+                p.links,
+                p.licence_type,
+                p.tags,
+                p.countries,
+                p.labels,
+                p.policy_level,
+                p.app_id,
+                p.custom_policy_level,
+                p.vendor,
+                p.part_of_products,
+                p.line_of_business,
+                p.industry,
+                p.resource_hash,
+                p.support_info
+FROM packages p
+         JOIN (SELECT a1.id,
+                      a1.tenant_id AS tenant_id
+               FROM tenant_applications a1
+               UNION ALL
+               SELECT apps_subaccounts.id,
+                      apps_subaccounts.tenant_id
+               FROM apps_subaccounts) t_apps
+              ON p.app_id = t_apps.id;
+
+CREATE OR REPLACE VIEW packages_tenants
+            (id, ord_id, title, short_description, description, version, package_links, links, licence_type, tags,
+             countries, labels, policy_level, app_id, custom_policy_level, vendor, part_of_products, line_of_business,
+             industry, resource_hash, documentation_labels, support_info, tenant_id, owner)
+AS
+SELECT p.id,
+       p.ord_id,
+       p.title,
+       p.short_description,
+       p.description,
+       p.version,
+       p.package_links,
+       p.links,
+       p.licence_type,
+       p.tags,
+       p.countries,
+       p.labels,
+       p.policy_level,
+       p.app_id,
+       p.custom_policy_level,
+       p.vendor,
+       p.part_of_products,
+       p.line_of_business,
+       p.industry,
+       p.resource_hash,
+       p.documentation_labels,
+       p.support_info,
+       ta.tenant_id,
+       ta.owner
+FROM packages p
+         JOIN tenant_applications ta ON ta.id = p.app_id;
+
+DROP TYPE policy_level_old;
 
 COMMIT;
--- components/schema-migrator/migrations/director/20230224160000_add_policy_level_to_apis_and_events.up.sql
+++ components/schema-migrator/migrations/director/20230224160000_add_policy_level_to_apis_and_events.up.sql
@@ -1,8 +1,20 @@
 BEGIN;
 
+-- We need to drop these views because they references the 'policy_level' type. After we modify the `policy_level` type we can recreate the views as is done eventually in this script.
+DROP VIEW IF EXISTS tenants_packages;
+DROP VIEW IF EXISTS packages_tenants;
+
 DROP VIEW IF EXISTS tenants_specifications; -- drop tenant_specifications because tenants_apis and tenants_events depend on it. There are no specific changes on this view.
 DROP VIEW IF EXISTS tenants_apis;
 DROP VIEW IF EXISTS tenants_events;
+
+ALTER TYPE policy_level RENAME TO policy_level_old;
+
+CREATE TYPE policy_level AS ENUM ('custom', 'sap:core:v1', 'none');
+
+ALTER TABLE packages
+    ALTER COLUMN policy_level TYPE policy_level
+        USING policy_level::text::policy_level;
 
 ALTER TABLE api_definitions
     ADD COLUMN policy_level policy_level,
@@ -15,11 +27,11 @@ ALTER TABLE event_api_definitions
 CREATE OR REPLACE VIEW tenants_apis
             (tenant_id, id, app_id, name, description, group_name, default_auth, version_value,
              version_deprecated, version_deprecated_since, version_for_removal, ord_id, short_description,
-             system_instance_aware, api_protocol, tags, countries, links, api_resource_links, release_status,
+             system_instance_aware, policy_level, custom_policy_level, api_protocol, tags, countries, links, api_resource_links, release_status,
              sunset_date, changelog_entries, labels, package_id, visibility, disabled, part_of_products,
              line_of_business, industry, ready, created_at, updated_at, deleted_at, error, implementation_standard,
              custom_implementation_standard, custom_implementation_standard_description, target_urls, extensible,
-             successors, resource_hash, documentation_labels, policy_level, custom_policy_level)
+             successors, resource_hash, documentation_labels)
 AS
 SELECT DISTINCT t_apps.tenant_id,
                 apis.id,
@@ -35,6 +47,8 @@ SELECT DISTINCT t_apps.tenant_id,
                 apis.ord_id,
                 apis.short_description,
                 apis.system_instance_aware,
+                apis.policy_level,
+                apis.custom_policy_level,
                 apis.api_protocol,
                 apis.tags,
                 apis.countries,
@@ -62,9 +76,7 @@ SELECT DISTINCT t_apps.tenant_id,
                 apis.extensible,
                 apis.successors,
                 apis.resource_hash,
-                apis.documentation_labels,
-                apis.policy_level,
-                apis.custom_policy_level
+                apis.documentation_labels
 FROM api_definitions apis
          JOIN (SELECT a1.id,
                       a1.tenant_id AS tenant_id
@@ -78,9 +90,9 @@ FROM api_definitions apis
 CREATE OR REPLACE VIEW tenants_events
             (tenant_id, id, app_id, name, description, group_name, version_value,
              version_deprecated, version_deprecated_since, version_for_removal, ord_id, short_description,
-             system_instance_aware, changelog_entries, links, tags, countries, release_status, sunset_date, labels,
+             system_instance_aware, policy_level, custom_policy_level, changelog_entries, links, tags, countries, release_status, sunset_date, labels,
              package_id, visibility, disabled, part_of_products, line_of_business, industry, ready, created_at,
-             updated_at, deleted_at, error, extensible, successors, resource_hash, policy_level, custom_policy_level)
+             updated_at, deleted_at, error, extensible, successors, resource_hash)
 AS
 SELECT DISTINCT t_apps.tenant_id,
                 events.id,
@@ -95,6 +107,8 @@ SELECT DISTINCT t_apps.tenant_id,
                 events.ord_id,
                 events.short_description,
                 events.system_instance_aware,
+                events.policy_level,
+                events.custom_policy_level,
                 events.changelog_entries,
                 events.links,
                 events.tags,
@@ -115,9 +129,7 @@ SELECT DISTINCT t_apps.tenant_id,
                 events.error,
                 events.extensible,
                 events.successors,
-                events.resource_hash,
-                events.policy_level,
-                events.custom_policy_level
+                events.resource_hash
 FROM event_api_definitions events
          JOIN (SELECT a1.id,
                       a1.tenant_id AS tenant_id
@@ -152,5 +164,76 @@ FROM specifications spec
                       e.tenant_id
                FROM tenants_events e) t_api_event_def
               ON spec.api_def_id = t_api_event_def.id OR spec.event_def_id = t_api_event_def.id;
+
+CREATE OR REPLACE VIEW tenants_packages
+            (tenant_id, id, ord_id, title, short_description, description, version, package_links,
+             links, licence_type, tags, countries, labels, policy_level, app_id, custom_policy_level, vendor,
+             part_of_products, line_of_business, industry, resource_hash, support_info)
+AS
+SELECT DISTINCT t_apps.tenant_id,
+                p.id,
+                p.ord_id,
+                p.title,
+                p.short_description,
+                p.description,
+                p.version,
+                p.package_links,
+                p.links,
+                p.licence_type,
+                p.tags,
+                p.countries,
+                p.labels,
+                p.policy_level,
+                p.app_id,
+                p.custom_policy_level,
+                p.vendor,
+                p.part_of_products,
+                p.line_of_business,
+                p.industry,
+                p.resource_hash,
+                p.support_info
+FROM packages p
+         JOIN (SELECT a1.id,
+                      a1.tenant_id AS tenant_id
+               FROM tenant_applications a1
+               UNION ALL
+               SELECT apps_subaccounts.id,
+                      apps_subaccounts.tenant_id
+               FROM apps_subaccounts) t_apps
+              ON p.app_id = t_apps.id;
+
+CREATE OR REPLACE VIEW packages_tenants
+            (id, ord_id, title, short_description, description, version, package_links, links, licence_type, tags,
+             countries, labels, policy_level, app_id, custom_policy_level, vendor, part_of_products, line_of_business,
+             industry, resource_hash, documentation_labels, support_info, tenant_id, owner)
+AS
+SELECT p.id,
+       p.ord_id,
+       p.title,
+       p.short_description,
+       p.description,
+       p.version,
+       p.package_links,
+       p.links,
+       p.licence_type,
+       p.tags,
+       p.countries,
+       p.labels,
+       p.policy_level,
+       p.app_id,
+       p.custom_policy_level,
+       p.vendor,
+       p.part_of_products,
+       p.line_of_business,
+       p.industry,
+       p.resource_hash,
+       p.documentation_labels,
+       p.support_info,
+       ta.tenant_id,
+       ta.owner
+FROM packages p
+         JOIN tenant_applications ta ON ta.id = p.app_id;
+
+DROP TYPE policy_level_old;
 
 COMMIT;
