ba45639bd0a9f76b27ce2d8612628cfd93163c44
Lachezar Bogomilov <lachezar.bogomilov@sap.com>
2023-02-28 12:41:59 +0200

    Improve app cleanup defers.

--- tests/director/tests/labeling_api_test.go
+++ tests/director/tests/labeling_api_test.go
@@ -182,14 +182,14 @@ func TestSearchApplicationsByLabels(t *testing.T) {
 	labelKeyBar := "bar"
 
 	firstApp, err := fixtures.RegisterApplication(t, ctx, certSecuredGraphQLClient, "first", tenantId)
-	require.NotEmpty(t, firstApp.ID)
 	defer fixtures.CleanupApplication(t, ctx, certSecuredGraphQLClient, tenantId, &firstApp)
+	require.NotEmpty(t, firstApp.ID)
 	require.NoError(t, err)
 
 	//Create second application
 	secondApp, err := fixtures.RegisterApplication(t, ctx, certSecuredGraphQLClient, "second", tenantId)
-	require.NotEmpty(t, secondApp.ID)
 	defer fixtures.CleanupApplication(t, ctx, certSecuredGraphQLClient, tenantId, &secondApp)
+	require.NotEmpty(t, secondApp.ID)
 	require.NoError(t, err)
 
 	//Set label "foo" on both applications
@@ -409,10 +409,10 @@ func TestDeleteLastScenarioForApplication(t *testing.T) {
 	}
 
 	application, err := fixtures.RegisterApplicationFromInput(t, ctx, certSecuredGraphQLClient, tenantID, appInput)
-	require.NoError(t, err)
-	require.NotEmpty(t, application.ID)
 	defer fixtures.CleanupApplication(t, ctx, certSecuredGraphQLClient, tenantID, &application)
 	defer fixtures.UnassignApplicationFromScenarios(t, ctx, certSecuredGraphQLClient, tenantID, application.ID, scenarios)
+	require.NoError(t, err)
+	require.NotEmpty(t, application.ID)
 
 	//WHEN
 	appLabelRequest := fixtures.FixSetApplicationLabelRequest(application.ID, ScenariosLabel, []string{scenarios[0]})
