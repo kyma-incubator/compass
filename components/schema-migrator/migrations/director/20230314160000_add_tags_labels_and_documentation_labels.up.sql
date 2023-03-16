743a80cb93866341e8484493e9eb079d8cfaa1ec
Dobromir Zahariev <dobromir.zahariev@sap.com>
2023-03-16 09:45:48 +0200

    Adopt v1.2.0 - 3

--- components/schema-migrator/migrations/director/20230314160000_add_tags_labels_and_documentation_labels.up.sql
+++ components/schema-migrator/migrations/director/20230314160000_add_tags_labels_and_documentation_labels.up.sql
@@ -1,6 +1,7 @@
 BEGIN;
 
 DROP VIEW IF EXISTS tenants_apps;
+DROP VIEW IF EXISTS listening_applications;
 
 ALTER TABLE products
     ADD COLUMN tags JSONB;
@@ -118,5 +119,39 @@ FROM applications apps
                       'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa' AS formation_id
                FROM apps_subaccounts) t_apps
               ON apps.id = t_apps.id;
+
+CREATE
+OR REPLACE VIEW listening_applications
+            (id, app_template_id, system_number, local_tenant_id, name, description, status_condition, status_timestamp,
+             system_status, healthcheck_url, integration_system_id, provider_name, base_url, application_namespace,
+             labels, ready, created_at, updated_at, deleted_at, error, correlation_ids, tags, documentation_labels,
+             webhook_type)
+AS
+SELECT a.id,
+       a.app_template_id,
+       a.system_number,
+       a.local_tenant_id,
+       a.name,
+       a.description,
+       a.status_condition,
+       a.status_timestamp,
+       a.system_status,
+       a.healthcheck_url,
+       a.integration_system_id,
+       a.provider_name,
+       a.base_url,
+       a.application_namespace,
+       a.labels,
+       a.ready,
+       a.created_at,
+       a.updated_at,
+       a.deleted_at,
+       a.error,
+       a.correlation_ids,
+       a.tags,
+       a.documentation_labels,
+       w.type
+FROM applications a
+         JOIN webhooks w on w.app_id = a.id or w.app_template_id = a.app_template_id;
 
 COMMIT;
