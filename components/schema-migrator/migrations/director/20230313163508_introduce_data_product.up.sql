32769c77bf8bd8e189a93539f63bce741c9d2ef3
Nikolay Yordanov <n.yordanov@sap.com>
2023-03-22 13:08:35 +0200

    Expose DataProducts & Ports to ORD Service

--- components/schema-migrator/migrations/director/20230313163508_introduce_data_product.up.sql
+++ components/schema-migrator/migrations/director/20230313163508_introduce_data_product.up.sql
@@ -105,6 +105,97 @@ ALTER TABLE api_definitions
 -- ALTER TABLE specifications
 --     ALTER COLUMN api_spec_type TYPE api_spec_type USING (api_spec_type::api_spec_type);
 
+CREATE OR REPLACE VIEW tenants_data_products
+            (tenant_id, formation_id, id, app_id, ord_id, local_id, title, short_description, description,
+             version, release_status, visibility, package_id, tags, industry, line_of_business, product_type, data_product_owner)
+AS
+SELECT DISTINCT t_apps.tenant_id,
+                t_apps.formation_id,
+                dp.id,
+                dp.app_id,
+                dp.ord_id,
+                dp.local_id,
+                dp.title,
+                dp.short_description,
+                dp.description,
+                dp.version,
+                dp.release_status,
+                dp.visibility,
+                dp.package_id,
+                dp.tags,
+                dp.industry,
+                dp.line_of_business,
+                dp.product_type,
+                dp.data_product_owner
+FROM data_products dp
+         JOIN (SELECT a1.id,
+                      a1.tenant_id AS tenant_id,
+                      'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa' AS formation_id
+               FROM tenant_applications a1
+               UNION ALL
+               SELECT apps_subaccounts.id,
+                      apps_subaccounts.tenant_id,
+                      apps_subaccounts.formation_id
+               FROM apps_subaccounts
+               UNION ALL
+               SELECT apps_subaccounts.id,
+                      apps_subaccounts.tenant_id,
+                      'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa' AS formation_id
+               FROM apps_subaccounts) t_apps
+              ON dp.app_id = t_apps.id;
+
+
+CREATE VIEW ord_data_products_tags AS
+SELECT id                  AS data_product_id,
+       elements.value      AS value
+FROM data_products,
+     jsonb_array_elements_text(data_products.tags) AS elements;
+
+CREATE VIEW ord_data_products_industry AS
+SELECT id                  AS data_product_id,
+       elements.value      AS value
+FROM data_products,
+     jsonb_array_elements_text(data_products.industry) AS elements;
+
+CREATE VIEW ord_data_products_line_of_business AS
+SELECT id                  AS data_product_id,
+       elements.value      AS value
+FROM data_products,
+     jsonb_array_elements_text(data_products.line_of_business) AS elements;
+
+
+CREATE OR REPLACE VIEW tenants_ports
+            (tenant_id, formation_id, id, data_product_id, app_id, name, port_type, description,
+             producer_cardinality, disabled)
+AS
+SELECT DISTINCT t_apps.tenant_id,
+                t_apps.formation_id,
+                p.id,
+                p.data_product_id,
+                p.app_id,
+                p.name,
+                p.port_type,
+                p.description,
+                p.producer_cardinality,
+                p.disabled
+FROM ports p
+         JOIN (SELECT a1.id,
+                      a1.tenant_id AS tenant_id,
+                      'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa' AS formation_id
+               FROM tenant_applications a1
+               UNION ALL
+               SELECT apps_subaccounts.id,
+                      apps_subaccounts.tenant_id,
+                      apps_subaccounts.formation_id
+               FROM apps_subaccounts
+               UNION ALL
+               SELECT apps_subaccounts.id,
+                      apps_subaccounts.tenant_id,
+                      'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa' AS formation_id
+               FROM apps_subaccounts) t_apps
+              ON p.app_id = t_apps.id;
+
+
 COMMIT;
 
 ALTER TYPE api_spec_type ADD VALUE 'sap-sql-api-definition-v1' BEFORE 'custom';
