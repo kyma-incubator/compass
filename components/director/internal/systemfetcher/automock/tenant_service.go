123279cde746e1aad00e0c339cb78343a47b9620
Dobromir Zahariev <dobromir.zahariev@sap.com>
2023-04-05 09:38:49 +0300

    Update Values.yaml

--- components/director/internal/systemfetcher/automock/tenant_service.go
+++ components/director/internal/systemfetcher/automock/tenant_service.go
@@ -35,6 +35,29 @@ func (_m *TenantService) GetInternalTenant(ctx context.Context, externalTenant s
 	return r0, r1
 }
 
+// GetTenantByExternalID provides a mock function with given fields: ctx, id
+func (_m *TenantService) GetTenantByExternalID(ctx context.Context, id string) (*model.BusinessTenantMapping, error) {
+	ret := _m.Called(ctx, id)
+
+	var r0 *model.BusinessTenantMapping
+	if rf, ok := ret.Get(0).(func(context.Context, string) *model.BusinessTenantMapping); ok {
+		r0 = rf(ctx, id)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*model.BusinessTenantMapping)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(context.Context, string) error); ok {
+		r1 = rf(ctx, id)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // List provides a mock function with given fields: ctx
 func (_m *TenantService) List(ctx context.Context) ([]*model.BusinessTenantMapping, error) {
 	ret := _m.Called(ctx)
