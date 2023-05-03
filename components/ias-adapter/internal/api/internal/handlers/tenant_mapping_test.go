4dce9757d9a85e2de5403508fa65148b9d8be8d7
ZdravkoGyurov <zdravko.gyurov97@gmail.com>
2023-05-03 09:48:40 +0300

    use before each

--- components/ias-adapter/internal/api/internal/handlers/tenant_mapping_test.go
+++ components/ias-adapter/internal/api/internal/handlers/tenant_mapping_test.go
@@ -17,7 +17,7 @@ import (
 )
 
 var _ = Describe("Tenant Mapping Handler", func() {
-	var tenantMappingOkReverse = types.TenantMapping{
+	var tenantMapping = types.TenantMapping{
 		FormationID: "2d933ae2-10c4-4d6f-b4d4-5e1553e4ff05",
 		ReceiverTenant: types.ReceiverTenant{
 			ApplicationURL: "localhost",
@@ -30,33 +30,16 @@ var _ = Describe("Tenant Mapping Handler", func() {
 				Parameters: types.AssignedTenantParameters{
 					ClientID: "clientID",
 				},
-				ReverseAssignmentState: "READY",
+				ReverseAssignmentState: "",
 			},
 		},
 	}
-
-	var tenantMappingFailedReverse = types.TenantMapping{
-		FormationID: "2d933ae2-10c4-4d6f-b4d4-5e1553e4ff05",
-		ReceiverTenant: types.ReceiverTenant{
-			ApplicationURL: "localhost",
-		},
-		AssignedTenants: []types.AssignedTenant{
-			{
-				UCLApplicationID: "2d933ae2-10c4-4d6f-b4d4-5e1553e4ff05",
-				LocalTenantID:    "2d933ae2-10c4-4d6f-b4d4-5e1553e4ff05",
-				Operation:        types.OperationAssign,
-				Parameters: types.AssignedTenantParameters{
-					ClientID: "clientID",
-				},
-				ReverseAssignmentState: "CREATE_ERROR",
-			},
-		},
-	}
+	BeforeEach(func() {
+		tenantMapping.AssignedTenants[0].ReverseAssignmentState = ""
+	})
 	When("Tenant mapping cannot be decoded", func() {
 		It("Should fail with 400", func() {
-			handler := TenantMappingsHandler{
-				Service: &automock.TenantMappingsService{},
-			}
+			handler := TenantMappingsHandler{Service: &automock.TenantMappingsService{}}
 
 			body := strings.NewReader("unprocessable body")
 			w, ctx := createTestRequest(body)
@@ -70,9 +53,7 @@ var _ = Describe("Tenant Mapping Handler", func() {
 	})
 	When("Tenant mapping is invalid", func() {
 		It("Should fail with 400", func() {
-			handler := TenantMappingsHandler{
-				Service: &automock.TenantMappingsService{},
-			}
+			handler := TenantMappingsHandler{Service: &automock.TenantMappingsService{}}
 
 			body := strings.NewReader(`{"assignedTenants":[{"configuration": ""}]}`)
 			w, ctx := createTestRequest(body)
@@ -84,14 +65,16 @@ var _ = Describe("Tenant Mapping Handler", func() {
 			Expect(w.Code).To(Equal(http.StatusBadRequest))
 		})
 	})
-	When("Reverse assignment state is not ready", func() {
-		It("Should fail with 422", func() {
+	When("Reverse assignment state is neither INITIAL nor READY", func() {
+		BeforeEach(func() {
+			tenantMapping.AssignedTenants[0].ReverseAssignmentState = "not-initial/ready"
+		})
+		It("Should fail with 422 CONFIG_PENDING", func() {
 			service := &automock.TenantMappingsService{}
-			handler := TenantMappingsHandler{
-				Service: service,
-			}
+			service.On("ProcessTenantMapping", mock.Anything, mock.Anything).Return(nil)
+			handler := TenantMappingsHandler{Service: service}
 
-			data, err := json.Marshal(tenantMappingFailedReverse)
+			data, err := json.Marshal(tenantMapping)
 			Expect(err).Error().ToNot(HaveOccurred())
 			body := bytes.NewReader(data)
 			w, ctx := createTestRequest(body)
@@ -101,14 +84,15 @@ var _ = Describe("Tenant Mapping Handler", func() {
 		})
 	})
 	When("Consumed APIs cannot be updated", func() {
+		BeforeEach(func() {
+			tenantMapping.AssignedTenants[0].ReverseAssignmentState = types.StateInitial
+		})
 		It("Should fail with 500", func() {
 			service := &automock.TenantMappingsService{}
 			service.On("ProcessTenantMapping", mock.Anything, mock.Anything).Return(errors.New("error"))
-			handler := TenantMappingsHandler{
-				Service: service,
-			}
+			handler := TenantMappingsHandler{Service: service}
 
-			data, err := json.Marshal(tenantMappingOkReverse)
+			data, err := json.Marshal(tenantMapping)
 			Expect(err).Error().ToNot(HaveOccurred())
 			body := bytes.NewReader(data)
 			w, ctx := createTestRequest(body)
@@ -118,14 +102,15 @@ var _ = Describe("Tenant Mapping Handler", func() {
 		})
 	})
 	When("Consumed APIs are successfully updated", func() {
+		BeforeEach(func() {
+			tenantMapping.AssignedTenants[0].ReverseAssignmentState = types.StateReady
+		})
 		It("Should return 200", func() {
 			service := &automock.TenantMappingsService{}
 			service.On("ProcessTenantMapping", mock.Anything, mock.Anything).Return(nil)
-			handler := TenantMappingsHandler{
-				Service: service,
-			}
+			handler := TenantMappingsHandler{Service: service}
 
-			data, err := json.Marshal(tenantMappingOkReverse)
+			data, err := json.Marshal(tenantMapping)
 			Expect(err).Error().ToNot(HaveOccurred())
 			body := bytes.NewReader(data)
 			w, ctx := createTestRequest(body)
