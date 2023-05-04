e911a304d71dc68c6f0ce6987a8f3e5061251417
Dragomir Todorov <dragomir.todorov@sap.com>
2023-05-04 09:58:45 +0300

    update graphqlizer applications query

--- components/director/pkg/graphql/graphqlizer/fields.go
+++ components/director/pkg/graphql/graphqlizer/fields.go
@@ -103,6 +103,7 @@ func (fp *GqlFieldsProvider) ForApplication(ctx ...FieldCtx) string {
 		systemStatus
 		integrationSystemID
 		applicationTemplateID
+		tenantBusinessType {id code name}
 		labels
 		deletedAt
 		updatedAt
