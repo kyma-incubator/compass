666822aaa9b1b2614b5f4d09eb8d09cac18704ea
PetarTodorovv <todorovv.petar@gmail.com>
2023-05-10 19:43:34 +0300

    destination creation changes

--- components/tm-adapter/internal/api/handlers/tenant_mapping.go
+++ components/tm-adapter/internal/api/handlers/tenant_mapping.go
@@ -12,9 +12,11 @@ import (
 	"github.com/kyma-incubator/compass/components/tm-adapter/internal/external_caller"
 	"github.com/kyma-incubator/compass/components/tm-adapter/pkg/httputil"
 	"github.com/pkg/errors"
+	"github.com/tidwall/gjson"
 	"io/ioutil"
 	"net/http"
 	"net/url"
+	"text/template"
 )
 
 const subaccountKey = "subaccount_id"
@@ -54,6 +56,18 @@ func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	if err := validate(tm); err != nil {
+		log.C(ctx).Errorf("Failed to validate tenant mapping request body: %v", err)
+		httputil.RespondWithError(ctx, w, http.StatusBadRequest, errors.New(""))
+		return
+	}
+
+	if string(tm.Items[0].Configuration) != "" {
+		log.C(ctx).Info("The configuration in the tenant mapping body is provided and no service instance/binding will be created. Returning...")
+		httputil.Respond(w, http.StatusOK)
+		return
+	}
+
 	catalogName := "certificate-service" // todo::: should be provided as label on the runtime/app-template and will be used through TM notification body
 	planName := "standard"               // todo::: should be provided as label on the runtime/app-template and will be used through TM notification body
 	h.tenantID = tm.ReceiverTenant.SubaccountID
@@ -88,16 +102,86 @@ func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	responseBody := types.Response{Configuration: serviceKey.Credentials}
+	//// old response body containing service key credentials
+	//responseBody := types.Response{Configuration: serviceKey.Credentials} // todo::: deletee
+
+	data, err := h.buildTemplateData(serviceKey.Credentials)
+	if err != nil {
+		log.C(ctx).Error(err)
+		httputil.RespondWithError(ctx, w, http.StatusInternalServerError, errResp)
+		return
+	}
+
+	mockURL := "https://uclmockapi.free.beeceptor.com/v1/tenantMappings/"
+	req, err := http.NewRequest(http.MethodPatch, mockURL, nil)
+	if err != nil {
+		log.C(ctx).Error("An error occurred while creating request to the mock API")
+		httputil.RespondWithError(ctx, w, http.StatusInternalServerError, errResp)
+		return
+	}
+
+	log.C(ctx).Info("Calling beeceptor mock API...")
+	resp, err := h.caller.Call(req)
+	if err != nil {
+		log.C(ctx).Error("An error occurred while calling beeceptor mock API")
+		httputil.RespondWithError(ctx, w, http.StatusInternalServerError, errResp)
+		return
+	}
+	defer closeResponseBody(ctx, resp)
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.C(ctx).Errorf("Failed to read response body from beeceptor mock API request: %v", err)
+		httputil.RespondWithError(ctx, w, http.StatusInternalServerError, errResp)
+		return
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		log.C(ctx).Errorf("Response status code is not the exepcted one, should be: %d, got: %d", http.StatusOK, resp.StatusCode)
+		httputil.RespondWithError(ctx, w, http.StatusInternalServerError, errResp)
+		return
+	}
+
+	t, err := template.New("").Parse(string(body))
+	if err != nil {
+		log.C(ctx).Errorf("An error occurred while creating template: %v", err)
+		httputil.RespondWithError(ctx, w, http.StatusInternalServerError, errResp)
+		return
+	}
+
+	res := new(bytes.Buffer)
+	if err = t.Execute(res, data); err != nil {
+		log.C(ctx).Errorf("An error occurred while executing template: %v", err)
+		httputil.RespondWithError(ctx, w, http.StatusInternalServerError, errResp)
+		return
+	}
+
+	var jsonRawMsg json.RawMessage
+	err = json.Unmarshal([]byte(res.String()), &jsonRawMsg)
+	if err != nil {
+		return
+	}
 
 	log.C(ctx).Infof("Successfully processed tenant mapping notification")
-	httputil.RespondWithBody(ctx, w, http.StatusOK, responseBody)
+	httputil.RespondWithBody(ctx, w, http.StatusOK, jsonRawMsg)
 }
 
 func closeResponseBody(ctx context.Context, resp *http.Response) {
 	if err := resp.Body.Close(); err != nil {
 		log.C(ctx).Errorf("An error has occurred while closing response body: %v", err)
 	}
+}
+
+func validate(tm types.TenantMapping) error {
+	if len(tm.Items) != 1 {
+		return errors.New("The items in the tenant mapping request body should consists of one element")
+	}
+
+	if tm.ReceiverTenant.SubaccountID != "" {
+		return errors.New("The subaccount ID in the tenant mapping request body should not be empty")
+	}
+
+	return nil
 }
 
 func (h *Handler) retrieveServiceOffering(ctx context.Context, catalogName string) (string, error) {
@@ -308,4 +392,37 @@ func buildURL(baseURL, path, tenantKey, tenantValue string) (string, error) {
 	base.RawQuery = params.Encode()
 
 	return base.String(), nil
+}
+
+func (h *Handler) buildTemplateData(serviceKeyCredentials json.RawMessage) (map[string]string, error) {
+	svcKeyURL := gjson.Get(string(serviceKeyCredentials), "certificateservice.apiurl").String()
+	if svcKeyURL == "" {
+		return nil, errors.New("could not find 'certificateservice.apiurl' property")
+	}
+
+	svcKeyClientID, ok := gjson.Get(string(serviceKeyCredentials), "uaa.clientid").Value().(string)
+	if !ok {
+		return nil, errors.New("could not find 'uaa.clientid' property")
+	}
+
+	svcKeyClientSecret, ok := gjson.Get(string(serviceKeyCredentials), "uaa.clientsecret").Value().(string)
+	if !ok {
+		return nil, errors.New("could not find 'uaa.clientsecret' property")
+	}
+
+	svcKeyTokenURL, ok := gjson.Get(string(serviceKeyCredentials), "uaa.url").Value().(string)
+	if !ok {
+		return nil, errors.New("could not find 'uaa.url' property")
+	}
+	tokenPath := "/oauth/token"
+
+	data := map[string]string{
+		"URL":          svcKeyURL,
+		"TokenURL":     svcKeyTokenURL + tokenPath,
+		"ClientID":     svcKeyClientID,
+		"ClientSecret": svcKeyClientSecret,
+		"SubaccountID": h.tenantID,
+	}
+
+	return data, nil
 }
--- components/tm-adapter/internal/api/types/tenant_mapping.go
+++ components/tm-adapter/internal/api/types/tenant_mapping.go
@@ -1,9 +1,16 @@
 package types
 
+import "encoding/json"
+
 type TenantMapping struct {
 	ReceiverTenant ReceiverTenant `json:"receiverTenant"`
+	Items          []Item         `json:"items"`
 }
 
 type ReceiverTenant struct {
-	SubaccountID         string `json:"subaccountId"`
+	SubaccountID string `json:"subaccountId"`
+}
+
+type Item struct {
+	Configuration json.RawMessage `json:"configuration"`
 }
