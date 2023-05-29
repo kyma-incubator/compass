fd2e2aa394308a7ccea02d56c2fc06fb668531fc
Dragomir Todorov <dragomir.todorov@sap.com>
2023-05-29 14:34:20 +0300

    initial implementation

--- components/director/internal/domain/subscription/resolver.go
+++ components/director/internal/domain/subscription/resolver.go
@@ -2,6 +2,8 @@ package subscription
 
 import (
 	"context"
+	"encoding/json"
+	"github.com/tidwall/gjson"
 
 	"github.com/kyma-incubator/compass/components/director/pkg/log"
 	"github.com/kyma-incubator/compass/components/director/pkg/persistence"
@@ -9,14 +11,27 @@ import (
 	"github.com/pkg/errors"
 )
 
+const SubscriptionIDKey = "subscriptionGUID"
+
+// DependentServiceInstanceInfo represents the dependent service instance info field in a subscription payload.
+type DependentServiceInstanceInfo struct {
+	AppID                string `json:"appId"` //check the json
+	AppName              string `json:"appName"`
+	ProviderSubaccountID string `json:"providerSubaccountId"`
+}
+
+type DependentServiceInstancesInfo struct {
+	Instances []DependentServiceInstanceInfo `json:"dependentServiceInstancesInfo"`
+}
+
 // SubscriptionService responsible for service-layer Subscription operations
 //
 //go:generate mockery --name=SubscriptionService --output=automock --outpkg=automock --case=underscore --disable-version-string
 type SubscriptionService interface {
-	SubscribeTenantToRuntime(ctx context.Context, providerID, subaccountTenantID, providerSubaccountID, consumerTenantID, region, subscriptionAppName string) (bool, error)
-	UnsubscribeTenantFromRuntime(ctx context.Context, providerID, subaccountTenantID, providerSubaccountID, consumerTenantID, region string) (bool, error)
-	SubscribeTenantToApplication(ctx context.Context, providerID, subaccountTenantID, consumerTenantID, region, subscribedAppName string, subscriptionPayload string) (bool, error)
-	UnsubscribeTenantFromApplication(ctx context.Context, providerID, subaccountTenantID, region string) (bool, error)
+	SubscribeTenantToRuntime(ctx context.Context, providerID, subaccountTenantID, providerSubaccountID, consumerTenantID, region, subscriptionAppName, subscriptionID string) (bool, error)
+	UnsubscribeTenantFromRuntime(ctx context.Context, providerID, subaccountTenantID, providerSubaccountID, consumerTenantID, region, subscriptionID string) (bool, error)
+	SubscribeTenantToApplication(ctx context.Context, providerID, subaccountTenantID, consumerTenantID, region, subscribedAppName, subscriptionID string, subscriptionPayload string) (bool, error)
+	UnsubscribeTenantFromApplication(ctx context.Context, providerID, subaccountTenantID, region, subscriptionID string) (bool, error)
 	DetermineSubscriptionFlow(ctx context.Context, providerID, region string) (resource.Type, error)
 }
 
@@ -35,6 +50,9 @@ func NewResolver(transact persistence.Transactioner, subscriptionSvc Subscriptio
 }
 
 // SubscribeTenant subscribes tenant to runtime labeled with `providerID` and `region`
+// providerId = appId (xsappname)
+// providerSubaccountID=providerSubaccountID = къде е deploynat
+// subscriptionAppName=appName
 func (r *Resolver) SubscribeTenant(ctx context.Context, providerID, subaccountTenantID, providerSubaccountID, consumerTenantID, region, subscriptionAppName string, subscriptionPayload string) (bool, error) {
 	tx, err := r.transact.Begin()
 	if err != nil {
@@ -49,23 +67,33 @@ func (r *Resolver) SubscribeTenant(ctx context.Context, providerID, subaccountTe
 		return false, errors.Wrap(err, "while determining subscription flow")
 	}
 
+	var dependentSvcInstancesInfo DependentServiceInstancesInfo
+	if err = json.Unmarshal([]byte(subscriptionPayload), &dependentSvcInstancesInfo); err != nil {
+		return false, errors.Wrapf(err, "while unmarshaling dependent service instance info")
+	}
 	var success bool
 
-	switch flowType {
-	case resource.ApplicationTemplate:
-		log.C(ctx).Infof("Entering application subscription flow")
-		success, err = r.subscriptionSvc.SubscribeTenantToApplication(ctx, providerID, subaccountTenantID, consumerTenantID, region, subscriptionAppName, subscriptionPayload)
-		if err != nil {
-			return false, err
+	for _, instance := range dependentSvcInstancesInfo.Instances {
+		providerID = instance.AppID
+		providerSubaccountID = instance.ProviderSubaccountID
+		subscriptionAppName = instance.AppName
+		subscriptionID := gjson.GetBytes([]byte(subscriptionPayload), SubscriptionIDKey).String()
+		switch flowType {
+		case resource.ApplicationTemplate:
+			log.C(ctx).Infof("Entering application subscription flow")
+			success, err = r.subscriptionSvc.SubscribeTenantToApplication(ctx, providerID, subaccountTenantID, consumerTenantID, region, subscriptionAppName, subscriptionID, subscriptionPayload)
+			if err != nil {
+				return false, err
+			}
+		case resource.Runtime:
+			log.C(ctx).Infof("Entering runtime subscription flow")
+			success, err = r.subscriptionSvc.SubscribeTenantToRuntime(ctx, providerID, subaccountTenantID, providerSubaccountID, consumerTenantID, region, subscriptionAppName, subscriptionID)
+			if err != nil {
+				return false, err
+			}
+		default:
+			log.C(ctx).Infof("Nothing to subscribe to provider (%q) in region (%q)", providerID, region)
 		}
-	case resource.Runtime:
-		log.C(ctx).Infof("Entering runtime subscription flow")
-		success, err = r.subscriptionSvc.SubscribeTenantToRuntime(ctx, providerID, subaccountTenantID, providerSubaccountID, consumerTenantID, region, subscriptionAppName)
-		if err != nil {
-			return false, err
-		}
-	default:
-		log.C(ctx).Infof("Nothing to subscribe to provider (%q) in region (%q)", providerID, region)
 	}
 
 	if err = tx.Commit(); err != nil {
@@ -76,7 +104,7 @@ func (r *Resolver) SubscribeTenant(ctx context.Context, providerID, subaccountTe
 }
 
 // UnsubscribeTenant unsubscribes tenant to runtime labeled with `providerID` and `region`
-func (r *Resolver) UnsubscribeTenant(ctx context.Context, providerID, subaccountTenantID, providerSubaccountID, consumerTenantID, region string) (bool, error) {
+func (r *Resolver) UnsubscribeTenant(ctx context.Context, providerID, subaccountTenantID, providerSubaccountID, consumerTenantID, region, subscriptionPayload string) (bool, error) {
 	tx, err := r.transact.Begin()
 	if err != nil {
 		return false, err
@@ -90,18 +118,19 @@ func (r *Resolver) UnsubscribeTenant(ctx context.Context, providerID, subaccount
 		return false, errors.Wrap(err, "while determining subscription flow")
 	}
 
+	subscriptionID := gjson.GetBytes([]byte(subscriptionPayload), SubscriptionIDKey).String()
 	var success bool
 
 	switch flowType {
 	case resource.ApplicationTemplate:
 		log.C(ctx).Infof("Entering application subscription flow")
-		success, err = r.subscriptionSvc.UnsubscribeTenantFromApplication(ctx, providerID, subaccountTenantID, region)
+		success, err = r.subscriptionSvc.UnsubscribeTenantFromApplication(ctx, providerID, subaccountTenantID, region, subscriptionID)
 		if err != nil {
 			return false, err
 		}
 	case resource.Runtime:
 		log.C(ctx).Infof("Entering runtime subscription flow")
-		success, err = r.subscriptionSvc.UnsubscribeTenantFromRuntime(ctx, providerID, subaccountTenantID, providerSubaccountID, consumerTenantID, region)
+		success, err = r.subscriptionSvc.UnsubscribeTenantFromRuntime(ctx, providerID, subaccountTenantID, providerSubaccountID, consumerTenantID, region, subscriptionID)
 		if err != nil {
 			return false, err
 		}
