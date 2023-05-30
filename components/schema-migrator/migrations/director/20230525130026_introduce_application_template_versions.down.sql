06d59c0315205087f835861043474bea32197b71
Alex Targov <alextargov@gmail.com>
2023-05-30 13:21:38 +0300

    Enhance migrations

--- components/schema-migrator/migrations/director/20230525130026_introduce_application_template_versions.down.sql
+++ components/schema-migrator/migrations/director/20230525130026_introduce_application_template_versions.down.sql
@@ -3,42 +3,58 @@ BEGIN;
 DROP TABLE IF EXISTS app_template_versions;
 
 ALTER TABLE api_definitions
-    DROP CONSTRAINT api_definitions_app_template_version_id_fk;
+    DROP CONSTRAINT api_definitions_app_template_version_id_fk,
+    DROP CONSTRAINT api_definitions_app_template_version_id_ord_id_unique,
+    DROP CONSTRAINT api_definitions_app_template_version_id;
 ALTER TABLE api_definitions
     DROP COLUMN app_template_version_id;
 
 ALTER TABLE bundles
-    DROP CONSTRAINT app_template_version_id;
+    DROP CONSTRAINT app_template_version_id,
+    DROP CONSTRAINT bundles_app_template_version_id_ord_id_unique,
+    DROP CONSTRAINT bundles_app_template_version_id;
 ALTER TABLE bundles
+    ALTER COLUMN app_id SET NOT NULL,
     DROP COLUMN app_template_version_id;
 
 ALTER TABLE documents
-    DROP CONSTRAINT documents_app_template_version_id_fk;
+    DROP CONSTRAINT documents_app_template_version_id_fk,
+    DROP CONSTRAINT documents_app_template_version_id;
 ALTER TABLE documents
     DROP COLUMN app_template_version_id;
 
 ALTER TABLE event_api_definitions
-    DROP CONSTRAINT app_template_version_id;
+    DROP CONSTRAINT app_template_version_id,
+    DROP CONSTRAINT event_api_definitions_app_template_version_id_ord_id_unique,
+    DROP CONSTRAINT event_api_definitions_app_template_version_id;
 ALTER TABLE event_api_definitions
     DROP COLUMN app_template_version_id;
 
 ALTER TABLE packages
-    DROP CONSTRAINT packages_app_template_version_id_fk;
+    DROP CONSTRAINT packages_app_template_version_id_fk,
+    DROP CONSTRAINT packages_app_template_version_id_ord_id_unique,
+    DROP CONSTRAINT packages_app_template_version_id;
 ALTER TABLE packages
+    ALTER COLUMN app_id SET NOT NULL,
     DROP COLUMN app_template_version_id;
 
 ALTER TABLE products
-    DROP CONSTRAINT products_app_template_version_id_fk;
+    DROP CONSTRAINT products_app_template_version_id_fk,
+    DROP CONSTRAINT products_app_template_version_id;
 ALTER TABLE products
+    ALTER COLUMN app_id SET NOT NULL,
     DROP COLUMN app_template_version_id;
 
 ALTER TABLE tombstones
-    DROP CONSTRAINT tombstones_app_template_version_id_fk;
+    DROP CONSTRAINT tombstones_app_template_version_id_fk,
+    DROP CONSTRAINT tombstones_app_template_version_id;
 ALTER TABLE tombstones
+    ALTER COLUMN app_id SET NOT NULL,
     DROP COLUMN app_template_version_id;
 
 ALTER TABLE vendors
-    DROP CONSTRAINT vendors_app_template_version_id_fk;
+    DROP CONSTRAINT vendors_app_template_version_id_fk,
+    DROP CONSTRAINT vendors_app_template_version_id;
 ALTER TABLE vendors
     DROP COLUMN app_template_version_id;
 
--- components/schema-migrator/migrations/director/20230525130026_introduce_application_template_versions.up.sql
+++ components/schema-migrator/migrations/director/20230525130026_introduce_application_template_versions.up.sql
@@ -15,12 +15,15 @@ CREATE TABLE IF NOT EXISTS app_template_versions
 ALTER TABLE api_definitions
     ADD COLUMN app_template_version_id UUID,
     ADD CONSTRAINT api_definitions_app_template_version_id_fk
-        FOREIGN KEY (app_template_version_id) REFERENCES app_template_versions (id) ON DELETE CASCADE ;
+        FOREIGN KEY (app_template_version_id) REFERENCES app_template_versions (id) ON DELETE CASCADE,
+    ADD CONSTRAINT api_definitions_app_template_version_id_ord_id_unique UNIQUE (app_template_version_id, ord_id);
 
 ALTER TABLE bundles
     ADD COLUMN app_template_version_id UUID,
+    ALTER COLUMN app_id DROP NOT NULL,
     ADD CONSTRAINT bundles_app_template_version_id_fk
-        FOREIGN KEY (app_template_version_id) REFERENCES app_template_versions (id) ON DELETE CASCADE ;
+        FOREIGN KEY (app_template_version_id) REFERENCES app_template_versions (id) ON DELETE CASCADE,
+    ADD CONSTRAINT bundles_app_template_version_id_ord_id_unique UNIQUE (app_template_version_id, ord_id);
 
 ALTER TABLE documents
     ADD COLUMN app_template_version_id UUID,
@@ -30,20 +33,25 @@ ALTER TABLE documents
 ALTER TABLE event_api_definitions
     ADD COLUMN app_template_version_id UUID,
     ADD CONSTRAINT event_api_definitions_app_template_version_id_fk
-        FOREIGN KEY (app_template_version_id) REFERENCES app_template_versions (id) ON DELETE CASCADE ;
+        FOREIGN KEY (app_template_version_id) REFERENCES app_template_versions (id) ON DELETE CASCADE,
+    ADD CONSTRAINT event_api_definitions_app_template_version_id_ord_id_unique UNIQUE (app_template_version_id, ord_id);
 
 ALTER TABLE packages
     ADD COLUMN app_template_version_id UUID,
+    ALTER COLUMN app_id DROP NOT NULL,
     ADD CONSTRAINT packages_app_template_version_id_fk
-        FOREIGN KEY (app_template_version_id) REFERENCES app_template_versions (id) ON DELETE CASCADE ;
+        FOREIGN KEY (app_template_version_id) REFERENCES app_template_versions (id) ON DELETE CASCADE,
+    ADD CONSTRAINT packages_app_template_version_id_ord_id_unique UNIQUE (app_template_version_id, ord_id);
 
 ALTER TABLE products
     ADD COLUMN app_template_version_id UUID,
+    ALTER COLUMN app_id DROP NOT NULL,
     ADD CONSTRAINT products_app_template_version_id_fk
         FOREIGN KEY (app_template_version_id) REFERENCES app_template_versions (id) ON DELETE CASCADE ;
 
 ALTER TABLE tombstones
     ADD COLUMN app_template_version_id UUID,
+    ALTER COLUMN app_id DROP NOT NULL,
     ADD CONSTRAINT tombstones_app_template_version_id_fk
         FOREIGN KEY (app_template_version_id) REFERENCES app_template_versions (id) ON DELETE CASCADE ;
 
@@ -51,5 +59,22 @@ ALTER TABLE vendors
     ADD COLUMN app_template_version_id UUID,
     ADD CONSTRAINT vendors_app_template_version_id_fk
         FOREIGN KEY (app_template_version_id) REFERENCES app_template_versions (id) ON DELETE CASCADE ;
+
+
+CREATE INDEX api_definitions_app_template_version_id ON api_definitions (app_template_version_id);
+
+CREATE INDEX bundles_app_template_version_id ON bundles (app_template_version_id);
+
+CREATE INDEX documents_app_template_version_id ON documents (app_template_version_id);
+
+CREATE INDEX event_api_definitions_app_template_version_id ON event_api_definitions (app_template_version_id);
+
+CREATE INDEX packages_app_template_version_id ON packages (app_template_version_id);
+
+CREATE INDEX products_app_template_version_id ON products (app_template_version_id);
+
+CREATE INDEX tombstones_app_template_version_id ON tombstones (app_template_version_id);
+
+CREATE INDEX vendors_app_template_version_id ON vendors (app_template_version_id);
 
 COMMIT;
