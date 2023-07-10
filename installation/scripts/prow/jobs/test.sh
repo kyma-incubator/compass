4d64ad889c9909d13e10003d2a6b95417fc15d74
Alex Targov <alextargov@gmail.com>
2023-07-10 11:49:56 +0300

    test

--- installation/scripts/prow/jobs/test.sh
+++ installation/scripts/prow/jobs/test.sh
@@ -15,9 +15,7 @@ log::info "Kyma CLI installed with version: $KYMA_CLI_VERSION"
 log::info "Installing Compass"
 
 ls -R
-cd ../ && ls
-cd ../ && ls
 
-~/compass/installation/cmd/run.sh
+../../installation/cmd/run.sh
 
 log::info "Compass installed"
