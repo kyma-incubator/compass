6a7dbbe57d3e78710fa0ee5a8b99b10e1242f15d
ognyvrac <ognyan.vrachanski@gmail.com>
2023-07-31 14:18:28 +0300

    update documentation

--- chart/ory/README.md
+++ chart/ory/README.md
@@ -2,12 +2,25 @@
 
 [ORY](https://www.ory.sh/) Open Source OAuth 2.0 & OpenID Connect
 
+> **_NOTE:_** The chart was copied from [Kyma](https://github.com/kyma-project/kyma/tree/main/resources/ory), this means that there are custom implementations
+
 ## Introduction
 
-This chart bootstraps [Hydra](https://www.ory.sh/docs/hydra/) and [Oathkeeper](https://www.ory.sh/docs/oathkeeper/) components on a [Kubernetes](http://kubernetes.io) cluster using the [Helm](https://helm.sh) package manager.
+This chart bootstraps [Hydra](https://www.ory.sh/docs/hydra/) and [Oathkeeper](https://www.ory.sh/docs/oathkeeper/) components on a [Kubernetes](http://kubernetes.io) cluster using the [Helm](https://helm.sh) package manager. 
 
-## Chart Details
+Furthermore, it includes two other subcharts for storage persistence:
 
-This chart installs two ORY components as subcharts:
-- hydra
-- oathkeeper
+- [gcloud-sqlproxy](https://github.com/rimusz/charts/tree/master/stable/gcloud-sqlproxy) - used to establish connection with databases situated on Google Cloud
+- [postgres](https://github.com/bitnami/charts/tree/main/bitnami/postgresql) - used for in-cluster storage persistence
+
+## Caveats
+
+The current state of `values.yaml` has been configured to work with the `run.sh` script; this script is used for local installation as explained in the [installation document](https://github.com/kyma-incubator/compass/blob/main/docs/compass/04-01-installation.md).
+
+### Hydra Secret
+
+The Hydra deployment expects an already existing K8s Secret with the name `existingSecret: "ory-hydra-credentials"`. The K8s Secret is created by the `installation/scripts/install-ory.sh` script, which is invoked by `run.sh`. This approach was chosen as the Hydra secrets need to be rotated in [a specific manner](https://www.ory.sh/docs/hydra/self-hosted/secrets-key-rotation#rotation-of-hmac-token-signing-and-database-and-cookie-encryption-keys); if the Secret was dynamically created with each Helm release it would break the deployment.
+
+### Oathkeeper Secret
+
+The Oathkeeper Secret is created by a CronJob that is part of the Helm release. However, the job needs to be patched and configured properly; this is also handled by `installation/scripts/install-ory.sh`. This interaction requires that the Helm release is installed by the Helm CLI without the `--wait` flag.
--- installation/scripts/install-ory.sh
+++ installation/scripts/install-ory.sh
@@ -4,13 +4,56 @@ set -o errexit
 
 ROOT_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../..
 
-# Copy the IDP configuration from the Compass chart; the Compass values are changed by the `run.sh`
-VALUES_FILE_COMPASS="${ROOT_PATH}"/chart/compass/values.yaml
-IDP_HOST=$(yq ".global.cockpit.auth.idpHost" $VALUES_FILE_COMPASS)
-AUTH_PATH=$(yq ".global.cockpit.auth.path" $VALUES_FILE_COMPASS)
-
 VALUES_FILE_ORY="${ROOT_PATH}"/chart/ory/values.yaml
 OVERRIDE_TEMP_ORY=ory-temp-values.yaml
+
+TIMEOUT=30m
+
+# Always create temporary override file, which is used whether "--overrides-file" is used or not
+cp "$VALUES_FILE_ORY" "$OVERRIDE_TEMP_ORY"
+
+# checkInputParameterValue is a function to check if input parameter is valid
+# There HAS to be provided argument:
+# $1 - value for input parameter
+# for example in installation/cmd/run.sh we can set --vm-driver argument, which has to have a value.
+
+function checkInputParameterValue() {
+    if [ -z "${1}" ] || [ "${1:0:2}" == "--" ]; then
+        echo "Wrong parameter value"
+        echo "Make sure parameter value is neither empty nor start with two hyphens"
+        exit 1
+    fi
+}
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+
+    case ${key} in
+        --overrides-file)
+            checkInputParameterValue "${2}"
+            yq eval-all --inplace '. as $item ireduce ({}; . * $item )' ${OVERRIDE_TEMP_ORY} ${2}
+            shift # past argument
+            shift
+        ;;
+        --timeout)
+            checkInputParameterValue "${2}"
+            TIMEOUT="${2}"
+            shift # past argument
+            shift
+        ;;
+        --*)
+            echo "Unknown flag ${1}"
+            exit 1
+        ;;
+        *)    # unknown option
+            POSITIONAL+=("$1") # save it in an array for later
+            shift # past argument
+        ;;
+    esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
 
 # Remove the temporary Ory values.yaml file created
 function cleanup_trap(){
@@ -24,17 +67,17 @@ function generate_random(){
   cat /dev/urandom | LC_ALL=C tr -dc 'a-z0-9' | fold -w ${1} | head -n 1
 }
 
+# Copy the IDP configuration from the Compass chart; the Compass values are changed by the `run.sh`
+VALUES_FILE_COMPASS="${ROOT_PATH}"/chart/compass/values.yaml
+IDP_HOST=$(yq ".global.cockpit.auth.idpHost" $VALUES_FILE_COMPASS)
+AUTH_PATH=$(yq ".global.cockpit.auth.path" $VALUES_FILE_COMPASS)
+
 # If IDP has been configured override the default values in the ORY chart values
 if [ ! -z "$IDP_HOST" ]; then
   JWKS_URL=$IDP_HOST$AUTH_PATH
 
-  cp "$VALUES_FILE_ORY" "$OVERRIDE_TEMP_ORY"
-
   # Overwrite the jwks_url of the Ory Oathkeeper to match the Compass values
   yq -i ".oathkeeper.oathkeeper.config.authenticators.jwt.config.jwks_urls = [\"$JWKS_URL\"]" "${OVERRIDE_TEMP_ORY}"
-  
-  # Use the temp ORY Helm values ofr the installation if IDP was provided
-  VALUES_FILE_ORY="$OVERRIDE_TEMP_ORY"
 fi
 
 trap cleanup_trap EXIT INT TERM
@@ -46,15 +89,17 @@ SECRET_NAME=ory-hydra-credentials
 
 kubectl create ns $RELEASE_NS || true
 
+LOCAL_PERSISTENCE=$(yq ".global.ory.hydra.persistence.postgresql.enabled" ${OVERRIDE_TEMP_ORY})
+
 # Create Secret that is referenced as 'existingSecret' under chart/ory/values.yaml
 # Secret should not be recreated if it exists, mainly during Helm updates, as it will create new random values.
 # The new random values will triggered the redeployment of the postgres db and Hydra - that breaks the deployment
 # Rotating the secrets has to be done manually; the rotation of the Hydra Secrets should be done following this guide: https://www.ory.sh/docs/hydra/self-hosted/secrets-key-rotation
-if [[ ! $(kubectl get secret $SECRET_NAME -n ${RELEASE_NS}) ]]; then
+if [ ! "$(kubectl get secret $SECRET_NAME -n ${RELEASE_NS})" -a "$LOCAL_PERSISTENCE" = true ]; then
   echo "Creating secret to be used by the Ory Helm Chart..."
-  POSTGRES_USERNAME=$(yq .global.postgresql.postgresqlUsername ${VALUES_FILE_ORY})
+  POSTGRES_USERNAME=$(yq .global.postgresql.postgresqlUsername ${OVERRIDE_TEMP_ORY})
   POSTGRES_PASSWORD=$(generate_random 10)
-  POSTGRES_DB=$(yq .global.postgresql.postgresqlDatabase ${VALUES_FILE_ORY})
+  POSTGRES_DB=$(yq .global.postgresql.postgresqlDatabase ${OVERRIDE_TEMP_ORY})
 
   DSN=postgres://${POSTGRES_USERNAME}:${POSTGRES_PASSWORD}@${RELEASE_NAME}-postgresql.${RELEASE_NS}.svc.cluster.local:5432/${POSTGRES_DB}?sslmode=disable\&max_conn_lifetime=10s
 
@@ -81,8 +126,9 @@ EOF
   echo "${SECRET}" | kubectl apply -f -
 fi
 
-# --wait is excluded as the deployment hangs; it hangs as there is a cronjob that creates the jwks secret
-helm upgrade --install $RELEASE_NAME -f "${VALUES_FILE_ORY}" -n $RELEASE_NS "${ROOT_PATH}"/chart/ory
+# --wait is excluded as the deployment hangs; it hangs as there is a cronjob that creates the jwks secret for Oathkeeper
+# This cronjob is triggered in the statements below
+helm upgrade --install $RELEASE_NAME -f "${OVERRIDE_TEMP_ORY}" -n $RELEASE_NS "${ROOT_PATH}"/chart/ory
 
 CRONJOB=oathkeeper-jwks-rotator
 # CronJob creates a Secret that is needed for the successful deployment of Oathkeeper
@@ -96,8 +142,8 @@ kubectl patch cronjob -n $RELEASE_NS $CRONJOB -p '{"spec":{"schedule": "0 0 1 * 
 
 # Wait for Ory deployment to roll out as they needs to be ready for successful compass installation
 echo "Waiting for Ory deployments to roll out..."
-if ! [ "$(kubectl rollout status deployment $RELEASE_NAME-hydra -n $RELEASE_NS --timeout=30m)" -a "$(kubectl rollout status deployment $RELEASE_NAME-oathkeeper -n $RELEASE_NS --timeout=30m)" ]; then
-  echo "Oathkeeper did not deploy correctly..."
+if ! [ "$(kubectl rollout status deployment $RELEASE_NAME-hydra -n $RELEASE_NS --timeout=$TIMEOUT)" -a "$(kubectl rollout status deployment $RELEASE_NAME-oathkeeper -n $RELEASE_NS --timeout=$TIMEOUT)" ]; then
+  echo "Ory components did not deploy correctly..."
   echo "Uninstalling Ory Helm chart and removing namespace"
   helm uninstall $RELEASE_NAME -n $RELEASE_NS
   kubectl delete ns $RELEASE_NS
