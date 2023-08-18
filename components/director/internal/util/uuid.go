7752524cb6d185ca04be60771d45793c0385a43e
Dobromir Zahariev <dobromir.zahariev@sap.com>
2023-08-18 11:03:55 +0300

    Add String to int64 function

---
+++ components/director/internal/util/uuid.go
@@ -0,0 +1,34 @@
+package util
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"math/big"
+	"strings"
+
+	"github.com/google/uuid"
+)
+
+func UUIDToInt64(identifier uuid.UUID) (int64, error) {
+	return StringToInt64(identifier.String())
+}
+
+func StringToInt64(input string) (int64, error) {
+	if len(strings.TrimSpace(input)) == 0 {
+		return int64(0), errors.New("input cannot be empty")
+	}
+	h := sha256.New()
+	h.Write([]byte(input))
+	return hashToInt64(fmt.Sprintf("%x", h.Sum(nil)))
+}
+
+func hashToInt64(hash string) (int64, error) {
+	hashBytes, err := hex.DecodeString(hash)
+	if err != nil {
+		return int64(0), err
+	}
+	intValue := new(big.Int).SetBytes(hashBytes)
+	return intValue.Int64(), nil
+}
---
+++ components/director/internal/util/uuid_test.go
@@ -0,0 +1,52 @@
+package util
+
+import (
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/require"
+)
+
+func TestStringToInt64(t *testing.T) {
+	uuidString := "3a31599c-7a86-455d-83db-0014a7d459e8"
+	uuidID, _ := uuid.Parse(uuidString)
+	uuidExpected := int64(491666746554389322)
+	name := "some name here"
+	nameExpected := int64(-6756220559490625949)
+
+	t.Run("Success with UUID to UUIDToInt64", func(t *testing.T) {
+		result, err := UUIDToInt64(uuidID)
+		require.NoError(t, err)
+		require.Equal(t, uuidExpected, result)
+	})
+
+	t.Run("Success with UUID as string to StringToInt64", func(t *testing.T) {
+		result, err := StringToInt64(uuidString)
+		require.NoError(t, err)
+		require.Equal(t, uuidExpected, result)
+	})
+
+	t.Run("Success with regular string to StringToInt64", func(t *testing.T) {
+		result, err := StringToInt64(name)
+		require.NoError(t, err)
+		require.Equal(t, nameExpected, result)
+	})
+
+	t.Run("Fail when empty string send to StringToInt64", func(t *testing.T) {
+		result, err := StringToInt64("")
+		require.ErrorContains(t, err, "input cannot be empty")
+		require.Equal(t, int64(0), result)
+	})
+
+	t.Run("Check conversiopn for uniqunes", func(t *testing.T) {
+		results := map[int64]string{}
+		for i := 0; i < 1000000; i++ {
+			uuidID := uuid.New()
+			result, err := UUIDToInt64(uuidID)
+			require.NoError(t, err)
+			results[result] = uuidID.String()
+		}
+		require.Equal(t, 1000000, len(results))
+	})
+
+}
