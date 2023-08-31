2ca96c3c1c1c958030ebd466e1cfbea3203dd345
StanislavStefanov <stanislavsvetlinovstefanov@gmail.com>
2023-08-31 10:03:21 +0300

    add new ctx provider

--- components/hydrator/cmd/main.go
+++ components/hydrator/cmd/main.go
@@ -229,6 +229,7 @@ func getTenantMappingHandlerFunc(authenticators []authenticator.Config, internal
 		tenantmappingconst.UserObjectContextProvider:             tenantmapping.NewUserContextProvider(internalDirectorClientProvider.Client(), staticGroupsRepo),
 		tenantmappingconst.SystemAuthObjectContextProvider:       tenantmapping.NewSystemAuthContextProvider(internalDirectorClientProvider.Client(), cfgProvider),
 		tenantmappingconst.AuthenticatorObjectContextProvider:    tenantmapping.NewAuthenticatorContextProvider(internalDirectorClientProvider.Client(), authenticators),
+		tenantmappingconst.DemoObjectContextProvider:             tenantmapping.NewdemoContextProvider(internalDirectorClientProvider.Client(), authenticators),
 		tenantmappingconst.CertServiceObjectContextProvider:      tenantmapping.NewCertServiceContextProvider(internalDirectorClientProvider.Client(), cfgProvider),
 		tenantmappingconst.TenantHeaderObjectContextProvider:     tenantmapping.NewAccessLevelContextProvider(internalDirectorClientProvider.Client(), cfgProvider),
 		tenantmappingconst.ConsumerProviderObjectContextProvider: tenantmapping.NewConsumerContextProvider(internalGatewayClientProvider.Client(), consumerClaimsKeysConfig),
