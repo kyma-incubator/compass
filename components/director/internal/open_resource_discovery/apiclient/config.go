c3e6a3ef504d35228db0c009c299f12144fdb9f6
Kaloyan Spiridonov <kaloyan.spiridonov@sap.com>
2023-09-26 09:56:03 +0300

    Minor ORD Operation Consumer improvements

--- components/director/internal/open_resource_discovery/apiclient/config.go
+++ components/director/internal/open_resource_discovery/apiclient/config.go
@@ -6,7 +6,7 @@ import (
 
 // OrdAggregatorClientConfig is the configuration needed for ord aggregator client
 type OrdAggregatorClientConfig struct {
-	ClientTimeout             time.Duration `envconfig:"default=30s"`
+	ClientTimeout             time.Duration `envconfig:"APP_ORD_AGGREGATOR_CLIENT_TIMEOUT,default=30s"`
 	OrdAggregatorAggregateAPI string        `envconfig:"APP_ORD_AGGREGATOR_AGGREGATE_API"`
 	SkipSSLValidation         bool          `envconfig:"default=false,APP_ORD_AGGREGATOR_SKIP_SSL_VALIDATION"`
 }
