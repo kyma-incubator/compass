917a8155bfe34d6fe06c71162a872fb2ca4bbfb4
Dobromir Zahariev <dobromir.zahariev@sap.com>
2023-10-05 10:34:32 +0300

    Make imports

--- components/director/internal/scopes_sync/service.go
+++ components/director/internal/scopes_sync/service.go
@@ -21,12 +21,14 @@ type SyncService interface {
 }
 
 // SystemAuthRepo missing godoc
+//
 //go:generate mockery --name=SystemAuthRepo --output=automock --outpkg=automock --case=underscore --disable-version-string
 type SystemAuthRepo interface {
 	ListGlobalWithConditions(ctx context.Context, conditions repo.Conditions) ([]model.SystemAuth, error)
 }
 
 // OAuthService missing godoc
+//
 //go:generate mockery --name=OAuthService --output=automock --outpkg=automock --case=underscore --disable-version-string
 type OAuthService interface {
 	ListClients() ([]*models.OAuth2Client, error)
