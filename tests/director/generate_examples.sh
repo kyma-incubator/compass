ee8cff847850a797e8bcde915292b9a8314af104
Lachezar Bogomilov <lachezar.bogomilov@sap.com>
2023-10-10 11:45:26 +0300

    Remove leftovers

--- tests/director/generate_examples.sh
+++ tests/director/generate_examples.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 make e2e-test
-#make e2e-test-application
-#make e2e-test-notification
-#make e2e-test-runtime
-#make e2e-test-formation
+make e2e-test-application
+make e2e-test-notification
+make e2e-test-runtime
+make e2e-test-formation
 
 ./copy-examples.sh
