8365072e90fc8d45cc4843107c32aff457bd6300
Lachezar Bogomilov <lachezar.bogomilov@sap.com>
2023-10-10 10:45:16 +0300

    Initial implementation.

--- tests/director/generate_examples.sh
+++ tests/director/generate_examples.sh
@@ -1,17 +1,9 @@
 #!/usr/bin/env bash
 
 make e2e-test
-make e2e-test-application
-make e2e-test-notification
-make e2e-test-runtime
-make e2e-test-formation
+#make e2e-test-application
+#make e2e-test-notification
+#make e2e-test-runtime
+#make e2e-test-formation
 
-docker cp k3d-kyma-agent-0:/examples/ ../../components/director/
-
-if [[ $? != 0 ]]
-then
-  echo "Searching examples in k3d-kyma-server-0..."
-  docker cp k3d-kyma-server-0:/examples/ ../../components/director/
-fi
-
-../../components/director/hack/prettify-examples.sh
+./copy-examples.sh
