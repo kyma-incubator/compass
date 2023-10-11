6a2c64eb7e47afc1a803d999c056bd718b7b069d
Lachezar Bogomilov <64094901+la4ezar@users.noreply.github.com>
2023-10-11 15:10:32 +0300

    Add flag for generating examples in the sandbox test runs (#3373)
    * Initial implementation.
    
    * Add default value for `withExamples` env var in the Makefile
    
    * Remove leftovers

--- tests/director/generate_examples.sh
+++ tests/director/generate_examples.sh
@@ -6,12 +6,4 @@ make e2e-test-notification
 make e2e-test-runtime
 make e2e-test-formation
 
-docker cp k3d-kyma-agent-0:/examples/ ../../components/director/
-
-if [[ $? != 0 ]]
-then
-  echo "Searching examples in k3d-kyma-server-0..."
-  docker cp k3d-kyma-server-0:/examples/ ../../components/director/
-fi
-
-../../components/director/hack/prettify-examples.sh
+./copy-examples.sh
