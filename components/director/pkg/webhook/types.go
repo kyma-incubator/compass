51c5395d98fe0638e1a316caf9a8ec307fdbf602
ivanetenevvasilev <ivantenevvasilev@abv.bg>
2023-11-02 11:33:36 +0200

    add comment and change quotes

--- components/director/pkg/webhook/types.go
+++ components/director/pkg/webhook/types.go
@@ -174,8 +174,17 @@ func parseTemplate(tmpl *string, data interface{}, dest interface{}) error {
 	if err = t.Execute(res, data); err != nil {
 		return err
 	}
-	resBytes := bytes.ReplaceAll(res.Bytes(), []byte("\"<nil>\""), []byte("\"\""))
-	resBytes = bytes.ReplaceAll(resBytes, []byte("<nil>"), []byte("null"))
+
+	// <nil> comes after parsing the template with a go field that is a nil pointer
+	// As we are expecting the resulting object to be valid JSON object, the <nil> value on its own is misleading in the following contexts
+	// If we are working with a *string value we add quotes in the template around it.
+	// If the *string is nil, it would result in "<nil>", which is not what we want,
+	// but rather an empty string as it is the default value for an empty string in JSON.
+	// This is in order to remove the <nil> that comes in templates that are surrounded by quotes that come from the template.
+	resBytes := bytes.ReplaceAll(res.Bytes(), []byte(`"<nil>"`), []byte(`""`))
+	// In other cases, we do not add quotes around the template, in such cases the value should be null,
+	// as it is the correct default value for null JSON objects
+	resBytes = bytes.ReplaceAll(resBytes, []byte(`<nil>`), []byte(`null`))
 	if err = json.Unmarshal(resBytes, dest); err != nil {
 		return err
 	}
