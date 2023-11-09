d5c9b73164dbbf4682ebe480618a0a302dc0910c
Kaloyan Spiridonov <kaloyan.spiridonov@sap.com>
2023-11-09 17:03:34 +0200

    make linter happy

--- components/director/internal/open_resource_discovery/processor/vendor.go
+++ components/director/internal/open_resource_discovery/processor/vendor.go
@@ -2,6 +2,7 @@ package processor
 
 import (
 	"context"
+
 	"github.com/kyma-incubator/compass/components/director/internal/model"
 	"github.com/kyma-incubator/compass/components/director/pkg/persistence"
 	"github.com/kyma-incubator/compass/components/director/pkg/resource"
