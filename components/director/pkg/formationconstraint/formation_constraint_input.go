e1277e0fcf9c35b2d8b9df8b0508ab29655503fb
StanislavStefanov <stanislavsvetlinovstefanov@gmail.com>
2023-11-14 09:54:05 +0200

    export fields of Notification status report

--- components/director/pkg/formationconstraint/formation_constraint_input.go
+++ components/director/pkg/formationconstraint/formation_constraint_input.go
@@ -41,8 +41,11 @@ type DestinationCreatorInput struct {
 	Operation                              model.FormationOperation `json:"operation"`
 	ResourceType                           model.ResourceType       `json:"resource_type"`
 	ResourceSubtype                        string                   `json:"resource_subtype"`
+	NotificationStatusReportAddress        uintptr                  `json:"notification_status_report_address"`
 	JoinPointDetailsFAMemoryAddress        uintptr                  `json:"details_formation_assignment_memory_address"`         // contains the memory address of the join point details' formation assignment in form of an integer
 	JoinPointDetailsReverseFAMemoryAddress uintptr                  `json:"details_reverse_formation_assignment_memory_address"` // contains the memory address of the join point details' reverse formation assignment in form of an integer
+	FAMemoryAddress                        uintptr                  `json:"formation_assignment_memory_address"`                 // contains the memory address of the join point details' formation assignment in form of an integer
+	ReverseFAMemoryAddress                 uintptr                  `json:"reverse_formation_assignment_memory_address"`         // contains the memory address of the join point details' reverse formation assignment in form of an integer
 	Location                               JoinPointLocation        `json:"join_point_location"`
 	SkipSubaccountValidation               bool                     `json:"skip_subaccount_validation"`
 	UseCertSvcKeystoreForSAML              bool                     `json:"use_cert_svc_keystore_for_saml"`
@@ -57,8 +60,11 @@ type ConfigMutatorInput struct {
 	Operation                              model.FormationOperation `json:"operation"`
 	ResourceType                           model.ResourceType       `json:"resource_type"`
 	ResourceSubtype                        string                   `json:"resource_subtype"`
+	NotificationStatusReportAddress        uintptr                  `json:"notification_status_report_address"`
 	JoinPointDetailsFAMemoryAddress        uintptr                  `json:"details_formation_assignment_memory_address"`         // contains the memory address of the join point details' formation assignment in form of an integer
 	JoinPointDetailsReverseFAMemoryAddress uintptr                  `json:"details_reverse_formation_assignment_memory_address"` // contains the memory address of the join point details' reverse formation assignment in form of an integer
+	FAMemoryAddress                        uintptr                  `json:"formation_assignment_memory_address"`                 // contains the memory address of the join point details' formation assignment in form of an integer
+	ReverseFAMemoryAddress                 uintptr                  `json:"reverse_formation_assignment_memory_address"`         // contains the memory address of the join point details' reverse formation assignment in form of an integer
 	Location                               JoinPointLocation        `json:"join_point_location"`
 }
 
