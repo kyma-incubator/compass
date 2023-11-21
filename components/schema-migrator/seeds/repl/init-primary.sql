7e9064b007b061cf4713c4925fa6c353871199ef
krasish <krasishontov@gmail.com>
2023-11-21 09:38:35 +0200

    fix sql setup

--- components/schema-migrator/seeds/repl/init-primary.sql
+++ components/schema-migrator/seeds/repl/init-primary.sql
@@ -3,9 +3,3 @@ SELECT pglogical.create_node(
                node_name := 'primary',
                dsn := 'host=test-postgres-replica port=5432 dbname=compass'
        );
-SELECT pglogical.create_subscription(
-               subscription_name := 'subscription1',
-               provider_dsn := 'host=test-postgres port=5432 dbname=compass'
-       );
-
-SELECT pglogical.wait_for_subscription_sync_complete('subscription1');
--- components/schema-migrator/seeds/repl/init-replica.sql
+++ components/schema-migrator/seeds/repl/init-replica.sql
@@ -3,3 +3,10 @@ SELECT pglogical.create_node(
                node_name := 'replica',
                dsn := 'host=thishost port=5432 dbname=compass'
        );
+
+SELECT pglogical.create_subscription(
+               subscription_name := 'subscription1',
+               provider_dsn := 'host=test-postgres port=5432 dbname=compass'
+       );
+
+SELECT pglogical.wait_for_subscription_sync_complete('subscription1');
