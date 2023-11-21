8136fbe7a89d25bfdc20de8cbf734235c492be78
krasish <krasishontov@gmail.com>
2023-11-21 18:55:56 +0200

    improvements

---
+++ components/schema-migrator/seeds/repl/ubuntu.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export DEBIAN_FRONTEND=noninteractive
+apt-get update -y
+apt-get install postgresql -y
+apt-get install postgresql-14-wal2json -y
+
+echo "test-postgres-replica:5432:compass:postgres:pgsql@12345" > /.pgpass
+chmod 0600 ./.pgpass
+export PGPASSFILE='/.pgpass'
+
+echo "Sleep while databases get set-up"
+sleep 30
+
+pg_recvlogical -h test-postgres-replica -d compass -U postgres --slot test_slot --create-slot -P wal2json
+pg_recvlogical -h test-postgres-replica -d compass  -U postgres --slot test_slot --start -o pretty-print=1 -o add-msg-prefixes=wal2json -f -
