2f48fe980749a0c762bfe2d192a5191172f54878
krasish <krasishontov@gmail.com>
2023-11-21 14:13:14 +0200

    fix inits

--- components/schema-migrator/seeds/repl/init-replica.sql
+++ components/schema-migrator/seeds/repl/init-replica.sql
@@ -1,12 +1,12 @@
 CREATE EXTENSION IF NOT EXISTS pglogical;
 SELECT pglogical.create_node(
                node_name := 'replica',
-               dsn := 'host=thishost port=5432 dbname=compass'
+               dsn := 'host=test-postgres-replica port=5432 dbname=compass'
        );
 
 SELECT pglogical.create_subscription(
                subscription_name := 'subscription1',
-               provider_dsn := 'host=test-postgres port=5432 dbname=compass'
+               provider_dsn := 'host=test-postgres port=5432 dbname=compass password=pgsql@12345'
        );
 
 SELECT pglogical.wait_for_subscription_sync_complete('subscription1');
