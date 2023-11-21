12bba43bf3e8b08e3a1661412fc5156582addd0f
krasish <krasishontov@gmail.com>
2023-11-21 11:18:09 +0200

    add more needed packages

--- components/schema-migrator/seeds/repl/entrypoint.sh
+++ components/schema-migrator/seeds/repl/entrypoint.sh
@@ -2,9 +2,12 @@
 
 apt-get update
 apt-get install curl -y
-apt-get -y install gnupg2
+apt-get install wget -y
+apt-get install gnupg2 -y
+apt-get install lsb-release -y
 
 curl https://techsupport.enterprisedb.com/api/repository/dl/default/release/deb | bash
+sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list' && wget -qO- https://www.postgresql.org/media/keys/ACCC4CF8.asc | tee /etc/apt/trusted.gpg.d/pgdg.asc &>/dev/null
 apt-get update
 
 apt-get install -y "postgresql-$2" postgresql-client
