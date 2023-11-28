11f686b09bd5d0ad315cfd6a80d23082df85649f
DimitarPetrov <dimitar.petrov@sap.com>
2023-11-28 16:20:17 +0200

    Initial PoC

--- components/schema-migrator/migrations/director/20231127143409_api-insights-poc.down.sql
+++ components/schema-migrator/migrations/director/20231127143409_api-insights-poc.down.sql
@@ -1,2 +1,10 @@
 BEGIN;
+
+DROP TABLE technical_integrations;
+
+DROP VIEW app_templates_business_tenants;
+DROP VIEW apps_business_tenants;
+DROP VIEW tenant_to_tenant_technical_integrations;
+DROP VIEW oauth2_client_credentials_details;
+
 COMMIT;
--- components/schema-migrator/migrations/director/20231127143409_api-insights-poc.up.sql
+++ components/schema-migrator/migrations/director/20231127143409_api-insights-poc.up.sql
@@ -29,7 +29,7 @@ WITH RECURSIVE tenants AS
                    (SELECT t1.id, t1.parent, l.app_template_id
                     FROM business_tenant_mappings t1
                              JOIN labels l ON key = 'global_subaccount_id' AND app_template_id is not null
-                    WHERE external_tenant = l.value::varchar
+                    WHERE external_tenant = value ->> 0
                     UNION ALL
                     SELECT t2.id, t2.parent, t.app_template_id
                     FROM business_tenant_mappings t2
@@ -40,26 +40,40 @@ FROM tenants;
 
 CREATE OR REPLACE VIEW apps_business_tenants(id, tenant_id)
 AS
-SELECT id, tenant_id
+SELECT DISTINCT id, tenant_id
 FROM tenant_applications
 WHERE owner = true;
 
-CREATE OR REPLACE VIEW tenant_to_tenant_technical_integrations(id, sender, receiver, name, type)
+CREATE OR REPLACE VIEW business_tenants(id, external_tenant, external_name, type, license_type, region, subdomain, parent_external_tenant)
 AS
-SELECT id, sender, receiver, name, type
-FROM technical_integrations
+SELECT DISTINCT btm.id ,btm.external_tenant, btm.external_name, btm.type, ll.value ->> 0, lr.value ->> 0, ls.value ->> 0, parents.external_tenant
+FROM business_tenant_mappings btm
+    LEFT JOIN business_tenant_mappings parents ON btm.parent = parents.id
+    LEFT JOIN labels ll ON btm.id = ll.tenant_id AND ll.key = 'licensetype'
+    LEFT JOIN labels lr ON btm.id = lr.tenant_id AND lr.key = 'region'
+    LEFT JOIN labels ls ON btm.id = ls.tenant_id AND ls.key = 'subdomain';
+
+CREATE OR REPLACE VIEW tenant_to_tenant_technical_integrations(id, tenant_id, sender, receiver, name, type)
+AS
+SELECT DISTINCT ti.id, ta.tenant_id, ti.sender, ti.receiver, ti.name, ti.type
+FROM technical_integrations ti
+         JOIN tenant_applications ta ON ta.id = ti.sender OR ta.id = ti.receiver
 WHERE sender_type = 'APPLICATION'
   AND receiver_type = 'APPLICATION';
 
 CREATE OR REPLACE VIEW oauth2_client_credentials_details(tech_int_id, token_service_url, client_id)
 AS
-SELECT id                        AS tech_int_id,
-       details.token_service_url AS token_service_url,
-       details.client_id         AS client_id
-FROM technical_integrations t,
-     jsonb_to_recordset(t.value) AS details(token_service_url TEXT, client_id TEXT)
+SELECT DISTINCT id                        AS tech_int_id,
+                t.value->>'tokenServiceUrl' AS token_service_url,
+                t.value->>'clientId'         AS client_id
+FROM technical_integrations t
 WHERE sender_type = 'APPLICATION'
   AND receiver_type = 'APPLICATION'
   AND type = 'oauth2ClientCredentials';
 
 COMMIT;
+
+--- Dummy Data
+
+INSERT INTO technical_integrations (id, sender, sender_type, receiver, receiver_type, name, type, value)
+VALUES ('cba94a64-c55b-4caa-8b46-df13f09261e9', '775115ca-12fc-43f5-90e5-7733abb44792', 'APPLICATION', '7d2ad5ef-8d41-47db-a4e7-f7f35d60dc5a', 'APPLICATION', NULL, 'oauth2ClientCredentials', '{"tokenServiceUrl":"https://consumer-y7sc8ue0.authentication.sap.hana.ondemand.com/oauth/token","clientId":"sb-revenue-cloud!t8509"}'::jsonb)
