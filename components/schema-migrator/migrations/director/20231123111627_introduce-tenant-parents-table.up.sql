0d144f1fbbffcb04021e8334efaa85883509baa2
StanislavStefanov <stanislavsvetlinovstefanov@gmail.com>
2023-11-29 10:11:21 +0200

    recreate views

--- components/schema-migrator/migrations/director/20231123111627_introduce-tenant-parents-table.up.sql
+++ components/schema-migrator/migrations/director/20231123111627_introduce-tenant-parents-table.up.sql
@@ -55,4 +55,162 @@ ALTER TABLE tenant_runtime_contexts
 ALTER TABLE tenant_runtime_contexts
     ADD PRIMARY KEY (tenant_id, id, source);
 
+
+DROP VIEW IF EXISTS formation_templates_webhooks_tenants;
+
+CREATE OR REPLACE VIEW formation_templates_webhooks_tenants (id, app_id, url, type, auth, mode, correlation_id_key, retry_interval, timeout, url_template,
+                                                             input_template, header_template, output_template, status_template, runtime_id, integration_system_id,
+                                                             app_template_id, formation_template_id, tenant_id, owner)
+AS
+SELECT w.id,
+       w.app_id,
+       w.url,
+       w.type,
+       w.auth,
+       w.mode,
+       w.correlation_id_key,
+       w.retry_interval,
+       w.timeout,
+       w.url_template,
+       w.input_template,
+       w.header_template,
+       w.output_template,
+       w.status_template,
+       w.runtime_id,
+       w.integration_system_id,
+       w.app_template_id,
+       w.formation_template_id,
+       ft.tenant_id,
+       true
+FROM webhooks w
+         JOIN formation_templates ft on w.formation_template_id = ft.id
+UNION ALL
+SELECT w.id,
+       w.app_id,
+       w.url,
+       w.type,
+       w.auth,
+       w.mode,
+       w.correlation_id_key,
+       w.retry_interval,
+       w.timeout,
+       w.url_template,
+       w.input_template,
+       w.header_template,
+       w.output_template,
+       w.status_template,
+       w.runtime_id,
+       w.integration_system_id,
+       w.app_template_id,
+       w.formation_template_id,
+       btm.id,
+       true
+FROM webhooks w
+         JOIN formation_templates ft on w.formation_template_id = ft.id
+         JOIN tenant_parents tp on ft.tenant_id = tp.parent_id;
+
+
+
+
+
+DROP VIEW IF EXISTS webhooks_tenants;
+
+CREATE OR REPLACE VIEW webhooks_tenants
+            (id, app_id, url, type, auth, mode, correlation_id_key, retry_interval, timeout, url_template,
+             input_template, header_template, output_template, status_template, runtime_id, integration_system_id,
+             app_template_id, formation_template_id, tenant_id, owner)
+AS
+SELECT w.id,
+       w.app_id,
+       w.url,
+       w.type,
+       w.auth,
+       w.mode,
+       w.correlation_id_key,
+       w.retry_interval,
+       w.timeout,
+       w.url_template,
+       w.input_template,
+       w.header_template,
+       w.output_template,
+       w.status_template,
+       w.runtime_id,
+       w.integration_system_id,
+       w.app_template_id,
+       w.formation_template_id,
+       ta.tenant_id,
+       ta.owner
+FROM webhooks w
+         JOIN tenant_applications ta ON w.app_id = ta.id
+UNION ALL
+SELECT w.id,
+       w.app_id,
+       w.url,
+       w.type,
+       w.auth,
+       w.mode,
+       w.correlation_id_key,
+       w.retry_interval,
+       w.timeout,
+       w.url_template,
+       w.input_template,
+       w.header_template,
+       w.output_template,
+       w.status_template,
+       w.runtime_id,
+       w.integration_system_id,
+       w.app_template_id,
+       w.formation_template_id,
+       tr.tenant_id,
+       tr.owner
+FROM webhooks w
+         JOIN tenant_runtimes tr ON w.runtime_id = tr.id
+UNION ALL
+SELECT w.id,
+       w.app_id,
+       w.url,
+       w.type,
+       w.auth,
+       w.mode,
+       w.correlation_id_key,
+       w.retry_interval,
+       w.timeout,
+       w.url_template,
+       w.input_template,
+       w.header_template,
+       w.output_template,
+       w.status_template,
+       w.runtime_id,
+       w.integration_system_id,
+       w.app_template_id,
+       w.formation_template_id,
+       ft.tenant_id,
+       true
+FROM webhooks w
+         JOIN formation_templates ft on w.formation_template_id = ft.id
+UNION ALL
+SELECT w.id,
+       w.app_id,
+       w.url,
+       w.type,
+       w.auth,
+       w.mode,
+       w.correlation_id_key,
+       w.retry_interval,
+       w.timeout,
+       w.url_template,
+       w.input_template,
+       w.header_template,
+       w.output_template,
+       w.status_template,
+       w.runtime_id,
+       w.integration_system_id,
+       w.app_template_id,
+       w.formation_template_id,
+       btm.id,
+       true
+FROM webhooks w
+         JOIN formation_templates ft on w.formation_template_id = ft.id
+         JOIN tenant_parents tp on ft.tenant_id = tp.parent_id;
+
 COMMIT;
