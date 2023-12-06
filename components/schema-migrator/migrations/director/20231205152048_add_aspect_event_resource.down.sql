62f4ebb02a1fceb6a429a3c060ea47b20aa8a97a
Nikolay Yordanov <n.yordanov@sap.com>
2023-12-06 10:40:32 +0200

    Enhance migration

--- components/schema-migrator/migrations/director/20231205152048_add_aspect_event_resource.down.sql
+++ components/schema-migrator/migrations/director/20231205152048_add_aspect_event_resource.down.sql
@@ -1,12 +1,79 @@
 BEGIN;
 
--- Drop views --
-DROP VIEW IF EXISTS aspect_event_resources_tenants;
+-- Drop new views
 DROP VIEW IF EXISTS tenants_aspect_event_resources;
+DROP VIEW IF EXISTS aspect_event_resources_subset;
+DROP VIEW IF EXISTS aspect_event_resources_tenants;
+DROP VIEW IF EXISTS aspects_tenants;
+DROP VIEW IF EXISTS tenants_aspects;
 
 -- Drop index for aspect_event_resources table
 DROP INDEX IF EXISTS aspect_event_resources_app_id;
 
 -- Drop table aspect_event_resources
-    DROP TABLE IF EXISTS aspect_event_resources;
+DROP TABLE IF EXISTS aspect_event_resources;
+
+-- Add column back
+ALTER TABLE aspects
+    ADD COLUMN event_resources JSONB;
+
+-- Recreate old views
+CREATE VIEW aspect_event_resources AS
+SELECT asp.id                               AS aspect_id,
+       events.id                            AS event_resource_id,
+       entries."ordId"                      AS ord_id,
+       entries."minVersion"                 AS min_version,
+       entries.subset                       AS subset
+FROM aspects asp JOIN event_api_definitions events ON asp.app_id = events.app_id, jsonb_to_recordset(asp.event_resources) AS entries("ordId" TEXT, "minVersion" TEXT, subset JSONB)
+WHERE events.ord_id = entries."ordId";
+
+CREATE VIEW aspect_event_resources_subset AS
+SELECT
+    a.event_resource_id                 AS event_resource_id,
+    entries."eventType"                 AS event_type
+FROM aspect_event_resources a,
+     jsonb_to_recordset(a.subset) AS entries("eventType" TEXT); -- if the JSON key has a capital letter then it should be defined with "" in the AS clause
+
+-- Recreate views
+CREATE VIEW aspects_tenants AS
+SELECT a.*, ta.tenant_id, ta.owner FROM aspects AS a
+                                            INNER JOIN tenant_applications ta ON ta.id = a.app_id;
+
+CREATE VIEW tenants_aspects
+            (tenant_id, formation_id, id, integration_dependency_id, app_id, title, description, mandatory, support_multiple_providers, api_resources, event_resources, ready, created_at,
+             updated_at, deleted_at, error)
+AS
+SELECT DISTINCT t_apps.tenant_id,
+                t_apps.formation_id,
+                a.id,
+                a.integration_dependency_id,
+                a.app_id,
+                a.title,
+                a.description,
+                a.mandatory,
+                a.support_multiple_providers,
+                a.api_resources,
+                a.event_resources,
+                a.ready,
+                a.created_at,
+                a.updated_at,
+                a.deleted_at,
+                a.error
+FROM aspects a
+         JOIN (SELECT a1.id,
+                      a1.tenant_id,
+                      'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa' AS formation_id
+               FROM tenant_applications a1
+               UNION ALL
+               SELECT apps_subaccounts.id,
+                      apps_subaccounts.tenant_id,
+                      apps_subaccounts.formation_id
+               FROM apps_subaccounts
+               UNION ALL
+               SELECT apps_subaccounts.id,
+                      apps_subaccounts.tenant_id,
+                      'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa' AS formation_id
+               FROM apps_subaccounts) t_apps ON a.app_id = t_apps.id;
+
+
 COMMIT;
--- components/schema-migrator/migrations/director/20231205152048_add_aspect_event_resource.up.sql
+++ components/schema-migrator/migrations/director/20231205152048_add_aspect_event_resource.up.sql
@@ -3,6 +3,8 @@ BEGIN;
 -- Drop views --
 DROP VIEW IF EXISTS aspect_event_resources_subset;
 DROP VIEW IF EXISTS aspect_event_resources;
+DROP VIEW IF EXISTS aspects_tenants; -- this view will not be changed but it depends on the column aspects.event_resources; that's why the view has to be dropped and re-created in order to drop the column
+DROP VIEW IF EXISTS tenants_aspects;
 
 -- Create aspect_event_resources table
 CREATE TABLE aspect_event_resources (
@@ -14,6 +16,7 @@ CREATE TABLE aspect_event_resources (
                          app_template_version_id UUID,
                          CONSTRAINT aspect_event_resources_app_template_version_id_fk FOREIGN KEY (app_template_version_id) REFERENCES app_template_versions (id) ON DELETE CASCADE,
                          ord_id VARCHAR(256) NOT NULL,
+                         CONSTRAINT aspect_event_resources_ord_id_unique UNIQUE (app_id, ord_id),
                          min_version VARCHAR(256),
                          subset JSONB,
                          ready BOOLEAN DEFAULT TRUE,
@@ -23,6 +26,10 @@ CREATE TABLE aspect_event_resources (
                          deleted_at TIMESTAMP,
                          error JSONB
 );
+
+-- a manual migration of this column will be performed that's why we drop it directly
+ALTER TABLE aspects
+    DROP COLUMN event_resources;
 
 -- Create index for aspect_event_resources table
 CREATE INDEX IF NOT EXISTS aspect_event_resources_app_id ON aspect_event_resources (app_id);
@@ -47,23 +54,72 @@ SELECT DISTINCT t_apps.tenant_id,
 FROM aspect_event_resources a
          JOIN (SELECT a1.id,
                       a1.tenant_id,
-                      'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa' AS formation_id
+                      'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa'::uuid AS formation_id
                FROM tenant_applications a1
                UNION ALL
-               SELECT apps_subaccounts.id,
-                      apps_subaccounts.tenant_id,
-                      apps_subaccounts.formation_id
-               FROM apps_subaccounts
+               SELECT af.app_id,
+                      'bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbbb'::uuid AS tenant_id,
+                      af.formation_id
+               FROM apps_formations_id af
                UNION ALL
                SELECT apps_subaccounts.id,
                       apps_subaccounts.tenant_id,
-                      'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa' AS formation_id
+                      'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa'::uuid AS formation_id
                FROM apps_subaccounts) t_apps ON a.app_id = t_apps.id;
 
+
+CREATE VIEW aspect_event_resources_subset AS
+SELECT
+    id                                  AS event_resource_id,
+    entries."eventType"                 AS event_type
+FROM aspect_event_resources a,
+     jsonb_to_recordset(a.subset) AS entries("eventType" TEXT); -- if the JSON key has a capital letter then it should be defined with "" in the AS clause
 
 -- Create aspect_event_resources_tenants view
 CREATE VIEW aspect_event_resources_tenants AS
 SELECT a.*, ta.tenant_id, ta.owner FROM aspect_event_resources AS a
                                             INNER JOIN tenant_applications ta ON ta.id = a.app_id;
+
+-- Recreate views
+CREATE VIEW aspects_tenants AS
+SELECT a.*, ta.tenant_id, ta.owner FROM aspects AS a
+                                            INNER JOIN tenant_applications ta ON ta.id = a.app_id;
+
+-- event_resources column is removed
+CREATE OR REPLACE VIEW tenants_aspects
+            (tenant_id, formation_id, id, integration_dependency_id, app_id, title, description, mandatory,
+             support_multiple_providers, api_resources, ready, created_at, updated_at, deleted_at,
+             error)
+AS
+SELECT DISTINCT t_apps.tenant_id,
+                t_apps.formation_id,
+                a.id,
+                a.integration_dependency_id,
+                a.app_id,
+                a.title,
+                a.description,
+                a.mandatory,
+                a.support_multiple_providers,
+                a.api_resources,
+                a.ready,
+                a.created_at,
+                a.updated_at,
+                a.deleted_at,
+                a.error
+FROM aspects a
+         JOIN (SELECT a1.id,
+                      a1.tenant_id,
+                      'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa'::uuid AS formation_id
+               FROM tenant_applications a1
+               UNION ALL
+               SELECT af.app_id,
+                      'bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbbb'::uuid AS tenant_id,
+                      af.formation_id
+               FROM apps_formations_id af
+               UNION ALL
+               SELECT apps_subaccounts.id,
+                      apps_subaccounts.tenant_id,
+                      'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa'::uuid AS formation_id
+               FROM apps_subaccounts) t_apps ON a.app_id = t_apps.id;
 
 COMMIT;
