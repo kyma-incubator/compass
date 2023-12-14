d5ed9dca2895ba23422a2ddcbc05aa1f67afc3ff
PetarTodorovv <todorovv.petar@gmail.com>
2023-12-14 10:15:06 +0200

    add godoc

--- components/default-tenant-mapping-handler/internal/types/tenantmapping.go
+++ components/default-tenant-mapping-handler/internal/types/tenantmapping.go
@@ -4,12 +4,14 @@ import (
 	"fmt"
 )
 
+// TenantMapping represents the tenant mapping notification data
 type TenantMapping struct {
 	ReceiverTenant ReceiverTenant `json:"receiverTenant"`
 	AssignedTenant AssignedTenant `json:"assignedTenant"`
 	Context        Context        `json:"context"`
 }
 
+// ReceiverTenant contains metadata for the receiver tenant - the tenant that receives the notification
 type ReceiverTenant struct {
 	State                string `json:"state"`
 	AssignmentID         string `json:"uclAssignmentId"`
@@ -23,6 +25,7 @@ type ReceiverTenant struct {
 	SystemTenantID       string `json:"uclSystemTenantId"`
 }
 
+// AssignedTenant contains metadata for the assigned tenant - the tenant that the notification is about
 type AssignedTenant struct {
 	State                string `json:"state"`
 	AssignmentID         string `json:"uclAssignmentId"`
@@ -36,6 +39,7 @@ type AssignedTenant struct {
 	SystemTenantID       string `json:"uclSystemTenantId"`
 }
 
+// Context contains common data for the tenant mapping notification
 type Context struct {
 	Platform        string `json:"platform"`
 	CrmID           string `json:"crmId"`
@@ -46,6 +50,8 @@ type Context struct {
 	Operation       string `json:"operation"`
 }
 
+// Print prints the data in the tenant mapping notifications excepts the configuration part because it could have sensitive data.
+// We should NOT include the configuration in this method.
 func (tm *TenantMapping) Print() string {
 	return fmt.Sprintf("Context: {Platform: %s, CrmID: %s, AccountID: %s, FormationID: %s, FormationName: %s, FormationTypeID: %s, Operation: %s}, ReceiverTenant: {State: %s, AssignmentID: %s, DeploymentRegion: %s, ApplicationNamespace: %s, ApplicationURL: %s, ApplicationTenantID: %s, SubaccountID: %s, Subdomain: %s, SystemName: %s, SystemTenantID: %s}, AssignedTenant: {State: %s, AssignmentID: %s, DeploymentRegion: %s, ApplicationNamespace: %s, ApplicationURL: %s, ApplicationTenantID: %s, SubaccountID: %s, Subdomain: %s, SystemName: %s, SystemTenantID: %s}", tm.Context.Platform, tm.Context.CrmID, tm.Context.AccountID, tm.Context.FormationID, tm.Context.FormationName, tm.Context.FormationTypeID, tm.Context.Operation, tm.ReceiverTenant.State, tm.ReceiverTenant.AssignmentID, tm.ReceiverTenant.DeploymentRegion, tm.ReceiverTenant.ApplicationNamespace, tm.ReceiverTenant.ApplicationURL, tm.ReceiverTenant.ApplicationTenantID, tm.ReceiverTenant.SubaccountID, tm.ReceiverTenant.Subdomain, tm.ReceiverTenant.SystemName, tm.ReceiverTenant.SystemTenantID, tm.AssignedTenant.State, tm.AssignedTenant.AssignmentID, tm.AssignedTenant.DeploymentRegion, tm.AssignedTenant.ApplicationNamespace, tm.AssignedTenant.ApplicationURL, tm.AssignedTenant.ApplicationTenantID, tm.AssignedTenant.SubaccountID, tm.AssignedTenant.Subdomain, tm.AssignedTenant.SystemName, tm.AssignedTenant.SystemTenantID)
 }
