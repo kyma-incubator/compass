62e3f71ff5c54186343f54effdd1e4204b7fc479
DimitarPetrov <dimitar.petrov@sap.com>
2023-12-28 18:09:55 +0200

    Fix down migration

--- components/schema-migrator/migrations/director/20231228152252_add-discovery-consumers-allowlist.down.sql
+++ components/schema-migrator/migrations/director/20231228152252_add-discovery-consumers-allowlist.down.sql
@@ -3,7 +3,7 @@ BEGIN;
 ALTER TABLE formation_templates
     DROP COLUMN discovery_consumers;
 
-DROP INDEX discovery_consumers_gin
-DROP INDEX scenario_label_gin
+DROP INDEX discovery_consumers_gin;
+DROP INDEX scenario_label_gin;
 
 COMMIT;
