5691adc2c67d406014dd103912a8b425dfd3f54f
DimitarPetrov <dimitar.petrov@sap.com>
2023-12-28 20:02:29 +0200

    Bump version

--- components/schema-migrator/migrations/director/20231228152252_add-discovery-consumers-allowlist.down.sql
+++ components/schema-migrator/migrations/director/20231228152252_add-discovery-consumers-allowlist.down.sql
@@ -1,9 +1,9 @@
 BEGIN;
 
-ALTER TABLE formation_templates
-    DROP COLUMN discovery_consumers;
-
 DROP INDEX discovery_consumers_gin;
 DROP INDEX scenario_label_gin;
+
+ALTER TABLE formation_templates
+    DROP COLUMN discovery_consumers;
 
 COMMIT;
