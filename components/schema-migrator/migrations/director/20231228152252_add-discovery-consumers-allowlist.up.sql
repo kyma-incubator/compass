63045c0e45403661b8aecc5961b0a0e3e0d8d572
DimitarPetrov <dimitar.petrov@sap.com>
2024-01-02 09:10:41 +0200

    Improve migration

--- components/schema-migrator/migrations/director/20231228152252_add-discovery-consumers-allowlist.up.sql
+++ components/schema-migrator/migrations/director/20231228152252_add-discovery-consumers-allowlist.up.sql
@@ -6,4 +6,7 @@ ALTER TABLE formation_templates
 CREATE INDEX discovery_consumers_gin ON formation_templates USING gin (discovery_consumers);
 CREATE INDEX scenario_label_gin ON labels USING gin (value) WHERE key = 'scenarios';
 
+-- Initial set of discovery consumers to ensure existing scenarios will continue working
+UPDATE formation_templates SET discovery_consumers = runtime_types;
+
 COMMIT;
