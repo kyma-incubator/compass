e9b73f5605a9d05ab5cb1986fd0b5de46ed80542
Dobromir Zahariev <dobromir.zahariev@sap.com>
2024-01-08 14:11:08 +0200

    Typo in comment

--- components/director/internal/systemfetcher/model.go
+++ components/director/internal/systemfetcher/model.go
@@ -10,7 +10,7 @@ import (
 )
 
 var (
-	// ApplicationTemplates contains available Applicaiton Templates, should only be used for the unmarshaling of system data
+	// ApplicationTemplates contains available Application Templates, should only be used for the unmarshaling of system data
 	// It represents a model.ApplicationTemplate with its labels in the form of map[string]*model.Label
 	ApplicationTemplates []TemplateMapping
 	// ApplicationTemplateLabelFilter represent a label for the Application Templates which has a value that
