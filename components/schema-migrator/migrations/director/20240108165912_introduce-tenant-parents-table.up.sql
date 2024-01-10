89b4008bde6286339eb2302549be3f234f7217f6
Kaloyan Spiridonov <kaloyan.spiridonov@sap.com>
2024-01-10 16:22:06 +0200

    reorder view creation

--- components/schema-migrator/migrations/director/20240108165912_introduce-tenant-parents-table.up.sql
+++ components/schema-migrator/migrations/director/20240108165912_introduce-tenant-parents-table.up.sql
@@ -492,6 +492,161 @@ DROP VIEW IF EXISTS tenants_entity_type_mappings;
 DROP VIEW IF EXISTS tenants_events;
 DROP VIEW IF EXISTS tenants_apis;
 
+CREATE OR REPLACE VIEW tenants_apis
+            (tenant_id, formation_id, id, app_id, name, description, group_name, default_auth, version_value,
+             version_deprecated, version_deprecated_since, version_for_removal, ord_id, local_tenant_id,
+             short_description, system_instance_aware, policy_level, custom_policy_level, api_protocol, tags,
+             supported_use_cases, countries, links, api_resource_links, release_status, sunset_date, changelog_entries,
+             labels, package_id, visibility, disabled, part_of_products, line_of_business, industry, ready, created_at,
+             updated_at, deleted_at, error, implementation_standard, custom_implementation_standard,
+             custom_implementation_standard_description, target_urls, extensible_supported, extensible_description,
+             successors, resource_hash, documentation_labels, correlation_ids, direction, last_update, deprecation_date, responsible, usage)
+AS
+SELECT DISTINCT t_apps.tenant_id,
+                t_apps.formation_id,
+                apis.id,
+                apis.app_id,
+                apis.name,
+                apis.description,
+                apis.group_name,
+                apis.default_auth,
+                apis.version_value,
+                apis.version_deprecated,
+                apis.version_deprecated_since,
+                apis.version_for_removal,
+                apis.ord_id,
+                apis.local_tenant_id,
+                apis.short_description,
+                apis.system_instance_aware,
+                apis.policy_level,
+                apis.custom_policy_level,
+                apis.api_protocol,
+                apis.tags,
+                apis.supported_use_cases,
+                apis.countries,
+                apis.links,
+                apis.api_resource_links,
+                apis.release_status,
+                apis.sunset_date,
+                apis.changelog_entries,
+                apis.labels,
+                apis.package_id,
+                apis.visibility,
+                apis.disabled,
+                apis.part_of_products,
+                apis.line_of_business,
+                apis.industry,
+                apis.ready,
+                apis.created_at,
+                apis.updated_at,
+                apis.deleted_at,
+                apis.error,
+                apis.implementation_standard,
+                apis.custom_implementation_standard,
+                apis.custom_implementation_standard_description,
+                apis.target_urls,
+                actions.supported   AS extensible_supported,
+                actions.description AS extensible_description,
+                apis.successors,
+                apis.resource_hash,
+                apis.documentation_labels,
+                apis.correlation_ids,
+                apis.direction,
+                apis.last_update,
+                apis.deprecation_date,
+                apis.responsible,
+                apis.usage
+FROM api_definitions apis
+         JOIN (SELECT a1.id,
+                      a1.tenant_id,
+                      'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa'::uuid AS formation_id
+               FROM tenant_applications a1
+               UNION ALL
+               SELECT af.app_id,
+                      'bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbbb'::uuid AS tenant_id,
+                      af.formation_id
+               FROM apps_formations_id af
+               UNION ALL
+               SELECT apps_subaccounts.id,
+                      apps_subaccounts.tenant_id,
+                      'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa'::uuid AS formation_id
+               FROM apps_subaccounts) t_apps ON apis.app_id = t_apps.id,
+     jsonb_to_record(apis.extensible) actions(supported text, description text);
+
+CREATE OR REPLACE VIEW tenants_events
+            (tenant_id, formation_id, id, app_id, name, description, group_name, version_value, version_deprecated,
+             version_deprecated_since, version_for_removal, ord_id, local_tenant_id, short_description,
+             system_instance_aware, policy_level, custom_policy_level, changelog_entries, links, tags, countries,
+             release_status, sunset_date, labels, package_id, visibility, disabled, part_of_products, line_of_business,
+             industry, ready, created_at, updated_at, deleted_at, error, implementation_standard,
+             custom_implementation_standard, custom_implementation_standard_description, extensible_supported,
+             extensible_description, successors, resource_hash, correlation_ids, last_update, deprecation_date,
+             event_resource_links, responsible)
+AS
+SELECT DISTINCT t_apps.tenant_id,
+                t_apps.formation_id,
+                events.id,
+                events.app_id,
+                events.name,
+                events.description,
+                events.group_name,
+                events.version_value,
+                events.version_deprecated,
+                events.version_deprecated_since,
+                events.version_for_removal,
+                events.ord_id,
+                events.local_tenant_id,
+                events.short_description,
+                events.system_instance_aware,
+                events.policy_level,
+                events.custom_policy_level,
+                events.changelog_entries,
+                events.links,
+                events.tags,
+                events.countries,
+                events.release_status,
+                events.sunset_date,
+                events.labels,
+                events.package_id,
+                events.visibility,
+                events.disabled,
+                events.part_of_products,
+                events.line_of_business,
+                events.industry,
+                events.ready,
+                events.created_at,
+                events.updated_at,
+                events.deleted_at,
+                events.error,
+                events.implementation_standard,
+                events.custom_implementation_standard,
+                events.custom_implementation_standard_description,
+                actions.supported   AS extensible_supported,
+                actions.description AS extensible_description,
+                events.successors,
+                events.resource_hash,
+                events.correlation_ids,
+                events.last_update,
+                events.deprecation_date,
+                events.event_resource_links,
+                events.responsible
+FROM event_api_definitions events
+         JOIN (SELECT a1.id,
+                      a1.tenant_id,
+                      'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa'::uuid AS formation_id
+               FROM tenant_applications a1
+               UNION ALL
+               SELECT af.app_id,
+                      'bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbbb'::uuid AS tenant_id,
+                      af.formation_id
+               FROM apps_formations_id af
+               UNION ALL
+               SELECT apps_subaccounts.id,
+                      apps_subaccounts.tenant_id,
+                      'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa'::uuid AS formation_id
+               FROM apps_subaccounts) t_apps ON events.app_id = t_apps.id,
+     jsonb_to_record(events.extensible) actions(supported text, description text);
+
 CREATE OR REPLACE VIEW tenants_entity_type_mappings
             (tenant_id, id, api_definition_id, event_definition_id, api_model_selectors, entity_type_targets)
 AS
@@ -605,162 +760,6 @@ SELECT fr.*, ta.tenant_id, ta.owner FROM fetch_requests AS fr
 CREATE OR REPLACE VIEW documents_tenants AS
 SELECT d.*, ta.tenant_id, ta.owner FROM documents AS d
                                             INNER JOIN tenant_applications ta ON ta.id = d.app_id;
-
-CREATE OR REPLACE VIEW tenants_apis
-            (tenant_id, formation_id, id, app_id, name, description, group_name, default_auth, version_value,
-             version_deprecated, version_deprecated_since, version_for_removal, ord_id, local_tenant_id,
-             short_description, system_instance_aware, policy_level, custom_policy_level, api_protocol, tags,
-             supported_use_cases, countries, links, api_resource_links, release_status, sunset_date, changelog_entries,
-             labels, package_id, visibility, disabled, part_of_products, line_of_business, industry, ready, created_at,
-             updated_at, deleted_at, error, implementation_standard, custom_implementation_standard,
-             custom_implementation_standard_description, target_urls, extensible_supported, extensible_description,
-             successors, resource_hash, documentation_labels, correlation_ids, direction, last_update, deprecation_date, responsible, usage)
-AS
-SELECT DISTINCT t_apps.tenant_id,
-                t_apps.formation_id,
-                apis.id,
-                apis.app_id,
-                apis.name,
-                apis.description,
-                apis.group_name,
-                apis.default_auth,
-                apis.version_value,
-                apis.version_deprecated,
-                apis.version_deprecated_since,
-                apis.version_for_removal,
-                apis.ord_id,
-                apis.local_tenant_id,
-                apis.short_description,
-                apis.system_instance_aware,
-                apis.policy_level,
-                apis.custom_policy_level,
-                apis.api_protocol,
-                apis.tags,
-                apis.supported_use_cases,
-                apis.countries,
-                apis.links,
-                apis.api_resource_links,
-                apis.release_status,
-                apis.sunset_date,
-                apis.changelog_entries,
-                apis.labels,
-                apis.package_id,
-                apis.visibility,
-                apis.disabled,
-                apis.part_of_products,
-                apis.line_of_business,
-                apis.industry,
-                apis.ready,
-                apis.created_at,
-                apis.updated_at,
-                apis.deleted_at,
-                apis.error,
-                apis.implementation_standard,
-                apis.custom_implementation_standard,
-                apis.custom_implementation_standard_description,
-                apis.target_urls,
-                actions.supported   AS extensible_supported,
-                actions.description AS extensible_description,
-                apis.successors,
-                apis.resource_hash,
-                apis.documentation_labels,
-                apis.correlation_ids,
-                apis.direction,
-                apis.last_update,
-                apis.deprecation_date,
-                apis.responsible,
-                apis.usage
-FROM api_definitions apis
-         JOIN (SELECT a1.id,
-                      a1.tenant_id,
-                      'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa'::uuid AS formation_id
-               FROM tenant_applications a1
-               UNION ALL
-               SELECT af.app_id,
-                      'bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbbb'::uuid AS tenant_id,
-                       af.formation_id
-               FROM apps_formations_id af
-               UNION ALL
-               SELECT apps_subaccounts.id,
-                      apps_subaccounts.tenant_id,
-                      'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa'::uuid AS formation_id
-               FROM apps_subaccounts) t_apps ON apis.app_id = t_apps.id,
-     jsonb_to_record(apis.extensible) actions(supported text, description text);
-
-
-CREATE OR REPLACE VIEW tenants_events
-            (tenant_id, formation_id, id, app_id, name, description, group_name, version_value, version_deprecated,
-             version_deprecated_since, version_for_removal, ord_id, local_tenant_id, short_description,
-             system_instance_aware, policy_level, custom_policy_level, changelog_entries, links, tags, countries,
-             release_status, sunset_date, labels, package_id, visibility, disabled, part_of_products, line_of_business,
-             industry, ready, created_at, updated_at, deleted_at, error, implementation_standard,
-             custom_implementation_standard, custom_implementation_standard_description, extensible_supported,
-             extensible_description, successors, resource_hash, correlation_ids, last_update, deprecation_date,
-             event_resource_links, responsible)
-AS
-SELECT DISTINCT t_apps.tenant_id,
-                t_apps.formation_id,
-                events.id,
-                events.app_id,
-                events.name,
-                events.description,
-                events.group_name,
-                events.version_value,
-                events.version_deprecated,
-                events.version_deprecated_since,
-                events.version_for_removal,
-                events.ord_id,
-                events.local_tenant_id,
-                events.short_description,
-                events.system_instance_aware,
-                events.policy_level,
-                events.custom_policy_level,
-                events.changelog_entries,
-                events.links,
-                events.tags,
-                events.countries,
-                events.release_status,
-                events.sunset_date,
-                events.labels,
-                events.package_id,
-                events.visibility,
-                events.disabled,
-                events.part_of_products,
-                events.line_of_business,
-                events.industry,
-                events.ready,
-                events.created_at,
-                events.updated_at,
-                events.deleted_at,
-                events.error,
-                events.implementation_standard,
-                events.custom_implementation_standard,
-                events.custom_implementation_standard_description,
-                actions.supported   AS extensible_supported,
-                actions.description AS extensible_description,
-                events.successors,
-                events.resource_hash,
-                events.correlation_ids,
-                events.last_update,
-                events.deprecation_date,
-                events.event_resource_links,
-                events.responsible
-FROM event_api_definitions events
-         JOIN (SELECT a1.id,
-                      a1.tenant_id,
-                      'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa'::uuid AS formation_id
-               FROM tenant_applications a1
-               UNION ALL
-               SELECT af.app_id,
-                      'bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbbb'::uuid AS tenant_id,
-                       af.formation_id
-               FROM apps_formations_id af
-               UNION ALL
-               SELECT apps_subaccounts.id,
-                      apps_subaccounts.tenant_id,
-                      'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa'::uuid AS formation_id
-               FROM apps_subaccounts) t_apps ON events.app_id = t_apps.id,
-     jsonb_to_record(events.extensible) actions(supported text, description text);
 
 
 CREATE OR REPLACE VIEW entity_type_mappings_tenants(id, tenant_id, owner)
@@ -1422,4 +1421,5 @@ SELECT t.*, ta.tenant_id, ta.owner FROM tombstones AS t
 CREATE OR REPLACE VIEW vendors_tenants AS
 SELECT v.*, ta.tenant_id, ta.owner FROM vendors AS v
                                             INNER JOIN tenant_applications AS ta ON ta.id = v.app_id;
+
 COMMIT;
