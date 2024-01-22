4b40dac24d5688557cc84c64a95e4c04c967d170
PetarTodorovv <todorovv.petar@gmail.com>
2024-01-22 09:41:27 +0200

    apply review comment

--- components/schema-migrator/migrations/director/20240119172919_add-formation-assignments-timestamps.up.sql
+++ components/schema-migrator/migrations/director/20240119172919_add-formation-assignments-timestamps.up.sql
@@ -1,21 +1,11 @@
 BEGIN;
 
-ALTER table formations
-    ADD COLUMN last_state_change_timestamp TIMESTAMP,
+ALTER TABLE formations
+    ADD COLUMN last_state_change_timestamp TIMESTAMP DEFAULT NOW(),
     ADD COLUMN last_notification_sent_timestamp TIMESTAMP;
 
-UPDATE formations SET last_state_change_timestamp = now()
-    WHERE formations.last_state_change_timestamp IS NULL;
-
-UPDATE formations SET last_notification_sent_timestamp = NULL;
-
-ALTER table formation_assignments
-    ADD COLUMN last_state_change_timestamp TIMESTAMP,
+ALTER TABLE formation_assignments
+    ADD COLUMN last_state_change_timestamp TIMESTAMP DEFAULT NOW(),
     ADD COLUMN last_notification_sent_timestamp TIMESTAMP;
-
-UPDATE formation_assignments SET last_state_change_timestamp = now()
-    WHERE formation_assignments.last_state_change_timestamp IS NULL;
-
-UPDATE formation_assignments SET last_notification_sent_timestamp = NULL;
 
 COMMIT;
