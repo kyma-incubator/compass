3da7bcbfcfe9ce8b817209b848899b733ccf8ead
Aleksandar Targov <aleksandar.targov@sap.com>
2024-01-31 14:45:16 +0200

    make imports

--- components/director/internal/tenantfetchersvc/provisioner.go
+++ components/director/internal/tenantfetchersvc/provisioner.go
@@ -2,6 +2,7 @@ package tenantfetchersvc
 
 import (
 	"context"
+
 	"github.com/kyma-incubator/compass/components/director/pkg/graphql"
 
 	"github.com/kyma-incubator/compass/components/director/internal/model"
