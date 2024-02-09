804e1fda72be94a7f2d472b002f8225ac6d200d8
StanislavStefanov <stanislavsvetlinovstefanov@gmail.com>
2024-02-09 11:43:27 +0200

    use join query

--- components/director/internal/domain/tenant/repository.go
+++ components/director/internal/domain/tenant/repository.go
@@ -34,7 +34,8 @@ const (
 	labelDefinitionsTableName      string = `public.label_definitions`
 	labelDefinitionsTenantIDColumn string = `tenant_id`
 
-	maxParameterChunkSize int = 50000 // max parameters size in PostgreSQL is 65535
+	maxParameterChunkSize     int = 50000 // max parameters size in PostgreSQL is 65535
+	getTenantsByParentAndType     = `SELECT %s from %s join %s on %s = %s where %s = ? and %s = ?`
 )
 
 var (
@@ -644,19 +645,19 @@ func (r *pgRepository) ListBySubscribedRuntimesAndApplicationTemplates(ctx conte
 
 // ListByParentAndType list tenants by parent ID and tenant.Type
 func (r *pgRepository) ListByParentAndType(ctx context.Context, parentID string, tenantType tenant.Type) ([]*model.BusinessTenantMapping, error) {
-	tenantIDs, err := r.tenantParentRepo.ListByParent(ctx, parentID)
-	if err != nil {
-		return nil, errors.Wrapf(err, "wlile listing tenant parent records for parent with id %s", parentID)
-	}
-
-	conditions := repo.Conditions{
-		repo.NewInConditionForStringValues(idColumn, tenantIDs),
-		repo.NewEqualCondition(typeColumn, tenantType),
-	}
-
 	var entityCollection tenant.EntityCollection
-	if err := r.listerGlobal.ListGlobal(ctx, &entityCollection, conditions...); err != nil {
-		return nil, errors.Wrapf(err, "while listing tenants of type %s with ids %v", tenantType, tenantIDs)
+
+	persist, err := persistence.FromCtx(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	getTenantsByParentAndTypeStmt := buildTenantsByParentAndTypeQuery()
+
+	log.C(ctx).Debugf("Executing DB query: %s", getTenantsByParentAndTypeStmt)
+
+	if err = persist.SelectContext(ctx, &entityCollection, getTenantsByParentAndTypeStmt, parentID, tenantType); err != nil {
+		return nil, errors.Wrapf(err, "while listing tenants of type %s with parent ID %s", tenantType, parentID)
 	}
 
 	return r.enrichManyWithParents(ctx, entityCollection)
@@ -737,4 +738,24 @@ func (r *pgRepository) enrichManyWithParents(ctx context.Context, entityCollecti
 		}
 	}
 	return btms, nil
+}
+
+func buildTenantsByParentAndTypeQuery() string {
+	resultColumns := make([]string, 0, len(insertColumns))
+	for _, column := range insertColumns {
+		resultColumns = append(resultColumns, prefixWithTableName(tableName, column))
+	}
+
+	tenantTypeColumn := prefixWithTableName(tableName, typeColumn)
+	tenantsTableIDColumn := prefixWithTableName(tableName, idColumn)
+	parentsTableTenantIDColumn := prefixWithTableName(tenantparentmapping.TenantParentsTable, tenantparentmapping.TenantIDColumn)
+	parentsTableParentIDColumn := prefixWithTableName(tenantparentmapping.TenantParentsTable, tenantparentmapping.ParentIDColumn)
+
+	getTenantsByParentAndTypeStmt := fmt.Sprintf(getTenantsByParentAndType, strings.Join(resultColumns, ", "), tableName, tenantparentmapping.TenantParentsTable, tenantsTableIDColumn, parentsTableTenantIDColumn, parentsTableParentIDColumn, tenantTypeColumn)
+	getTenantsByParentAndTypeStmt = sqlx.Rebind(sqlx.DOLLAR, getTenantsByParentAndTypeStmt)
+	return getTenantsByParentAndTypeStmt
+}
+
+func prefixWithTableName(tableName, columnName string) string {
+	return tableName + "." + columnName
 }
