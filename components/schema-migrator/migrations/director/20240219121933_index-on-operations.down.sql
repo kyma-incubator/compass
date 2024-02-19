3d99a5940c28a29b2e0c8e87b808f2fe4146168a
Dobromir Zahariev <dobromir.zahariev@sap.com>
2024-02-19 12:44:25 +0200

    Typo

--- components/schema-migrator/migrations/director/20240219121933_index-on-operations.down.sql
+++ components/schema-migrator/migrations/director/20240219121933_index-on-operations.down.sql
@@ -1,8 +1,8 @@
 BEGIN;
 
-DROP INDEX IF EXISTS operations_on_updated_at;
-DROP INDEX IF EXISTS operations_on_status;
-DROP INDEX IF EXISTS operations_on_priority;
-DROP INDEX IF EXISTS operations_on_op_type;
+DROP INDEX IF EXISTS operation_on_updated_at;
+DROP INDEX IF EXISTS operation_on_status;
+DROP INDEX IF EXISTS operation_on_priority;
+DROP INDEX IF EXISTS operation_on_op_type;
 
 COMMIT;
--- components/schema-migrator/migrations/director/20240219121933_index-on-operations.up.sql
+++ components/schema-migrator/migrations/director/20240219121933_index-on-operations.up.sql
@@ -1,8 +1,8 @@
 BEGIN;
 
-CREATE INDEX  operations_on_op_type ON operations (op_type);
-CREATE INDEX  operations_on_priority ON operations (priority DESC);
-CREATE INDEX  operations_on_status ON operations (status);
-CREATE INDEX  operations_on_updated_at ON operations (updated_at);
+CREATE INDEX  operation_on_op_type ON operation (op_type);
+CREATE INDEX  operation_on_priority ON operation (priority DESC);
+CREATE INDEX  operation_on_status ON operation (status);
+CREATE INDEX  operation_on_updated_at ON operation (updated_at);
 
 COMMIT;
