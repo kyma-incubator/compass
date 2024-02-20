71d2ea27d3e1d423487210dc8b679f5e22658ed3
StanislavStefanov <stanislavsvetlinovstefanov@gmail.com>
2024-02-20 15:42:00 +0200

    fix typo

--- components/schema-migrator/migrations/director/20240220141642_add-tenant-access-to-new-crms.down.sql
+++ components/schema-migrator/migrations/director/20240220141642_add-tenant-access-to-new-crms.down.sql
@@ -2,9 +2,9 @@ BEGIN;
 
 DROP INDEX IF EXISTS business_tenant_mappings_type_idx;
 
-DROP INDEX IF EXISTS X labels_key_idx;
+DROP INDEX IF EXISTS labels_key_idx;
 
-DROP INDEX IF EXISTS X formation_assignments_source_idx;
+DROP INDEX IF EXISTS formation_assignments_source_idx;
 
 DROP INDEX IF EXISTS  formation_assignments_target_idx;
 
