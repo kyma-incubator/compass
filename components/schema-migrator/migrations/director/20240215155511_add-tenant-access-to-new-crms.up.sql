9268d7eb3c8fbe83b94a89cb2eeccb0d79ac4da7
StanislavStefanov <stanislavsvetlinovstefanov@gmail.com>
2024-02-21 15:56:49 +0200

    add migration

---
+++ components/schema-migrator/migrations/director/20240215155511_add-tenant-access-to-new-crms.up.sql
@@ -0,0 +1,22 @@
+BEGIN;
+
+INSERT INTO tenant_applications
+    (SELECT tp.parent_id, ta.id, ta.owner, ta.tenant_id
+     FROM tenant_applications ta
+              JOIN tenant_parents tp ON ta.tenant_id = tp.tenant_id
+     WHERE NOT EXISTS(SELECT 1
+                      FROM tenant_applications ti
+                      WHERE ta.owner = ti.owner
+                        AND ta.id = ti.id
+                        AND ta.tenant_id = ti.source
+                        AND ta.source = ti.tenant_id)) ON CONFLICT (tenant_id,id,source) DO NOTHING;
+
+CREATE INDEX IF NOT EXISTS business_tenant_mappings_type_idx ON business_tenant_mappings (type);
+
+CREATE INDEX IF NOT EXISTS labels_key_idx ON labels (key);
+
+CREATE INDEX IF NOT EXISTS formation_assignments_source_idx ON formation_assignments (source);
+
+CREATE INDEX IF NOT EXISTS formation_assignments_target_idx ON formation_assignments (target);
+
+COMMIT;
