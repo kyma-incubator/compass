06d865aa820e0048ee0e75114bec20b2833c49e1
StanislavStefanov <stanislavsvetlinovstefanov@gmail.com>
2024-02-21 10:50:15 +0200

    do not create reverse records for the diretive records

--- components/schema-migrator/migrations/director/20240220141642_add-tenant-access-to-new-crms.up.sql
+++ components/schema-migrator/migrations/director/20240220141642_add-tenant-access-to-new-crms.up.sql
@@ -3,8 +3,13 @@ BEGIN;
 INSERT INTO tenant_applications
     (SELECT tp.parent_id, ta.id, ta.owner, ta.tenant_id
      FROM tenant_applications ta
-              JOIN tenant_parents tp ON ta.tenant_id = tp.tenant_id)
-    ON CONFLICT (tenant_id,id,source) DO NOTHING;
+              JOIN tenant_parents tp ON ta.tenant_id = tp.tenant_id
+     WHERE NOT EXISTS(SELECT 1
+                      FROM tenant_applications ti
+                      WHERE ta.owner = ti.owner
+                        AND ta.id = ti.id
+                        AND ta.tenant_id = ti.source
+                        AND ta.source = ti.tenant_id)) ON CONFLICT (tenant_id,id,source) DO NOTHING;
 
 CREATE INDEX business_tenant_mappings_type_idx ON business_tenant_mappings (type);
 
