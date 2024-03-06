ae3e68b3b56c010a40a100cfd42a09730d329b9b
StanislavStefanov <stanislavsvetlinovstefanov@gmail.com>
2024-03-06 15:58:43 +0200

    add migration

---
+++ components/schema-migrator/migrations/director/20240306155731_add-draft-formation-state.down.sql
@@ -0,0 +1,9 @@
+BEGIN;
+
+ALTER TABLE formations DROP CONSTRAINT formations_state_check;
+
+ALTER TABLE formations
+    ADD CONSTRAINT formations_state_check
+        CHECK (state = ANY
+               (ARRAY['INITIAL'::text, 'READY'::text, 'CREATE_ERROR'::text, 'DELETE_ERROR'::text, 'DELETING'::text))
+    COMMIT;
