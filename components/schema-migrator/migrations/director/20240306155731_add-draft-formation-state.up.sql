33d70a501ba1346972781af9e4f5caf121151d01
StanislavStefanov <stanislavsvetlinovstefanov@gmail.com>
2024-03-07 12:10:19 +0200

    fix typo

--- components/schema-migrator/migrations/director/20240306155731_add-draft-formation-state.up.sql
+++ components/schema-migrator/migrations/director/20240306155731_add-draft-formation-state.up.sql
@@ -5,7 +5,6 @@ ALTER TABLE formations DROP CONSTRAINT formations_state_check;
 ALTER TABLE formations
     ADD CONSTRAINT formations_state_check
         CHECK (state = ANY
-               (ARRAY['INITIAL'::text, 'READY'::text, 'CREATE_ERROR'::text, 'DELETE_ERROR'::text, 'DELETING'::text,
-                'DRAFT'::text]));
+               (ARRAY['INITIAL'::text, 'READY'::text, 'CREATE_ERROR'::text, 'DELETE_ERROR'::text, 'DELETING'::text, 'DRAFT'::text]));
 
 COMMIT;
