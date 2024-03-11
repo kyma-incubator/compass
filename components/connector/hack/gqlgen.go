c7a1c9a6af1314cc249383ef26a28072c56b4b8f
Lachezar Bogomilov <lachezar.bogomilov@sap.com>
2024-03-11 16:47:09 +0200

    Fix connector external schema.

--- components/connector/hack/gqlgen.go
+++ components/connector/hack/gqlgen.go
@@ -2,8 +2,25 @@
 
 package main
 
-import "github.com/99designs/gqlgen/cmd"
+import (
+	"fmt"
+	"os"
+
+	"github.com/99designs/gqlgen/api"
+	"github.com/99designs/gqlgen/codegen/config"
+)
 
 func main() {
-	cmd.Execute()
+	cfg, err := config.LoadConfig("config.yaml")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "failed to load config", err.Error())
+		os.Exit(2)
+	}
+
+	err = api.Generate(cfg)
+
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(3)
+	}
 }
