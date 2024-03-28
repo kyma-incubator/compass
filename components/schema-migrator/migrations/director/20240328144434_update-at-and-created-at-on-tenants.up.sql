87646e9f918ffb4253948bf24bc13184a5e172fd
Dobromir Zahariev <dobromir.zahariev@sap.com>
2024-03-28 17:35:05 +0200

    Introduce lifecycle tmestamps for tenants

---
+++ components/schema-migrator/migrations/director/20240328144434_update-at-and-created-at-on-tenants.up.sql
@@ -0,0 +1,48 @@
+BEGIN;
+
+-- Add columns
+ALTER TABLE business_tenant_mappings
+    ADD COLUMN created_at timestamp,
+    ADD COLUMN updated_at timestamp;
+
+-- Update all existing records with initial data
+UPDATE business_tenant_mappings SET created_at=NOW() WHERE created_at IS NULL;
+UPDATE business_tenant_mappings SET updated_at=NOW() WHERE updated_at IS NULL;
+
+-- Introduce funciton that sets the created_at to now
+CREATE OR REPLACE FUNCTION set_created_at()
+    RETURNS TRIGGER
+AS
+$$
+BEGIN
+    NEW.created_at = NOW();
+    RETURN NEW;
+END
+$$ LANGUAGE plpgsql;
+
+-- Define a trigger that uses funciton for created_at on business_tenant_mappings table
+CREATE TRIGGER set_created_at_on_business_tenant_mappings
+    BEFORE INSERT 
+    ON business_tenant_mappings
+    FOR EACH ROW
+EXECUTE FUNCTION set_created_at();
+
+-- Introduce funciton that sets the updated_at to now
+CREATE OR REPLACE FUNCTION set_updated_at()
+    RETURNS TRIGGER
+AS
+$$
+BEGIN
+    NEW.updated_at = NOW();
+    RETURN NEW;
+END
+$$ LANGUAGE plpgsql;
+
+-- Define a trigger that uses funciton for created_at on business_tenant_mappings table
+CREATE TRIGGER set_updated_at_on_business_tenant_mappings
+    BEFORE INSERT OR UPDATE 
+    ON business_tenant_mappings
+    FOR EACH ROW
+EXECUTE FUNCTION set_updated_at();
+
+COMMIT;
