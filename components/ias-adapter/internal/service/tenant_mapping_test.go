f79a9bc00eb104c12d3c4bd97bc138c4b7b638ea
stefan-sto <43176875+stefan-sto@users.noreply.github.com>
2024-03-29 20:02:48 +0200

    IAS Adapter - Handle S/4 participants (#3768) (#3780)
    * IAS Adapter - Handle S/4 participants (#3768)
    
    * IAS Adapter - Handle S/4 participants
    
    * Add tests
    
    * Adapt getting ias application
    
    * Create s/4 app only if it doesn't exist
    
    * Minor improvements
    
    * Revisit logs
    
    * Adapt tm validation
    
    * Update version
    
    * Update ias adapter version

--- components/ias-adapter/internal/service/tenant_mapping_test.go
+++ components/ias-adapter/internal/service/tenant_mapping_test.go
@@ -2,9 +2,9 @@ package service
 
 import (
 	"context"
-	"errors"
 	"testing"
 
+	"github.com/kyma-incubator/compass/components/ias-adapter/internal/errors"
 	"github.com/kyma-incubator/compass/components/ias-adapter/internal/service/automock"
 	"github.com/kyma-incubator/compass/components/ias-adapter/internal/types"
 
@@ -19,43 +19,88 @@ func TestHandlers(t *testing.T) {
 }
 
 var _ = Describe("Tenant mappings service", func() {
-	var tenantMapping = types.TenantMapping{
-		FormationID: "2d933ae2-10c4-4d6f-b4d4-5e1553e4ff05",
-		ReceiverTenant: types.ReceiverTenant{
-			ApplicationURL: "localhost",
-		},
-		AssignedTenants: []types.AssignedTenant{
-			{
-				UCLApplicationID: "2d933ae2-10c4-4d6f-b4d4-5e1553e4ff05",
-				LocalTenantID:    "2d933ae2-10c4-4d6f-b4d4-5e1553e4ff05",
-				Operation:        types.OperationAssign,
-				Parameters: types.AssignedTenantParameters{
-					ClientID: "clientID",
-				},
-				Configuration: types.AssignedTenantConfiguration{
-					ConsumedAPIs: []string{},
-				},
-				ReverseAssignmentState: "",
+	var (
+		ctx                   context.Context
+		tenantMapping         types.TenantMapping
+		tenantMappingsStorage *automock.TenantMappingsStorage
+		iasService            *automock.IASService
+	)
+
+	BeforeEach(func() {
+		ctx = context.Background()
+		tenantMappingsStorage = &automock.TenantMappingsStorage{}
+		iasService = &automock.IASService{}
+		tenantMapping = types.TenantMapping{
+			FormationID: "2d933ae2-10c4-4d6f-b4d4-5e1553e4ff05",
+			ReceiverTenant: types.ReceiverTenant{
+				ApplicationURL: "localhost",
 			},
-		},
-	}
+			AssignedTenants: []types.AssignedTenant{
+				{
+					UCLApplicationID:   "2d933ae2-10c4-4d6f-b4d4-5e1553e4ff05",
+					UCLApplicationType: "test-app-type",
+					LocalTenantID:      "2d933ae2-10c4-4d6f-b4d4-5e1553e4ff05",
+					Operation:          types.OperationAssign,
+					Parameters: types.AssignedTenantParameters{
+						ClientID: "clientID",
+					},
+					Configuration: types.AssignedTenantConfiguration{
+						ConsumedAPIs: []string{},
+					},
+					ReverseAssignmentState: "",
+				},
+			},
+		}
+	})
 
 	When("receive tenant mapping with empty ConsumedAPIs and the tenant mapping is stored in DB", func() {
 		It("should not try to insert it again in the DB", func() {
-			ctx := context.Background()
-			tenantMappingsStorage := &automock.TenantMappingsStorage{}
-			iasService := &automock.IASService{}
 			tenantMappingsInDB := map[string]types.TenantMapping{
 				"2d933ae2-10c4-4d6f-b4d4-5e1553e4ff05": tenantMapping,
 				"11111111-10c4-4d6f-b4d4-5e1553e4ff05": tenantMapping,
 			}
-			tenantMappingsStorage.On("ListTenantMappings", mock.Anything, mock.Anything).Return(tenantMappingsInDB, nil)
+			tenantMappingsStorage.On("ListTenantMappings", ctx, mock.Anything).Return(tenantMappingsInDB, nil)
 			tms := TenantMappingsService{Storage: tenantMappingsStorage, IASService: iasService}
-			iasService.On("GetApplication", ctx, mock.Anything, mock.Anything, mock.Anything).Return(types.Application{}, errors.New("error"))
+			iasService.On("GetApplicationByClientID", ctx, mock.Anything, mock.Anything, mock.Anything).Return(types.Application{}, errors.New("error"))
 			Expect(tenantMappingsStorage.AssertNotCalled(GinkgoT(), "UpsertTenantMapping")).To(BeTrue())
 			err := tms.ProcessTenantMapping(ctx, tenantMapping)
 			Expect(err).Error().To(HaveOccurred())
 			Expect(err.Error()).To(ContainSubstring("failed to get IAS application"))
+		})
+	})
+
+	When("tenant mapping with S/4 participant is received", func() {
+		BeforeEach(func() {
+			tenantMapping.AssignedTenants[0].UCLApplicationType = types.S4ApplicationType
+			tenantMapping.AssignedTenants[0].Parameters.ClientID = ""
+			tenantMappingsStorage.On("ListTenantMappings", ctx, mock.Anything).Return(map[string]types.TenantMapping{}, nil)
+		})
+		It("should return error when default S/4 certificate is not provided", func() {
+			tms := TenantMappingsService{Storage: tenantMappingsStorage, IASService: iasService}
+			err := tms.ProcessTenantMapping(ctx, tenantMapping)
+			Expect(err).Error().To(MatchError(errors.S4CertificateNotFound))
+		})
+		It("should create application for S/4 in IAS if it doesn't exist", func() {
+			iasAppID := "appId"
+			tenantMapping.AssignedTenants[0].Configuration.Credentials.InboundCommunicationCredentials.OAuth2mTLSAuthentication.Certificate = "s4TestCert"
+			tenantMappingsStorage.On("UpsertTenantMapping", ctx, mock.Anything).Return(nil)
+			iasService.On("GetApplicationByName", ctx, mock.Anything, mock.Anything).Return(types.Application{}, errors.IASApplicationNotFound)
+			iasService.On("CreateApplication", ctx, mock.Anything, mock.Anything).Return(iasAppID, nil)
+			tms := TenantMappingsService{Storage: tenantMappingsStorage, IASService: iasService}
+			err := tms.ProcessTenantMapping(ctx, tenantMapping)
+			Expect(tenantMapping.AssignedTenants[0].Parameters.IASApplicationID).To(Equal(iasAppID))
+			Expect(err).Error().ToNot(HaveOccurred())
+		})
+		It("should get the application for S/4 in IAS if it exists", func() {
+			iasAppID := "appId"
+			tenantMapping.AssignedTenants[0].Configuration.Credentials.InboundCommunicationCredentials.OAuth2mTLSAuthentication.Certificate = "s4TestCert"
+			tenantMappingsStorage.On("UpsertTenantMapping", ctx, mock.Anything).Return(nil)
+			iasService.On("GetApplicationByName", ctx, mock.Anything, mock.Anything).Return(types.Application{ID: iasAppID}, nil)
+			Expect(tenantMappingsStorage.AssertNotCalled(GinkgoT(), "CreateApplication")).To(BeTrue())
+			tms := TenantMappingsService{Storage: tenantMappingsStorage, IASService: iasService}
+			err := tms.ProcessTenantMapping(ctx, tenantMapping)
+			Expect(tenantMapping.AssignedTenants[0].Parameters.IASApplicationID).To(Equal(iasAppID))
+			Expect(err).Error().ToNot(HaveOccurred())
 		})
 	})
 })
