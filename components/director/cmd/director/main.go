f6a366582f2468b2cda43da705a26b9572704086
ognyvrac <ognyan.vrachanski@gmail.com>
2024-04-04 10:06:40 +0300

    health endpoint

--- components/director/cmd/director/main.go
+++ components/director/cmd/director/main.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"crypto/tls"
 	"encoding/json"
-	"fmt"
+	"github.com/kyma-incubator/compass/components/director/internal/domain/schema"
 	"net/http"
 	"net/url"
 	"os"
@@ -421,10 +421,9 @@ func main() {
 	internalOperationsAPIRouter.HandleFunc("", operationUpdaterHandler.ServeHTTP)
 
 	logger.Infof("Registering readiness endpoint...")
-	// schemaRepo := schema.NewRepository()
-	// ready := healthz.NewReady(transact, cfg.ReadyConfig, schemaRepo)
-	// mainRouter.HandleFunc(readyzEndpoint, healthz.NewReadinessHandler(ready))
-	mainRouter.HandleFunc(readyzEndpoint, healthz.NewLivenessHandler())
+	schemaRepo := schema.NewRepository()
+	ready := healthz.NewReady(transact, cfg.ReadyConfig, schemaRepo)
+	mainRouter.HandleFunc(readyzEndpoint, healthz.NewReadinessHandler(ready))
 
 	logger.Infof("Registering liveness endpoint...")
 	mainRouter.HandleFunc(livezEndpoint, healthz.NewLivenessHandler())
@@ -433,11 +432,7 @@ func main() {
 	health, err := healthz.New(ctx, cfg.HealthConfig)
 	exitOnError(err, "Could not initialize health")
 	health.RegisterIndicator(healthz.NewIndicator(healthz.DBIndicatorName, healthz.NewDBIndicatorFunc(transact))).Start()
-	// mainRouter.HandleFunc(healthzEndpoint, healthz.NewHealthHandler(health))
-	mainRouter.HandleFunc(healthzEndpoint, func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-		fmt.Fprint(w, "UP")
-	})
+	mainRouter.HandleFunc(healthzEndpoint, healthz.NewHealthHandler(health))
 
 	logger.Infof("Registering info endpoint...")
 	mainRouter.HandleFunc(cfg.InfoConfig.APIEndpoint, info.NewInfoHandler(ctx, cfg.InfoConfig, certCache))
