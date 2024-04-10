56e9100a3a5a847d47bba3579a56fa64fe56c4e2
ZdravkoGyurov <37419999+ZdravkoGyurov@users.noreply.github.com>
2024-04-10 10:14:37 +0300

    remove leftover sync status report from IAS Adapter (#3798)

--- components/ias-adapter/internal/service/processor/processor.go
+++ components/ias-adapter/internal/service/processor/processor.go
@@ -45,7 +45,7 @@ func (p AsyncProcessor) ProcessTMRequest(ctx context.Context, tenantMapping type
 		if reverseAssignmentState != types.StateInitial && reverseAssignmentState != types.StateReady {
 			log.Warn().Msgf("skipping processing tenant mapping notification with $.assignedTenant.state '%s'",
 				reverseAssignmentState)
-			p.reportStatus(ctx, ucl.StatusReport{State: types.StateConfigPending})
+			p.ReportStatus(ctx, ucl.StatusReport{State: types.StateConfigPending})
 			return
 		}
 	}
@@ -57,41 +57,27 @@ func (p AsyncProcessor) ProcessTMRequest(ctx context.Context, tenantMapping type
 
 		if operation == types.OperationAssign {
 			if errors.Is(err, errors.IASApplicationNotFound) {
-				p.reportStatus(ctx, ucl.StatusReport{State: errorState(operation), Error: err.Error()})
+				p.ReportStatus(ctx, ucl.StatusReport{State: types.ErrorState(operation), Error: err.Error()})
 				return
 			}
 
 			if errors.Is(err, errors.S4CertificateNotFound) {
 				log.Info().Msgf("S/4 certificate not provided. Responding with CONFIG_PENDING.")
-				p.reportStatus(ctx, ucl.StatusReport{State: types.StateConfigPending, Configuration: s4Config})
+				p.ReportStatus(ctx, ucl.StatusReport{State: types.StateConfigPending, Configuration: s4Config})
 				return
 			}
 		}
 
-		p.reportStatus(ctx, ucl.StatusReport{State: errorState(operation), Error: err.Error()})
+		p.ReportStatus(ctx, ucl.StatusReport{State: types.ErrorState(operation), Error: err.Error()})
 		return
 	}
 
-	p.reportStatus(ctx, ucl.StatusReport{State: readyState(operation)})
+	p.ReportStatus(ctx, ucl.StatusReport{State: types.ReadyState(operation)})
 }
 
-func (p AsyncProcessor) reportStatus(ctx context.Context, statusReport ucl.StatusReport) {
+func (p AsyncProcessor) ReportStatus(ctx context.Context, statusReport ucl.StatusReport) {
 	statusReportURL := ctx.Value(locationHeader).(string)
 	if err := p.UCLService.ReportStatus(ctx, statusReportURL, statusReport); err != nil {
 		logger.FromContext(ctx).Error().Msgf("failed to report status to '%s': %s", statusReportURL, err)
 	}
-}
-
-func readyState(operation types.Operation) types.State {
-	if operation == types.OperationAssign {
-		return types.StateCreateReady
-	}
-	return types.StateDeleteReady
-}
-
-func errorState(operation types.Operation) types.State {
-	if operation == types.OperationAssign {
-		return types.StateCreateError
-	}
-	return types.StateDeleteError
 }
