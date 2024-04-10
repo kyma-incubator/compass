a7156293b14d12d03e3edc6f03ae2043f70392d2
StanislavStefanov <stanislavsvetlinovstefanov@gmail.com>
2024-04-10 17:29:12 +0300

    remove cascade deletion

---
+++ components/schema-migrator/migrations/director/20240410172316_remove_constrain_cascade_deletion.down.sql
@@ -0,0 +1,6 @@
+BEGIN;
+
+ALTER TABLE formation_template_constraint_references DROP CONSTRAINT formation_template_constraint_refe_formation_constraint_id_fkey;
+ALTER TABLE formation_template_constraint_references ADD CONSTRAINT formation_template_constraint_refe_formation_constraint_id_fkey FOREIGN KEY (formation_constraint_id) REFERENCES formation_constraints(id) ON DELETE CASCADE;
+
+COMMIT;
