548a8acd7ef80da58aa5c2f4bf5d0f22678b4346
Lachezar Bogomilov <lachezar.bogomilov@sap.com>
2024-04-15 11:30:37 +0300

    Address PR comments.

--- components/schema-migrator/migrations/director/20240415112919_add_region_of_application_in_ord.down.sql
+++ components/schema-migrator/migrations/director/20240415112919_add_region_of_application_in_ord.down.sql
@@ -1,14 +1,14 @@
 BEGIN;
 
-CREATE INDEX IF NOT EXISTS labels_app_region_idx ON labels (app_id, value) WHERE key = 'region';
-CREATE INDEX IF NOT EXISTS labels_app_tmpl_region_idx ON labels (app_template_id, value) WHERE key = 'region';
+DROP INDEX IF EXISTS labels_app_region_idx;
+DROP INDEX IF EXISTS labels_app_tmpl_region_idx;
 
 DROP VIEW IF EXISTS tenants_apps;
 
 CREATE OR REPLACE VIEW tenants_apps
             (tenant_id, formation_id, id, name, description, status_condition, status_timestamp, healthcheck_url,
              integration_system_id, provider_name, base_url, labels, tags, ready, created_at, updated_at, deleted_at,
-             error, app_template_id, correlation_ids, system_number, application_namespace, region, local_tenant_id,
+             error, app_template_id, correlation_ids, system_number, application_namespace, local_tenant_id,
              product_type, fa_formation_id, assignment_id, formation_type_id, target_id)
 AS
 SELECT DISTINCT t_apps.tenant_id,
@@ -33,13 +33,12 @@ SELECT DISTINCT t_apps.tenant_id,
                 apps.correlation_ids,
                 apps.system_number,
                 COALESCE(apps.application_namespace, tmpl.application_namespace) AS application_namespace,
-                COALESCE(labels_app.value, labels_tmpl.value) AS region,
                 apps.local_tenant_id,
                 tmpl.name                                                        AS product_type,
-                formation_details.formation_id, -- this may seem as a duplicate of 't_apps.formation_id' but is not, because 't_apps.formation_id' can have default value but 'formation_details.formation_id' will always have the 'real' formation id
+                formation_details.formation_id,
                 formation_details.assignment_id,
                 formation_details.formation_type_id,
-                COALESCE(formation_details.target_id, 'eeeeeeee-eeee-eeee-eeee-eeeeeeeeeeee'::uuid) -- we need a default value for the target_id claim for backwards compatibility when we call the ORD Service with tenant filtering
+                COALESCE(formation_details.target_id, 'eeeeeeee-eeee-eeee-eeee-eeeeeeeeeeee'::uuid)
 FROM applications apps
          LEFT JOIN app_templates tmpl ON apps.app_template_id = tmpl.id
          JOIN (SELECT a1.id,
@@ -56,12 +55,11 @@ FROM applications apps
                       apps_subaccounts.tenant_id,
                       'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa'::uuid AS formation_id
                FROM apps_subaccounts) t_apps ON apps.id = t_apps.id
-         LEFT JOIN (SELECT DISTINCT fa.id AS assignment_id, -- left join so that we don't omit apps that are not sources
+         LEFT JOIN (SELECT DISTINCT fa.id AS assignment_id,
                                     fa.formation_id,
                                     f.formation_template_id AS formation_type_id,
                                     fa.source,
                                     fa.target AS target_id
-                    FROM formation_assignments fa JOIN formations f ON fa.formation_id = f.id) formation_details ON formation_details.source = t_apps.id AND formation_details.formation_id = t_apps.formation_id -- the second join by formation_id is required so that we have records (tenant + default formation_id + null formation_details) that are needed for backwards compatibility when we call the ORD Service only with tenant filtering; otherwise, since the join will be only on source, there are records (tenant + default formation_id + valid formation_details) which are invalid because we want null ones as there is no formation filtering
-         LEFT JOIN labels AS labels_app ON labels_app.app_id = apps.id AND labels_app.key = 'region'
-         LEFT JOIN labels AS labels_tmpl ON labels_tmpl.app_template_id = tmpl.id AND labels_tmpl.key = 'region';
+                    FROM formation_assignments fa JOIN formations f ON fa.formation_id = f.id) formation_details ON formation_details.source = t_apps.id AND formation_details.formation_id = t_apps.formation_id; -- the second join by formation_id is required so that we have records (tenant + default formation_id + null formation_details) that are needed for backwards compatibility when we call the ORD Service only with tenant filtering; otherwise, since the join will be only on source, there are records (tenant + default formation_id + valid formation_details) which are invalid because we want null ones as there is no formation filtering
+
 COMMIT;
