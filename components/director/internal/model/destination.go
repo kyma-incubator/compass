f18f4d50bc81ba2e38e70c40992587495569dc3a
Aleksandar Targov <aleksandar.targov@sap.com>
2024-04-25 14:14:36 +0300

    poc dest fetcher property

--- components/director/internal/model/destination.go
+++ components/director/internal/model/destination.go
@@ -10,7 +10,7 @@ type DestinationInput struct {
 	Type              string `json:"Type"`
 	URL               string `json:"URL"`
 	Authentication    string `json:"Authentication"`
-	XCorrelationID    string `json:"x-correlation-id"`
+	XCorrelationID    string `json:"correlationIds"`
 	XSystemTenantID   string `json:"x-system-id"`
 	XSystemTenantName string `json:"x-system-name"`
 	XSystemType       string `json:"x-system-type"`
