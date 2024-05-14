cbe91105263602d9c77788c269a54c0075285ac8
Nikolay Yordanov <n.yordanov@sap.com>
2024-05-14 16:50:28 +0300

    Move new logic to old migration

--- components/schema-migrator/migrations/director/20240514110900_introduce_assignment_operation_entity.up.sql
+++ components/schema-migrator/migrations/director/20240514110900_introduce_assignment_operation_entity.up.sql
@@ -34,6 +34,6 @@ WHERE fa.state IN ('READY');
 INSERT INTO assignment_operations (id, type, formation_assignment_id, formation_id, triggered_by, started_at_timestamp)
 SELECT uuid_generate_v4(), 'UNASSIGN', fa.id, fa.formation_id, 'UNASSIGN_OBJECT', CURRENT_TIMESTAMP
 FROM formation_assignments fa
-WHERE fa.state IN ('DELETING', 'DELETE_ERROR');
+WHERE fa.state IN ('DELETING', 'DELETE_ERROR', 'INSTANCE_CREATOR_DELETING', 'INSTANCE_CREATOR_DELETE_ERROR');
 
 COMMIT;
